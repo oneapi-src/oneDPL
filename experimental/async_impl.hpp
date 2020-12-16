5eaca1e0153d22abd7a1d8080e7c014d7afd5f14
Pablo Reble <pablo.reble@intel.com>
2020-12-16 11:48:05 -0600

    add copyright

--- experimental/async_impl.hpp
+++ experimental/async_impl.hpp
@@ -1,9 +1,18 @@
-//
-//  async_reduce.hpp
-//  
-//
-//  Created by Pablo Reble (Intel) on 2/3/20.
-//
+/*
+ *  Copyright (c) 2020 Intel Corporation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 
 #ifndef async_reduce_impl_hpp
 #define async_reduce_impl_hpp
