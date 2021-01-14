97bdbe7de5a8b16126a02c2b33175183b21c639e
Pablo Reble <pablo.reble@intel.com>
2021-01-14 16:08:51 -0600

    rework future (still wip) * including specialization for sycl_iterator

--- experimental/future.hpp
+++ experimental/future.hpp
@@ -25,60 +25,61 @@ namespace dpl
 namespace __internal
 {
 
-// TODO: Rework async data structures for return value
-template <typename _Tp>
+template <typename _T, typename _Tp = sycl_iterator<sycl::access::mode::read_write,_T,sycl::buffer_allocator>>
 struct __async_value
 {
-    virtual _Tp
+    virtual _T
     data() = 0;
     virtual ~__async_value() = default;
-    virtual sycl::buffer<int>
+    virtual _Tp
     raw_data() const;
 };
 
-template <typename _Tp>
-struct __async_direct : public __async_value<_Tp>
+template <typename _Tp, typename _T = typename std::iterator_traits<_Tp>::value_type>
+struct __async_direct : public __async_value<_T>
 {
-    using _T = typename std::iterator_traits<_Tp>::value_type;
     _Tp __data;
     __async_direct(_Tp _d) : __data(_d) {}
-    _Tp data() { return __data; }
-    sycl::buffer<_T>
-    raw_data() const
+    _T
+    data()
     {
-        return __data.get_buffer();
+        return _T(0);
     }
+    virtual sycl_iterator<sycl::access::mode::read_write,_T,sycl::buffer_allocator>
+    raw_data() const { return __data;}
 };
 
-template <typename _Tp, typename _Op = ::std::plus<_Tp>, typename _Buf = sycl::buffer<_Tp>>
-struct __async_indirect : public __async_value<_Tp>
+// return transform iterator that applies: op(ret_val,init)
+
+template <typename _Tp, typename _Op = ::std::plus<_Tp>, typename _Buf = sycl::buffer<_Tp> /*, typename _T = std::iterator_traits<_Tp>::value_type*/>
+struct __async_transform : public __async_value<_Tp>
 {
     _Buf __buf;
     _Op __op;
     _Tp __init;
-    __async_indirect(_Buf _b, _Tp _v = 0, _Op _o = std::plus<_Tp>()) : __buf(_b), __op(_o), __init(_v) {}
+    __async_transform(_Buf _b, _Tp _v = 0, _Op _o = std::plus<_Tp>()) : __buf(_b), __op(_o), __init(_v) {}
     _Tp
     data()
     {
         auto ret_val = __buf.template get_access<access_mode::read>()[0];
         return __op(ret_val, __init);
     }
-    sycl::buffer<_Tp>
+    virtual sycl_iterator<sycl::access::mode::read_write,_Tp,sycl::buffer_allocator>
     raw_data() const
     {
-        return __buf;
+        return oneapi::dpl::begin(__buf);
     }
 };
 
-struct _tmp_base
+struct __tmp_base
 {
 };
 
 template <typename... Ts>
-struct _TempObjs : public _tmp_base
+struct __TempObjs : public __tmp_base
 {
     std::tuple<Ts&...> __my_tmps;
-    _TempObjs(Ts&... __t) : __my_tmps(::std::forward_as_tuple(__t...)) {}
+    __TempObjs(Ts&... __t) : __my_tmps(::std::forward_as_tuple(__t...)) {}
 };
 
 #if 1
@@ -99,7 +100,7 @@ class __future_base {
 public:
     __future_base(event __e) : __my_event(__e) {}
     event get_event() const { return __my_event; }
-    void wait() { __my_event.wait(); }
+    void wait() /* const */ { __my_event.wait(); }
     operator event() const { return event(__my_event); }
 };
 
@@ -109,26 +110,20 @@ template <typename _Tp>
 class __future : public __future_base
 {
     ::std::unique_ptr<__async_value<_Tp>> __data; // This is a value/buffer for read access!
-    _tmp_base __tmp;
+    __tmp_base __tmp;
 
   public:
     template <typename... _Ts>
-    __future(event __e, _Tp __d, _Ts... __t)
-        : __future_base(__e), __data(::std::unique_ptr<__async_direct<_Tp>>(new __async_direct<_Tp>(__d))),
-          __tmp(_TempObjs<_Ts...>{__t...})
-    {
-    }
-    template <typename... _Ts>
     __future(event __e, sycl::buffer<_Tp> __d, _Ts... __t)
-        : __future_base(__e), __data(::std::unique_ptr<__async_indirect<_Tp>>(new __async_indirect<_Tp>(__d))),
-          __tmp(_TempObjs<_Ts...>{__t...})
+        : __future_base(__e), __data(::std::unique_ptr<__async_transform<_Tp>>(new __async_transform<_Tp>(__d))),
+          __tmp(__TempObjs<_Ts...>{__t...})
     {
     }
     // Constructor for reduce_transform pattern
     template <typename _Op>
     __future(const __future<_Tp>& _fp, _Tp __i, _Op __o)
         : __future_base(_fp.get_event()),
-          __data(::std::unique_ptr<__async_indirect<_Tp, _Op>>(new __async_indirect<_Tp, _Op>(_fp.raw_data(), __i, __o))), __tmp(_fp.__tmp)
+          __data(::std::unique_ptr<__async_transform<_Tp, _Op>>(new __async_transform<_Tp, _Op>(_fp.raw_data().get_buffer(), __i, __o))), __tmp(_fp.__tmp)
     {
     }
     // Return underlying buffer
@@ -138,23 +133,48 @@ class __future : public __future_base
         return __data->raw_data();
     }
     _Tp
-    data() const
+    get()
     {
+        this->wait();
+        return __data->data();
+    }
+};
+
+// Specialization for sycl_iterator
+template <typename T>
+class __future<sycl_iterator<sycl::access::mode::read_write,T,sycl::buffer_allocator>> : public __future_base
+{
+    using _Tp = sycl_iterator<sycl::access::mode::read_write,T,sycl::buffer_allocator>;
+    ::std::unique_ptr<__async_value<T>> __data;
+    __tmp_base __tmp;
+    
+  public:
+    template <typename... _Ts>
+    __future(event __e, _Tp __d, _Ts... __t)
+        : __future_base(__e), __data(::std::unique_ptr<__async_direct<_Tp>>(new __async_direct<_Tp>(__d))),
+          __tmp(__TempObjs<_Ts...>{__t...})
+    {
+    }
+    _Tp
+    get()
+    {
+        this->wait();
         return __data->data();
     }
 };
 
 template<>
-class __future<void> : public __internal::__future_base
+class __future<void> : public __future_base
 {
-    _tmp_base __tmp;
+    __tmp_base __tmp;
 
   public:
     template <typename... _Ts>
     __future(event __e, _Ts... __t)
-        : __future_base(__e), __tmp(_TempObjs<_Ts...>{__t...})
+        : __future_base(__e), __tmp(__TempObjs<_Ts...>{__t...})
     {
     }
+    void get() { this->wait(); }
 };
 
 } // end namespace __internal
