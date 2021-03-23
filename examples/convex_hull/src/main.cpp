94ad69b91746534afb2e19916d90cd6c00fb09f4
Fedorov, Andrey <andrey.fedorov@intel.com>
2021-03-23 19:25:32 +0300

    align host examples with heterogeneous examples

--- examples/convex_hull/src/main.cpp
+++ examples/convex_hull/src/main.cpp
@@ -1,49 +1,22 @@
-// -*- C++ -*-
-//===-- convex_hull.cpp ---------------------------------------------------===//
+//==============================================================
+// Copyright © 2017-2020 Intel Corporation
 //
-// Copyright (C) 2017-2020 Intel Corporation
-//
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-// This file incorporates work covered by the following copyright and permission
-// notice:
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-//
-//===----------------------------------------------------------------------===//
+// SPDX-License-Identifier: MIT
+// =============================================================
 
-/*
-    This file contains the Parallel STL-based implementation of quickhull algorithm.
-    Quickhull algorithm description:
-    1. Initial phase
-      1) Find two points that guaranteed to belong to the convex hull. Min and max points in X can be used for it.
-      2) Divide the initial set of points in two subsets by the line formed by two points from previous step.
-         This subset will be processed recursively.
-
-    2. Iteration Phase
-      1) Divide current subset by dividing line [p1,p2] into right and left subsets.
-      2) New point (p) of the convex hull is found as farthest point of right subset from the dividing line.
-      3) If the right subset has more than 1 point, repeat the iteration phase with the right subset and dividing lines [p1,p] and [p,p2].
-
-    The implementation based on ::std::copy_if, ::std::max_element and ::std::minmax_element algorithms of Parallel STL.
-    Each of the algorithms use par_unseq policy. In order to get effect of the policy usage problem size should be big enough.
-    By default problem size was set as 5M points. With point set with less than 500K points par_unseq policy could be inefficient.
-    Correctness of the convex hull is checked by ::std::any_of algorithm with counting iterators.
-*/
-
-#include <chrono>
-#include <vector>
-#include <fstream>
+// oneDPL headers should be included before standard headers
 #include <oneapi/dpl/algorithm>
 #include <oneapi/dpl/numeric>
 #include <oneapi/dpl/execution>
 #include <oneapi/dpl/iterator>
+#include <chrono>
+#include <vector>
+#include <fstream>
 
 #include "utils.h"
 
 typedef util::point<double> point_t;
-typedef ::std::vector< point_t > pointVec_t;
+typedef std::vector< point_t > pointVec_t;
 const size_t grain_size = 500000;
 
 template<typename Policy, typename Iterator>
@@ -72,7 +45,7 @@ void find_hull_points(Policy exec, Iterator first, Iterator last, pointVec_t &H,
     pointVec_t P_reduced(last - first);
 
     //Find points from the range [first, last-1] that are on the right side of the segment [p1,p2]
-    Iterator end = ::std::copy_if(exec, first, last, P_reduced.begin(),
+    Iterator end = std::copy_if(exec, first, last, P_reduced.begin(),
         [&p1, &p2](const point_t& pnt) {
         return cross_product(p1, p2, pnt) > 0;
     });
@@ -84,7 +57,7 @@ void find_hull_points(Policy exec, Iterator first, Iterator last, pointVec_t &H,
     }
     else {
         //Find the farthest point from the segment [p1,p1], it will be in the convex hull
-        auto far_point = *::std::max_element(exec, P_reduced.cbegin(), end,
+        auto far_point = *std::max_element(exec, P_reduced.cbegin(), end,
             [&p1, &p2](const point_t & pnt1, const point_t & pnt2) {
             double how_far1 = cross_product(p1, p2, pnt1);
             double how_far2 = cross_product(p1, p2, pnt2);
@@ -106,10 +79,10 @@ void quickhull(const pointVec_t &points, pointVec_t &hull) {
     //Find left and right most points, they will be in the convex hull
 #if (__INTEL_COMPILER >= 1900 && __INTEL_COMPILER <= 1910) && (PSTL_VERSION >= 200 && PSTL_VERSION <= 204)
     // A workaround for incorrectly working minmax_element
-    point_t p1 = *::std::min_element(oneapi::dpl::execution::par_unseq, points.cbegin(), points.cend());
-    point_t p2 = *::std::max_element(oneapi::dpl::execution::par_unseq, points.cbegin(), points.cend());
+    point_t p1 = *std::min_element(oneapi::dpl::execution::par_unseq, points.cbegin(), points.cend());
+    point_t p2 = *std::max_element(oneapi::dpl::execution::par_unseq, points.cbegin(), points.cend());
 #else
-    auto minmaxx = ::std::minmax_element(oneapi::dpl::execution::par_unseq, points.cbegin(), points.cend());
+    auto minmaxx = std::minmax_element(oneapi::dpl::execution::par_unseq, points.cbegin(), points.cend());
     point_t p1 = *minmaxx.first;
     point_t p2 = *minmaxx.second;
 #endif
@@ -120,7 +93,7 @@ void quickhull(const pointVec_t &points, pointVec_t &hull) {
 
 // Check if a polygon is convex
 bool is_convex(const pointVec_t & points) {
-    return ::std::all_of(oneapi::dpl::execution::par_unseq,
+    return std::all_of(oneapi::dpl::execution::par_unseq,
         oneapi::dpl::counting_iterator<size_t>(size_t(0)),
         oneapi::dpl::counting_iterator<size_t>(points.size()),
         [&points](size_t i) {
@@ -134,35 +107,36 @@ bool is_convex(const pointVec_t & points) {
 int main(int argc, char* argv[]) {
 
     const size_t numberOfPoints = 5000000;
-    const ::std::string output_file("ConvexHull.csv");
+    const std::string output_file("ConvexHull.csv");
 
     pointVec_t points(numberOfPoints);
     pointVec_t hull;
 
     //initialize set of points
     // Note: we use par policy due to functor's difficulty for vectorization
-    ::std::generate(oneapi::dpl::execution::par, points.begin(), points.end(), util::random_point<double>);
-    ::std::cout << "Points were initialized. Number of the points " << points.size() << ::std::endl;
+    std::generate(oneapi::dpl::execution::par, points.begin(), points.end(), util::random_point<double>);
+    std::cout << "Points were initialized. Number of the points " << points.size() << std::endl;
 
-    using ms = ::std::chrono::milliseconds;
+    using ms = std::chrono::milliseconds;
 
-    auto tm_start = ::std::chrono::high_resolution_clock::now();
+    auto tm_start = std::chrono::high_resolution_clock::now();
     //execution of the quickhull algorithm
     quickhull(points, hull);
-    auto tm_end = ::std::chrono::high_resolution_clock::now();
+    auto tm_end = std::chrono::high_resolution_clock::now();
 
-    ::std::cout << "Computational time " << ::std::chrono::duration_cast<ms> (tm_end - tm_start).count() << "ms"
-        << "  Points in the hull: " << hull.size() << "  The convex hull is " << (is_convex(hull) ? "correct" : "incorrect") << ::std::endl;
+    std::cout << "Computational time " << std::chrono::duration_cast<ms> (tm_end - tm_start).count() << "ms"
+        << "  Points in the hull: " << hull.size() << "  The convex hull is " << (is_convex(hull) ? "correct" : "incorrect") << std::endl;
 
     //writing the results
-    ::std::ofstream fout(output_file);
+    std::ofstream fout(output_file);
     if (fout.is_open()) {
         for (auto p : hull)
-            fout << p << ::std::endl;
-        ::std::cout << "The convex hull has been stored to a file " << output_file << ::std::endl;
+            fout << p << std::endl;
+        std::cout << "success" << res << std::endl;
+        std::cout << "The convex hull has been stored to a file " << output_file << std::endl;
     }
     else {
-        ::std::cout << "Cannot open a file " << output_file << " to store result" << ::std::endl;
+        std::cout << "Cannot open a file " << output_file << " to store result" << std::endl;
     }
 
     return 0;
---
+++ examples/dot_product/README.md
@@ -0,0 +1,45 @@
+# Dot product
+
+This example contains the oneDPL-based implementation of dot product based on `std::transform_reduce`.
+
+| Optimized for                   | Description                                                                                                                          |
+|---------------------------------|--------------------------------------------------------------------------------------------------------------------------------------|
+| OS                              | Linux* Ubuntu* 18.04                                                                                                                 |
+| Hardware                        | Skylake or newer                                                                                                           |
+| Software                        | Intel&reg; oneAPI DPC++/C++ Compiler; Intel&reg; oneAPI DPC++ Library (oneDPL); Intel&reg; oneAPI Threading Building Blocks (oneTBB) |
+| Time to complete                | At most 5 minutes                                                                                                                    |
+
+## License
+
+This example is licensed under MIT license.
+
+## Building the 'Dot product' Program
+
+### On a Linux* System
+Perform the following steps:
+
+1. Build the program using the following `cmake` commands.
+```
+    $ mkdir build
+    $ cd build
+    $ cmake ..
+    $ make
+```
+
+2. Run the program:
+```
+    $ make run
+```
+
+3. Clean the program using:
+```
+    $ make clean
+```
+
+## Running the Sample
+### Example of Output
+
+```
+success
+The dot product is: 2.49872e+06
+```
