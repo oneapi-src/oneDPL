7217fbdeac29dcb935bbdb4f5f23672cb0e7b38e
Sobolev, Dmitriy <dmitriy.sobolev@intel.com>
2021-03-28 18:24:03 +0300

    Correct comments
    Signed-off-by: Sobolev, Dmitriy <dmitriy.sobolev@intel.com>

--- test/parallel_api/algorithm/alg.sorting/alg.set.operations/includes.pass.cpp
+++ test/parallel_api/algorithm/alg.sorting/alg.set.operations/includes.pass.cpp
@@ -57,7 +57,7 @@ struct test_without_compare
         auto expect_res = ::std::includes(first1, last1, first2, last2);
         auto res = ::std::includes(exec, first1, last1, first2, last2);
 
-        EXPECT_TRUE(expect_res == res, "wrong result for includes");
+        EXPECT_TRUE(expect_res == res, "wrong result for includes without predicate");
     }
 
     template <typename Policy, typename InputIterator1, typename InputIterator2>
@@ -80,7 +80,7 @@ struct test_with_compare
         auto expect_res = ::std::includes(first1, last1, first2, last2, comp);
         auto res = ::std::includes(exec, first1, last1, first2, last2, comp);
 
-        EXPECT_TRUE(expect_res == res, "wrong result for includes");
+        EXPECT_TRUE(expect_res == res, "wrong result for includes with predicate");
     }
 
     template <typename Policy, typename InputIterator1, typename InputIterator2, typename Compare>
