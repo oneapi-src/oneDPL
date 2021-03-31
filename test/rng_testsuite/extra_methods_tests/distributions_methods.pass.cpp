889c067059fba7eaa231c5c4373962a43441df76
Semin, Nikita <nikita.semin@intel.com>
2021-03-31 21:39:14 +0300

    chmod and clang format

--- test/rng_testsuite/extra_methods_tests/distributions_methods.pass.cpp
+++ test/rng_testsuite/extra_methods_tests/distributions_methods.pass.cpp
@@ -21,77 +21,89 @@
 #include <iostream>
 
 #if _ONEDPL_BACKEND_SYCL
-#include <cmath>
-#include <vector>
-#include <CL/sycl.hpp>
-#include <oneapi/dpl/random>
+#    include <cmath>
+#    include <vector>
+#    include <CL/sycl.hpp>
+#    include <oneapi/dpl/random>
 
-#define SEED                          777
-#define N_GEN                        1000
+#    define SEED 777
+#    define N_GEN 1000
 
-template<class T>
-std::int32_t check_params(oneapi::dpl::uniform_int_distribution<T>& distr,
-    typename oneapi::dpl::internal::type_traits_t<T>::element_type a,
-    typename oneapi::dpl::internal::type_traits_t<T>::element_type b) {
-    return ((distr.a() != a) || (distr.b() != b) || (distr.min() != a) ||
-        (distr.max() != b) || (distr.param().first != a) || (distr.param().second != b));
+template <class T>
+std::int32_t
+check_params(oneapi::dpl::uniform_int_distribution<T>& distr,
+             typename oneapi::dpl::internal::type_traits_t<T>::element_type a,
+             typename oneapi::dpl::internal::type_traits_t<T>::element_type b)
+{
+    return ((distr.a() != a) || (distr.b() != b) || (distr.min() != a) || (distr.max() != b) ||
+            (distr.param().first != a) || (distr.param().second != b));
 }
 
-template<class T>
-std::int32_t check_params(oneapi::dpl::uniform_real_distribution<T>& distr,
-    typename oneapi::dpl::internal::type_traits_t<T>::element_type a,
-    typename oneapi::dpl::internal::type_traits_t<T>::element_type b) {
-    return ((distr.a() != a) || (distr.b() != b) || (distr.min() != a) ||
-        (distr.max() != b) || (distr.param().first != a) || (distr.param().second != b));
+template <class T>
+std::int32_t
+check_params(oneapi::dpl::uniform_real_distribution<T>& distr,
+             typename oneapi::dpl::internal::type_traits_t<T>::element_type a,
+             typename oneapi::dpl::internal::type_traits_t<T>::element_type b)
+{
+    return ((distr.a() != a) || (distr.b() != b) || (distr.min() != a) || (distr.max() != b) ||
+            (distr.param().first != a) || (distr.param().second != b));
 }
 
-template<class T>
-std::int32_t check_params(oneapi::dpl::normal_distribution<T>& distr,
-    typename oneapi::dpl::internal::type_traits_t<T>::element_type mean,
-    typename oneapi::dpl::internal::type_traits_t<T>::element_type stddev) {
-    return ((distr.mean() != mean) || (distr.stddev() != stddev) ||
-        std::isinf(distr.min()) || std::isinf(distr.max()) ||
-        (distr.param().first != mean) || (distr.param().second != stddev));
+template <class T>
+std::int32_t
+check_params(oneapi::dpl::normal_distribution<T>& distr,
+             typename oneapi::dpl::internal::type_traits_t<T>::element_type mean,
+             typename oneapi::dpl::internal::type_traits_t<T>::element_type stddev)
+{
+    return ((distr.mean() != mean) || (distr.stddev() != stddev) || std::isinf(distr.min()) ||
+            std::isinf(distr.max()) || (distr.param().first != mean) || (distr.param().second != stddev));
 }
 
-template<class Distr>
-bool test() {
+template <class Distr>
+bool
+test()
+{
     // Catch asynchronous exceptions
-    auto exception_handler = [] (sycl::exception_list exceptions) {
-        for (std::exception_ptr const& e : exceptions) {
-            try {
+    auto exception_handler = [](sycl::exception_list exceptions) {
+        for (std::exception_ptr const& e : exceptions)
+        {
+            try
+            {
                 std::rethrow_exception(e);
-            } catch(sycl::exception const& e) {
-                std::cout << "Caught asynchronous SYCL exception during generation:\n"
-                << e.what() << std::endl;
+            }
+            catch (sycl::exception const& e)
+            {
+                std::cout << "Caught asynchronous SYCL exception during generation:\n" << e.what() << std::endl;
             }
         }
     };
 
     typename Distr::param_type params1(static_cast<typename Distr::scalar_type>(0),
-        static_cast<typename Distr::scalar_type>(10));
+                                       static_cast<typename Distr::scalar_type>(10));
     typename Distr::param_type params2(static_cast<typename Distr::scalar_type>(2),
-        static_cast<typename Distr::scalar_type>(8));
+                                       static_cast<typename Distr::scalar_type>(8));
 
     sycl::queue queue(sycl::default_selector{}, exception_handler);
     int sum = 0;
 
     // Memory allocation
     std::vector<std::int32_t> dpstd_res(N_GEN);
-    constexpr std::int32_t num_elems = oneapi::dpl::internal::type_traits_t<typename Distr::result_type>::num_elems == 0 ? 1 : oneapi::dpl::internal::type_traits_t<typename Distr::result_type>::num_elems;
+    constexpr std::int32_t num_elems =
+        oneapi::dpl::internal::type_traits_t<typename Distr::result_type>::num_elems == 0
+            ? 1
+            : oneapi::dpl::internal::type_traits_t<typename Distr::result_type>::num_elems;
 
     // Random number generation
     {
         sycl::buffer<std::int32_t, 1> dpstd_buffer(dpstd_res.data(), dpstd_res.size());
 
-        try {
+        try
+        {
 
-            queue.submit([&](sycl::handler &cgh) {
+            queue.submit([&](sycl::handler& cgh) {
                 auto dpstd_acc = dpstd_buffer.template get_access<sycl::access::mode::write>(cgh);
 
-                cgh.parallel_for<>(sycl::range<1>(N_GEN / 4),
-                    [=](sycl::item<1> idx) {
-
+                cgh.parallel_for<>(sycl::range<1>(N_GEN / 4), [=](sycl::item<1> idx) {
                     unsigned long long offset = idx.get_linear_id();
                     oneapi::dpl::minstd_rand engine(SEED, offset);
                     Distr d1;
@@ -101,10 +113,13 @@ bool test() {
                     typename Distr::scalar_type res0;
                     typename Distr::scalar_type res1;
                     if constexpr (std::is_same<typename Distr::result_type,
-                        sycl::vec<typename Distr::scalar_type, num_elems>>::value) {
+                                               sycl::vec<typename Distr::scalar_type, num_elems>>::value)
+                    {
                         res0 = d1(engine, params2, 1)[0];
                         res1 = d1(engine, params1, 1)[0];
-                    } else {
+                    }
+                    else
+                    {
                         res0 = d1(engine, params2);
                         res1 = d1(engine, params1);
                     }
@@ -113,17 +128,18 @@ bool test() {
                 });
             });
         }
-        catch(sycl::exception const& e) {
+        catch (sycl::exception const& e)
+        {
             std::cout << "\t\tSYCL exception during generation\n"
-                      << e.what() << std::endl << "OpenCL status: " << e.get_cl_code() << std::endl;
+                      << e.what() << std::endl
+                      << "OpenCL status: " << e.get_cl_code() << std::endl;
             return 1;
         }
 
         queue.wait_and_throw();
         Distr distr;
         // 0 and 1 are default parameters for uniform_int, uniform_real and normal distributions
-        check_params(distr, static_cast<typename Distr::scalar_type>(0),
-            static_cast<typename Distr::scalar_type>(1));
+        check_params(distr, static_cast<typename Distr::scalar_type>(0), static_cast<typename Distr::scalar_type>(1));
     }
 
     return sum;
@@ -131,7 +147,9 @@ bool test() {
 
 #endif // _ONEDPL_BACKEND_SYCL
 
-int main() {
+int
+main()
+{
 
 #if _ONEDPL_BACKEND_SYCL
 
@@ -142,13 +160,14 @@ int main() {
     std::cout << "uniform_int_distribution<std::int32_t>" << std::endl;
     std::cout << "---------------------------------------------------" << std::endl;
     err += test<oneapi::dpl::uniform_int_distribution<std::int32_t>>();
-    err += test<oneapi::dpl::uniform_int_distribution<sycl::vec<std::int32_t,1>>>();
-    err += test<oneapi::dpl::uniform_int_distribution<sycl::vec<std::int32_t,2>>>();
-    err += test<oneapi::dpl::uniform_int_distribution<sycl::vec<std::int32_t,3>>>();
-    err += test<oneapi::dpl::uniform_int_distribution<sycl::vec<std::int32_t,4>>>();
-    err += test<oneapi::dpl::uniform_int_distribution<sycl::vec<std::int32_t,8>>>();
-    err += test<oneapi::dpl::uniform_int_distribution<sycl::vec<std::int32_t,16>>>();
-    if (err) {
+    err += test<oneapi::dpl::uniform_int_distribution<sycl::vec<std::int32_t, 1>>>();
+    err += test<oneapi::dpl::uniform_int_distribution<sycl::vec<std::int32_t, 2>>>();
+    err += test<oneapi::dpl::uniform_int_distribution<sycl::vec<std::int32_t, 3>>>();
+    err += test<oneapi::dpl::uniform_int_distribution<sycl::vec<std::int32_t, 4>>>();
+    err += test<oneapi::dpl::uniform_int_distribution<sycl::vec<std::int32_t, 8>>>();
+    err += test<oneapi::dpl::uniform_int_distribution<sycl::vec<std::int32_t, 16>>>();
+    if (err)
+    {
         std::cout << "Test FAILED" << std::endl;
     }
     global_err += err;
@@ -158,13 +177,14 @@ int main() {
     std::cout << "uniform_int_distribution<std::uint32_t>" << std::endl;
     std::cout << "---------------------------------------------------" << std::endl;
     err += test<oneapi::dpl::uniform_int_distribution<std::uint32_t>>();
-    err += test<oneapi::dpl::uniform_int_distribution<sycl::vec<std::uint32_t,1>>>();
-    err += test<oneapi::dpl::uniform_int_distribution<sycl::vec<std::uint32_t,2>>>();
-    err += test<oneapi::dpl::uniform_int_distribution<sycl::vec<std::uint32_t,3>>>();
-    err += test<oneapi::dpl::uniform_int_distribution<sycl::vec<std::uint32_t,4>>>();
-    err += test<oneapi::dpl::uniform_int_distribution<sycl::vec<std::uint32_t,8>>>();
-    err += test<oneapi::dpl::uniform_int_distribution<sycl::vec<std::uint32_t,16>>>();
-    if (err) {
+    err += test<oneapi::dpl::uniform_int_distribution<sycl::vec<std::uint32_t, 1>>>();
+    err += test<oneapi::dpl::uniform_int_distribution<sycl::vec<std::uint32_t, 2>>>();
+    err += test<oneapi::dpl::uniform_int_distribution<sycl::vec<std::uint32_t, 3>>>();
+    err += test<oneapi::dpl::uniform_int_distribution<sycl::vec<std::uint32_t, 4>>>();
+    err += test<oneapi::dpl::uniform_int_distribution<sycl::vec<std::uint32_t, 8>>>();
+    err += test<oneapi::dpl::uniform_int_distribution<sycl::vec<std::uint32_t, 16>>>();
+    if (err)
+    {
         std::cout << "Test FAILED" << std::endl;
     }
     global_err += err;
@@ -174,13 +194,14 @@ int main() {
     std::cout << "uniform_real_distribution<float>" << std::endl;
     std::cout << "---------------------------------------------------" << std::endl;
     err += test<oneapi::dpl::uniform_real_distribution<float>>();
-    err += test<oneapi::dpl::uniform_real_distribution<sycl::vec<float,1>>>();
-    err += test<oneapi::dpl::uniform_real_distribution<sycl::vec<float,2>>>();
-    err += test<oneapi::dpl::uniform_real_distribution<sycl::vec<float,3>>>();
-    err += test<oneapi::dpl::uniform_real_distribution<sycl::vec<float,4>>>();
-    err += test<oneapi::dpl::uniform_real_distribution<sycl::vec<float,8>>>();
-    err += test<oneapi::dpl::uniform_real_distribution<sycl::vec<float,16>>>();
-    if (err) {
+    err += test<oneapi::dpl::uniform_real_distribution<sycl::vec<float, 1>>>();
+    err += test<oneapi::dpl::uniform_real_distribution<sycl::vec<float, 2>>>();
+    err += test<oneapi::dpl::uniform_real_distribution<sycl::vec<float, 3>>>();
+    err += test<oneapi::dpl::uniform_real_distribution<sycl::vec<float, 4>>>();
+    err += test<oneapi::dpl::uniform_real_distribution<sycl::vec<float, 8>>>();
+    err += test<oneapi::dpl::uniform_real_distribution<sycl::vec<float, 16>>>();
+    if (err)
+    {
         std::cout << "Test FAILED" << std::endl;
     }
     global_err += err;
@@ -190,13 +211,14 @@ int main() {
     std::cout << "uniform_real_distribution<double>" << std::endl;
     std::cout << "---------------------------------------------------" << std::endl;
     err += test<oneapi::dpl::uniform_real_distribution<double>>();
-    err += test<oneapi::dpl::uniform_real_distribution<sycl::vec<double,1>>>();
-    err += test<oneapi::dpl::uniform_real_distribution<sycl::vec<double,2>>>();
-    err += test<oneapi::dpl::uniform_real_distribution<sycl::vec<double,3>>>();
-    err += test<oneapi::dpl::uniform_real_distribution<sycl::vec<double,4>>>();
-    err += test<oneapi::dpl::uniform_real_distribution<sycl::vec<double,8>>>();
-    err += test<oneapi::dpl::uniform_real_distribution<sycl::vec<double,16>>>();
-    if (err) {
+    err += test<oneapi::dpl::uniform_real_distribution<sycl::vec<double, 1>>>();
+    err += test<oneapi::dpl::uniform_real_distribution<sycl::vec<double, 2>>>();
+    err += test<oneapi::dpl::uniform_real_distribution<sycl::vec<double, 3>>>();
+    err += test<oneapi::dpl::uniform_real_distribution<sycl::vec<double, 4>>>();
+    err += test<oneapi::dpl::uniform_real_distribution<sycl::vec<double, 8>>>();
+    err += test<oneapi::dpl::uniform_real_distribution<sycl::vec<double, 16>>>();
+    if (err)
+    {
         std::cout << "Test FAILED" << std::endl;
     }
     global_err += err;
@@ -206,13 +228,14 @@ int main() {
     std::cout << "normal_distribution<float>" << std::endl;
     std::cout << "---------------------------------------------------" << std::endl;
     err += test<oneapi::dpl::normal_distribution<float>>();
-    err += test<oneapi::dpl::normal_distribution<sycl::vec<float,1>>>();
-    err += test<oneapi::dpl::normal_distribution<sycl::vec<float,2>>>();
-    err += test<oneapi::dpl::normal_distribution<sycl::vec<float,3>>>();
-    err += test<oneapi::dpl::normal_distribution<sycl::vec<float,4>>>();
-    err += test<oneapi::dpl::normal_distribution<sycl::vec<float,8>>>();
-    err += test<oneapi::dpl::normal_distribution<sycl::vec<float,16>>>();
-    if (err) {
+    err += test<oneapi::dpl::normal_distribution<sycl::vec<float, 1>>>();
+    err += test<oneapi::dpl::normal_distribution<sycl::vec<float, 2>>>();
+    err += test<oneapi::dpl::normal_distribution<sycl::vec<float, 3>>>();
+    err += test<oneapi::dpl::normal_distribution<sycl::vec<float, 4>>>();
+    err += test<oneapi::dpl::normal_distribution<sycl::vec<float, 8>>>();
+    err += test<oneapi::dpl::normal_distribution<sycl::vec<float, 16>>>();
+    if (err)
+    {
         std::cout << "Test FAILED" << std::endl;
     }
     global_err += err;
@@ -222,19 +245,21 @@ int main() {
     std::cout << "normal_distribution<double>" << std::endl;
     std::cout << "---------------------------------------------------" << std::endl;
     err += test<oneapi::dpl::normal_distribution<double>>();
-    err += test<oneapi::dpl::normal_distribution<sycl::vec<double,1>>>();
-    err += test<oneapi::dpl::normal_distribution<sycl::vec<double,2>>>();
-    err += test<oneapi::dpl::normal_distribution<sycl::vec<double,3>>>();
-    err += test<oneapi::dpl::normal_distribution<sycl::vec<double,4>>>();
-    err += test<oneapi::dpl::normal_distribution<sycl::vec<double,8>>>();
-    err += test<oneapi::dpl::normal_distribution<sycl::vec<double,16>>>();
-    if (err) {
+    err += test<oneapi::dpl::normal_distribution<sycl::vec<double, 1>>>();
+    err += test<oneapi::dpl::normal_distribution<sycl::vec<double, 2>>>();
+    err += test<oneapi::dpl::normal_distribution<sycl::vec<double, 3>>>();
+    err += test<oneapi::dpl::normal_distribution<sycl::vec<double, 4>>>();
+    err += test<oneapi::dpl::normal_distribution<sycl::vec<double, 8>>>();
+    err += test<oneapi::dpl::normal_distribution<sycl::vec<double, 16>>>();
+    if (err)
+    {
         std::cout << "Test FAILED" << std::endl;
     }
     global_err += err;
     err = 0;
 
-    if (global_err) {
+    if (global_err)
+    {
         return 1;
     }
 
--- test/rng_testsuite/extra_methods_tests/engines_methods.pass.cpp
+++ test/rng_testsuite/extra_methods_tests/engines_methods.pass.cpp
@@ -21,71 +21,82 @@
 #include <iostream>
 
 #if _ONEDPL_BACKEND_SYCL
-#include <vector>
-#include <CL/sycl.hpp>
-#include <oneapi/dpl/random>
+#    include <vector>
+#    include <CL/sycl.hpp>
+#    include <oneapi/dpl/random>
 
-#define SEED                          777
-#define N_GEN                         960
-#define MINSTD_A                    48271
-#define MINSTD_C                        0
-#define MINSTD_M               2147483647
-#define MINSTD_MIN                      1
-#define MINSTD_MAX             2147483646
-#define RANLUX24_BASE_W                24
-#define RANLUX24_BASE_S                10
-#define RANLUX24_BASE_R                24
-#define RANLUX24_BASE_MIN               0
-#define RANLUX24_BASE_MAX        16777215
-#define RANLUX24_P                    223
-#define RANLUX24_R                     23
+#    define SEED 777
+#    define N_GEN 960
+#    define MINSTD_A 48271
+#    define MINSTD_C 0
+#    define MINSTD_M 2147483647
+#    define MINSTD_MIN 1
+#    define MINSTD_MAX 2147483646
+#    define RANLUX24_BASE_W 24
+#    define RANLUX24_BASE_S 10
+#    define RANLUX24_BASE_R 24
+#    define RANLUX24_BASE_MIN 0
+#    define RANLUX24_BASE_MAX 16777215
+#    define RANLUX24_P 223
+#    define RANLUX24_R 23
 
-template<class Engine, int N>
-std::int32_t check_params(Engine& engine) {
-    if constexpr (std::is_same<Engine, oneapi::dpl::minstd_rand>::value) {
+template <class Engine, int N>
+std::int32_t
+check_params(Engine& engine)
+{
+    if constexpr (std::is_same<Engine, oneapi::dpl::minstd_rand>::value)
+    {
         return ((oneapi::dpl::minstd_rand::multiplier != MINSTD_A) ||
-            (oneapi::dpl::minstd_rand::increment != MINSTD_C) ||
-            (oneapi::dpl::minstd_rand::modulus != MINSTD_M) ||
-            (engine.min() != MINSTD_MIN) || (engine.max() != MINSTD_MAX));
+                (oneapi::dpl::minstd_rand::increment != MINSTD_C) || (oneapi::dpl::minstd_rand::modulus != MINSTD_M) ||
+                (engine.min() != MINSTD_MIN) || (engine.max() != MINSTD_MAX));
     }
-    if constexpr (std::is_same<Engine, oneapi::dpl::minstd_rand_vec<N>>::value) {
+    if constexpr (std::is_same<Engine, oneapi::dpl::minstd_rand_vec<N>>::value)
+    {
         return ((oneapi::dpl::minstd_rand_vec<N>::multiplier != MINSTD_A) ||
-            (oneapi::dpl::minstd_rand_vec<N>::increment != MINSTD_C) ||
-            (oneapi::dpl::minstd_rand_vec<N>::modulus != MINSTD_M) ||
-            (engine.min() != MINSTD_MIN) || (engine.max() != MINSTD_MAX));
+                (oneapi::dpl::minstd_rand_vec<N>::increment != MINSTD_C) ||
+                (oneapi::dpl::minstd_rand_vec<N>::modulus != MINSTD_M) || (engine.min() != MINSTD_MIN) ||
+                (engine.max() != MINSTD_MAX));
     }
-    if constexpr (std::is_same<Engine, oneapi::dpl::ranlux24_base>::value) {
+    if constexpr (std::is_same<Engine, oneapi::dpl::ranlux24_base>::value)
+    {
         return ((oneapi::dpl::ranlux24_base::word_size != RANLUX24_BASE_W) ||
-            (oneapi::dpl::ranlux24_base::short_lag != RANLUX24_BASE_S) ||
-            (oneapi::dpl::ranlux24_base::long_lag != RANLUX24_BASE_R) ||
-            (engine.min() != RANLUX24_BASE_MIN) || (engine.max() != RANLUX24_BASE_MAX));
+                (oneapi::dpl::ranlux24_base::short_lag != RANLUX24_BASE_S) ||
+                (oneapi::dpl::ranlux24_base::long_lag != RANLUX24_BASE_R) || (engine.min() != RANLUX24_BASE_MIN) ||
+                (engine.max() != RANLUX24_BASE_MAX));
     }
-    if constexpr (std::is_same<Engine, oneapi::dpl::ranlux24_base_vec<N>>::value) {
+    if constexpr (std::is_same<Engine, oneapi::dpl::ranlux24_base_vec<N>>::value)
+    {
         return ((oneapi::dpl::ranlux24_base_vec<N>::word_size != RANLUX24_BASE_W) ||
-            (oneapi::dpl::ranlux24_base_vec<N>::short_lag != RANLUX24_BASE_S) ||
-            (oneapi::dpl::ranlux24_base_vec<N>::long_lag != RANLUX24_BASE_R) ||
-            (engine.min() != RANLUX24_BASE_MIN) || (engine.max() != RANLUX24_BASE_MAX));
+                (oneapi::dpl::ranlux24_base_vec<N>::short_lag != RANLUX24_BASE_S) ||
+                (oneapi::dpl::ranlux24_base_vec<N>::long_lag != RANLUX24_BASE_R) ||
+                (engine.min() != RANLUX24_BASE_MIN) || (engine.max() != RANLUX24_BASE_MAX));
     }
-    if constexpr (std::is_same<Engine, oneapi::dpl::ranlux24>::value) {
+    if constexpr (std::is_same<Engine, oneapi::dpl::ranlux24>::value)
+    {
         return ((oneapi::dpl::ranlux24::block_size != RANLUX24_P) ||
-            (oneapi::dpl::ranlux24::used_block != RANLUX24_R) ||
-            (engine.min() != RANLUX24_BASE_MIN) || (engine.max() != RANLUX24_BASE_MAX));
+                (oneapi::dpl::ranlux24::used_block != RANLUX24_R) || (engine.min() != RANLUX24_BASE_MIN) ||
+                (engine.max() != RANLUX24_BASE_MAX));
     }
     return 0;
 }
 
-template<class Engine>
-bool test() {
+template <class Engine>
+bool
+test()
+{
     using result_type = typename Engine::scalar_type;
 
     // Catch asynchronous exceptions
-    auto exception_handler = [] (sycl::exception_list exceptions) {
-        for (std::exception_ptr const& e : exceptions) {
-            try {
+    auto exception_handler = [](sycl::exception_list exceptions) {
+        for (std::exception_ptr const& e : exceptions)
+        {
+            try
+            {
                 std::rethrow_exception(e);
-            } catch(sycl::exception const& e) {
-                std::cout << "Caught asynchronous SYCL exception during generation:\n"
-                << e.what() << std::endl;
+            }
+            catch (sycl::exception const& e)
+            {
+                std::cout << "Caught asynchronous SYCL exception during generation:\n" << e.what() << std::endl;
             }
         }
     };
@@ -95,19 +106,21 @@ bool test() {
 
     // Memory allocation
     std::vector<std::int32_t> dpstd_res(N_GEN);
-    constexpr std::int32_t num_elems = oneapi::dpl::internal::type_traits_t<typename Engine::result_type>::num_elems == 0 ? 1 : oneapi::dpl::internal::type_traits_t<typename Engine::result_type>::num_elems;
+    constexpr std::int32_t num_elems =
+        oneapi::dpl::internal::type_traits_t<typename Engine::result_type>::num_elems == 0
+            ? 1
+            : oneapi::dpl::internal::type_traits_t<typename Engine::result_type>::num_elems;
 
     // Random number generation
     {
         sycl::buffer<std::int32_t, 1> dpstd_buffer(dpstd_res.data(), dpstd_res.size());
 
-        try {
-            queue.submit([&](sycl::handler &cgh) {
+        try
+        {
+            queue.submit([&](sycl::handler& cgh) {
                 auto dpstd_acc = dpstd_buffer.template get_access<sycl::access::mode::write>(cgh);
 
-                cgh.parallel_for<>(sycl::range<1>(N_GEN),
-                        [=](sycl::item<1> idx) {
-
+                cgh.parallel_for<>(sycl::range<1>(N_GEN), [=](sycl::item<1> idx) {
                     unsigned long long offset = idx.get_linear_id() * num_elems;
                     Engine engine0(SEED);
                     Engine engine1;
@@ -115,43 +128,58 @@ bool test() {
                     engine0.discard(offset);
                     engine1.discard(offset);
                     typename Engine::result_type res0;
-                    if constexpr (std::is_same<Engine, oneapi::dpl::ranlux24>::value) {
+                    if constexpr (std::is_same<Engine, oneapi::dpl::ranlux24>::value)
+                    {
                         Engine engine(engine1);
                         auto eng = engine.base();
                         res0 = engine();
-                    } else {
+                    }
+                    else
+                    {
                         res0 = engine0();
                     }
                     typename Engine::result_type res1 = engine1();
                     if constexpr ((num_elems > 1) || (std::is_same<typename Engine::result_type,
-                        sycl::vec<typename Engine::scalar_type, 1>>::value)) {
+                                                                   sycl::vec<typename Engine::scalar_type, 1>>::value))
+                    {
                         std::int32_t is_inequal = 0;
-                        for (std::int32_t i = 0; i < num_elems; ++i) {
-                            if (res0[i] != res1[i]) {
+                        for (std::int32_t i = 0; i < num_elems; ++i)
+                        {
+                            if (res0[i] != res1[i])
+                            {
                                 is_inequal = 1;
                             }
                         }
                         dpstd_acc[offset] = is_inequal;
-                    } else {
-                        if (res0 != res1) {
+                    }
+                    else
+                    {
+                        if (res0 != res1)
+                        {
                             dpstd_acc[offset] = 1;
-                        } else {
+                        }
+                        else
+                        {
                             dpstd_acc[offset] = 0;
                         }
                     }
                 });
             });
             auto dpstd_acc = dpstd_buffer.template get_access<sycl::access::mode::write>();
-            for (int i = 0; i < N_GEN; ++i) {
+            for (int i = 0; i < N_GEN; ++i)
+            {
                 sum += dpstd_acc[i];
             }
-            if (sum) {
+            if (sum)
+            {
                 std::cout << "Error occured in " << sum << " elements" << std::endl;
             }
         }
-        catch(sycl::exception const& e) {
+        catch (sycl::exception const& e)
+        {
             std::cout << "\t\tSYCL exception during generation\n"
-                      << e.what() << std::endl << "OpenCL status: " << e.get_cl_code() << std::endl;
+                      << e.what() << std::endl
+                      << "OpenCL status: " << e.get_cl_code() << std::endl;
             return 1;
         }
 
@@ -165,7 +193,9 @@ bool test() {
 
 #endif // _ONEDPL_BACKEND_SYCL
 
-int main() {
+int
+main()
+{
 
 #if _ONEDPL_BACKEND_SYCL
 
@@ -182,7 +212,8 @@ int main() {
     err += test<oneapi::dpl::minstd_rand_vec<4>>();
     err += test<oneapi::dpl::minstd_rand_vec<8>>();
     err += test<oneapi::dpl::minstd_rand_vec<16>>();
-    if (err) {
+    if (err)
+    {
         std::cout << "Test FAILED" << std::endl;
     }
     global_err += err;
@@ -198,7 +229,8 @@ int main() {
     err += test<oneapi::dpl::ranlux24_base_vec<4>>();
     err += test<oneapi::dpl::ranlux24_base_vec<8>>();
     err += test<oneapi::dpl::ranlux24_base_vec<16>>();
-    if (err) {
+    if (err)
+    {
         std::cout << "Test FAILED" << std::endl;
     }
     global_err += err;
@@ -214,12 +246,14 @@ int main() {
     err += test<oneapi::dpl::ranlux24_vec<4>>();
     err += test<oneapi::dpl::ranlux24_vec<8>>();
     err += test<oneapi::dpl::ranlux24_vec<16>>();
-    if (err) {
+    if (err)
+    {
         std::cout << "Test FAILED" << std::endl;
     }
     global_err += err;
 
-    if (global_err) {
+    if (global_err)
+    {
         return 1;
     }
 
