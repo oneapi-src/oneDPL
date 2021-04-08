65c0315b4d5510fbc376913c7e1d03eeee3a1332
Semin, Nikita <nikita.semin@intel.com>
2021-04-08 09:30:38 +0300

    w/a for isinf problem

--- test/rng_testsuite/interface_tests/distributions_methods.pass.cpp
+++ test/rng_testsuite/interface_tests/distributions_methods.pass.cpp
@@ -58,8 +58,10 @@ check_params(oneapi::dpl::normal_distribution<T>& distr)
 {
     Element_type<T> mean = static_cast<Element_type<T>>(0);
     Element_type<T> stddev = static_cast<Element_type<T>>(1);
-    return ((distr.mean() != mean) || (distr.stddev() != stddev) || !std::isinf(distr.min()) ||
-            !std::isinf(distr.max()) || (distr.param().first != mean) || (distr.param().second != stddev));
+    return ((distr.mean() != mean) || (distr.stddev() != stddev) ||
+            (distr.min() > -std::numeric_limits<Element_type<T>>::max()) ||
+            (distr.max() < std::numeric_limits<Element_type<T>>::max()) ||
+            (distr.param().first != mean) || (distr.param().second != stddev));
 }
 
 template <class Distr>
@@ -278,6 +280,7 @@ main()
     err += test_vec<oneapi::dpl::normal_distribution<sycl::vec<float, 4>>>();
     err += test_vec<oneapi::dpl::normal_distribution<sycl::vec<float, 8>>>();
     err += test_vec<oneapi::dpl::normal_distribution<sycl::vec<float, 16>>>();
+    std::cout << "err = " << err << std::endl;
     EXPECT_TRUE(!err, "Test FAILED");
 
     std::cout << "---------------------------------------------------" << std::endl;
