93a087a386440a59194976700b348c8093d95060
gejin <ge.jin@intel.com>
2021-04-09 13:42:39 +0800

    adjust <iterator>
    Signed-off-by: gejin <ge.jin@intel.com>

--- include/oneapi/dpl/internal/serial_numeric_impl.h
+++ include/oneapi/dpl/internal/serial_numeric_impl.h
@@ -16,6 +16,7 @@
 
 #ifndef _ONEDPL_SERIAL_NUMERIC_IMPL_H
 #define _ONEDPL_SERIAL_NUMERIC_IMPL_H
+#include <iterator>
 
 #if __cplusplus < 201703L
 #    define _ONEDPL_HAS_NUMERIC_SERIAL_IMPL 1
@@ -30,7 +31,6 @@ namespace oneapi
 namespace dpl
 {
 #ifdef _ONEDPL_HAS_NUMERIC_SERIAL_IMPL
-#    include <iterator>
 template <class _InputIterator, class _Tp, class _BinaryOp>
 _Tp
 reduce(_InputIterator __first, _InputIterator __last, _Tp __init, _BinaryOp __b)
