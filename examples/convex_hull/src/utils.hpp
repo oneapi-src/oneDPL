9dd3a0e2651716bed3f572de05a9fe11a060a05e
Fedorov, Andrey <andrey.fedorov@intel.com>
2021-04-16 12:42:49 +0300

    update license

--- examples/convex_hull/src/utils.hpp
+++ examples/convex_hull/src/utils.hpp
@@ -1,8 +1,17 @@
-//==============================================================
-// Copyright © 2017-2020 Intel Corporation
+// -*- C++ -*-
+//===----------------------------------------------------------------------===//
 //
-// SPDX-License-Identifier: MIT
-// =============================================================
+// Copyright (C) Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// This file incorporates work covered by the following copyright and permission
+// notice:
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+//
+//===----------------------------------------------------------------------===//
 
 #include <iostream>
 #include <random>
