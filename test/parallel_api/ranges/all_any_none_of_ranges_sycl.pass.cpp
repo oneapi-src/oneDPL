c8f76f2bdcf53f96e36f89108f50634666a79baa
Mikhail Dvorskiy <mikhail.dvorskiy@intel.com>
2021-04-20 12:48:38 +0300

    [dpc++][ranges] + test for range-based none_of, adjacent_find

--- test/parallel_api/ranges/all_any_none_of_ranges_sycl.pass.cpp
+++ test/parallel_api/ranges/all_any_none_of_ranges_sycl.pass.cpp
@@ -33,9 +33,9 @@ main()
     int data1[max_n]     = {-1, 1, -1, 3, 4, 5, 6, -1, 8, 9};
     int data2[max_n]     = {0, 2, 4, 6, 8, 10, 12, 14, 16, 18};
 
-    auto lambda = [](auto i) { return i % 2 == 0; };
+    auto lambda = [](auto i) { return i%2 == 0; };
 
-    bool res1 = 0, res2 = 0;
+    bool res1 = false, res2 = false, res3 = false;
     using namespace oneapi::dpl::experimental::ranges;
     {
         sycl::buffer<int> A(data1, sycl::range<1>(max_n));
@@ -44,13 +44,16 @@ main()
         auto exec1 = TestUtils::default_dpcpp_policy;
         using Policy = decltype(exec1);
         auto exec2 = TestUtils::make_new_policy<TestUtils::new_kernel_name<Policy, 0>>(exec1);
+        auto exec3 = TestUtils::make_new_policy<TestUtils::new_kernel_name<Policy, 1>>(exec1);
                                        
         res1 = any_of(exec1, views::all(A), lambda);
         res2 = all_of(exec2, B, lambda);
+        res3 = none_of(exec3, B, [](auto i) { return i == -1;});
     }
 
     EXPECT_TRUE(res1, "wrong result from any_of with sycl ranges");
     EXPECT_TRUE(res2, "wrong result from all_of with sycl ranges");
+    EXPECT_TRUE(res3, "wrong result from none_of with sycl ranges");
 #endif //_ENABLE_RANGES_TESTING
     ::std::cout << TestUtils::done() << ::std::endl;
     return 0;
