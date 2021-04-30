5e73e488893e5db03adfda93054d03793ba7d279
Denis Paranichev <denis.paranichev@intel.com>
2021-04-30 15:54:10 +0300

    Code coverage improvement for the inplace_merge algorithm without predicate (#211)
    * Improved code coverage for inplace_merge algo without comparator
    
    * Erased code for debug
    
    * Simplify implementation
    
    * Moved if under for loop
    
    * Made loop shorter

--- test/parallel_api/algorithm/alg.merge/inplace_merge.pass.cpp
+++ test/parallel_api/algorithm/alg.merge/inplace_merge.pass.cpp
@@ -32,19 +32,43 @@ struct test_one_policy
     operator()(Policy&& exec, BiDirIt1 first1, BiDirIt1 last1, BiDirIt1 first2, BiDirIt1 last2, Size n, Size m,
                Generator1 generator1, Generator2 generator2, Compare comp)
     {
-        const BiDirIt1 mid1 = ::std::next(first1, m);
-        fill_data(first1, mid1, generator1);
-        fill_data(mid1, last1, generator2);
-
-        const BiDirIt1 mid2 = ::std::next(first2, m);
-        fill_data(first2, mid2, generator1);
-        fill_data(mid2, last2, generator2);
-
-        ::std::inplace_merge(first1, mid1, last1, comp);
-        ::std::inplace_merge(exec, first2, mid2, last2, comp);
+        auto mid = init(first1, last1, first2, last2, generator1, generator2, m);
+        ::std::inplace_merge(first1, mid.first, last1, comp);
+        ::std::inplace_merge(exec, first2, mid.second, last2, comp);
         EXPECT_EQ_N(first1, first2, n, "wrong effect from inplace_merge with predicate");
     }
 
+    template <typename Policy, typename BiDirIt1, typename Size, typename Generator1, typename Generator2>
+    typename ::std::enable_if<!is_same_iterator_category<BiDirIt1, ::std::forward_iterator_tag>::value, void>::type
+    operator()(Policy&& exec, BiDirIt1 first1, BiDirIt1 last1, BiDirIt1 first2, BiDirIt1 last2, Size n, Size m,
+               Generator1 generator1, Generator2 generator2)
+    {
+        auto mid = init(first1, last1, first2, last2, generator1, generator2, m);
+        ::std::inplace_merge(first1, mid.first, last1);
+        ::std::inplace_merge(exec, first2, mid.second, last2);
+        EXPECT_EQ_N(first1, first2, n, "wrong effect from inplace_merge without predicate");
+    }
+
+    template<typename BiDirIt, typename Generator1, typename Generator2, typename Size>
+    ::std::pair<const BiDirIt, const BiDirIt> init(BiDirIt first1, BiDirIt last1, BiDirIt first2, BiDirIt last2,
+                                                    Generator1 generator1, Generator2 generator2, Size m)
+    {
+        const BiDirIt mid1 = ::std::next(first1, m);
+        fill_data(first1, mid1, generator1);
+        fill_data(mid1, last1, generator2);
+
+        const BiDirIt mid2 = ::std::next(first2, m);
+        fill_data(first2, mid2, generator1);
+        fill_data(mid2, last2, generator2);
+        return ::std::make_pair(mid1, mid2);
+    }
+
+    template <typename Policy, typename BiDirIt1, typename Size, typename Generator1, typename Generator2>
+    typename ::std::enable_if<is_same_iterator_category<BiDirIt1, ::std::forward_iterator_tag>::value, void>::type
+    operator()(Policy&& /* exec */, BiDirIt1 /* first1 */, BiDirIt1 /* last1 */, BiDirIt1 /* first2 */, BiDirIt1 /* last2 */, Size /* n */, Size /* m */,
+               Generator1 /* generator1 */, Generator2 /* generator2 */)
+    {
+    }
     template <typename Policy, typename BiDirIt1, typename Size, typename Generator1, typename Generator2,
               typename Compare>
     typename ::std::enable_if<is_same_iterator_category<BiDirIt1, ::std::forward_iterator_tag>::value, void>::type
@@ -56,7 +80,7 @@ struct test_one_policy
 
 template <typename T, typename Generator1, typename Generator2, typename Compare>
 void
-test_by_type(Generator1 generator1, Generator2 generator2, Compare comp)
+test_by_type(Generator1 generator1, Generator2 generator2, bool comp_flag, Compare comp)
 {
     using namespace std;
     size_t max_size = 100000;
@@ -64,20 +88,31 @@ test_by_type(Generator1 generator1, Generator2 generator2, Compare comp)
     Sequence<T> exp(max_size, [](size_t v) { return T(v); });
     size_t m;
 
+    
     for (size_t n = 0; n <= max_size; n = n <= 16 ? n + 1 : size_t(3.1415 * n))
     {
         m = 0;
-        invoke_on_all_policies<0>()(test_one_policy<T>(), in1.begin(), in1.begin() + n, exp.begin(), exp.begin() + n, n, m,
-                               generator1, generator2, comp);
-
+        if(comp_flag)
+            invoke_on_all_policies<0>()(test_one_policy<T>(), in1.begin(), in1.begin() + n, exp.begin(), exp.begin() + n, n, m,
+                                   generator1, generator2, comp);
+        else
+            invoke_on_all_policies<1>()(test_one_policy<T>(), in1.begin(), in1.begin() + n, exp.begin(), exp.begin() + n, n, m,
+                               generator1, generator2);
         m = n / 3;
-        invoke_on_all_policies<1>()(test_one_policy<T>(), in1.begin(), in1.begin() + n, exp.begin(), exp.begin() + n, n, m,
-                               generator1, generator2, comp);
-
+        if(comp_flag)
+            invoke_on_all_policies<2>()(test_one_policy<T>(), in1.begin(), in1.begin() + n, exp.begin(), exp.begin() + n, n, m,
+                                   generator1, generator2, comp);
+        else
+            invoke_on_all_policies<3>()(test_one_policy<T>(), in1.begin(), in1.begin() + n, exp.begin(), exp.begin() + n, n, m,
+                               generator1, generator2);
 #if !ONEDPL_FPGA_DEVICE
         m = 2 * n / 3;
-        invoke_on_all_policies<2>()(test_one_policy<T>(), in1.begin(), in1.begin() + n, exp.begin(), exp.begin() + n, n, m,
-                               generator1, generator2, comp);
+        if(comp_flag)
+            invoke_on_all_policies<4>()(test_one_policy<T>(), in1.begin(), in1.begin() + n, exp.begin(), exp.begin() + n, n, m,
+                                   generator1, generator2, comp);
+        else
+            invoke_on_all_policies<5>()(test_one_policy<T>(), in1.begin(), in1.begin() + n, exp.begin(), exp.begin() + n, n, m,
+                               generator1, generator2);
 #endif
     }
 }
@@ -128,19 +163,19 @@ int
 main()
 {
 #if !ONEDPL_FPGA_DEVICE
-    test_by_type<float64_t>([](int32_t i) { return -2 * i; }, [](int32_t i) { return -(2 * i + 1); },
+    test_by_type<float64_t>([](int32_t i) { return -2 * i; }, [](int32_t i) { return -(2 * i + 1); }, true,
                             [](const float64_t x, const float64_t y) { return x > y; });
 #endif
 
-    test_by_type<int32_t>([](int32_t i) { return 10 * i; }, [](int32_t i) { return i + 1; }, ::std::less<int32_t>());
+    test_by_type<int32_t>([](int32_t i) { return 10 * i; }, [](int32_t i) { return i + 1; }, false, ::std::less<int32_t>());
 
 #if !TEST_DPCPP_BACKEND_PRESENT
     test_by_type<LocalWrapper<float32_t>>([](int32_t i) { return LocalWrapper<float32_t>(2 * i + 1); },
-                                          [](int32_t i) { return LocalWrapper<float32_t>(2 * i); },
+                                          [](int32_t i) { return LocalWrapper<float32_t>(2 * i); }, true,
                                           ::std::less<LocalWrapper<float32_t>>());
     test_by_type<MemoryChecker>(
         [](::std::size_t idx){ return MemoryChecker{::std::int32_t(idx * 2)}; },
-        [](::std::size_t idx){ return MemoryChecker{::std::int32_t(idx * 2 + 1)}; },
+        [](::std::size_t idx){ return MemoryChecker{::std::int32_t(idx * 2 + 1)}; }, true,
         [](const MemoryChecker& val1, const MemoryChecker& val2){ return val1.value() < val2.value(); });
     EXPECT_TRUE(MemoryChecker::alive_objects() == 0, "wrong effect from inplace_merge: number of ctor and dtor calls is not equal");
 #endif
