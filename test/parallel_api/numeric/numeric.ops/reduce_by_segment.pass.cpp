5f32d51043780a8e5249c58a64e43ffa3c81c8a6
Timmie Smith <timmie.smith@intel.com>
2021-05-19 17:20:58 -0500

    Add specialization for host policies and correct initialization function to work with iterator iterface to support reverse_iterator tests.

--- test/parallel_api/numeric/numeric.ops/reduce_by_segment.pass.cpp
+++ test/parallel_api/numeric/numeric.ops/reduce_by_segment.pass.cpp
@@ -25,45 +25,42 @@
 #include <iomanip>
 
 #if TEST_DPCPP_BACKEND_PRESENT
-#    include "support/utils_sycl.h"
-
 #    include <CL/sycl.hpp>
 
-using namespace TestUtils;
 using namespace oneapi::dpl::execution;
+#endif
+using namespace TestUtils;
 
 struct test_reduce_by_segment
 {
 
     template <typename Iterator1, typename Iterator2, typename Iterator3, typename Iterator4, typename Size>
     void
-    initialize_data(Iterator1 keys_first, Iterator2 vals_first, Iterator3 key_res_first, Iterator4 val_res_first,
-                    Size n)
+    initialize_data(Iterator1 host_keys, Iterator2 host_vals, Iterator3 host_key_res, Iterator4 host_val_res, Size n)
     {
         //T keys[n1] = { 1, 2, 3, 4, 1, 1, 3, 3, 1, 1, 3, 3, ..., 0 };
         //T vals[n1] = { 1, 2, 3, 4, 1, 1, 3, 3, 1, 1, 3, 3, ..., 0 };
-
-        auto host_keys = get_host_pointer(keys_first);
-        auto host_vals = get_host_pointer(vals_first);
-        auto host_key_res = get_host_pointer(key_res_first);
-        auto host_val_res = get_host_pointer(val_res_first);
-
-        for (int i = 0; i<n - 1; i> 3 ? i += 2 : ++i)
+        for (int i = 0; i<n - 1; i> 3 ? i += 2 : ++i, ++host_keys, ++host_vals, ++host_key_res, ++host_val_res)
         {
-            host_keys[i] = i % 4 + 1;
-            host_vals[i] = i % 4 + 1;
-            host_key_res[i] = 9;
-            host_val_res[i] = 1;
-            if (i > 3)
+            *host_keys = i % 4 + 1;
+            *host_vals = i % 4 + 1;
+            *host_key_res = 9;
+            *host_val_res = 1;
+            if ((i > 3) && (i + 1 <= n - 1))
             {
-                host_keys[i + 1] = host_keys[i];
-                host_vals[i + 1] = host_vals[i];
-                host_key_res[i + 1] = 9;
-                host_val_res[i + 1] = 1;
+                auto tmp = *host_keys;
+                ++host_keys;
+                *host_keys = tmp;
+                tmp = *host_vals;
+                ++host_vals;
+                *host_vals = tmp;
+                ++host_key_res;
+                *host_key_res = 9;
+                *host_val_res = 1;
             }
         }
-        host_keys[n - 1] = 0;
-        host_vals[n - 1] = 0;
+        *host_keys = 0;
+        *host_vals = 0;
     }
 
     template <typename Iterator1, typename Iterator2, typename Size>
@@ -73,37 +70,39 @@ struct test_reduce_by_segment
         // keys_result = {1, 2, 3, 4, 1, 3, 1, 3, ..., 0};
         // vals_result = {1, 2, 3, 4, 2, 6, 2, 6, ..., 0};
 
-        auto host_key_res = get_host_pointer(key_res);
-        auto host_val_res = get_host_pointer(val_res);
-
         for (auto i = 0; i != n; ++i)
         {
             if (i == n - 1)
             {
-                EXPECT_TRUE(host_key_res[i] == 0, "wrong effect from reduce_by_segment");
-                EXPECT_TRUE(host_val_res[i] == 0, "wrong effect from reduce_by_segment");
+                EXPECT_TRUE(key_res[i] == 0, "wrong effect from reduce_by_segment");
+                EXPECT_TRUE(val_res[i] == 0, "wrong effect from reduce_by_segment");
             }
             else if (i < 4)
             {
-                EXPECT_TRUE(host_key_res[i] == i + 1, "wrong effect from reduce_by_segment");
-                EXPECT_TRUE(host_val_res[i] == i + 1, "wrong effect from reduce_by_segment");
+                EXPECT_TRUE(key_res[i] == i + 1, "wrong effect from reduce_by_segment");
+                EXPECT_TRUE(val_res[i] == i + 1, "wrong effect from reduce_by_segment");
             }
             else if (i % 2 == 0)
             {
-                EXPECT_TRUE(host_key_res[i] == 1, "wrong effect from reduce_by_segment");
-                EXPECT_TRUE(host_val_res[i] == 2 || host_val_res[i] == 1, "wrong effect from reduce_by_segment");
+                EXPECT_TRUE(key_res[i] == 1, "wrong effect from reduce_by_segment");
+                EXPECT_TRUE(val_res[i] == 2 || val_res[i] == 1, "wrong effect from reduce_by_segment");
             }
             else
             {
-                EXPECT_TRUE(host_key_res[i] == 3, "wrong effect from reduce_by_segment");
-                EXPECT_TRUE(host_val_res[i] == 6 || host_val_res[i] == 3, "wrong effect from reduce_by_segment");
+                EXPECT_TRUE(key_res[i] == 3, "wrong effect from reduce_by_segment");
+                EXPECT_TRUE(val_res[i] == 6 || val_res[i] == 3, "wrong effect from reduce_by_segment");
             }
         }
     }
 
+    // specialization for hetero policy
     template <typename Policy, typename Iterator1, typename Iterator2, typename Iterator3, typename Iterator4,
               typename Size>
-    void
+    typename ::std::enable_if<
+        oneapi::dpl::__internal::__is_hetero_execution_policy<typename ::std::decay<Policy>::type>::value &&
+            !is_same_iterator_category<Iterator3, ::std::bidirectional_iterator_tag>::value &&
+            !is_same_iterator_category<Iterator3, ::std::forward_iterator_tag>::value,
+        void>::type
     operator()(Policy&& exec, Iterator1 keys_first, Iterator1 keys_last, Iterator2 vals_first, Iterator3 key_res,
                Iterator4 val_res, Size n)
     {
@@ -111,46 +110,121 @@ struct test_reduce_by_segment
         typedef typename ::std::iterator_traits<Iterator2>::value_type ValT;
 
         // call algorithm with no optional arguments
-        initialize_data(keys_first, vals_first, key_res, val_res, n);
+        auto host_keys = get_host_pointer(keys_first);
+        auto host_vals = get_host_pointer(vals_first);
+        auto host_key_res = get_host_pointer(key_res);
+        auto host_val_res = get_host_pointer(val_res);
+
+        initialize_data(host_keys, host_vals, host_key_res, host_val_res, n);
 
         auto new_policy = make_new_policy<new_kernel_name<Policy, 0>>(exec);
         auto res1 = oneapi::dpl::reduce_by_segment(new_policy, keys_first, keys_last, vals_first, key_res, val_res);
-#    if _PSTL_SYCL_TEST_USM
+#if _PSTL_SYCL_TEST_USM
         exec.queue().wait_and_throw();
-#    endif
+#endif
+        Size result_size = std::distance(key_res, res1.first);
+
+        host_key_res = get_host_pointer(key_res);
+        host_val_res = get_host_pointer(val_res);
+        check_values(host_key_res, host_val_res, result_size);
+
+        // call algorithm with equality comparator
+        host_keys = get_host_pointer(keys_first);
+        host_vals = get_host_pointer(vals_first);
+        host_key_res = get_host_pointer(key_res);
+        host_val_res = get_host_pointer(val_res);
+
+        initialize_data(host_keys, host_vals, host_key_res, host_val_res, n);
+
+        auto new_policy2 = make_new_policy<new_kernel_name<Policy, 1>>(exec);
+        auto res2 = oneapi::dpl::reduce_by_segment(new_policy2, keys_first, keys_last, vals_first, key_res, val_res,
+                                                   ::std::equal_to<KeyT>());
+#if _PSTL_SYCL_TEST_USM
+        exec.queue().wait_and_throw();
+#endif
+        result_size = std::distance(key_res, res2.first);
+
+        host_key_res = get_host_pointer(key_res);
+        host_val_res = get_host_pointer(val_res);
+        check_values(host_key_res, host_val_res, result_size);
+
+        // call algorithm with addition operator
+        host_keys = get_host_pointer(keys_first);
+        host_vals = get_host_pointer(vals_first);
+        host_key_res = get_host_pointer(key_res);
+        host_val_res = get_host_pointer(val_res);
+
+        initialize_data(host_keys, host_vals, host_key_res, host_val_res, n);
+
+        auto new_policy3 = make_new_policy<new_kernel_name<Policy, 2>>(exec);
+        auto res3 = oneapi::dpl::reduce_by_segment(new_policy3, keys_first, keys_last, vals_first, key_res, val_res,
+                                                   ::std::equal_to<KeyT>(), ::std::plus<ValT>());
+#if _PSTL_SYCL_TEST_USM
+        exec.queue().wait_and_throw();
+#endif
+        result_size = std::distance(key_res, res3.first);
+
+        host_key_res = get_host_pointer(key_res);
+        host_val_res = get_host_pointer(val_res);
+        check_values(host_key_res, host_val_res, result_size);
+    }
+
+    // specialization for host execution policies
+    template <typename Policy, typename Iterator1, typename Iterator2, typename Iterator3, typename Iterator4,
+              typename Size>
+    typename ::std::enable_if<
+        !oneapi::dpl::__internal::__is_hetero_execution_policy<typename ::std::decay<Policy>::type>::value &&
+            !is_same_iterator_category<Iterator3, ::std::bidirectional_iterator_tag>::value &&
+            !is_same_iterator_category<Iterator3, ::std::forward_iterator_tag>::value,
+        void>::type
+    operator()(Policy&& exec, Iterator1 keys_first, Iterator1 keys_last, Iterator2 vals_first, Iterator3 key_res,
+               Iterator4 val_res, Size n)
+    {
+        typedef typename ::std::iterator_traits<Iterator1>::value_type KeyT;
+        typedef typename ::std::iterator_traits<Iterator2>::value_type ValT;
+
+        initialize_data(keys_first, vals_first, key_res, val_res, n);
+
+        auto res1 = oneapi::dpl::reduce_by_segment(exec, keys_first, keys_last, vals_first, key_res, val_res);
         Size result_size = std::distance(key_res, res1.first);
         check_values(key_res, val_res, result_size);
 
         // call algorithm with equality comparator
         initialize_data(keys_first, vals_first, key_res, val_res, n);
-
-        auto new_policy2 = make_new_policy<new_kernel_name<Policy, 1>>(exec);
-        auto res2 = oneapi::dpl::reduce_by_segment(new_policy2, keys_first, keys_last, vals_first, key_res, val_res,
+        auto res2 = oneapi::dpl::reduce_by_segment(exec, keys_first, keys_last, vals_first, key_res, val_res,
                                                    ::std::equal_to<KeyT>());
-#    if _PSTL_SYCL_TEST_USM
-        exec.queue().wait_and_throw();
-#    endif
         result_size = std::distance(key_res, res2.first);
         check_values(key_res, val_res, result_size);
 
         // call algorithm with addition operator
         initialize_data(keys_first, vals_first, key_res, val_res, n);
-
-        auto new_policy3 = make_new_policy<new_kernel_name<Policy, 2>>(exec);
-        auto res3 = oneapi::dpl::reduce_by_segment(new_policy3, keys_first, keys_last, vals_first, key_res, val_res,
+        auto res3 = oneapi::dpl::reduce_by_segment(exec, keys_first, keys_last, vals_first, key_res, val_res,
                                                    ::std::equal_to<KeyT>(), ::std::plus<ValT>());
-#    if _PSTL_SYCL_TEST_USM
-        exec.queue().wait_and_throw();
-#    endif
         result_size = std::distance(key_res, res3.first);
         check_values(key_res, val_res, result_size);
     }
+
+    // specialization for non-random_access iterators
+    template <typename Policy, typename Iterator1, typename Iterator2, typename Iterator3, typename Iterator4,
+              typename Size>
+    typename ::std::enable_if<is_same_iterator_category<Iterator3, ::std::bidirectional_iterator_tag>::value ||
+                                  is_same_iterator_category<Iterator3, ::std::forward_iterator_tag>::value,
+                              void>::type
+    operator()(Policy&& exec, Iterator1 keys_first, Iterator1 keys_last, Iterator2 vals_first, Iterator3 key_res,
+               Iterator4 val_res, Size n)
+    {
+    }
 };
+
+#if !TEST_DPCPP_BACKEND_PRESENT
+const int max_n = 100000;
+#endif
 
 template <typename T, typename TestName>
 void
 test4buffers()
 {
+#if TEST_DPCPP_BACKEND_PRESENT
     const sycl::queue& queue = my_queue; // usm requires queue
 #    if _PSTL_SYCL_TEST_USM
     {
@@ -193,8 +267,19 @@ test4buffers()
     {
         invoke_on_all_hetero_policies<1>()(test_reduce_by_segment(), keys, keys + n, vals, key_res, val_res, n);
     }
-}
 #endif
+
+    for (size_t n = 1; n <= max_n; n = n <= 16 ? n + 1 : size_t(3.1415 * n))
+    {
+        Sequence<uint64_t> keys_vec(n);
+        Sequence<uint64_t> vals_vec(n);
+        Sequence<uint64_t> key_res_vec(n);
+        Sequence<uint64_t> val_res_vec(n);
+
+        invoke_on_all_policies()(test_reduce_by_segment(), keys_vec.begin(), keys_vec.begin() + n, vals_vec.begin(),
+                                 key_res_vec.begin(), val_res_vec.begin(), n);
+    }
+}
 
 int
 main()
