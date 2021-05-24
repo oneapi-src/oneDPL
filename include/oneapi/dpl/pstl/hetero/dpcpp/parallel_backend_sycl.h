528e7fd3a5b160e982560d0ef6c14116c8852df1
MikeDvorskiy <mikhail.dvorskiy@intel.com>
2021-05-24 14:13:41 +0300

    [dpc++][sycl] +  passing sycl::bundle_state::executable for sycl::get_kernel_bundle (#254)
    * [dpc++] + usage of kernel_bundle instead of the deprecated sycl::program
    
    * [dpc++][sycl] + 'kernel bundle' feature support
    
    * [dpc++][sycl] +  passing sycl::bundle_state::object for sycl::get_kernel_bundle
    
    * + usage of executable state for kernel bundle
    
    * + revert usage of "sycl::get_kernel_bundle"
    
    * + clang format

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
@@ -191,8 +191,8 @@ class __kernel_name_base
     static sycl::kernel
     __compile_kernel(_Exec&& __exec)
     {
-#if _ONEDPL_KERNEL_BUNDLE_PRESENT
-        auto __kernel_bundle = sycl::get_kernel_bundle(__exec.queue().get_context());
+#if 0 //_ONEDPL_KERNEL_BUNDLE_PRESENT
+        auto __kernel_bundle = sycl::get_kernel_bundle<sycl::bundle_state::executable>(__exec.queue().get_context());
         return __kernel_bundle.get_kernel(sycl::get_kernel_id<_DerivedKernelName>());
 #else
         sycl::program __program(__exec.queue().get_context());
