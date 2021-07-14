4414b42826382ebb5bcd3a17afbbb9a658d46f31
Christopher Nelson <nadiasvertex@gmail.com>
2021-07-14 12:12:08 -0400

    [pstl][omp] Correctly implement parallel merge

--- include/oneapi/dpl/pstl/omp/parallel_merge.h
+++ include/oneapi/dpl/pstl/omp/parallel_merge.h
@@ -5,59 +5,43 @@ namespace dpl
 namespace __omp_backend
 {
 
-template <typename _RandomAccessIterator1, typename _RandomAccessIterator2>
-void
-__parallel_merge_assign(
-    _RandomAccessIterator1 __src, _RandomAccessIterator2 __dst,
-    typename std::enable_if_t<
-        std::is_copy_assignable<typename std::iterator_traits<_RandomAccessIterator2>::value_type>::value, bool> = true)
-{
-    *__dst = *__src;
-}
-
-template <typename _RandomAccessIterator1, typename _RandomAccessIterator2>
-void
-__parallel_merge_assign(
-    _RandomAccessIterator1 __src, _RandomAccessIterator2 __dst,
-    typename std::enable_if_t<
-        !std::is_copy_assignable<typename std::iterator_traits<_RandomAccessIterator2>::value_type>::value, bool> =
-        true)
-{
-    *__dst = std::move(*__src);
-}
-
 template <typename _RandomAccessIterator1, typename _RandomAccessIterator2, typename _RandomAccessIterator3,
-          typename _Compare>
+          typename _Compare, typename _LeafMerge>
 void
 __parallel_merge_body(std::size_t __size_x, std::size_t __size_y, _RandomAccessIterator1 __xs,
                       _RandomAccessIterator1 __xe, _RandomAccessIterator2 __ys, _RandomAccessIterator2 __ye,
-                      _RandomAccessIterator3 __zs, _Compare __comp)
+                      _RandomAccessIterator3 __zs, _Compare __comp, _LeafMerge __leaf_merge)
 {
-    // Split the (__ys, __ye] range into chunks.
-    std::size_t __n_chunks{0}, __chunk_size{0}, __first_chunk_size{0};
-    __chunk_partitioner(__ys, __ye, __n_chunks, __chunk_size, __first_chunk_size, __default_chunk_size);
 
-    /*
-     * Standard parallel merge: for each chunk locate the target offset of each of it's
-     * values. Write that value into zs (this assumes that "zs" is a separate container.)
-     */
-    _PSTL_PRAGMA(omp taskloop)
-    for (std::size_t __chunk = 0; __chunk < __n_chunks; ++__chunk)
-    {
-        auto __this_chunk_size = __chunk == 0 ? __first_chunk_size : __chunk_size;
-        auto __index = __chunk == 0 ? 0 : (__chunk * __chunk_size) + (__first_chunk_size - __chunk_size);
-        auto __begin = std::next(__ys, __index);
-        auto __end = std::next(__begin, __this_chunk_size);
-
-        for (auto __value = __begin; __value < __end; ++__value)
-        {
-            auto __ys_index = std::distance(__ys, __value);
-            auto __xs_index = std::distance(__xs, std::lower_bound(__xs, __xe, *__value, __comp));
-
-            __parallel_merge_assign<_RandomAccessIterator2, _RandomAccessIterator3>(__value,
-                                                                                    __zs + __xs_index + __ys_index);
-        }
+    if (__size_x + __size_y <= __default_chunk_size) {
+        __leaf_merge(__xs, __xe, __ys, __ye, __zs, __comp);
+        return;
     }
+
+    _RandomAccessIterator1 __xm;
+    _RandomAccessIterator2 __ym;
+
+    if (__size_x < __size_y) {
+        __ym = __ys + (__size_y / 2);
+        __xm = ::std::upper_bound(__xs, __xe, *__ym, __comp);
+    } else {
+        __xm = __xs + (__size_x / 2);
+        __ym = ::std::lower_bound(__ys, __ye, *__xm, __comp);
+    }
+
+    auto __zm = __zs + std::distance(__xs, __xm) + std::distance(__ys, __ym);
+
+    _PSTL_PRAGMA(omp task untied mergeable)
+    __parallel_merge_body(std::distance(__xs, __xm),
+                          std::distance(__ys, __ym),
+                          __xs, __xm, __ys, __ym, __zs,
+                          __comp, __leaf_merge);
+
+    _PSTL_PRAGMA(omp task untied mergeable)
+    __parallel_merge_body(std::distance(__xm, __xe),
+                          std::distance(__ym, __ye),
+                          __xm, __xe, __ym, __ye, __zm,
+                          __comp, __leaf_merge);
 }
 
 template <class _ExecutionPolicy, typename _RandomAccessIterator1, typename _RandomAccessIterator2,
@@ -85,29 +69,18 @@ __parallel_merge(_ExecutionPolicy&& /*__exec*/, _RandomAccessIterator1 __xs, _Ra
 
     if (omp_in_parallel())
     {
-        if (__size_x > __size_y)
-        {
-            __parallel_merge_body(__size_y, __size_x, __ys, __ye, __xs, __xe, __zs, __comp);
-        }
-        else
-        {
-            __parallel_merge_body(__size_x, __size_y, __xs, __xe, __ys, __ye, __zs, __comp);
-        }
+        __parallel_merge_body(__size_x, __size_y, __xs, __xe, __ys, __ye, __zs, __comp, __leaf_merge);
+        _PSTL_PRAGMA(omp barrier)
     }
     else
     {
         _PSTL_PRAGMA(omp parallel)
-        _PSTL_PRAGMA(omp single)
         {
-            if (__size_x > __size_y)
-            {
-                __parallel_merge_body(__size_y, __size_x, __ys, __ye, __xs, __xe, __zs, __comp);
-            }
-            else
-            {
-                __parallel_merge_body(__size_x, __size_y, __xs, __xe, __ys, __ye, __zs, __comp);
-            }
+            _PSTL_PRAGMA(omp single)
+            __parallel_merge_body(__size_x, __size_y, __xs, __xe, __ys, __ye, __zs, __comp, __leaf_merge);
+            _PSTL_PRAGMA(omp barrier)
         }
+
     }
 
     /* __serial_backend::__parallel_merge(std::forward<_ExecutionPolicy>(__exec), __xs, __xe, __ys, __ye, __zs, __comp,
