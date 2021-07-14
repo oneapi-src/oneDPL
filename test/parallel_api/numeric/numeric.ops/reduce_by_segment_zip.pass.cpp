3b09526e7430c82f826a59abf5cf68b85771ffe4
Mikhail Dvorskiy <mikhail.dvorskiy@intel.com>
2021-07-14 16:17:06 +0300

    [oneDPL][usm] + usage of shared memory (instead of device memory) in the test

--- test/parallel_api/numeric/numeric.ops/reduce_by_segment_zip.pass.cpp
+++ test/parallel_api/numeric/numeric.ops/reduce_by_segment_zip.pass.cpp
@@ -29,7 +29,6 @@
 
 #include <functional>
 #include <iostream>
-#include <vector>
 
 //The code below for test a call of reduce_by_segment with zip iterators was kept "as is", as an example reported by a user; just "memory deallocation" added.
 int main()
@@ -39,35 +38,39 @@ int main()
 
     const int n = 9, n_res = 6;
 
-    std::vector<int> keys1{11, 11, 21, 20, 21, 21, 21, 37, 37};
-    std::vector<int> keys2{11, 11, 20, 20, 20, 21, 21, 37, 37};
-    std::vector<int> values{0,  1,  2,  3,  4,  5,  6,  7,  8};
-    std::vector<int> output_keys1(keys1.size());
-    std::vector<int> output_keys2(keys2.size());    
-    std::vector<int> output_values(values.size());
+    //shared memory allocation
+    int* d_keys1         = sycl::malloc_shared<int>(n, q);
+    int* d_keys2         = sycl::malloc_shared<int>(n, q);
+    int* d_values        = sycl::malloc_shared<int>(n, q);
+    int* d_output_keys1  = sycl::malloc_shared<int>(n, q);
+    int* d_output_keys2  = sycl::malloc_shared<int>(n, q);
+    int* d_output_values = sycl::malloc_shared<int>(n, q);
 
-    int* d_keys1         = sycl::malloc_device<int>(n, q);
-    int* d_keys2         = sycl::malloc_device<int>(n, q);
-    int* d_values        = sycl::malloc_device<int>(n, q);
-    int* d_output_keys1  = sycl::malloc_device<int>(n, q);
-    int* d_output_keys2  = sycl::malloc_device<int>(n, q);
-    int* d_output_values = sycl::malloc_device<int>(n, q);
+    //data initialization
+    const int keys1[n] = {11, 11, 21, 20, 21, 21, 21, 37, 37};
+    const int keys2[n] = {11, 11, 20, 20, 20, 21, 21, 37, 37};
+    const int values[n] = {0,  1,  2,  3,  4,  5,  6,  7,  8};
+    std::copy(keys1, keys1 + n, d_keys1);
+    std::copy(keys2, keys2 + n, d_keys2);
+    std::copy(values, values + n, d_values);
 
-    q.memcpy(d_keys1, keys1.data(), sizeof(int)*n);
-    q.memcpy(d_keys2, keys2.data(), sizeof(int)*n);
-    q.memcpy(d_values, values.data(), sizeof(int)*n);
-
+    //make zip iterators
     auto begin_keys_in = oneapi::dpl::make_zip_iterator(d_keys1, d_keys2);
     auto end_keys_in   = oneapi::dpl::make_zip_iterator(d_keys1 + n, d_keys2 + n);
     auto begin_keys_out= oneapi::dpl::make_zip_iterator(d_output_keys1, d_output_keys2);
 
+    //run reduce_by_segment algorithm 
     auto new_last = oneapi::dpl::reduce_by_segment(oneapi::dpl::execution::make_device_policy(q),
         begin_keys_in, end_keys_in, d_values, begin_keys_out, d_output_values);
 
-    q.memcpy(output_keys1.data(), d_output_keys1, sizeof(int)*n);
-    q.memcpy(output_keys2.data(), d_output_keys2, sizeof(int)*n);    
-    q.memcpy(output_values.data(), d_output_values, sizeof(int)*n);
     q.wait();
+
+//Dump
+#if 0
+    for(int i=0; i < n_res; i++) {
+      std::cout << "{" << d_output_keys1[i] << ", " << d_output_keys2[i] << "}: " << d_output_values[i] << std::endl;
+    }
+#endif
 
     // Expected output
     // {11, 11}: 1
@@ -76,19 +79,12 @@ int main()
     // {21, 20}: 4
     // {21, 21}: 11
     // {37, 37}: 15
-    int exp_keys1[n_res] = {11, 21, 20, 21, 21,37};
-    int exp_keys2[n_res] = {11, 20, 20, 20, 21, 37};
-    int exp_values[n_res] = {1, 2, 3, 4, 11, 15};
-    EXPECT_EQ_N(exp_keys1, output_keys1.begin(), n_res, "wrong keys1 from reduce_by_segment");
-    EXPECT_EQ_N(exp_keys2, output_keys2.begin(), n_res, "wrong keys2 from reduce_by_segment");
-    EXPECT_EQ_N(exp_values, output_values.begin(), n_res, "wrong values from reduce_by_segment");
-
-//Dump
-#if 0
-    for(int i=0; i < n_res; i++) {
-      std::cout << "{" << output_keys1[i] << ", " << output_keys2[i] << "}: " << output_values[i] << std::endl;
-    }
-#endif
+    const int exp_keys1[n_res] = {11, 21, 20, 21, 21,37};
+    const int exp_keys2[n_res] = {11, 20, 20, 20, 21, 37};
+    const int exp_values[n_res] = {1, 2, 3, 4, 11, 15};
+    EXPECT_EQ_N(exp_keys1, d_output_keys1, n_res, "wrong keys1 from reduce_by_segment");
+    EXPECT_EQ_N(exp_keys2, d_output_keys2, n_res, "wrong keys2 from reduce_by_segment");
+    EXPECT_EQ_N(exp_values, d_output_values, n_res, "wrong values from reduce_by_segment");
 
     // Deallocate memory
     sycl::free(d_keys1, q);
