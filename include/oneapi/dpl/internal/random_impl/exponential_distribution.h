d64bacc0c6956fc5e3551f69302f2c16e2194ac4
ksenia zaitseva <ksenia.zaitseva@intel.com>
2021-07-26 15:18:35 +0300

    Change header guards

--- include/oneapi/dpl/internal/random_impl/exponential_distribution.h
+++ include/oneapi/dpl/internal/random_impl/exponential_distribution.h
@@ -17,8 +17,8 @@
 //
 // Public header file provides implementation for Exponential Distribution
 
-#ifndef DPSTD_EXPONENTIAL_DISTRIBUTION
-#define DPSTD_EXPONENTIAL_DISTRIBUTION
+#ifndef _ONEDPL_EXPONENTIAL_DISTRIBUTION
+#define _ONEDPL_EXPONENTIAL_DISTRIBUTION
 
 namespace oneapi
 {
@@ -141,16 +141,17 @@ class exponential_distribution
     }
 
     // Specialization of the vector generation  with size = [1; 3]
-    template <int __N, class _Engine>
+    /* template <int __N, class _Engine>
     typename ::std::enable_if<(__N <= 3), result_type>::type
     generate_vec(_Engine& __engine, const param_type& __params)
     {
         return generate_n_elems<_Engine>(__engine, __params, __N);
-    }
+    }*/
 
     // Specialization of the vector generation with size = [4; 8; 16]
     template <int __N, class _Engine>
-    typename ::std::enable_if<(__N > 3), result_type>::type
+    // typename ::std::enable_if<(__N > 3), result_type>::type
+    result_type
     generate_vec(_Engine& __engine, const param_type& __params)
     {
         oneapi::dpl::uniform_real_distribution<result_type> __u;
@@ -192,4 +193,4 @@ class exponential_distribution
 } // namespace dpl
 } // namespace oneapi
 
-#endif // #ifndf DPSTD_EXPONENTIAL_DISTRIBUTION
+#endif // #ifndf _ONEDPL_EXPONENTIAL_DISTRIBUTION
