0511b3953185d5271ae601dd12b7706427b00b6e
Mikhail Dvorskiy <mikhail.dvorskiy@intel.com>
2021-07-30 17:18:58 +0300

    + fix a typo with double underscore

--- test/general/buffer_wrapper.pass.cpp
+++ test/general/buffer_wrapper.pass.cpp
@@ -53,7 +53,7 @@ main()
     test(oneapi::dpl::begin(buf), oneapi::dpl::end(buf), data_ptr, size);
     test(oneapi::dpl::begin(buf, sycl::write_only), oneapi::dpl::end(buf, sycl::write_only), data_ptr, size);
     test(oneapi::dpl::begin(buf, sycl::write_only, __sycl::__no_init{}), oneapi::dpl::end(buf, sycl::write_only, __sycl::__no_init{}), data_ptr, size);
-    test(oneapi::dpl::begin(buf, __sycl::no_init{}), oneapi::dpl::end(buf, __sycl::__no_init{}), data_ptr, size);
+    test(oneapi::dpl::begin(buf, __sycl::__no_init{}), oneapi::dpl::end(buf, __sycl::__no_init{}), data_ptr, size);
 
 #endif
     return done(TEST_DPCPP_BACKEND_PRESENT);
