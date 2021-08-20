bf12cd6254e520dd775f51739253760d19df0c8b
Ruslan Arutyunyan <ruslan.arutyunyan@intel.com>
2021-08-20 11:46:04 +0300

    Fix kernel redefinition in scan

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
@@ -415,9 +415,9 @@ struct __parallel_scan_submitter<_CustomName, __internal::__optional_kernel_name
     {
         using _Type = typename _InitType::__value_type;
         using _LocalScanKernel = oneapi::dpl::__par_backend_hetero::__internal::__kernel_name_generator<
-            __parallel_scan_local_kernel, _CustomName, _Range1, _Range2, _Type, _LocalScan>;
+            __parallel_scan_local_kernel, _CustomName, _Range1, _Range2, _Type, _LocalScan, _GroupScan, _GlobalScan>;
         using _GroupScanKernel = oneapi::dpl::__par_backend_hetero::__internal::__kernel_name_generator<
-            __parallel_scan_group_kernel, _CustomName, _Range1, _Range2, _Type, _GroupScan>;
+            __parallel_scan_group_kernel, _CustomName, _Range1, _Range2, _Type, _LocalScan, _GroupScan, _GlobalScan>;
 
         auto __n = __rng1.size();
         assert(__n > 0);
