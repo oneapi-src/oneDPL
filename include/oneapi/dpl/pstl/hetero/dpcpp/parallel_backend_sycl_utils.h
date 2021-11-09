97ac4c106b99a7e3faee8a75c539cb73c5f0afcd
Kseniia Bakina <kseniia.bakina@intel.com>
2021-11-09 14:57:35 +0300

    start Jenkins testing

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_utils.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_utils.h
@@ -216,6 +216,7 @@ template <typename _ExecutionPolicy, ::std::size_t _size_type = 0, typename... _
 class __work_group_size_producer
 {
   private:
+
     using _KernelTypeTuple = ::std::tuple<_KernelType...>;
     ::std::map<int, ::std::shared_ptr<sycl::kernel>> __kernels_vector;
 
