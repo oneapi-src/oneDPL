9ebd111f8c3cfdab245ea58eb35a1ecc34f3a8fe
Sergey Kopienko <sergey.kopienko@intel.com>
2021-12-07 09:30:09 +0300

    Fix errors in inclusive_scan_by_segment.pass.cpp

--- test/parallel_api/numeric/numeric.ops/inclusive_scan_by_segment.pass.cpp
+++ test/parallel_api/numeric/numeric.ops/inclusive_scan_by_segment.pass.cpp
@@ -101,6 +101,10 @@ struct test_inclusive_scan_by_segment
             auto host_val_res = get_host_access(val_res_first);
 
             initialize_data(host_keys, host_vals, host_val_res, n);
+
+            refresh_usm_from_host_pointer(host_keys, keys_first, n);
+            refresh_usm_from_host_pointer(host_vals, vals_first, n);
+            refresh_usm_from_host_pointer(host_val_res, val_res_first, n);
         }
 
         auto new_policy = make_new_policy<new_kernel_name<Policy, 0>>(exec);
@@ -116,6 +120,10 @@ struct test_inclusive_scan_by_segment
             auto host_vals = get_host_access(vals_first);
 
             initialize_data(host_keys, host_vals, host_val_res, n);
+
+            refresh_usm_from_host_pointer(host_keys, keys_first, n);
+            refresh_usm_from_host_pointer(host_vals, vals_first, n);
+            refresh_usm_from_host_pointer(host_val_res, val_res_first, n);
         }
 
         auto new_policy2 = make_new_policy<new_kernel_name<Policy, 1>>(exec);
@@ -131,6 +139,10 @@ struct test_inclusive_scan_by_segment
             auto host_vals = get_host_access(vals_first);
 
             initialize_data(host_keys, host_vals, host_val_res, n);
+
+            refresh_usm_from_host_pointer(host_keys, keys_first, n);
+            refresh_usm_from_host_pointer(host_vals, vals_first, n);
+            refresh_usm_from_host_pointer(host_val_res, val_res_first, n);
         }
 
         auto new_policy3 = make_new_policy<new_kernel_name<Policy, 2>>(exec);
