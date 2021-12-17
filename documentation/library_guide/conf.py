fbdf9cd5503ea7ae5a8026a8e8173609e9d992cd
Valentina Kats <valentina.kats@intel.com>
2021-12-17 16:19:44 +0300

    Update documentation version for 2021.6 release (#453)

--- documentation/library_guide/conf.py
+++ documentation/library_guide/conf.py
@@ -37,7 +37,7 @@ copyright = 'Intel Corporation'
 author = 'Intel'
 
 # The full version, including alpha/beta/rc tags
-release = '2021.5'
+release = '2021.6'
 
 rst_epilog = """
 .. include:: /variables.txt
