18e58eef273493eb99f55e883dbe91e3ecf17cc8
Sergey Kopienko <sergey.kopienko@intel.com>
2022-01-13 12:02:51 +0300

    Change check in test_inclusive_scan_by_segment::check_values for any source data

--- test/parallel_api/numeric/numeric.ops/inclusive_scan_by_segment.pass.cpp
+++ test/parallel_api/numeric/numeric.ops/inclusive_scan_by_segment.pass.cpp
@@ -27,6 +27,8 @@ using namespace oneapi::dpl::execution;
 #endif
 using namespace TestUtils;
 
+//#define DUMP_CHECK_RESULTS
+
 struct test_inclusive_scan_by_segment
 {
     // TODO: replace data generation with random data and update check to compare result to
@@ -53,31 +55,76 @@ struct test_inclusive_scan_by_segment
         }
     }
 
-    template <typename Iterator1, typename Iterator2, typename Size>
-    void
-    check_values(Iterator1 host_keys, Iterator2 val_res, Size n)
+#ifdef DUMP_CHECK_RESULTS
+    template <typename Iterator, typename Size>
+    void display_param(const char* msg, Iterator it, Size n)
     {
-        //T keys[n1] = { 1, 2, 3, 4, 1, 1, 2, 2, 3, 3, 4, 4, 1, 1, 1, ...};
-        //T vals[n1] = { 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 3, ...};
-
-        int segment_length = 1;
-        auto expected_segment_sum = 1;
-        auto current_key = host_keys[0];
-        auto current_sum = 0;
-        for (int i = 0; i != n; ++i)
+        std::cout << msg;
+        for (int i = 0; i < n; ++i)
         {
-            if (current_key == host_keys[i])
-            {
-              current_sum += val_res[i];
-            } else {
-                EXPECT_TRUE(current_sum == expected_segment_sum, "wrong effect from exclusive_scan_by_segment");
-                current_sum = val_res[i];
-                current_key = host_keys[i];
-                if (current_key == 1) {
-                    ++segment_length;
-                    expected_segment_sum = segment_length * (segment_length + 1) / 2;
-                }
-            }
+            if (i > 0)
+                std::cout << ", ";
+            std::cout << it[i];
+        }
+        std::cout << std::endl;
+    }
+#endif // DUMP_CHECK_RESULTS
+
+    template <typename Iterator1, typename Iterator2, typename Iterator3, typename Size>
+    void
+    check_values(Iterator1 host_keys, Iterator2 host_vals, Iterator3 val_res, Size n)
+    {
+        // https://docs.oneapi.io/versions/latest/onedpl/extension_api.html
+        // keys:   [ 0, 0, 0, 1, 1, 1 ]
+        // values: [ 1, 2, 3, 4, 5, 6 ]
+        // result: [ 1, 1 + 2 = 3, 1 + 2 + 3 = 6, 4, 4 + 5 = 9, 4 + 5 + 6 = 15 ]
+
+#ifdef DUMP_CHECK_RESULTS
+        std::cout << "check_values(n = " << n << ") : " << std::endl;
+        display_param("keys:   ", host_keys, n);
+        display_param("values: ", host_vals, n);
+        display_param("result: ", val_res,   n);
+#endif // DUMP_CHECK_RESULTS
+
+        if (n < 1)
+            return;
+
+        if (n == 1)
+        {
+            EXPECT_TRUE(host_vals[0] == val_res[0], "wrong effect from exclusive_scan_by_segment");
+            return;
+        }
+
+        using ValT = typename ::std::decay<decltype(host_vals[0])>::type;
+        const ValT init = {};
+
+        // Last summ info
+        int last_segment_begin = 0;         // Start index of last summ
+        int last_segment_end = 0;           // End index of last summ
+        ValT last_segment_summ = init;      // Last summ
+
+        int segment_start_idx = 0;
+        int val_res_idx = 0;
+        for (int current_key_idx = 1; current_key_idx <= n; ++current_key_idx)
+        {
+            // Eval current summ
+            auto expected_segment_sum = init;
+            if (last_segment_begin == segment_start_idx && last_segment_end + 1 == current_key_idx)
+                expected_segment_sum = last_segment_summ + host_vals[current_key_idx - 1];
+            else
+                expected_segment_sum = ::std::accumulate(get_host_iterator(host_vals) + segment_start_idx,
+                                                         get_host_iterator(host_vals) + current_key_idx, init);
+
+            // Update last summ  info
+            last_segment_begin = segment_start_idx;
+            last_segment_end = current_key_idx;
+            last_segment_summ = expected_segment_sum;
+
+            EXPECT_TRUE(val_res[val_res_idx] == expected_segment_sum, "wrong effect from exclusive_scan_by_segment");
+            ++val_res_idx;
+
+            if (current_key_idx < n && host_keys[segment_start_idx] != host_keys[current_key_idx])
+                segment_start_idx = current_key_idx;
         }
     }
 
@@ -109,11 +156,11 @@ struct test_inclusive_scan_by_segment
 
         {
             auto host_keys = get_host_access(keys_first);
+            auto host_vals = get_host_access(vals_first);
             auto host_val_res = get_host_access(val_res_first);
-            check_values(host_keys, host_val_res, n);
+            check_values(host_keys, host_vals, host_val_res, n);
 
             // call algorithm with equality comparator
-            auto host_vals = get_host_access(vals_first);
 
             initialize_data(host_keys, host_vals, host_val_res, n);
         }
@@ -124,11 +171,11 @@ struct test_inclusive_scan_by_segment
         exec.queue().wait_and_throw();
         {
             auto host_keys = get_host_access(keys_first);
+            auto host_vals = get_host_access(vals_first);
             auto host_val_res = get_host_access(val_res_first);
-            check_values(host_keys, host_val_res, n);
+            check_values(host_keys, host_vals, host_val_res, n);
 
             // call algorithm with addition operator
-            auto host_vals = get_host_access(vals_first);
 
             initialize_data(host_keys, host_vals, host_val_res, n);
         }
@@ -139,8 +186,9 @@ struct test_inclusive_scan_by_segment
                                                            [](ValT first, ValT second) { return first + second; });
         exec.queue().wait_and_throw();
         auto host_keys = get_host_access(keys_first);
+        auto host_vals = get_host_access(vals_first);
         auto host_val_res = get_host_access(val_res_first);
-        check_values(host_keys, host_val_res, n);
+        check_values(host_keys, host_vals, host_val_res, n);
     }
 #endif
 
@@ -161,21 +209,20 @@ struct test_inclusive_scan_by_segment
         // call algorithm with no optional arguments
         initialize_data(keys_first, vals_first, val_res_first, n);
         auto res1 = oneapi::dpl::inclusive_scan_by_segment(exec, keys_first, keys_last, vals_first, val_res_first);
-        check_values(keys_first, val_res_first, n);
+        check_values(keys_first, vals_first, val_res_first, n);
 
         // call algorithm with equality comparator
         initialize_data(keys_first, vals_first, val_res_first, n);
         auto res2 = oneapi::dpl::inclusive_scan_by_segment(exec, keys_first, keys_last, vals_first, val_res_first,
                                                            [](KeyT first, KeyT second) { return first == second; });
-        check_values(keys_first, val_res_first, n);
+        check_values(keys_first, vals_first, val_res_first, n);
 
         // call algorithm with addition operator
         initialize_data(keys_first, vals_first, val_res_first, n);
         auto res3 = oneapi::dpl::inclusive_scan_by_segment(exec, keys_first, keys_last, vals_first, val_res_first,
                                                            [](KeyT first, KeyT second) { return first == second; },
                                                            [](ValT first, ValT second) { return first + second; });
-        check_values(keys_first, val_res_first, n);
-
+        check_values(keys_first, vals_first, val_res_first, n);
     }
 
     // specialization for non-random_access iterators
