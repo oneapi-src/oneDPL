1c027562f4f0530adc6602adee3a2f2b3c87863a
Sobolev, Dmitriy <dmitriy.sobolev@intel.com>
2022-01-19 16:54:29 +0300

    clang-format
    Signed-off-by: Sobolev, Dmitriy <dmitriy.sobolev@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/unseq_backend_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/unseq_backend_sycl.h
@@ -52,7 +52,7 @@ using __has_known_identity =
     typename ::std::conjunction<
         ::std::is_arithmetic<_Tp>,
         ::std::disjunction<::std::is_same<typename ::std::decay<BinaryOp>::type, ::std::plus<_Tp>>,
-                           ::std::is_same<typename ::std::decay<BinaryOp>::type,  __dpl_sycl::__plus<_Tp>>>>;
+                           ::std::is_same<typename ::std::decay<BinaryOp>::type, __dpl_sycl::__plus<_Tp>>>>;
 #    endif //__LIBSYCL_VERSION >= 50200
 
 #else //_USE_GROUP_ALGOS
