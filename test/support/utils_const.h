80f30c39e95989b09e7006182f97fc0540dbd205
Sergey Kopienko <sergey.kopienko@intel.com>
2022-02-07 10:20:33 +0300

    Fix review comment: change type of max_n : int -> ::std::size_t

--- test/support/utils_const.h
+++ test/support/utils_const.h
@@ -21,7 +21,7 @@ namespace TestUtils
 #define _SKIP_RETURN_CODE 77
 
 // Test data ranges other than those that start at the beginning of an input.
-constexpr int max_n = 100000;
+constexpr ::std::size_t max_n = 100000;
 
 
 // All these offset consts used for indirect testing of calculation an offset parameter
