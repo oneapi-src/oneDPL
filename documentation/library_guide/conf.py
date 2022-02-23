16a12ff727d743340ec389c9173b86613382a57a
Dylan <dylan.benito@intel.com>
2022-02-23 09:32:08 -0800

    Adding analytics data to conf.py and layout.html files. (#518)
    Signed-off-by: Dylan Benito <dylan.benito@intel.com>

--- documentation/library_guide/conf.py
+++ documentation/library_guide/conf.py
@@ -116,5 +116,6 @@ html_theme_options = {
     'path_to_docs': 'documentation/library_guide',
     'use_issues_button': True,
     'use_edit_page_button': True,
-    'repository_branch': 'main'
+    'repository_branch': 'main',
+    'extra_footer': '<p align="right"><a href="https://www.intel.com/content/www/us/en/privacy/intel-cookie-notice.html">Cookies</a></p>'
 }
