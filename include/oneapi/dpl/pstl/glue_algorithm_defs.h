0a8d7093611a0324d39cdadfe6c9da99b4fd8f30
Sergey Kopienko <sergey.kopienko@intel.com>
2022-03-22 11:19:58 +0300

    Comment declarations of is_sorted, is_sorted_until in include/oneapi/dpl/pstl/glue_algorithm_defs.h

--- include/oneapi/dpl/pstl/glue_algorithm_defs.h
+++ include/oneapi/dpl/pstl/glue_algorithm_defs.h
@@ -391,21 +391,21 @@ partial_sort(_ExecutionPolicy&& __exec, _RandomAccessIterator __first, _RandomAc
 //                  _RandomAccessIterator __d_first, _RandomAccessIterator __d_last);
 
 // [is.sorted]
-template <class _ExecutionPolicy, class _ForwardIterator, class _Compare>
-oneapi::dpl::__internal::__enable_if_execution_policy<_ExecutionPolicy, _ForwardIterator>
-is_sorted_until(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIterator __last, _Compare __comp);
-
-template <class _ExecutionPolicy, class _ForwardIterator>
-oneapi::dpl::__internal::__enable_if_execution_policy<_ExecutionPolicy, _ForwardIterator>
-is_sorted_until(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIterator __last);
-
-template <class _ExecutionPolicy, class _ForwardIterator, class _Compare>
-oneapi::dpl::__internal::__enable_if_execution_policy<_ExecutionPolicy, bool>
-is_sorted(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIterator __last, _Compare __comp);
-
-template <class _ExecutionPolicy, class _ForwardIterator>
-oneapi::dpl::__internal::__enable_if_execution_policy<_ExecutionPolicy, bool>
-is_sorted(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIterator __last);
+//template <class _ExecutionPolicy, class _ForwardIterator, class _Compare>
+//oneapi::dpl::__internal::__enable_if_execution_policy<_ExecutionPolicy, _ForwardIterator>
+//is_sorted_until(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIterator __last, _Compare __comp);
+//
+//template <class _ExecutionPolicy, class _ForwardIterator>
+//oneapi::dpl::__internal::__enable_if_execution_policy<_ExecutionPolicy, _ForwardIterator>
+//is_sorted_until(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIterator __last);
+//
+//template <class _ExecutionPolicy, class _ForwardIterator, class _Compare>
+//oneapi::dpl::__internal::__enable_if_execution_policy<_ExecutionPolicy, bool>
+//is_sorted(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIterator __last, _Compare __comp);
+//
+//template <class _ExecutionPolicy, class _ForwardIterator>
+//oneapi::dpl::__internal::__enable_if_execution_policy<_ExecutionPolicy, bool>
+//is_sorted(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIterator __last);
 
 // [alg.nth.element]
 
