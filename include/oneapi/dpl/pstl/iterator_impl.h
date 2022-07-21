d59bb8de0396c8fd9d448e61e490c070617f87c7
Sergey Kopienko <sergey.kopienko@intel.com>
2022-07-21 10:27:28 +0200

    Fix error in permutation iterator (#600)
    [oneDPL] + Fix error in permutation iterator
    [oneDPL][test] + Prepare new test exclusive_scan_by_segment_perm_it.pass.cpp
    
    (cherry picked from commit 70fdaeb45e1bbde944d1feb1b5bae41a28336425)

--- include/oneapi/dpl/pstl/iterator_impl.h
+++ include/oneapi/dpl/pstl/iterator_impl.h
@@ -574,7 +574,7 @@ class permutation_iterator
     IndexMap
     map() const
     {
-        return my_index_map;
+        return my_index_map + my_index;
     }
 
     reference operator*() const { return my_source_it[my_index_map[my_index]]; }
@@ -618,7 +618,7 @@ class permutation_iterator
     }
 
     permutation_iterator
-    operator-(difference_type backward)
+    operator-(difference_type backward) const
     {
         return permutation_iterator(my_source_it, my_index_map, my_index - backward);
     }
