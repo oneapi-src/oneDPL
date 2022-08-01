98869b4d0cef05e096ebe863f5e6a452e2c8e67d
Sergey Kopienko <sergey.kopienko@intel.com>
2022-08-01 09:40:50 +0200

    Add using and test for std::exp - continue after first fail

--- test/xpu_api/complex/complex_fnc_exp.pass.cpp
+++ test/xpu_api/complex/complex_fnc_exp.pass.cpp
@@ -63,17 +63,23 @@ private:
 int
 main()
 {
-    TestUtils::Complex::test_on_host<TestComplexExp>();
+    bool bSuccess = true;
+
+    if (!TestUtils::Complex::test_on_host<TestComplexExp>())
+        bSuccess = false;
 
 #if TEST_DPCPP_BACKEND_PRESENT
     try
     {
         sycl::queue deviceQueue{ TestUtils::default_selector };
 
-        TestUtils::Complex::test_in_kernel<TestComplexExp>(deviceQueue);
+        if (!TestUtils::Complex::test_in_kernel<TestComplexExp>(deviceQueue))
+            bSuccess = false;
     }
     catch (const std::exception& exc)
     {
+        bSuccess = false;
+
         std::string errorMsg = "Exception occurred";
         if (exc.what())
         {
@@ -84,6 +90,9 @@ main()
         EXPECT_TRUE(false, errorMsg.c_str());
     }
 #endif // TEST_DPCPP_BACKEND_PRESENT
+
+    if (!bSuccess)
+        TestUtils::exit_on_error();
 
     return TestUtils::done();
 }
