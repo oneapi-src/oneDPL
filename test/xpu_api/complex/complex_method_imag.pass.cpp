41fcd412cc6c5c6c7854b7f42967c37bd010bdb7
Sergey Kopienko <sergey.kopienko@intel.com>
2022-08-01 09:42:16 +0200

    Add test for std::complex::imag - continue after first fail

--- test/xpu_api/complex/complex_method_imag.pass.cpp
+++ test/xpu_api/complex/complex_method_imag.pass.cpp
@@ -155,17 +155,23 @@ private:
 int
 main()
 {
-    TestUtils::Complex::test_on_host<TextComplexMethodImag>();
+    bool bSuccess = true;
+
+    if (!TestUtils::Complex::test_on_host<TextComplexMethodImag>())
+        bSuccess = false;
 
 #if TEST_DPCPP_BACKEND_PRESENT
     try
     {
         sycl::queue deviceQueue{ TestUtils::default_selector };
 
-        TestUtils::Complex::test_in_kernel<TextComplexMethodImag>(deviceQueue);
+        if (!TestUtils::Complex::test_in_kernel<TextComplexMethodImag>(deviceQueue))
+            bSuccess = false;
     }
     catch (const std::exception& exc)
     {
+        bSuccess = false;
+
         std::string errorMsg = "Exception occurred";
         if (exc.what())
         {
@@ -176,6 +182,9 @@ main()
         EXPECT_TRUE(false, errorMsg.c_str());
     }
 #endif // TEST_DPCPP_BACKEND_PRESENT
+
+    if (!bSuccess)
+        TestUtils::exit_on_error();
 
     return TestUtils::done();
 }
