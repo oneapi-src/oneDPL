911b27209d6da3e634b14240d6a515fcd5c02b0b
Valentina Kats <valentina.kats@intel.com>
2022-08-02 14:53:44 +0200

    Update documentation version for oneDPL 2021.7.1

--- documentation/library_guide/conf.py
+++ documentation/library_guide/conf.py
@@ -37,7 +37,7 @@ copyright = 'Intel Corporation'
 author = 'Intel'
 
 # The full version, including alpha/beta/rc tags
-release = '2021.7'
+release = '2021.7.1'
 
 rst_epilog = """
 .. include:: /variables.txt
