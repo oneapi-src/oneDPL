206012870b5d32a89ff7643f111cda9eb6a6d552
Sergey Kopienko <sergey.kopienko@intel.com>
2022-08-16 12:05:35 +0200

    Add test for std::complex arithmetic #1

--- test/xpu_api/numerics/complex.number/complex.member.ops/minus_equal_complex.pass.cpp
+++ test/xpu_api/numerics/complex.number/complex.member.ops/minus_equal_complex.pass.cpp
@@ -10,10 +10,7 @@
 
 // complex& operator-=(const complex& rhs);
 
-#include <complex>
-#include <cassert>
-
-#include "test_macros.h"
+#include "support/test_complex.h"
 
 template <class T>
 void
@@ -45,16 +42,23 @@ test()
     assert(c3.imag() == -6);
 }
 
-void run_test()
+template <typename EnableDouble, typename EnableLongDouble>
+void
+run_test()
 {
     test<float>();
-    test<double>();
-    test<long double>();
+    oneapi::dpl::__internal::__invoke_if(EnableDouble{}, [&]() { test<double>(); });
+    oneapi::dpl::__internal::__invoke_if(EnableLongDouble{}, [&]() { test<long double>(); });
 }
 
 int main(int, char**)
 {
-    run_test();
+    // Run on host
+    run_test<::std::true_type, ::std::true_type>();
 
-  return 0;
+    // Run test in Kernel
+    TestUtils::run_test_in_kernel([&]() { run_test<::std::true_type, ::std::false_type>(); },
+                                  [&]() { run_test<::std::false_type, ::std::false_type>(); });
+
+    return TestUtils::done();
 }
