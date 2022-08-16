a8ebe580ea916f3b92594c83c01faf3de719d457
Sergey Kopienko <sergey.kopienko@intel.com>
2022-08-16 12:05:35 +0200

    Add test for std::complex arithmetic #1

--- test/xpu_api/numerics/complex.number/complex.member.ops/assignment_complex.pass.cpp
+++ test/xpu_api/numerics/complex.number/complex.member.ops/assignment_complex.pass.cpp
@@ -11,10 +11,7 @@
 // complex& operator=(const complex&);
 // template<class X> complex& operator= (const complex<X>&);
 
-#include <complex>
-#include <cassert>
-
-#include "test_macros.h"
+#include "support/test_complex.h"
 
 template <class T, class X>
 void
@@ -33,24 +30,31 @@ test()
     assert(c.imag() == -4.5);
 }
 
-void run_test()
+template <typename EnableDouble, typename EnableLongDouble>
+void
+run_test()
 {
     test<float, float>();
-    test<float, double>();
-    test<float, long double>();
+    oneapi::dpl::__internal::__invoke_if(EnableDouble{}, [&]() { test<float, double>(); });
+    oneapi::dpl::__internal::__invoke_if(EnableLongDouble{}, [&]() { test<float, long double>(); });
 
-    test<double, float>();
-    test<double, double>();
-    test<double, long double>();
+    oneapi::dpl::__internal::__invoke_if(EnableDouble{}, [&]() { test<double, float>(); });
+    oneapi::dpl::__internal::__invoke_if(EnableDouble{}, [&]() { test<double, double>(); });
+    oneapi::dpl::__internal::__invoke_if(EnableLongDouble{}, [&]() { test<double, long double>(); });
 
-    test<long double, float>();
-    test<long double, double>();
-    test<long double, long double>();
+    oneapi::dpl::__internal::__invoke_if(EnableLongDouble{}, [&]() { test<long double, float>(); });
+    oneapi::dpl::__internal::__invoke_if(EnableLongDouble{}, [&]() { test<long double, double>(); });
+    oneapi::dpl::__internal::__invoke_if(EnableLongDouble{}, [&]() { test<long double, long double>(); });
 }
 
 int main(int, char**)
 {
-    run_test();
+    // Run on host
+    run_test<::std::true_type, ::std::true_type>();
 
-  return 0;
+    // Run test in Kernel
+    TestUtils::run_test_in_kernel([&]() { run_test<::std::true_type, ::std::false_type>(); },
+                                  [&]() { run_test<::std::false_type, ::std::false_type>(); });
+
+    return TestUtils::done();
 }
