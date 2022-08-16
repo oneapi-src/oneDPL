3dc9f9b11e62cb36d4828151e95f5f597627fa91
Sergey Kopienko <sergey.kopienko@intel.com>
2022-08-16 12:30:48 +0200

    Add test for std::complex arithmetic #3

--- test/xpu_api/numerics/complex.number/complex.special/float_double_explicit.pass.cpp
+++ test/xpu_api/numerics/complex.number/complex.special/float_double_explicit.pass.cpp
@@ -14,32 +14,40 @@
 //     explicit constexpr complex(const complex<double>&);
 // };
 
-#include <complex>
-#include <cassert>
+#include "support/test_complex.h"
 
-#include "test_macros.h"
-
-void run_test()
+template <typename EnableDouble, typename EnableLongDouble>
+void
+run_test()
 {
-    {
-    const dpl::complex<double> cd(2.5, 3.5);
-    dpl::complex<float> cf(cd);
-    assert(cf.real() == cd.real());
-    assert(cf.imag() == cd.imag());
-    }
+    oneapi::dpl::__internal::__invoke_if(EnableDouble{},
+                                         [&]()
+                                         {
+                                             const dpl::complex<double> cd(2.5, 3.5);
+                                             dpl::complex<float> cf(cd);
+                                             assert(cf.real() == cd.real());
+                                             assert(cf.imag() == cd.imag());
+                                         });
 #if TEST_STD_VER >= 11
-    {
-    constexpr dpl::complex<double> cd(2.5, 3.5);
-    constexpr dpl::complex<float> cf(cd);
-    static_assert(cf.real() == cd.real(), "");
-    static_assert(cf.imag() == cd.imag(), "");
-    }
+    oneapi::dpl::__internal::__invoke_if(EnableDouble{},
+                                         [&]()
+                                         {
+                                             constexpr dpl::complex<double> cd(2.5, 3.5);
+                                             constexpr dpl::complex<float> cf(cd);
+                                             static_assert(cf.real() == cd.real(), "");
+                                             static_assert(cf.imag() == cd.imag(), "");
+                                         });
 #endif
 }
 
 int main(int, char**)
 {
-    run_test();
+    // Run on host
+    run_test<::std::true_type, ::std::true_type>();
 
-  return 0;
+    // Run test in Kernel
+    TestUtils::run_test_in_kernel([&]() { run_test<::std::true_type, ::std::false_type>(); },
+                                  [&]() { run_test<::std::false_type, ::std::false_type>(); });
+
+    return TestUtils::done();
 }
