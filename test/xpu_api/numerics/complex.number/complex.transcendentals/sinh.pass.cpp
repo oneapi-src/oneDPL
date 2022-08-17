79134bebaae2505e812a0b3d9af1569cd8468ec0
Sergey Kopienko <sergey.kopienko@intel.com>
2022-08-17 10:19:50 +0200

    Add test for std::sinh(std::complex)

---
+++ test/xpu_api/numerics/complex.number/complex.transcendentals/sinh.pass.cpp
@@ -0,0 +1,129 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// <complex>
+
+// template<class T>
+//   complex<T>
+//   sinh(const complex<T>& x);
+
+#include "support/test_complex.h"
+
+#include "../cases.h"
+
+template <class T>
+void
+test(const dpl::complex<T>& c, dpl::complex<T> x)
+{
+    assert(dpl::sinh(c) == x);
+}
+
+template <class T>
+void
+test()
+{
+    test(dpl::complex<T>(0, 0), dpl::complex<T>(0, 0));
+}
+
+void test_edges()
+{
+    const unsigned N = sizeof(testcases) / sizeof(testcases[0]);
+    for (unsigned i = 0; i < N; ++i)
+    {
+        dpl::complex<double> r = dpl::sinh(testcases[i]);
+        if (testcases[i].real() == 0 && testcases[i].imag() == 0)
+        {
+            assert(r.real() == 0);
+            assert(std::signbit(r.real()) == std::signbit(testcases[i].real()));
+            assert(r.imag() == 0);
+            assert(std::signbit(r.imag()) == std::signbit(testcases[i].imag()));
+        }
+        else if (testcases[i].real() == 0 && std::isinf(testcases[i].imag()))
+        {
+            assert(r.real() == 0);
+            assert(std::isnan(r.imag()));
+        }
+        else if (std::isfinite(testcases[i].real()) && std::isinf(testcases[i].imag()))
+        {
+            assert(std::isnan(r.real()));
+            assert(std::isnan(r.imag()));
+        }
+        else if (testcases[i].real() == 0 && std::isnan(testcases[i].imag()))
+        {
+            assert(r.real() == 0);
+            assert(std::isnan(r.imag()));
+        }
+        else if (std::isfinite(testcases[i].real()) && std::isnan(testcases[i].imag()))
+        {
+            assert(std::isnan(r.real()));
+            assert(std::isnan(r.imag()));
+        }
+        else if (std::isinf(testcases[i].real()) && testcases[i].imag() == 0)
+        {
+            assert(std::isinf(r.real()));
+            assert(std::signbit(r.real()) == std::signbit(testcases[i].real()));
+            assert(r.imag() == 0);
+            assert(std::signbit(r.imag()) == std::signbit(testcases[i].imag()));
+        }
+        else if (std::isinf(testcases[i].real()) && std::isfinite(testcases[i].imag()))
+        {
+            assert(std::isinf(r.real()));
+            assert(std::signbit(r.real()) == std::signbit(testcases[i].real() * cos(testcases[i].imag())));
+            assert(std::isinf(r.imag()));
+            assert(std::signbit(r.imag()) == std::signbit(sin(testcases[i].imag())));
+        }
+        else if (std::isinf(testcases[i].real()) && std::isinf(testcases[i].imag()))
+        {
+            assert(std::isinf(r.real()));
+            assert(std::isnan(r.imag()));
+        }
+        else if (std::isinf(testcases[i].real()) && std::isnan(testcases[i].imag()))
+        {
+            assert(std::isinf(r.real()));
+            assert(std::isnan(r.imag()));
+        }
+        else if (std::isnan(testcases[i].real()) && testcases[i].imag() == 0)
+        {
+            assert(std::isnan(r.real()));
+            assert(r.imag() == 0);
+            assert(std::signbit(r.imag()) == std::signbit(testcases[i].imag()));
+        }
+        else if (std::isnan(testcases[i].real()) && std::isfinite(testcases[i].imag()))
+        {
+            assert(std::isnan(r.real()));
+            assert(std::isnan(r.imag()));
+        }
+        else if (std::isnan(testcases[i].real()) && std::isnan(testcases[i].imag()))
+        {
+            assert(std::isnan(r.real()));
+            assert(std::isnan(r.imag()));
+        }
+    }
+}
+
+template <typename EnableDouble, typename EnableLongDouble>
+void
+run_test()
+{
+    test<float>();
+    oneapi::dpl::__internal::__invoke_if(EnableDouble{}, [&]() { test<double>(); });
+    oneapi::dpl::__internal::__invoke_if(EnableLongDouble{}, [&]() { test<long double>(); });
+    oneapi::dpl::__internal::__invoke_if(EnableDouble{}, [&]() { test_edges(); });
+}
+
+int main(int, char**)
+{
+    // Run on host
+    run_test<::std::true_type, ::std::true_type>();
+
+    // Run test in Kernel
+    TestUtils::run_test_in_kernel([&]() { run_test<::std::true_type, ::std::false_type>(); },
+                                  [&]() { run_test<::std::false_type, ::std::false_type>(); });
+
+    return TestUtils::done();
+}
