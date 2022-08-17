782aa099c2361e0ab2075e6e2fdf568718819ee1
Sergey Kopienko <sergey.kopienko@intel.com>
2022-08-17 09:27:09 +0200

    Add test for std::proj(std::complex)

---
+++ test/xpu_api/numerics/complex.number/cmplx.over/proj.pass.cpp
@@ -0,0 +1,78 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// <complex>
+
+// template<class T>    complex<T>           proj(const complex<T>&);
+//                      complex<long double> proj(long double);
+//                      complex<double>      proj(double);
+// template<Integral T> complex<double>      proj(T);
+//                      complex<float>       proj(float);
+
+#include "support/test_complex.h"
+
+#include "../cases.h"
+
+template <class T>
+void
+test(T x, typename std::enable_if<std::is_integral<T>::value>::type* = 0)
+{
+    static_assert((std::is_same<decltype(std::proj(x)), dpl::complex<double> >::value), "");
+    assert(std::proj(x) == dpl::proj(dpl::complex<double>(x, 0)));
+}
+
+template <class T>
+void
+test(T x, typename std::enable_if<std::is_floating_point<T>::value>::type* = 0)
+{
+    static_assert((std::is_same<decltype(std::proj(x)), dpl::complex<T> >::value), "");
+    assert(std::proj(x) == dpl::proj(dpl::complex<T>(x, 0)));
+}
+
+template <class T>
+void
+test(T x, typename std::enable_if<!std::is_integral<T>::value &&
+                                  !std::is_floating_point<T>::value>::type* = 0)
+{
+    static_assert((std::is_same<decltype(std::proj(x)), dpl::complex<T> >::value), "");
+    assert(std::proj(x) == dpl::proj(dpl::complex<T>(x, 0)));
+}
+
+template <class T>
+void
+test()
+{
+    test<T>(0);
+    test<T>(1);
+    test<T>(10);
+}
+
+template <typename EnableDouble, typename EnableLongDouble>
+void
+run_test()
+{
+    test<float>();
+    oneapi::dpl::__internal::__invoke_if(EnableDouble{}, [&]() { test<double>(); });
+    oneapi::dpl::__internal::__invoke_if(EnableLongDouble{}, [&]() { test<long double>(); });
+    test<int>();
+    test<unsigned>();
+    test<long long>();
+}
+
+int
+main(int, char**)
+{
+    // Run on host
+    run_test<::std::true_type, ::std::true_type>();
+
+    // Run test in Kernel
+    TestUtils::run_test_in_kernel([&]() { run_test<::std::true_type, ::std::false_type>(); },
+                                  [&]() { run_test<::std::false_type, ::std::false_type>(); });
+
+    return TestUtils::done();
+}
