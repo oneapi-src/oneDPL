e074d9dadf49a0727c666a76627369162280b313
Sergey Kopienko <sergey.kopienko@intel.com>
2022-08-19 14:34:22 +0200

    Add macros ONEDPL_TEST_NUM_MAIN - add usage in test/xpu_api/numerics/complex.number/complex.value.ops

--- test/xpu_api/numerics/complex.number/complex.value.ops/real1.pass.cpp
+++ test/xpu_api/numerics/complex.number/complex.value.ops/real1.pass.cpp
@@ -22,23 +22,9 @@ test()
     assert(dpl::real(z) == 1.5);
 }
 
-template <typename EnableDouble, typename EnableLongDouble>
-void
-run_test()
+ONEDPL_TEST_NUM_MAIN
 {
     test<float>();
     oneapi::dpl::__internal::__invoke_if(EnableDouble{}, [&]() { test<double>(); });
     oneapi::dpl::__internal::__invoke_if(EnableLongDouble{}, [&]() { test<long double>(); });
-}
-
-int main(int, char**)
-{
-    // Run on host
-    run_test<::std::true_type, ::std::true_type>();
-
-    // Run test in Kernel
-    TestUtils::run_test_in_kernel([&]() { run_test<::std::true_type, ::std::false_type>(); },
-                                  [&]() { run_test<::std::false_type, ::std::false_type>(); });
-
-    return TestUtils::done();
 }
