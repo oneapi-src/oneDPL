c0437677d21853739412d98a470e8a93b587355a
Valentina Kats <valentina.kats@intel.com>
2022-08-29 19:31:20 +0200

    Update conf.py

--- documentation/library_guide/conf.py
+++ documentation/library_guide/conf.py
@@ -37,7 +37,7 @@ copyright = 'Intel Corporation'
 author = 'Intel'
 
 # The full version, including alpha/beta/rc tags
-release = '2021.7.1'
+release = '2021.8.0'
 
 rst_epilog = """
 .. include:: /variables.txt
