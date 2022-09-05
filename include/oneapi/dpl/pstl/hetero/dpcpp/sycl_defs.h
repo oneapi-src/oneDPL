8f974ff7b60313afcc6e2122a680bfdb865a9024
Sergey Kopienko <sergey.kopienko@intel.com>
2022-09-05 16:34:31 +0200

    [oneDPL] + Wrap sycl::buffer_allocator into __dpl_sycl::__buffer_allocator (#630)
    Wrap sycl::buffer_allocator into __dpl_sycl::__buffer_allocator alias.
    Implementation of __dpl_sycl::__buffer_allocator depends from __LIBSYCL_VERSION:
    if __LIBSYCL_VERSION >= 50707 then
        template
        using __buffer_allocator =s ycl::buffer_allocator<_DataT>;
    else
        template
        using __buffer_allocator =s ycl::buffer_allocator;

--- include/oneapi/dpl/pstl/hetero/dpcpp/sycl_defs.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/sycl_defs.h
@@ -207,6 +207,14 @@ constexpr __target __target_device =
     __target::global_buffer;
 #endif
 
+template <typename _DataT>
+using __buffer_allocator =
+#if __LIBSYCL_VERSION >= 50707
+    sycl::buffer_allocator<_DataT>;
+#else
+    sycl::buffer_allocator;
+#endif
+
 } // namespace __dpl_sycl
 
 #endif /* _ONEDPL_sycl_defs_H */
