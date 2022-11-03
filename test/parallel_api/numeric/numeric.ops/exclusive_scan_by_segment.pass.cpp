5ca1653464dd58906d06c5ef4601bff3e16ab02e
Dan Hoeflinger <dan.hoeflinger@intel.com>
2022-11-03 16:32:48 -0400

    specializing for known_identity with fallback

--- test/parallel_api/numeric/numeric.ops/exclusive_scan_by_segment.pass.cpp
+++ test/parallel_api/numeric/numeric.ops/exclusive_scan_by_segment.pass.cpp
@@ -21,7 +21,7 @@
 #include "support/utils.h"
 
 #if TEST_DPCPP_BACKEND_PRESENT
-#include "support/utils_sycl.h"
+#    include "support/utils_sycl.h"
 
 using namespace oneapi::dpl::execution;
 #endif // TEST_DPCPP_BACKEND_PRESENT
@@ -34,8 +34,7 @@ DEFINE_TEST(test_exclusive_scan_by_segment)
     // TODO: replace data generation with random data and update check to compare result to
     // the result of a serial implementation of the algorithm
     template <typename Accessor1, typename Accessor2, typename Accessor3, typename Size>
-    void
-    initialize_data(Accessor1 host_keys, Accessor2 host_vals, Accessor3 host_val_res, Size n)
+    void initialize_data(Accessor1 host_keys, Accessor2 host_vals, Accessor3 host_val_res, Size n)
     {
         //T keys[n1] = { 1, 2, 3, 4, 1, 1, 2, 2, 3, 3, 4, 4, 1, 1, 1, ...};
         //T vals[n1] = { 1, 1, 1, ... };
@@ -44,9 +43,9 @@ DEFINE_TEST(test_exclusive_scan_by_segment)
         int i = 0;
         while (i != n)
         {
-            for (int j = 0; j != 4*segment_length && i != n; ++j)
+            for (int j = 0; j != 4 * segment_length && i != n; ++j)
             {
-                host_keys[i] = j/segment_length + 1;
+                host_keys[i] = j / segment_length + 1;
                 host_vals[i] = 1;
                 host_val_res[i] = 0;
                 ++i;
@@ -56,8 +55,7 @@ DEFINE_TEST(test_exclusive_scan_by_segment)
     }
 
     template <typename Accessor1, typename Accessor2, typename T, typename Size>
-    void
-    check_values(Accessor1 host_keys, Accessor2 val_res, T init, Size n)
+    void check_values(Accessor1 host_keys, Accessor2 val_res, T init, Size n)
     {
         //T keys[n1] = { 1, 2, 3, 4, 1, 1, 2, 2, 3, 3, 4, 4, 1, 1, 1, ...};
         //T vals[n1] = { 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 3, ...};
@@ -72,13 +70,17 @@ DEFINE_TEST(test_exclusive_scan_by_segment)
             if (current_key == host_keys[i])
             {
                 current_sum += val_res[i];
-            } else {
+            }
+            else
+            {
                 EXPECT_TRUE(current_sum == expected_segment_sum, "wrong effect from exclusive_scan_by_segment");
                 current_sum = val_res[i];
                 current_key = host_keys[i];
-                if (current_key == 1) {
+                if (current_key == 1)
+                {
                     ++segment_length;
-                    expected_segment_sum = init == 1 ? segment_length * (segment_length + 1) / 2 : segment_length * (segment_length - 1) / 2;
+                    expected_segment_sum = init == 1 ? segment_length * (segment_length + 1) / 2
+                                                     : segment_length * (segment_length - 1) / 2;
                 }
             }
         }
@@ -94,9 +96,9 @@ DEFINE_TEST(test_exclusive_scan_by_segment)
     operator()(Policy&& exec, Iterator1 keys_first, Iterator1 keys_last, Iterator2 vals_first, Iterator2 vals_last,
                Iterator3 val_res_first, Iterator3 val_res_last, Size n)
     {
-        TestDataTransfer<UDTKind::eKeys, Size> host_keys   (*this, n);
-        TestDataTransfer<UDTKind::eVals, Size> host_vals   (*this, n);
-        TestDataTransfer<UDTKind::eRes,  Size> host_val_res(*this, n);
+        TestDataTransfer<UDTKind::eKeys, Size> host_keys(*this, n);
+        TestDataTransfer<UDTKind::eVals, Size> host_vals(*this, n);
+        TestDataTransfer<UDTKind::eRes, Size> host_val_res(*this, n);
 
         typedef typename ::std::iterator_traits<Iterator1>::value_type KeyT;
         typedef typename ::std::iterator_traits<Iterator2>::value_type ValT;
@@ -108,7 +110,8 @@ DEFINE_TEST(test_exclusive_scan_by_segment)
         update_data(host_keys, host_vals, host_val_res);
 
         auto new_policy = make_new_policy<new_kernel_name<Policy, 0>>(exec);
-        auto res1 = oneapi::dpl::exclusive_scan_by_segment(new_policy, keys_first, keys_last, vals_first, val_res_first, init);
+        auto res1 =
+            oneapi::dpl::exclusive_scan_by_segment(new_policy, keys_first, keys_last, vals_first, val_res_first, init);
         exec.queue().wait_and_throw();
 
         retrieve_data(host_vals, host_val_res);
@@ -119,8 +122,9 @@ DEFINE_TEST(test_exclusive_scan_by_segment)
         update_data(host_keys, host_vals, host_val_res);
 
         auto new_policy2 = make_new_policy<new_kernel_name<Policy, 1>>(exec);
-        auto res2 = oneapi::dpl::exclusive_scan_by_segment(new_policy2, keys_first, keys_last, vals_first, val_res_first,
-                                                           init, [](KeyT first, KeyT second) { return first == second; });
+        auto res2 =
+            oneapi::dpl::exclusive_scan_by_segment(new_policy2, keys_first, keys_last, vals_first, val_res_first, init,
+                                                   [](KeyT first, KeyT second) { return first == second; });
         exec.queue().wait_and_throw();
 
         retrieve_data(host_vals, host_val_res);
@@ -131,16 +135,18 @@ DEFINE_TEST(test_exclusive_scan_by_segment)
         update_data(host_keys, host_vals, host_val_res);
 
         auto new_policy3 = make_new_policy<new_kernel_name<Policy, 2>>(exec);
-        auto res3 = oneapi::dpl::exclusive_scan_by_segment(new_policy3, keys_first, keys_last, vals_first, val_res_first,
-                                                           init, [](KeyT first, KeyT second) { return first == second; },
-                                                           [](ValT first, ValT second) { return first + second; });
+        auto res3 =
+            oneapi::dpl::exclusive_scan_by_segment(new_policy3, keys_first, keys_last, vals_first, val_res_first, init,
+                                                   [](KeyT first, KeyT second) { return first == second; },
+                                                   [](ValT first, ValT second) { return first + second; });
         exec.queue().wait_and_throw();
 
         retrieve_data(host_keys, host_val_res);
         check_values(host_keys.get(), host_val_res.get(), init, n);
 
         auto new_policy4 = make_new_policy<new_kernel_name<Policy, 3>>(exec);
-        auto res4 = oneapi::dpl::exclusive_scan_by_segment(new_policy4, keys_first, keys_last, vals_first, val_res_first);
+        auto res4 =
+            oneapi::dpl::exclusive_scan_by_segment(new_policy4, keys_first, keys_last, vals_first, val_res_first);
         exec.queue().wait_and_throw();
 
         retrieve_data(host_keys, host_val_res);
@@ -172,7 +178,8 @@ DEFINE_TEST(test_exclusive_scan_by_segment)
 
         // call algorithm with initial value
         initialize_data(keys_first, vals_first, val_res_first, n);
-        auto res2 = oneapi::dpl::exclusive_scan_by_segment(exec, keys_first, keys_last, vals_first, val_res_first, init);
+        auto res2 =
+            oneapi::dpl::exclusive_scan_by_segment(exec, keys_first, keys_last, vals_first, val_res_first, init);
         check_values(keys_first, val_res_first, init, n);
 
         // call algorithm with equality comparator
@@ -183,8 +190,8 @@ DEFINE_TEST(test_exclusive_scan_by_segment)
 
         // call algorithm with addition operator
         initialize_data(keys_first, vals_first, val_res_first, n);
-        auto res4 = oneapi::dpl::exclusive_scan_by_segment(exec, keys_first, keys_last, vals_first, val_res_first,
-                                                           init, [](KeyT first, KeyT second) { return first == second; },
+        auto res4 = oneapi::dpl::exclusive_scan_by_segment(exec, keys_first, keys_last, vals_first, val_res_first, init,
+                                                           [](KeyT first, KeyT second) { return first == second; },
                                                            [](ValT first, ValT second) { return first + second; });
         check_values(keys_first, val_res_first, init, n);
     }
@@ -199,7 +206,8 @@ DEFINE_TEST(test_exclusive_scan_by_segment)
     }
 };
 
-int main()
+int
+main()
 {
     using ValueType = std::uint64_t;
 
