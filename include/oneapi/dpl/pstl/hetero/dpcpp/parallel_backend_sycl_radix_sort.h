3ea49312758539679096ec6b65f1aa285f34dca2
Adam Fidel <adam.fidel@intel.com>
2022-11-10 13:09:36 -0600

    Guard group ballot implementation based on LIBSYCL version

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_radix_sort.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_radix_sort.h
@@ -509,8 +509,7 @@ struct __peer_prefix_helper;
 template <typename _OffsetT, ::std::uint32_t __radix_bits>
 struct __peer_prefix_helper<_OffsetT, __radix_bits, __peer_prefix_algo::atomic_fetch_or>
 {
-    using _AtomicT = sycl::atomic_ref<::std::uint32_t, sycl::memory_order_relaxed, sycl::memory_scope::sub_group,
-                                      sycl::access::address_space::local_space>;
+    using _AtomicT = __dpl_sycl::__atomic_ref<::std::uint32_t, sycl::access::address_space::local_space>;
     using _TempStorageT =
         sycl::accessor<::std::uint32_t, 1, sycl::access::mode::read_write, sycl::access::target::local>;
 
@@ -523,7 +522,7 @@ struct __peer_prefix_helper<_OffsetT, __radix_bits, __peer_prefix_algo::atomic_f
     __peer_prefix_helper(sycl::nd_item<1> __self_item, _TempStorageT __lacc)
         : __sgroup(__self_item.get_sub_group()), __sg_size(__sgroup.get_local_linear_range()),
           __self_lidx(__self_item.get_local_linear_id()), __item_sg_mask(~(~0u << (__self_lidx))),
-          __atomic_peer_mask(__lacc[0])
+          __atomic_peer_mask(__lacc.get_pointer())
     {
     }
 
@@ -612,7 +611,7 @@ struct __peer_prefix_helper<_OffsetT, __radix_bits, __peer_prefix_algo::scan_the
     }
 };
 
-#if SYCL_EXT_ONEAPI_SUB_GROUP_MASK
+#if _ONEDPL_SYCL_SUB_GROUP_MASK_PRESENT
 template <typename _OffsetT, ::std::uint32_t __radix_bits>
 struct __peer_prefix_helper<_OffsetT, __radix_bits, __peer_prefix_algo::subgroup_ballot>
 {
@@ -860,7 +859,7 @@ __parallel_radix_sort_iteration(_ExecutionPolicy&& __exec, ::std::size_t __segme
     sycl::event __reorder_event{};
     if (__reorder_sg_size == 8 || __reorder_sg_size == 16 || __reorder_sg_size == 32)
     {
-#if SYCL_EXT_ONEAPI_SUB_GROUP_MASK
+#if _ONEDPL_SYCL_SUB_GROUP_MASK_PRESENT
         constexpr auto __peer_algorithm = __peer_prefix_algo::subgroup_ballot;
 #else
         constexpr auto __peer_algorithm = __peer_prefix_algo::atomic_fetch_or;
