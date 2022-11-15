ddb600c8f7631b3e8a9a7663933ae4409603717d
Mikhail Dvorskiy <mikhail.dvorskiy@intel.com>
2022-11-15 15:14:04 +0100

    [oneDPL] + outer namespace  __nanorange for "namespace nano" (NanoRange Library), to fix a potential name conflict

--- include/oneapi/dpl/pstl/ranges_defs.h
+++ include/oneapi/dpl/pstl/ranges_defs.h
@@ -38,11 +38,11 @@ using oneapi::dpl::__ranges::guard_view;
 using oneapi::dpl::__ranges::zip_view;
 
 //views
-using nano::ranges::drop_view;
-using nano::ranges::iota_view;
-using nano::ranges::reverse_view;
-using nano::ranges::take_view;
-using nano::ranges::transform_view;
+using __nanorange::nano::ranges::drop_view;
+using __nanorange::nano::ranges::iota_view;
+using __nanorange::nano::ranges::reverse_view;
+using __nanorange::nano::ranges::take_view;
+using __nanorange::nano::ranges::transform_view;
 
 //adaptors
 namespace views
@@ -53,16 +53,16 @@ using oneapi::dpl::__ranges::views::all_write;
 
 using oneapi::dpl::__ranges::views::host_all;
 
-using nano::views::drop;
-using nano::views::fill;
-using nano::views::generate;
-using nano::views::iota;
-using nano::views::reverse;
-using nano::views::rotate;
-using nano::views::take;
-using nano::views::transform;
+using __nanorange::nano::views::drop;
+using __nanorange::nano::views::fill;
+using __nanorange::nano::views::generate;
+using __nanorange::nano::views::iota;
+using __nanorange::nano::views::reverse;
+using __nanorange::nano::views::rotate;
+using __nanorange::nano::views::take;
+using __nanorange::nano::views::transform;
 
-using nano::subrange;
+using __nanorange::nano::subrange;
 } // namespace views
 
 } // namespace ranges
