b3448743dbe0a714630c853f691837c9eb939b2a
Ruslan Arutyunyan <ruslan.arutyunyan@intel.com>
2022-11-25 04:26:09 +0100

    Add radix bits type for kernel name generation

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_radix_sort.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_radix_sort.h
@@ -735,19 +735,20 @@ __parallel_radix_sort_iteration(_ExecutionPolicy&& __exec, ::std::size_t __segme
                                 sycl::event __dependency_event)
 {
     // Injecting ascending / descending status into custom name to prevent clashing kernel names
-    using _Ascending = std::bool_constant<__is_comp_asc>;
+    using _RadixBitsType = ::std::integral_constant<::std::uint32_t, __radix_bits>;
+    using _AscendingType = ::std::bool_constant<__is_comp_asc>;
     using _CustomName = typename __decay_t<_ExecutionPolicy>::kernel_name;
     using _RadixCountKernel = oneapi::dpl::__par_backend_hetero::__internal::__kernel_name_generator<
-        __radix_sort_count_kernel, _CustomName, __decay_t<_InRange>, __decay_t<_TmpBuf>, _Ascending>;
+        __radix_sort_count_kernel, _CustomName, _RadixBitsType, _AscendingType, __decay_t<_InRange>, __decay_t<_TmpBuf>>;
     using _RadixLocalScanKernel =
         oneapi::dpl::__par_backend_hetero::__internal::__kernel_name_generator<__radix_sort_scan_kernel_1, _CustomName,
                                                                                __decay_t<_TmpBuf>>;
     using _RadixGlobalScanKernel =
         oneapi::dpl::__par_backend_hetero::__internal::__kernel_name_provider<__radix_sort_scan_kernel_2<_CustomName>>;
     using _RadixReorderPeerKernel = oneapi::dpl::__par_backend_hetero::__internal::__kernel_name_generator<
-        __radix_sort_reorder_peer_kernel, _CustomName, __decay_t<_InRange>, __decay_t<_OutRange>, _Ascending>;
+        __radix_sort_reorder_peer_kernel, _CustomName, _RadixBitsType, _AscendingType, __decay_t<_InRange>, __decay_t<_OutRange>>;
     using _RadixReorderKernel = oneapi::dpl::__par_backend_hetero::__internal::__kernel_name_generator<
-        __radix_sort_reorder_kernel, _CustomName, __decay_t<_InRange>, __decay_t<_OutRange>, _Ascending>;
+        __radix_sort_reorder_kernel, _CustomName, _RadixBitsType, _AscendingType, __decay_t<_InRange>, __decay_t<_OutRange>>;
 
     ::std::size_t __max_sg_size = oneapi::dpl::__internal::__max_sub_group_size(__exec);
     ::std::size_t __scan_wg_size = oneapi::dpl::__internal::__max_work_group_size(__exec);
