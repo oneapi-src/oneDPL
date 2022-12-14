316e0cfe47106b9c1db1b256722981163be4368f
Sergey Kopienko <sergey.kopienko@intel.com>
2022-12-14 13:00:18 +0100

    Revert "test/support_stdlib/test_iterators.h - remove class output_iterator as unused in std tests"
    This reverts commit b4a624471c06b3fa745ee9f9e06e018d59a71c82.

--- test/support_stdlib/test_iterators.h
+++ test/support_stdlib/test_iterators.h
@@ -22,6 +22,36 @@
 #define DELETE_FUNCTION
 #endif
 
+template <class It>
+class output_iterator
+{
+    It it_;
+
+    template <class U> friend class output_iterator;
+public:
+    typedef          std::output_iterator_tag                  iterator_category;
+    typedef void                                               value_type;
+    typedef typename std::iterator_traits<It>::difference_type difference_type;
+    typedef It                                                 pointer;
+    typedef typename std::iterator_traits<It>::reference       reference;
+
+    It base() const {return it_;}
+
+    output_iterator () {}
+    explicit output_iterator(It it) : it_(it) {}
+    template <class U>
+        output_iterator(const output_iterator<U>& u) :it_(u.it_) {}
+
+    reference operator*() const {return *it_;}
+
+    output_iterator& operator++() {++it_; return *this;}
+    output_iterator operator++(int)
+        {output_iterator tmp(*this); ++(*this); return tmp;}
+
+    template <class T>
+    void operator,(T const &) DELETE_FUNCTION;
+};
+
 template <class It,
     class ItTraits = It>
 class input_iterator
