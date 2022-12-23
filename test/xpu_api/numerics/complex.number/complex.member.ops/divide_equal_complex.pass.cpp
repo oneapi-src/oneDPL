1e6c048dc807eb406156a4474e2e0a5e17532966
Sergey Kopienko <sergey.kopienko@intel.com>
2022-12-23 19:28:06 +0100

    test/xpu_api/numerics/complex.number/complex.member.ops/divide_equal_complex.pass.cpp - fix review comment: remove (T)

--- test/xpu_api/numerics/complex.number/complex.member.ops/divide_equal_complex.pass.cpp
+++ test/xpu_api/numerics/complex.number/complex.member.ops/divide_equal_complex.pass.cpp
@@ -22,11 +22,11 @@ test()
     assert(c.real() == -4);
     assert(c.imag() == 7.5);
     c /= c2;
-    is_about(c.real(), (T)1.5);
-    is_about(c.imag(), (T)2.5);
+    is_about(c.real(), T(1.5));
+    is_about(c.imag(), T(2.5));
     c /= c2;
-    is_about(c.real(), (T)1.0);
-    is_about(c.imag(), (T)0.0);
+    is_about(c.real(), T(1.0));
+    is_about(c.imag(), T(0.0));
 
     dpl::complex<T> c3;
 
@@ -34,15 +34,15 @@ test()
     c3 = c;
     dpl::complex<int> ic (1,1);
     c3 /= ic;
-    is_about(c3.real(), (T) 0.5);
-    is_about(c3.imag(), (T)-0.5);
+    is_about(c3.real(), T( 0.5));
+    is_about(c3.imag(), T(-0.5));
 #endif // !_PSTL_GLIBCXX_TEST_COMPLEX_DIV_EQ_BROKEN
 
     c3 = c;
     dpl::complex<float> fc (1,1);
     c3 /= fc;
-    is_about(c3.real(), (T) 0.5);
-    is_about(c3.imag(), (T)-0.5);
+    is_about(c3.real(), T( 0.5));
+    is_about(c3.imag(), T(-0.5));
 }
 
 ONEDPL_TEST_NUM_MAIN
