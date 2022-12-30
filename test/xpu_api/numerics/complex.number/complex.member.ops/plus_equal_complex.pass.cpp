e0eeae1cef38a8f579c838d0412600afd6770740
Sergey Kopienko <sergey.kopienko@intel.com>
2022-12-30 11:51:45 +0100

    test/xpu_api/numerics/complex.number/complex.member.ops/plus_equal_complex.pass.cpp - additional fixes

--- test/xpu_api/numerics/complex.number/complex.member.ops/plus_equal_complex.pass.cpp
+++ test/xpu_api/numerics/complex.number/complex.member.ops/plus_equal_complex.pass.cpp
@@ -24,8 +24,8 @@ test()
     assert(c.real() == 1.5f);
     assert(c.imag() == 2.5f);
     c += c2;
-    assert(c.real() == 3.f);
-    assert(c.imag() == 5.f);
+    assert(c.real() == 3);
+    assert(c.imag() == 5);
 
     dpl::complex<T> c3;
 
@@ -33,15 +33,15 @@ test()
     c3 = c;
     dpl::complex<int> ic (1,1);
     c3 += ic;
-    assert(c3.real() == 4.f);
-    assert(c3.imag() == 6.f);
+    assert(c3.real() == 4);
+    assert(c3.imag() == 6);
 #endif // !_PSTL_GLIBCXX_TEST_COMPLEX_PLUS_EQ_BROKEN
 
     c3 = c;
-    dpl::complex<float> fc (1.f, 1.f);
+    dpl::complex<float> fc (1,1);
     c3 += fc;
-    assert(c3.real() == 4.f);
-    assert(c3.imag() == 6.f);
+    assert(c3.real() == 4);
+    assert(c3.imag() == 6);
 }
 
 ONEDPL_TEST_NUM_MAIN
