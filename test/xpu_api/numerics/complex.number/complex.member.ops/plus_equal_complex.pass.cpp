23541e04426ea102090016375d310960c3ade3dd
Sergey Kopienko <sergey.kopienko@intel.com>
2022-12-30 09:05:42 +0100

    test/xpu_api/numerics/complex.number/complex.member.ops/plus_equal_complex.pass.cpp - avoid double type

--- test/xpu_api/numerics/complex.number/complex.member.ops/plus_equal_complex.pass.cpp
+++ test/xpu_api/numerics/complex.number/complex.member.ops/plus_equal_complex.pass.cpp
@@ -17,15 +17,15 @@ void
 test()
 {
     dpl::complex<T> c;
-    const dpl::complex<T> c2(1.5, 2.5);
+    const dpl::complex<T> c2(1.5f, 2.5f);
     assert(c.real() == 0);
     assert(c.imag() == 0);
     c += c2;
-    assert(c.real() == 1.5);
-    assert(c.imag() == 2.5);
+    assert(c.real() == 1.5f);
+    assert(c.imag() == 2.5f);
     c += c2;
-    assert(c.real() == 3);
-    assert(c.imag() == 5);
+    assert(c.real() == 3.f);
+    assert(c.imag() == 5.f);
 
     dpl::complex<T> c3;
 
@@ -33,15 +33,15 @@ test()
     c3 = c;
     dpl::complex<int> ic (1,1);
     c3 += ic;
-    assert(c3.real() == 4);
-    assert(c3.imag() == 6);
+    assert(c3.real() == 4.f);
+    assert(c3.imag() == 6.f);
 #endif // !_PSTL_GLIBCXX_TEST_COMPLEX_PLUS_EQ_BROKEN
 
     c3 = c;
-    dpl::complex<float> fc (1,1);
+    dpl::complex<float> fc (1.f, 1.f);
     c3 += fc;
-    assert(c3.real() == 4);
-    assert(c3.imag() == 6);
+    assert(c3.real() == 4.f);
+    assert(c3.imag() == 6.f);
 }
 
 ONEDPL_TEST_NUM_MAIN
