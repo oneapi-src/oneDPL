e3033616e69bad5ad1e0ca6ef610e17b9d55a9d3
Sergey Kopienko <sergey.kopienko@intel.com>
2022-12-30 09:06:52 +0100

    test/xpu_api/numerics/complex.number/complex.special/double_float_explicit.pass.cpp - avoid double type

--- test/xpu_api/numerics/complex.number/complex.special/double_float_explicit.pass.cpp
+++ test/xpu_api/numerics/complex.number/complex.special/double_float_explicit.pass.cpp
@@ -19,14 +19,14 @@
 ONEDPL_TEST_NUM_MAIN
 {
     {
-    const dpl::complex<float> cd(2.5, 3.5);
+    const dpl::complex<float> cd(2.5f, 3.5f);
     dpl::complex<double> cf(cd);
     assert(cf.real() == cd.real());
     assert(cf.imag() == cd.imag());
     }
 
     {
-    constexpr dpl::complex<float> cd(2.5, 3.5);
+    constexpr dpl::complex<float> cd(2.5f, 3.5f);
     constexpr dpl::complex<double> cf(cd);
     STD_COMPLEX_TESTS_STATIC_ASSERT(cf.real() == cd.real(), "");
     STD_COMPLEX_TESTS_STATIC_ASSERT(cf.imag() == cd.imag(), "");
