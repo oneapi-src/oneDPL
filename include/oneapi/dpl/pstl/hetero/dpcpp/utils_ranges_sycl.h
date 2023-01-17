ccdf338e40724727fc8b87d7ffdb7f6f67c170ee
Sergey Kopienko <sergey.kopienko@intel.com>
2023-01-17 15:41:21 +0100

    include/oneapi/dpl/pstl/hetero/dpcpp/utils_ranges_sycl.h - fix review comment

--- include/oneapi/dpl/pstl/hetero/dpcpp/utils_ranges_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/utils_ranges_sycl.h
@@ -489,7 +489,7 @@ struct __get_sycl_range
 
         const auto __offset = __first - oneapi::dpl::begin(__first.get_buffer());
         const auto __size = __dpl_sycl::__get_buffer_size(__first.get_buffer());
-        const auto __n = ::std::min(__last - __first, decltype(__last - __first)(__size));
+        const auto __n = ::std::min(decltype(__size)(__last - __first), __size);
         assert(__offset + __n <= __size);
 
         return __range_holder<oneapi::dpl::__ranges::all_view<value_type, AccMode>>{
