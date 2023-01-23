2ed5857ad66785b64ffee46b7f91318a15dceb83
Mikhail Dvorskiy <mikhail.dvorskiy@intel.com>
2023-01-23 19:22:09 +0100

    [oneDPL] one group radix_sort: + code uglification

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_radix_sort_one_wg.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_radix_sort_one_wg.h
@@ -23,25 +23,25 @@
 //namespace __par_backend_hetero
 //{
 
-template<typename KeyT, typename>
+template<typename _KeyT, typename>
 class _TempBuf;
 
-template<typename KeyT>
-class _TempBuf<KeyT, std::true_type /*shared local memory buffer*/>
+template<typename _KeyT>
+class _TempBuf<_KeyT, std::true_type /*shared local memory buffer*/>
 {
     uint16_t __buf_size;
 public:
     _TempBuf(uint16_t __n): __buf_size(__n) {}
     auto get_acc(sycl::handler& __cgh)
     {
-        return sycl::local_accessor<KeyT, 1>(__buf_size, __cgh);
+        return sycl::local_accessor<_KeyT, 1>(__buf_size, __cgh);
     }
 };
 
-template<typename KeyT>
-class _TempBuf<KeyT, std::false_type /*global memory buffer*/>
+template<typename _KeyT>
+class _TempBuf<_KeyT, std::false_type /*global memory buffer*/>
 {
-    sycl::buffer<KeyT> __buf;
+    sycl::buffer<_KeyT> __buf;
 
 public:
     _TempBuf(uint16_t __n): __buf(__n) {}
@@ -51,42 +51,41 @@ public:
     }
 };
 
-template <uint16_t __block_size, typename KeyT, typename _Wi, typename _Src, typename _Keys>
+template <uint16_t __block_size, typename _KeyT, typename _Wi, typename _Src, typename _Keys>
 void
 __block_load(const _Wi __wi, const _Src& __src, _Keys& __keys, const uint32_t __n)
 {
-    constexpr KeyT __default_key = KeyT{};
+    constexpr _KeyT __default_key = _KeyT{};
 
     #pragma unroll
-    for (uint16_t i = 0; i < __block_size; i++)
+    for (uint16_t __i = 0; __i < __block_size; ++__i)
     {
-        const uint16_t __offset = __wi*__block_size + i;
-        //boundary check is slow but necessary
+        const uint16_t __offset = __wi*__block_size + __i;
         if (__offset < __n)
-            __keys[i] = __src[__offset];
+            __keys[__i] = __src[__offset];
         else
-            __keys[i] = __default_key;
+            __keys[__i] = __default_key;
     }
 }
 
-template <uint16_t __block_size, typename _Item, typename _Wi, typename _Lacc, typename _Keys, typename _Ranks>
+template <uint16_t __block_size, typename _Item, typename _Wi, typename _Lacc, typename _Keys, typename _Indices>
 void
-__to_blocked(_Item __it, const _Wi __wi, _Lacc& __exchange_lacc, _Keys& __keys, const _Ranks& __ranks)
+__to_blocked(_Item __it, const _Wi __wi, _Lacc& __exchange_lacc, _Keys& __keys, const _Indices& __indices)
 {
     #pragma unroll
-    for (uint16_t i = 0; i < __block_size; i++)
-        __exchange_lacc[__ranks[i]] = __keys[i];
+    for (uint16_t __i = 0; __i < __block_size; ++__i)
+        __exchange_lacc[__indices[__i]] = __keys[__i];
 
     __dpl_sycl::__group_barrier(__it);
 
     #pragma unroll
-    for (uint16_t i = 0; i<__block_size; i++)
-        __keys[i] = __exchange_lacc[__wi*__block_size + i];
+    for (uint16_t __i = 0; __i < __block_size; ++__i)
+        __keys[__i] = __exchange_lacc[__wi*__block_size + __i];
 }
 
 template<typename _KernelName, uint16_t __wg_size = 256/*work group size*/, uint16_t __block_size = 16,
          ::std::uint32_t __radix = 4, bool __is_asc = true, typename _SLM_tag = std::true_type,
-         typename _RangeIn, uint16_t req_sub_group_size = (__block_size < 4 ? 32 : 16)>
+         typename _RangeIn, uint16_t __req_sub_group_size = (__block_size < 4 ? 32 : 16)>
 auto __subgroup_radix_sort(sycl::queue __q, _RangeIn&& __src)
 {
     constexpr uint16_t __bin_count = 1 << __radix;
@@ -99,103 +98,102 @@ auto __subgroup_radix_sort(sycl::queue __q, _RangeIn&& __src)
     auto __bundle = sycl::get_kernel_bundle<sycl::bundle_state::executable>(__q.get_context(), {__kernel_id});
 # endif
   
-    using KeyT = oneapi::dpl::__internal::__value_t<_RangeIn>;
+    using _KeyT = oneapi::dpl::__internal::__value_t<_RangeIn>;
 
-    _TempBuf<KeyT, _SLM_tag> __buf(__block_size*__wg_size);
+    _TempBuf<_KeyT, _SLM_tag> __buf(__block_size*__wg_size);
 
-    sycl::nd_range myRange {sycl::range{__wg_size}, sycl::range{__wg_size}};
-    auto __event = __q.submit([&](sycl::handler& cgh) {
-        oneapi::dpl::__ranges::__require_access(cgh, __src);
+    sycl::nd_range __range {sycl::range{__wg_size}, sycl::range{__wg_size}};
+    auto __event = __q.submit([&](sycl::handler& __cgh) {
+        oneapi::dpl::__ranges::__require_access(__cgh, __src);
 
-        auto exchange_lacc = __buf.get_acc(cgh); //exchange key, size is __block_size*__wg_size
-        auto counter_lacc = sycl::local_accessor<uint32_t, 1>(__wg_size * __bin_count, cgh);//counter, could be private but use slm here
+        auto __exchange_lacc = __buf.get_acc(__cgh); //exchange key, size is __block_size*__wg_size
+        auto __counter_lacc = sycl::local_accessor<uint32_t, 1>(__wg_size * __bin_count + 1, __cgh);//counter, could be private but use slm here
   
 # if _ONEDPL_KERNEL_BUNDLE_PRESENT
-        cgh.use_kernel_bundle(__bundle);
+        __cgh.use_kernel_bundle(__bundle);
 # endif
-        cgh.parallel_for<_KernelName>(
-            myRange, ([=](sycl::nd_item<1> it) [[sycl::reqd_sub_group_size(req_sub_group_size)]]
+        __cgh.parallel_for<_KernelName>(
+            __range, ([=](sycl::nd_item<1> __it) [[sycl::reqd_sub_group_size(__req_sub_group_size)]]
         {
   
-            KeyT keys[__block_size];
-            uint16_t wi_x = it.get_local_linear_id();
-            uint16_t begin_bit = 0;
-            constexpr uint16_t end_bit = sizeof(KeyT) * 8; 
+            _KeyT __keys[__block_size];
+            uint16_t __wi = __it.get_local_linear_id();
+            uint16_t __begin_bit = 0;
+            constexpr uint16_t __end_bit = sizeof(_KeyT) * 8; 
   
-            __block_load<__block_size, KeyT>(wi_x, __src, keys, __n);
+            __block_load<__block_size, _KeyT>(__wi, __src, __keys, __n);
   
-            __dpl_sycl::__group_barrier(it);
+            __dpl_sycl::__group_barrier(__it);
             while (true)
             {
-                uint16_t thread_prefixes[__block_size];
+                uint16_t __indices[__block_size];
                 {
-                    uint32_t* digit_counters[__block_size];
+                    uint32_t* __counters[__block_size];
                     //ResetCounters();
-                    auto pcounter = counter_lacc.get_pointer()+wi_x;
+                    auto __pcounter = __counter_lacc.get_pointer()+__wi;
                     #pragma unroll
-                    for (uint16_t LANE = 0; LANE < __bin_count; LANE++)
-                        pcounter[LANE*__wg_size] = 0;
+                    for (uint16_t __i = 0; __i < __bin_count; ++__i)
+                        __pcounter[__i * __wg_size] = 0;
 
                     #pragma unroll
-                    for (uint16_t ITEM = 0; ITEM < __block_size; ++ITEM)
+                    for (uint16_t __i = 0; __i < __block_size; ++__i)
                     {
-                        const int digit =
-                            __get_bucket</*mask*/__bin_count - 1>(__order_preserving_cast<__is_asc>(keys[ITEM]), begin_bit);
+                        const int __bin =
+                            __get_bucket</*mask*/__bin_count - 1>(__order_preserving_cast<__is_asc>(__keys[__i]), __begin_bit);
   
-                        digit_counters[ITEM] = &pcounter[digit*__wg_size];
-                        thread_prefixes[ITEM] = *digit_counters[ITEM];
-                        *digit_counters[ITEM] = thread_prefixes[ITEM] + 1;
+                        __counters[__i] = &__pcounter[__bin*__wg_size];
+                        __indices[__i] = *__counters[__i];
+                        *__counters[__i] = __indices[__i] + 1;
                     }
-                    __dpl_sycl::__group_barrier(it);
+                    __dpl_sycl::__group_barrier(__it);
   
                     // Scan shared memory counters
                     {
                         //access pattern might be further optimized
   
                         //scan contiguous numbers
-                        uint16_t bin_sum[__bin_count];
-                        bin_sum[0] = counter_lacc[wi_x * __bin_count];
-                        for (uint16_t i = 1; i < __bin_count; i++)
-                            bin_sum[i] = bin_sum[i-1] + counter_lacc[wi_x * __bin_count + i];
+                        uint16_t __bin_sum[__bin_count];
+                        __bin_sum[0] = __counter_lacc[__wi * __bin_count];
+                        for (uint16_t __i = 1; __i < __bin_count; ++__i)
+                            __bin_sum[__i] = __bin_sum[__i-1] + __counter_lacc[__wi * __bin_count + __i];
   
-                        __dpl_sycl::__group_barrier(it);
+                        __dpl_sycl::__group_barrier(__it);
                         //exclusive scan local sum
-                        uint16_t sum_scan = __dpl_sycl::__exclusive_scan_over_group(it.get_group(), bin_sum[__bin_count-1], sycl::plus<uint16_t>());
+                        uint16_t __sum_scan = __dpl_sycl::__exclusive_scan_over_group(__it.get_group(), __bin_sum[__bin_count-1], sycl::plus<uint16_t>());
                         //add to local sum, generate exclusive scan result
-                        for (uint16_t i = 0; i < __bin_count; i++)
-                            counter_lacc[wi_x * __bin_count + i + 1] = sum_scan + bin_sum[i];
+                        for (uint16_t __i = 0; __i < __bin_count; ++__i)
+                            __counter_lacc[__wi * __bin_count + __i + 1] = __sum_scan + __bin_sum[__i];
   
-                        if (wi_x == 0)
-                            counter_lacc[0] = 0;
-                        __dpl_sycl::__group_barrier(it);
+                        if (__wi == 0)
+                            __counter_lacc[0] = 0;
+                        __dpl_sycl::__group_barrier(__it);
                     }
   
-                    // Extract the local ranks of each key
+                    // Extract the local offsets of each key
                     #pragma unroll
-                    for (uint16_t ITEM = 0; ITEM < __block_size; ++ITEM)
+                    for (uint16_t __i = 0; __i < __block_size; ++__i)
                     {
                         // Add in thread block exclusive prefix
-                        thread_prefixes[ITEM] += *digit_counters[ITEM];
+                        __indices[__i] += *__counters[__i];
                     }
                 }
   
-                begin_bit += __radix;
+                __begin_bit += __radix;
   
-                __dpl_sycl::__group_barrier(it);
-                if (begin_bit >= end_bit)
+                __dpl_sycl::__group_barrier(__it);
+                if (__begin_bit >= __end_bit)
                 {
                     // end of iteration, write out result
-                    for (uint16_t i = 0; i<__block_size; i++)
+                    for (uint16_t __i = 0; __i < __block_size; ++__i)
                     {
-                        //boundary check is slow but necessary
-                        const uint16_t __r = thread_prefixes[i];
+                        const uint16_t __r = __indices[__i];
                         if (__r < __n)
-                            __src[__r] = keys[i];
+                            __src[__r] = __keys[__i];
                     }
                     return;
                 }
-                __to_blocked<__block_size>(it, wi_x, exchange_lacc, keys, thread_prefixes);
-                __dpl_sycl::__group_barrier(it);
+                __to_blocked<__block_size>(__it, __wi, __exchange_lacc, __keys, __indices);
+                __dpl_sycl::__group_barrier(__it);
             }
         }));
      });
