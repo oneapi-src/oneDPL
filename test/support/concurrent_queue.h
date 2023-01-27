e2d346ace06a60eb3435305e4a791b2ba3bc067e
Anuya Welling <anuya.welling@intel.com>
2023-01-26 18:23:41 -0600

    Added concurent queue

--- test/support/concurrent_queue.h
+++ test/support/concurrent_queue.h
@@ -1,4 +1,18 @@
-#include <iostream>
+// -*- C++ -*-
+//===---------------------------------------------------------------------===//
+//
+// Copyright (C) Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// This file incorporates work covered by the following copyright and permission
+// notice:
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+//
+//===----------------------------------------------------------------------===//
+
 #include <queue>
 #include <thread>
 #include <mutex>
