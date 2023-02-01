6005763f5f4399892ad70b4bd4d131a238206ff1
Sergey Kopienko <sergey.kopienko@intel.com>
2023-02-01 15:28:32 +0100

    test/xpu_api/numerics/numeric.ops/numeric.ops.lcm/xpu_lcm.pass.cpp - Warning G41AE70B8 unused variable 'sycl_write' [-Wunused-const-variable]

--- test/xpu_api/numerics/numeric.ops/numeric.ops.lcm/xpu_lcm.pass.cpp
+++ test/xpu_api/numerics/numeric.ops/numeric.ops.lcm/xpu_lcm.pass.cpp
@@ -21,7 +21,7 @@
 #include <cassert>
 #include <iostream>
 
-constexpr sycl::access::mode sycl_write = sycl::access::mode::write;
+[[maybe_unused]] constexpr sycl::access::mode sycl_write = sycl::access::mode::write;
 
 using oneapi::dpl::is_same;
 using oneapi::dpl::lcm;
