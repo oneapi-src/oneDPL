5f7710d2c2f4bc69072c4df764faff791f368d9a
Sergey Kopienko <sergey.kopienko@intel.com>
2023-02-01 10:54:40 +0100

    test/parallel_api/algorithm/alg.sorting/alg.binary.search/lower.bound/lower_bound.pass.cpp - Warning G41AE70B8 unused variable '...' [-Wunused-variable]

--- test/parallel_api/algorithm/alg.sorting/alg.binary.search/lower.bound/lower_bound.pass.cpp
+++ test/parallel_api/algorithm/alg.sorting/alg.binary.search/lower.bound/lower_bound.pass.cpp
@@ -68,7 +68,8 @@ DEFINE_TEST(test_lower_bound)
         update_data(host_keys, host_vals, host_res);
 
         auto new_policy = make_new_policy<new_kernel_name<Policy, 0>>(exec);
-        auto res1 = oneapi::dpl::lower_bound(new_policy, first, last, value_first, value_last, result_first);
+        [[maybe_unused]] auto res1 =
+            oneapi::dpl::lower_bound(new_policy, first, last, value_first, value_last, result_first);
         exec.queue().wait_and_throw();
 
         retrieve_data(host_vals, host_res);
@@ -77,8 +78,9 @@ DEFINE_TEST(test_lower_bound)
 
         // call algorithm with comparator
         auto new_policy2 = make_new_policy<new_kernel_name<Policy, 1>>(exec);
-        auto res2 = oneapi::dpl::lower_bound(new_policy2, first, last, value_first, value_last, result_first,
-                                             [](ValueT first, ValueT second) { return first < second; });
+        [[maybe_unused]] auto res2 =
+            oneapi::dpl::lower_bound(new_policy2, first, last, value_first, value_last, result_first,
+                                     [](ValueT first, ValueT second) { return first < second; });
         exec.queue().wait_and_throw();
 
         retrieve_data(host_vals, host_res);
@@ -101,12 +103,13 @@ DEFINE_TEST(test_lower_bound)
         // call algorithm with no optional arguments
         initialize_data(first, value_first, result_first, n);
 
-        auto res1 = oneapi::dpl::lower_bound(exec, first, last, value_first, value_last, result_first);
+        [[maybe_unused]] auto res1 = oneapi::dpl::lower_bound(exec, first, last, value_first, value_last, result_first);
         check_and_clean(result_first, value_first, n);
 
         // call algorithm with comparator
-        auto res2 = oneapi::dpl::lower_bound(exec, first, last, value_first, value_last, result_first,
-                                             [](ValueT first, ValueT second) { return first < second; });
+        [[maybe_unused]] auto res2 =
+            oneapi::dpl::lower_bound(exec, first, last, value_first, value_last, result_first,
+                                     [](ValueT first, ValueT second) { return first < second; });
         check_and_clean(result_first, value_first, n);
     }
 
