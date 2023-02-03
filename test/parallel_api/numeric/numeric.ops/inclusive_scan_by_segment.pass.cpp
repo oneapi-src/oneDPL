88d6689566a366035a3bb02b8338a55ac653b60b
Sergey Kopienko <sergey.kopienko@intel.com>
2023-02-03 10:41:18 +0100

    test/parallel_api/numeric/numeric.ops/inclusive_scan_by_segment.pass.cpp - Warning G41AE70B8 unused variable '...' [-Wunused-variable]

--- test/parallel_api/numeric/numeric.ops/inclusive_scan_by_segment.pass.cpp
+++ test/parallel_api/numeric/numeric.ops/inclusive_scan_by_segment.pass.cpp
@@ -131,8 +131,7 @@ DEFINE_TEST_1(test_inclusive_scan_by_segment, BinaryOperation)
         update_data(host_keys, host_vals, host_res);
 
         auto new_policy = make_new_policy<new_kernel_name<Policy, 0>>(exec);
-        [[maybe_unused]] auto res1 =
-            oneapi::dpl::inclusive_scan_by_segment(new_policy, keys_first, keys_last, vals_first, val_res_first);
+        auto res1 = oneapi::dpl::inclusive_scan_by_segment(new_policy, keys_first, keys_last, vals_first, val_res_first);
         exec.queue().wait_and_throw();
 
         retrieve_data(host_keys, host_vals, host_res);
@@ -143,9 +142,8 @@ DEFINE_TEST_1(test_inclusive_scan_by_segment, BinaryOperation)
         update_data(host_keys, host_vals, host_res);
 
         auto new_policy2 = make_new_policy<new_kernel_name<Policy, 1>>(exec);
-        [[maybe_unused]] auto res2 =
-            oneapi::dpl::inclusive_scan_by_segment(new_policy2, keys_first, keys_last, vals_first, val_res_first,
-                                                   [](KeyT first, KeyT second) { return first == second; });
+        auto res2 = oneapi::dpl::inclusive_scan_by_segment(new_policy2, keys_first, keys_last, vals_first, val_res_first,
+                                                           [](KeyT first, KeyT second) { return first == second; });
         exec.queue().wait_and_throw();
 
         retrieve_data(host_keys, host_vals, host_res);
@@ -156,9 +154,9 @@ DEFINE_TEST_1(test_inclusive_scan_by_segment, BinaryOperation)
         update_data(host_keys, host_vals, host_res);
 
         auto new_policy3 = make_new_policy<new_kernel_name<Policy, 2>>(exec);
-        [[maybe_unused]] auto res3 = oneapi::dpl::inclusive_scan_by_segment(
-            new_policy3, keys_first, keys_last, vals_first, val_res_first,
-            [](KeyT first, KeyT second) { return first == second; }, BinaryOperation());
+        auto res3 = oneapi::dpl::inclusive_scan_by_segment(new_policy3, keys_first, keys_last, vals_first, val_res_first,
+                                                           [](KeyT first, KeyT second) { return first == second; },
+                                                           BinaryOperation());
         exec.queue().wait_and_throw();
 
         retrieve_data(host_keys, host_vals, host_res);
@@ -181,22 +179,20 @@ DEFINE_TEST_1(test_inclusive_scan_by_segment, BinaryOperation)
 
         // call algorithm with no optional arguments
         initialize_data(keys_first, vals_first, val_res_first, n);
-        [[maybe_unused]] auto res1 =
-            oneapi::dpl::inclusive_scan_by_segment(exec, keys_first, keys_last, vals_first, val_res_first);
+        auto res1 = oneapi::dpl::inclusive_scan_by_segment(exec, keys_first, keys_last, vals_first, val_res_first);
         check_values(keys_first, vals_first, val_res_first, n);
 
         // call algorithm with equality comparator
         initialize_data(keys_first, vals_first, val_res_first, n);
-        [[maybe_unused]] auto res2 =
-            oneapi::dpl::inclusive_scan_by_segment(exec, keys_first, keys_last, vals_first, val_res_first,
-                                                   [](KeyT first, KeyT second) { return first == second; });
+        auto res2 = oneapi::dpl::inclusive_scan_by_segment(exec, keys_first, keys_last, vals_first, val_res_first,
+                                                           [](KeyT first, KeyT second) { return first == second; });
         check_values(keys_first, vals_first, val_res_first, n);
 
         // call algorithm with addition operator
         initialize_data(keys_first, vals_first, val_res_first, n);
-        [[maybe_unused]] auto res3 = oneapi::dpl::inclusive_scan_by_segment(
-            exec, keys_first, keys_last, vals_first, val_res_first,
-            [](KeyT first, KeyT second) { return first == second; }, BinaryOperation());
+        auto res3 = oneapi::dpl::inclusive_scan_by_segment(exec, keys_first, keys_last, vals_first, val_res_first,
+                                                           [](KeyT first, KeyT second) { return first == second; },
+                                                           BinaryOperation());
         check_values(keys_first, vals_first, val_res_first, n, BinaryOperation());
     }
 
