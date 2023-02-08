bc4129111fa4544b21708fecdb1f1ffb234160bf
MikeDvorskiy <mikhail.dvorskiy@intel.com>
2023-02-08 11:12:10 +0100

    [oneDPL] radix sort: removed spaces

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_radix_sort.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_radix_sort.h
@@ -679,7 +679,7 @@ __parallel_radix_sort(_ExecutionPolicy&& __exec, _Range&& __in_rng)
     {
         // Injecting ascending / descending status into a kernel name to prevent clashing kernel names
         using _RadixBitsType = ::std::integral_constant<::std::uint32_t, __radix_bits>;
-        using _AscendingType = ::std::bool_constant<__is_ascending>;        
+        using _AscendingType = ::std::bool_constant<__is_ascending>;
         using _CustomName = typename __decay_t<_ExecutionPolicy>::kernel_name;
         using _RadixSortKernel = oneapi::dpl::__par_backend_hetero::__internal::__kernel_name_generator<
             __radix_sort_one_group, _CustomName, _RadixBitsType, _AscendingType, __decay_t<_Range>>;
