a0d294056e90ce99904ffb903472a772c5aee557
Dmitriy Sobolev <dmitriy.sobolev@intel.com>
2023-03-27 11:59:17 -0500

    Add SIMD order_preserving_cast functions
    Signed-off-by: Dmitriy Sobolev <dmitriy.sobolev@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/esimd_radix_sort/esimd_radix_sort_utils.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/esimd_radix_sort/esimd_radix_sort_utils.h
@@ -109,5 +109,86 @@ inline sycl::ext::intel::esimd::simd<RT, 16> scan(sycl::ext::intel::esimd::simd<
 	return result;
 }
 
+template <bool __is_ascending, int _N>
+sycl::ext::intel::esimd::simd<bool, _N>
+__order_preserving_cast(sycl::ext::intel::esimd::simd<bool, _N> __src)
+{
+    if constexpr (__is_ascending)
+        return __src;
+    else
+        return !__src;
+}
+
+template <bool __is_ascending, typename _UInt, int _N, std::enable_if_t<::std::is_unsigned_v<_UInt>, int> = 0>
+sycl::ext::intel::esimd::simd<_UInt, _N>
+__order_preserving_cast(sycl::ext::intel::esimd::simd<_UInt, _N> __src)
+{
+    if constexpr (__is_ascending)
+        return __src;
+    else
+        return ~__src; //bitwise inversion
+}
+
+template <bool __is_ascending, typename _Int, int _N,
+          std::enable_if_t<::std::is_integral_v<_Int> && ::std::is_signed_v<_Int>, int> = 0>
+sycl::ext::intel::esimd::simd<::std::make_unsigned_t<_Int>, _N>
+__order_preserving_cast(sycl::ext::intel::esimd::simd<_Int, _N> __src)
+{
+    using _UInt = ::std::make_unsigned_t<_Int>;
+    // mask: 100..0 for ascending, 011..1 for descending
+    constexpr _UInt __mask = (__is_ascending) ? 1 << ::std::numeric_limits<_Int>::digits : ~_UInt(0) >> 1;
+    return __src.template bit_cast_view<_UInt>() ^ __mask;
+}
+
+const __SYCL_CONSTANT_AS char print_order_cast_float[] = "order cast float\n";
+
+template <bool __is_ascending, typename _Float, int _N,
+          std::enable_if_t<::std::is_floating_point_v<_Float> && sizeof(_Float) == sizeof(::std::uint32_t), int> = 0>
+sycl::ext::intel::esimd::simd<::std::uint32_t, _N>
+__order_preserving_cast(sycl::ext::intel::esimd::simd<_Float, _N> __src)
+{
+    auto __uint32_src = __src.template bit_cast_view<::std::uint32_t>();
+    sycl::ext::intel::esimd::simd<::std::uint32_t, _N> __mask;
+    sycl::ext::intel::esimd::simd_mask<_N> __sign_bit_m = (__uint32_src >> 31 == 0);
+    if constexpr (__is_ascending)
+    {
+        __mask = sycl::ext::intel::esimd::merge(
+            sycl::ext::intel::esimd::simd<::std::uint32_t, _N>(0x80000000u),
+            sycl::ext::intel::esimd::simd<::std::uint32_t, _N>(0xFFFFFFFFu), __sign_bit_m);
+    }
+    else
+    {
+        __mask = sycl::ext::intel::esimd::merge(
+            sycl::ext::intel::esimd::simd<::std::uint32_t, _N>(0x7FFFFFFFu),
+            sycl::ext::intel::esimd::simd<::std::uint32_t, _N>(static_cast<::std::uint32_t>(0)), __sign_bit_m);
+    }
+    sycl::ext::intel::esimd::simd<::std::uint32_t, _N> __res = __uint32_src ^ __mask;
+    return __res;
+}
+
+template <bool __is_ascending, typename _Float, int _N,
+          std::enable_if_t<::std::is_floating_point_v<_Float> && sizeof(_Float) == sizeof(::std::uint64_t), int> = 0>
+sycl::ext::intel::esimd::simd<::std::uint64_t, _N>
+__order_preserving_cast(sycl::ext::intel::esimd::simd<_Float, _N> __src)
+{
+    auto __uint64_src = __src.template bit_cast_view<::std::uint64_t>();
+    sycl::ext::intel::esimd::simd<::std::uint64_t, _N> __mask;
+    sycl::ext::intel::esimd::simd_mask<_N> __sign_bit_m = (__uint64_src >> 63 == 0);
+    if constexpr (__is_ascending)
+    {
+        __mask = sycl::ext::intel::esimd::merge(
+            sycl::ext::intel::esimd::simd<::std::uint64_t, _N>(0x8000000000000000u),
+            sycl::ext::intel::esimd::simd<::std::uint64_t, _N>(0xFFFFFFFFFFFFFFFFu), __sign_bit_m);
+    }
+    else
+    {
+        __mask = sycl::ext::intel::esimd::merge(
+            sycl::ext::intel::esimd::simd<::std::uint64_t, _N>(0x7FFFFFFFFFFFFFFFu),
+            sycl::ext::intel::esimd::simd<::std::uint64_t, _N>(static_cast<::std::uint64_t>(0)), __sign_bit_m);
+    }
+    sycl::ext::intel::esimd::simd<::std::uint64_t, _N> __res = __uint64_src ^ __mask;
+    return __res;
+}
+
 }
 #endif // _ONEDPL_esimd_radix_sort_utils_H
