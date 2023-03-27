3fe507b9e6ee43a2ae7ccb8cb53d671bab5ba989
Dmitriy Sobolev <dmitriy.sobolev@intel.com>
2023-03-27 11:59:17 -0500

    Add SIMD order_preserving_cast functions
    Signed-off-by: Dmitriy Sobolev <dmitriy.sobolev@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_esimd_radix_sort.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_esimd_radix_sort.h
@@ -80,7 +80,7 @@ radix_sort(_ExecutionPolicy&& __exec, _Range&& __rng)
         // TODO: avoid kernel duplication (generate the output storate with the same type as input storatge and use swap)
         // TODO: allow different RadixBits, make sure the data is in the input storage after the last stage
         // TODO: pass _ProcessSize according to __n
-        // TODO: fix when compiled in-O0 mode: "esimd_radix_sort_one_wg.h : 54 : 5>: SLM init call is supported only in kernels"
+        // TODO: fix when compiled in -O0 mode: "esimd_radix_sort_one_wg.h : 54 : 5>: SLM init call is supported only in kernels"
         oneapi::dpl::experimental::esimd::impl::onesweep<_ExecutionPolicy, _KeyT, _Range, RadixBits, /*_ProcessSize*/ 512>(
             ::std::forward<_ExecutionPolicy>(__exec), ::std::forward<_Range>(__rng), __n);
     }
