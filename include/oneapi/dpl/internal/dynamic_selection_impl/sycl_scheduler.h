9f298645a9e4c0249c53fffa61f825807db19e07
egfefey <elvis.g.fefey@intel.com>
2023-03-31 14:20:58 -0500

    Update sycl_scheduler.h
    Preventing compilation of implicitly converted selection handle in sycl scheduler

--- include/oneapi/dpl/internal/dynamic_selection_impl/sycl_scheduler.h
+++ include/oneapi/dpl/internal/dynamic_selection_impl/sycl_scheduler.h
@@ -78,12 +78,17 @@ namespace experimental {
       }
     }
 
+  
     template<typename SelectionHandle, typename Function, typename ...Args>
     auto submit(SelectionHandle h, Function&& f, Args&&... args) {
-      using PropertyHandle = typename SelectionHandle::property_handle_t;
-      auto w = new async_wait_impl_t<PropertyHandle>(h.get_property_handle(), f(h.get_native(), std::forward<Args>(args)...));
-      waiters_.push(w);
-      return *w;
+      if constexpr (!std::is_same_v <SelectionHandle, native_resource_t> || !std::is_same_v <SelectionHandle, execution_resource_t>) {
+          using PropertyHandle = typename SelectionHandle::property_handle_t;
+          auto w = new async_wait_impl_t<PropertyHandle>(h.get_property_handle(), f(h.get_native(), std::forward<Args>(args)...));
+          waiters_.push(w);
+          return *w;
+      } else {
+        return;
+      }
     }
 
     void wait_for_all() {
