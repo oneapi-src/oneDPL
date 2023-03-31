54dfe76846d39c88c7f69a273328a523e608574b
Julian Miller <julian.miller@intel.com>
2023-03-31 17:16:14 +0200

    Improve group reduction with unknown identity

--- include/oneapi/dpl/pstl/hetero/dpcpp/unseq_backend_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/unseq_backend_sycl.h
@@ -298,18 +298,14 @@ struct reduce_over_group
         auto __local_idx = __item_id.get_local_id(0);
         auto __group_size = __item_id.get_local_range().size();
 
-        auto __k = 1;
-
-        do
+        for (_Size __k = 1; __k < __group_size; __k <<= 1)
         {
             __dpl_sycl::__group_barrier(__item_id);
-            if (__local_idx % (2 * __k) == 0 && __local_idx + __k < __group_size && __global_idx < __n &&
-                __global_idx + __k < __n)
+            if (__local_idx % (2 * __k) == 0 && __local_idx + __k < __group_size && __global_idx + __k < __n)
             {
                 __local_mem[__local_idx] = __bin_op1(__local_mem[__local_idx], __local_mem[__local_idx + __k]);
             }
-            __k *= 2;
-        } while (__k < __group_size);
+        }
         return __local_mem[__local_idx];
     }
 
