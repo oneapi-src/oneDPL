045a77bc84ed76ba6e33324eedf6a405626501a4
Mikhail Dvorskiy <mikhail.dvorskiy@intel.com>
2023-04-05 16:43:42 +0200

    [oneDPL][hetero] + projection support for "general"(merge sort) sort for SYCL backend

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
@@ -1564,11 +1564,12 @@ template <
     __enable_if_t<oneapi::dpl::__internal::__is_device_execution_policy<__decay_t<_ExecutionPolicy>>::value &&
     !__is_radix_sort_usable_for_type<oneapi::dpl::__internal::__key_t<_Proj, _Range>, _Compare>::value, int> = 0>
 auto
-__parallel_stable_sort(_ExecutionPolicy&& __exec, _Range&& __rng, _Compare __comp, _Proj /*__proj*/)
+__parallel_stable_sort(_ExecutionPolicy&& __exec, _Range&& __rng, _Compare __comp, _Proj __proj)
 {
+    auto __cmp_f = [__comp, __proj](const auto& __a, const auto& __b) { return __comp(__proj(__a), __proj(__b));};
     return __parallel_sort_impl(::std::forward<_ExecutionPolicy>(__exec), ::std::forward<_Range>(__rng),
                                 // Pass special tag to choose 'full' merge subroutine at compile-time
-                                __full_merge_kernel(), __comp);
+                                __full_merge_kernel(), __cmp_f);
 }
 
 //------------------------------------------------------------------------
