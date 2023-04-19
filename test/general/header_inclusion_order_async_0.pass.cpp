acd2c6c6c435c3f0010f87ea728ba846f9ec4444
Sergey Kopienko <sergey.kopienko@intel.com>
2023-04-19 16:37:37 +0200

    Fix review comment: remove value = value;

--- test/general/header_inclusion_order_async_0.pass.cpp
+++ test/general/header_inclusion_order_async_0.pass.cpp
@@ -37,9 +37,7 @@ main()
     q.fill<T>(v, 1, n).wait();
 
     auto f = oneapi::dpl::experimental::reduce_async(my_policy, v, v + n, T(0), std::plus());
-
-    T value = f.get();
-    value = value;
+    f.get();
 
     sycl::free(v, q);
 
