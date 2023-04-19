c183cba647a7e30d22a3130b339646f5df8b7615
Sergey Kopienko <sergey.kopienko@intel.com>
2023-04-19 09:29:31 +0200

    Add new tests for check inclusion of async header

---
+++ test/general/header_inclusion_order_async_1.pass.cpp
@@ -0,0 +1,27 @@
+// -*- C++ -*-
+//===----------------------------------------------------------------------===//
+//
+// Copyright (C) Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// This file incorporates work covered by the following copyright and permission
+// notice:
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+//
+//===----------------------------------------------------------------------===//
+
+#include "support/test_config.h"
+
+#include _PSTL_TEST_HEADER(async)
+#include _PSTL_TEST_HEADER(execution)
+
+#include "support/utils.h"
+
+int
+main()
+{
+    return TestUtils::done();
+}
