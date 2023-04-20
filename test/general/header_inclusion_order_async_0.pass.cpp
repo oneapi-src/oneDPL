ae21f42752603ee53ad1f3ebcaa726c976bd6ee5
Sergey Kopienko <sergey.kopienko@intel.com>
2023-04-20 11:42:40 +0200

    Fix review comment: remove "Part of the LLVM Project ..."

--- test/general/header_inclusion_order_async_0.pass.cpp
+++ test/general/header_inclusion_order_async_0.pass.cpp
@@ -8,9 +8,6 @@
 // This file incorporates work covered by the following copyright and permission
 // notice:
 //
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-//
 //===----------------------------------------------------------------------===//
 
 #include "support/test_config.h"
