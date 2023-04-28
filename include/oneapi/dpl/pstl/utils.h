d060bdb1e677c44ac68499c3fd761e9ce34a631b
Benjamin Brock <benjamin.brock@intel.com>
2023-04-28 14:49:06 -0700

    Revert "Update to handle `<bit>` consistently."
    This reverts commit 51b2211657ac3602b11239b21959327568873df9.

--- include/oneapi/dpl/pstl/utils.h
+++ include/oneapi/dpl/pstl/utils.h
@@ -30,11 +30,9 @@
 #    include "hetero/dpcpp/sycl_iterator.h"
 #endif
 
-#if __has_include(<bit>)
+#if __cpp_lib_bit_cast >= 201806L && __has_include(<bit>)
 #    include <bit>
-#endif
-
-#if !(__cpp_lib_bit_cast >= 201806L)
+#else
 #    ifndef __has_builtin
 #        define __has_builtin(__x) 0
 #    endif
@@ -586,10 +584,10 @@ struct __lifetime_keeper : public __lifetime_keeper_base
 // Bitwise type casting, same as C++20 std::bit_cast
 template <typename _Dst, typename _Src>
 ::std::enable_if_t<
-    sizeof(_Dst) == sizeof(_Src) && ::std::is_trivially_copyable_v<_Dst>&& ::std::is_trivially_copyable_v<_Src>, _Dst>
+    sizeof(_Dst) == sizeof(_Src) && ::std::is_trivially_copyable_v<_Dst> && ::std::is_trivially_copyable_v<_Src>, _Dst>
 __dpl_bit_cast(const _Src& __src) noexcept
 {
-#if __cpp_lib_bit_cast >= 201806L
+#if __cpp_lib_bit_cast >= 201806L && __has_include(<bit>)
     return ::std::bit_cast<_Dst>(__src);
 #elif _ONEDPL_BACKEND_SYCL && _ONEDPL_LIBSYCL_VERSION >= 50300
     return sycl::bit_cast<_Dst>(__src);
@@ -604,12 +602,12 @@ __dpl_bit_cast(const _Src& __src) noexcept
 
 // The max power of 2 not exceeding the given value, same as C++20 std::bit_floor
 template <typename _T>
-::std::enable_if_t<::std::is_integral_v<_T>&& ::std::is_unsigned_v<_T>, _T>
+::std::enable_if_t<::std::is_integral_v<_T> && ::std::is_unsigned_v<_T>, _T>
 __dpl_bit_floor(_T __x) noexcept
 {
     if (__x == 0)
         return 0;
-#if __cpp_lib_int_pow2 >= 202002L
+#if __cpp_lib_int_pow2 >= 202002L || _ONEDPL___cplusplus >= 202002L && __has_include(<bit>)
     return ::std::bit_floor(__x);
 #elif _ONEDPL_BACKEND_SYCL
     // Use the count-leading-zeros function
@@ -619,12 +617,9 @@ __dpl_bit_floor(_T __x) noexcept
     __x |= (__x >> 1);
     __x |= (__x >> 2);
     __x |= (__x >> 4);
-    if constexpr (sizeof(_T) > 1)
-        __x |= (__x >> 8);
-    if constexpr (sizeof(_T) > 2)
-        __x |= (__x >> 16);
-    if constexpr (sizeof(_T) > 4)
-        __x |= (__x >> 32);
+    if constexpr (sizeof(_T) > 1) __x |= (__x >> 8);
+    if constexpr (sizeof(_T) > 2) __x |= (__x >> 16);
+    if constexpr (sizeof(_T) > 4) __x |= (__x >> 32);
     __x += 1; // Now it equals to the next greater power of 2, or 0 in case of wraparound
     return (__x == 0) ? 1 << (sizeof(_T) * CHAR_BIT - 1) : __x >> 1;
 #endif
