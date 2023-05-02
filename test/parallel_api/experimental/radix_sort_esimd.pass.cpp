b5ca1f35cea0831245ae163c80bcc2111db15562
Sergey Kopienko <sergey.kopienko@intel.com>
2023-05-02 12:55:39 +0200

    test/parallel_api/experimental/radix_sort_esimd.pass.cpp - <256,16> -> <kWorkGroupSize, kDataPerWorkItem>

--- test/parallel_api/experimental/radix_sort_esimd.pass.cpp
+++ test/parallel_api/experimental/radix_sort_esimd.pass.cpp
@@ -34,6 +34,9 @@
 #include <string>
 #include <iostream>
 
+constexpr std::uint16_t kWorkGroupSize = 256;
+constexpr std::uint16_t kDataPerWorkItem = 16;
+
 template <typename T>
 typename ::std::enable_if_t<std::is_arithmetic_v<T>, void>
 generate_data(T* input, std::size_t size)
@@ -70,7 +73,7 @@ void test_all_view(std::size_t size)
     {
         sycl::buffer<T> buf(input.data(), input.size());
         oneapi::dpl::experimental::ranges::all_view<T, sycl::access::mode::read_write> view(buf);
-        oneapi::dpl::experimental::esimd::radix_sort<256,16>(policy, view);
+        oneapi::dpl::experimental::esimd::radix_sort<kWorkGroupSize, kDataPerWorkItem>(policy, view);
     }
 
     std::string msg = "wrong results with all_view, n: " + std::to_string(size);
@@ -90,7 +93,7 @@ void test_subrange_view(std::size_t size)
     std::sort(expected, expected + size);
 
     oneapi::dpl::experimental::ranges::views::subrange view(input, input + size);
-    oneapi::dpl::experimental::esimd::radix_sort<256,16>(policy, view);
+    oneapi::dpl::experimental::esimd::radix_sort<kWorkGroupSize, kDataPerWorkItem>(policy, view);
 
     T* host_input = sycl::malloc_host<T>(size, q);
     q.copy(input, host_input, size).wait();
@@ -114,7 +117,7 @@ void test_usm(std::size_t size)
     generate_data(expected, size);
     q.copy(expected, input, size).wait();
     std::sort(expected, expected + size);
-    oneapi::dpl::experimental::esimd::radix_sort<256,16>(policy, input, input + size);
+    oneapi::dpl::experimental::esimd::radix_sort<kWorkGroupSize, kDataPerWorkItem>(policy, input, input + size);
 
     T* host_input = sycl::malloc_host<T>(size, q);
     q.copy(input, host_input, size).wait();
@@ -138,7 +141,8 @@ void test_sycl_iterators(std::size_t size)
     std::vector<T> expected(input);
     std::sort(std::begin(expected), std::end(expected));
 
-    oneapi::dpl::experimental::esimd::radix_sort<256,16>(policy, oneapi::dpl::begin(input), oneapi::dpl::end(input));
+    oneapi::dpl::experimental::esimd::radix_sort<kWorkGroupSize, kDataPerWorkItem>(policy, oneapi::dpl::begin(input),
+                                                                                   oneapi::dpl::end(input));
 
     std::string msg = "wrong results with sycl_iterator, n: " + std::to_string(size);
     EXPECT_EQ_RANGES(expected, input, msg.c_str());
@@ -153,9 +157,11 @@ void test_small_sizes()
     generate_data(input.data(), input.size());
     std::vector<uint32_t> expected(input);
 
-    oneapi::dpl::experimental::esimd::radix_sort<256,16>(policy, oneapi::dpl::begin(input), oneapi::dpl::begin(input));
+    oneapi::dpl::experimental::esimd::radix_sort<kWorkGroupSize, kDataPerWorkItem>(policy, oneapi::dpl::begin(input),
+                                                                                   oneapi::dpl::begin(input));
     EXPECT_EQ_RANGES(expected, input, "sort modified input data when size == 0");
-    oneapi::dpl::experimental::esimd::radix_sort<256,16>(policy, oneapi::dpl::begin(input), oneapi::dpl::begin(input) + 1);
+    oneapi::dpl::experimental::esimd::radix_sort<kWorkGroupSize, kDataPerWorkItem>(policy, oneapi::dpl::begin(input),
+                                                                                   oneapi::dpl::begin(input) + 1);
     EXPECT_EQ_RANGES(expected, input, "sort modified input data when size == 1");
 }
 
