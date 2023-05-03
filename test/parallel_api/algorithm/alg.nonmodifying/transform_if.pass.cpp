2e40f847e3c892400ccb980dc0f48d4783a921f6
Dan Hoeflinger <dan.hoeflinger@intel.com>
2023-05-03 09:08:40 -0400

    testing update for refactored / new APIs
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- test/parallel_api/algorithm/alg.nonmodifying/transform_if.pass.cpp
+++ test/parallel_api/algorithm/alg.nonmodifying/transform_if.pass.cpp
@@ -22,7 +22,7 @@
 
 using namespace TestUtils;
 
-struct test_transform_if
+struct test_transform_if_binary
 {
     template <typename It1, typename It2, typename Out, typename Size>
     bool
@@ -74,11 +74,44 @@ struct test_transform_if
     operator()(Policy&& exec, InputIterator1 first, InputIterator1 last, InputIterator2 mask,
                InputIterator2 /* mask_end */, OutputIterator result, OutputIterator result_end, Size n)
     {
-
         // call transform_if
-        transform_if(exec, first, last, mask, result, std::negate<int>(), oneapi::dpl::identity());
+        oneapi::dpl::transform_if(exec, first, last, mask, result, [](const auto& a, const auto& b){return -a;}, [](const auto& a, const auto& b){return b==1;});
+        
+        EXPECT_TRUE(check(first, last, mask, result, n), "transform_if binary wrong result");
+        // reset output elements to 0
+        ::std::fill(result, result_end, 0);
+    }
+};
 
-        EXPECT_TRUE(check(first, last, mask, result, n), "transform_if wrong result");
+struct test_transform_if_unary
+{
+    template <typename It1, typename Out, typename Size>
+    bool
+    check(It1 first, It1 last, Out result, Size n)
+    {
+
+        for (; first != last; ++first, ++result)
+        {
+            if (*first % 3 == 0 && *result != -(*first))
+            {
+                return false;
+            }
+            else if (*first % 3 != 0 && *result != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    template <typename Policy, typename InputIterator1, typename OutputIterator, typename Size>
+    void
+    operator()(Policy&& exec, InputIterator1 first, InputIterator1 last, OutputIterator result, OutputIterator result_end, Size n)
+    {
+        // call transform_if
+        oneapi::dpl::transform_if(exec, first, last, result, ::std::negate<typename ::std::iterator_traits<InputIterator1>::value_type>(), [](const auto& a){return a % 3 == 0;});
+
+        EXPECT_TRUE(check(first, last, result, n), "transform_if unary wrong result");
         // reset output elements to 0
         ::std::fill(result, result_end, 0);
     }
@@ -90,22 +123,36 @@ test()
 {
     for (size_t n = 1; n <= 100000; n = n <= 16 ? n + 1 : size_t(3.1415 * n))
     {
+        {
         Sequence<In1> in1(n, [=](size_t k) { return (3 * k); });
         Sequence<In2> in2(n, [=](size_t k) { return k % 2 == 0 ? 1 : 0; });
 
         Sequence<Out> out(n, [=](size_t) { return 0; });
 
-        invoke_on_all_policies<0>()(test_transform_if(), in1.begin(), in1.end(), in2.begin(), in2.end(), out.begin(),
+            invoke_on_all_policies<0>()(test_transform_if_binary(), in1.begin(), in1.end(), in2.begin(), in2.end(), out.begin(),
                                     out.end(), n);
-        invoke_on_all_policies<1>()(test_transform_if(), in1.cbegin(), in1.cend(), in2.cbegin(), in2.cend(),
+            invoke_on_all_policies<1>()(test_transform_if_binary(), in1.cbegin(), in1.cend(), in2.cbegin(), in2.cend(),
                                     out.begin(), out.end(), n);
+        }
+        {
+            Sequence<In1> in1(n, [=](size_t k) { return k; });
+            Sequence<Out> out(n, [=](size_t) { return 0; });
+
+
+            invoke_on_all_policies<0>()(test_transform_if_unary(), in1.begin(), in1.end(), out.begin(),
+                                        out.end(), n);
+            invoke_on_all_policies<1>()(test_transform_if_unary(), in1.cbegin(), in1.cend(),
+                                        out.begin(), out.end(), n);
+        }
+
     }
 }
 
 int
 main()
 {
-    test<int32_t, int32_t, int32_t>();
+    test<::std::int32_t, ::std::int32_t, ::std::int32_t>();
+    test<::std::int64_t, ::std::int64_t, ::std::int64_t>();
 
     return done();
 }
