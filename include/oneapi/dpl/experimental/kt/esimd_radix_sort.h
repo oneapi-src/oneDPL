2cb9f271fde08a8917c74da0b87c7cd42a6f2827
Dmitriy Sobolev <dmitriy.sobolev@intel.com>
2023-05-11 11:08:02 -0500

    Add new onesweep implementation
    Signed-off-by: Dmitriy Sobolev <dmitriy.sobolev@intel.com>

--- include/oneapi/dpl/experimental/kt/esimd_radix_sort.h
+++ include/oneapi/dpl/experimental/kt/esimd_radix_sort.h
@@ -75,7 +75,7 @@ radix_sort(_ExecutionPolicy&& __exec, _Range&& __rng)
         // TODO: allow different RadixBits, make sure the data is in the input storage after the last stage
         // TODO: pass _ProcessSize according to __n
         // TODO: fix when compiled in -O0 mode: "esimd_radix_sort_one_wg.h : 54 : 5>: SLM init call is supported only in kernels"
-        oneapi::dpl::experimental::esimd::impl::onesweep<_ExecutionPolicy, _KeyT, _Range, RadixBits, IsAscending, /*_ProcessSize*/ 512>(
+        oneapi::dpl::experimental::esimd::impl::onesweep<_ExecutionPolicy, _KeyT, _Range, RadixBits, IsAscending, /*_ProcessSize*/ 416>(
             ::std::forward<_ExecutionPolicy>(__exec), ::std::forward<_Range>(__rng), __n);
     }
 }
