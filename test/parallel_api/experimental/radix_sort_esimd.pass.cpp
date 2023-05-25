5ece17e348d62f0637bc736108463726b22695dc
Sergey Kopienko <sergey.kopienko@intel.com>
2023-05-25 14:30:32 +0200

    [KT] Add support on float type into onesweep esimd radix sort (#961)

--- test/parallel_api/experimental/radix_sort_esimd.pass.cpp
+++ test/parallel_api/experimental/radix_sort_esimd.pass.cpp
@@ -44,9 +44,51 @@ struct Compare : public std::less<void> {};
 template <>
 struct Compare<!Ascending> : public std::greater<void> {};
 
+//#define LOG_TEST_INFO
+
 constexpr ::std::uint16_t kWorkGroupSize = 256;
 constexpr ::std::uint16_t kDataPerWorkItem = 16;
 constexpr bool Order = Ascending;
+
+#ifdef LOG_TEST_INFO
+struct TypeInfo
+{
+    template <typename T>
+    const std::string& name()
+    {
+        static const std::string kTypeName = "unknown type name";
+        return kTypeName;
+    }
+
+    template <>
+    const std::string& name<uint32_t>()
+    {
+        static const std::string kTypeName = "uint32_t";
+        return kTypeName;
+    }
+
+    template <>
+    const std::string& name<int>()
+    {
+        static const std::string kTypeName = "int";
+        return kTypeName;
+    }
+
+    template <>
+    const std::string& name<float>()
+    {
+        static const std::string kTypeName = "float";
+        return kTypeName;
+    }
+
+    template <>
+    const std::string& name<double>()
+    {
+        static const std::string kTypeName = "double";
+        return kTypeName;
+    }
+};
+#endif // LOG_TEST_INFO
 
 template <typename T>
 typename ::std::enable_if_t<std::is_arithmetic_v<T>, void>
@@ -122,13 +164,17 @@ void test_all_view(std::size_t size)
 template<typename T>
 void test_subrange_view(std::size_t size)
 {
+#ifdef LOG_TEST_INFO
+    std::cout << "\ttest_subrange_view(" << size << ") : " << TypeInfo().name<T>() << std::endl;
+#endif
+
     sycl::queue q = TestUtils::get_test_queue();
     auto policy = oneapi::dpl::execution::make_device_policy(q);
 
     std::vector<T> expected(size);
     generate_data(expected.data(), size);
 
-    TestUtils::usm_data_transfer<sycl::usm::alloc::shared, T> dt_input(q, expected.begin(), expected.end());
+    TestUtils::usm_data_transfer<sycl::usm::alloc::device, T> dt_input(q, expected.begin(), expected.end());
 
     std::sort(expected.begin(), expected.end(), Compare<Order>{});
 
@@ -141,6 +187,7 @@ void test_subrange_view(std::size_t size)
     std::string msg = "wrong results with views::subrange, n: " + std::to_string(size);
     EXPECT_EQ_N(expected.begin(), actual.begin(), size, msg.c_str());
 }
+
 #endif // _ENABLE_RANGES_TESTING
 
 template<typename T, sycl::usm::alloc _alloc_type>
@@ -165,9 +212,32 @@ void test_usm(std::size_t size)
     EXPECT_EQ_N(expected.begin(), actual.begin(), size, msg.c_str());
 }
 
+template <typename T>
+void
+test_usm(std::size_t size)
+{
+#ifdef LOG_TEST_INFO
+    std::cout << "\ttest_usm(std::size_t size) : " << TypeInfo().name<T>() << std::endl;
+#endif
+
+#ifdef LOG_TEST_INFO
+    std::cout << "\t\ttest_usm<T, sycl::usm::alloc::shared>(" << size << ");" << std::endl;
+#endif
+    test_usm<T, sycl::usm::alloc::shared>(size);
+
+#ifdef LOG_TEST_INFO
+    std::cout << "\t\ttest_usm<T, sycl::usm::alloc::device>(" << size << ");" << std::endl;
+#endif
+    test_usm<T, sycl::usm::alloc::device>(size);
+}
+
 template<typename T>
 void test_sycl_iterators(std::size_t size)
 {
+#ifdef LOG_TEST_INFO
+    std::cout << "\t\ttest_sycl_iterators<" << TypeInfo().name<T>() << ">(" << size << ");" << std::endl;
+#endif
+
     sycl::queue q = TestUtils::get_test_queue();
     auto policy = oneapi::dpl::execution::make_device_policy(q);
 
@@ -208,8 +278,7 @@ void test_general_cases(std::size_t size)
     test_all_view<T>(size);
     test_subrange_view<T>(size);
 #endif // _ENABLE_RANGES_TESTING
-    test_usm<T, sycl::usm::alloc::shared>(size);
-    test_usm<T, sycl::usm::alloc::device>(size);
+    test_usm<T>(size);
     test_sycl_iterators<T>(size);
 }
 #endif // TEST_DPCPP_BACKEND_PRESENT
@@ -239,11 +308,9 @@ int main()
         }
         for(auto size: onesweep_sizes)
         {
-            test_usm<uint32_t, sycl::usm::alloc::shared>(size);
-            test_usm<uint32_t, sycl::usm::alloc::device>(size);
-
-            test_usm<int, sycl::usm::alloc::shared>(size);
-            test_usm<int, sycl::usm::alloc::device>(size);
+            test_usm<uint32_t>(size);
+            test_usm<int>(size);
+            test_usm<float>(size);
         }
         test_small_sizes();
     }
