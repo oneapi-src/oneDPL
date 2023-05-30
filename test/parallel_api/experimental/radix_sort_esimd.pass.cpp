70950bf243329642628e05e10e7df4690425eee3
Sergey Kopienko <sergey.kopienko@intel.com>
2023-05-30 13:34:27 +0200

    @@@ test/parallel_api/experimental/radix_sort_esimd.pass.cpp - #define LOG_TEST_INFO 1

--- test/parallel_api/experimental/radix_sort_esimd.pass.cpp
+++ test/parallel_api/experimental/radix_sort_esimd.pass.cpp
@@ -38,9 +38,9 @@
 #include <cmath>
 #include <limits>
 
-#ifndef LOG_TEST_INFO
-#define LOG_TEST_INFO 0
-#endif
+//#ifndef LOG_TEST_INFO
+#define LOG_TEST_INFO 1
+//#endif
 
 #ifndef TEST_ALL_INPUTS
 #define TEST_ALL_INPUTS 0
