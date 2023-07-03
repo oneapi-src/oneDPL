fcae637a3c57fdbd1ec036fc13a12a66070681f0
Dmitriy Sobolev <Dmitriy.Sobolev@intel.com>
2023-07-03 04:09:07 -0500

    [KT] Enable support of 8-bit types in onesweep (#1007)
    * [KT] Enable support of 8-bit types in onesweep
    
    Make sure sorted data is in the input range upon work completion
    
    ---------
    
    Signed-off-by: Dmitriy Sobolev <dmitriy.sobolev@intel.com>

--- include/oneapi/dpl/experimental/kt/esimd_radix_sort.h
+++ include/oneapi/dpl/experimental/kt/esimd_radix_sort.h
@@ -58,25 +58,22 @@ radix_sort(_ExecutionPolicy&& __exec, _Range&& __rng)
 
     if (__n <= 16384)
     {
-        // TODO: enable support of char, uint8_t, int8_t types
         // TODO: support different RadixBits values (only 7 or 8 are currently supported), WorkGroupSize and DataPerWorkItem
         oneapi::dpl::experimental::esimd::impl::one_wg<_KernelName, _KeyT, _Range, RadixBits, IsAscending>(
             __exec.queue(), ::std::forward<_Range>(__rng), __n);
     }
     else if (__n <= 262144)
     {
-        // TODO: enable support of char, uint8_t, int8_t types
         // TODO: support different RadixBits, WorkGroupSize and DataPerWorkItem
         oneapi::dpl::experimental::esimd::impl::cooperative<_KernelName, _KeyT, _Range, RadixBits, IsAscending>(
             __exec.queue(), ::std::forward<_Range>(__rng), __n);
     }
     else
     {
-        // TODO: enable support of double, char, uint8_t, int8_t types
+        // TODO: enable support of double type
         // TODO: avoid kernel duplication (generate the output storage with the same type as input storage and use swap)
-        // TODO: support different RadixBits, make sure the data is in the input storage after the last stage
         // TODO: pass _ProcessSize according to DataPerWorkItem
-        // TODO: support different WorkGroupSize
+        // TODO: support different WorkGroupSize, RadixBits
         oneapi::dpl::experimental::esimd::impl::onesweep<_KernelName, _KeyT, _Range, RadixBits, IsAscending, /*_ProcessSize*/384>(
             __exec.queue(), ::std::forward<_Range>(__rng), __n);
     }
