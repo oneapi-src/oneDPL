6419f616660553e48af1137c325c8542d650a19a
Sergey Kopienko <sergey.kopienko@intel.com>
2023-08-02 09:57:53 +0200

    #define TEST_DATA_TYPE int8_t

--- test/parallel_api/experimental/radix_sort_esimd.pass.cpp
+++ test/parallel_api/experimental/radix_sort_esimd.pass.cpp
@@ -68,7 +68,7 @@ using DataPerWorkItems = ::std::initializer_list<DPWI>;
 #   define DataPerWorkItemsShortRun     64,     128,      192,      256,                     416,           512
 #endif // TEST_DPWI
 
-#define TEST_DATA_TYPE char
+#define TEST_DATA_TYPE int8_t
 
 // Test dimension 2 : types
 #ifdef TEST_DATA_TYPE
