b22b94969cf1b7b71e45a4021938fc055eede2c0
Sergey Kopienko <sergey.kopienko@intel.com>
2023-08-03 16:23:41 +0200

    test_usm_runner : skip  errors for int, uint32_t

--- test/parallel_api/experimental/radix_sort_esimd.pass.cpp
+++ test/parallel_api/experimental/radix_sort_esimd.pass.cpp
@@ -641,6 +641,7 @@ struct test_usm_runner
                 check_dpwi_size<192,    16385>(dpwi, size) ||      // int, 192 : size    16385
                 check_dpwi_size<192,    50000>(dpwi, size) ||      // int, 192 : size    50000
                 check_dpwi_size<192,    67543>(dpwi, size) ||      // int, 192 : size    67543
+                check_dpwi_size<192,   100000>(dpwi, size) ||      // int, 192 : size   100000
                 check_dpwi_size<256,    16385>(dpwi, size) ||      // int, 256 : size    16385
                 check_dpwi_size<256,    50000>(dpwi, size) ||      // int, 256 : size    50000
                 check_dpwi_size<416,    16385>(dpwi, size) ||      // int, 416 : size    16385
