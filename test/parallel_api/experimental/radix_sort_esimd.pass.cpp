7ffed119fcb36594dfee8c0a700d7de14046b363
Sergey Kopienko <sergey.kopienko@intel.com>
2023-08-08 11:16:10 +0200

    #define TEST_DPWI 32, 64, 96, 128, 160

--- test/parallel_api/experimental/radix_sort_esimd.pass.cpp
+++ test/parallel_api/experimental/radix_sort_esimd.pass.cpp
@@ -63,9 +63,9 @@ constexpr ::std::uint16_t WorkGroupSize = 256;
 using DPWI = ::std::uint16_t;
 using DataPerWorkItems = ::std::initializer_list<DPWI>;
 
-//#define TEST_DPWI 32, 64, 96, 128, 160
+#define TEST_DPWI 32, 64, 96, 128, 160
 //#define TEST_DPWI 192, 224, 256, 288, 320
-#define TEST_DPWI  352, 384, 416, 448, 480, 512
+//#define TEST_DPWI  352, 384, 416, 448, 480, 512
 
 #ifdef TEST_DPWI
 #   define DataPerWorkItemsLongRun  TEST_DPWI
