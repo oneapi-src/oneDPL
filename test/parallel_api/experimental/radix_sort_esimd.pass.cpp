400cab821d5ff48de7371b05a4bae582102d9a68
Sergey Kopienko <sergey.kopienko@intel.com>
2023-08-08 11:15:44 +0200

    #define TEST_DATA_TYPE double

--- test/parallel_api/experimental/radix_sort_esimd.pass.cpp
+++ test/parallel_api/experimental/radix_sort_esimd.pass.cpp
@@ -76,7 +76,7 @@ using DataPerWorkItems = ::std::initializer_list<DPWI>;
 #   define DataPerWorkItemsShortRun 32, 64, 96, 128, 160, 192, 224, 256, 288, 320, 352, 384, 416, 448, 480, 512
 #endif // TEST_DPWI
 
-#define TEST_DATA_TYPE float
+#define TEST_DATA_TYPE double
 
 // Test dimension 2 : types
 #ifdef TEST_DATA_TYPE
