cc2be2a88e8db78a10bdfe3fedfca1a875779b08
Sergey Kopienko <sergey.kopienko@intel.com>
2023-08-09 16:26:37 +0200

    test/parallel_api/experimental/future_impl.pass.cpp - add test for __future instance lifetime

--- test/parallel_api/experimental/future_impl.pass.cpp
+++ test/parallel_api/experimental/future_impl.pass.cpp
@@ -24,11 +24,23 @@
 
 #include "support/utils.h"
 
+// Required to comment next line for reproducing error case
+#define CALL_WAIT_AND_THROW_IN_DESTRUCTOR 1
 
 struct UserEvent
 {
+    inline static long wait_counter = 0;
+
+    ~UserEvent()
+    {
+#if CALL_WAIT_AND_THROW_IN_DESTRUCTOR
+        wait_and_throw();
+#endif // CALL_WAIT_AND_THROW_IN_DESTRUCTOR
+    }
+
     void wait_and_throw()
     {
+        ++wait_counter;
     }
 };
 
@@ -45,23 +57,36 @@ main()
 
     long counter_state = 0;
 
+    EXPECT_TRUE(UserEvent::wait_counter == 0, "wrong wait counter state #1");
+
     // Case 1: we save future in result
     {
         auto ptr = std::make_shared<int>();
+        long wait_counter_state = 0;
+
         {
-            auto res = create_future(UserEvent{}, ptr);
+            UserEvent ev;
+            auto res = create_future(ev, ptr);
+            wait_counter_state = UserEvent::wait_counter;
+
             counter_state = ptr.use_count();
             EXPECT_TRUE(counter_state == 2, "wrong counter state #1");
         }
 
-        EXPECT_TRUE(ptr.use_count() == 1, "wrong counter state #2");
+        EXPECT_TRUE(counter_state == 2, "wrong counter state #1");
+
+        EXPECT_TRUE(UserEvent::wait_counter != wait_counter_state, "wrong counter state #2");
     }
 
     // Case 2: we don't save the result
     {
         auto ptr = std::make_shared<int>();
+        long wait_counter_state = 0;
         {
+            UserEvent ev;
             /*auto res = */create_future(UserEvent{}, ptr);
+            wait_counter_state = UserEvent::wait_counter;
+
             EXPECT_TRUE(ptr.use_count() != counter_state, "wrong counter state #3");
 
             // This means that managed resources already was destroyed because the instance of __future class wasn't saved.
@@ -72,6 +97,8 @@ main()
         }
 
         EXPECT_TRUE(ptr.use_count() == 1, "wrong counter state #4");
+
+        EXPECT_TRUE(UserEvent::wait_counter != wait_counter_state, "wrong counter state #3");
     }
 #endif
 
