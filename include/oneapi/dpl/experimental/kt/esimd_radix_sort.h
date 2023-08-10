11f1d5129884561e9a3e8a702e8714393e8493c8
Sergey Kopienko <sergey.kopienko@intel.com>
2023-08-10 11:41:53 +0200

    include/oneapi/dpl/experimental/kt/esimd_radix_sort.h - add static assert for _RadixBits template param state: (only 7 or 8 are currently supported)

--- include/oneapi/dpl/experimental/kt/esimd_radix_sort.h
+++ include/oneapi/dpl/experimental/kt/esimd_radix_sort.h
@@ -31,6 +31,8 @@ template <bool _IsAscending, std::uint8_t _RadixBits, typename _KernelParam, typ
 sycl::event
 radix_sort(sycl::queue __q, _Range&& __rng, _KernelParam __param)
 {
+    static_assert(_RadixBits == 7 || _RadixBits == 8);
+
     const ::std::size_t __n = __rng.size();
     assert(__n > 1);
 
