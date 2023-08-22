97c34126eae43b989457d4d37a4947dcf480ce2a
egfefey <elvis.g.fefey@intel.com>
2023-08-21 21:50:34 -0500

    Add files via upload
    Updating the static policy file

--- include/oneapi/dpl/internal/dynamic_selection_impl/static_policy_impl.h
+++ include/oneapi/dpl/internal/dynamic_selection_impl/static_policy_impl.h
@@ -17,55 +17,59 @@ namespace dpl {
 namespace experimental {
 
   template <typename Backend>
-  struct static_policy_impl {
+  struct fixed_resource_policy_impl {
     using backend_t = Backend;
     using resource_container_t = typename backend_t::resource_container_t;
     using execution_resource_t = typename backend_t::execution_resource_t;
 
     //policy traits
     using resource_type = typename backend_t::resource_type;
-    using selection_type = oneapi::dpl::experimental::basic_selection_handle_t<static_policy_impl<Backend>, execution_resource_t>;
+    using selection_type = oneapi::dpl::experimental::basic_selection_handle_t<fixed_resource_policy_impl<Backend>, execution_resource_t>;
     using wait_type = typename backend_t::wait_type;
     std::shared_ptr<backend_t> backend_;
 
-
-    struct unit_t{
+    struct state_t {
         resource_container_t resources_;
+        int offset_;
     };
 
-    std::shared_ptr<unit_t> unit_;
+    std::shared_ptr<state_t> state_;
 
-    static_policy_impl() : backend_{std::make_shared<backend_t>()}, unit_{std::make_shared<unit_t>()}  {
-      unit_->resources_ = get_resources();
+    fixed_resource_policy_impl(int offset) : backend_{std::make_shared<backend_t>()}, state_{std::make_shared<state_t>()}  {
+      state_->resources_ = get_resources();
+      state_->offset_ = offset;
     }
 
-    static_policy_impl(resource_container_t u) : backend_{std::make_shared<backend_t>()}, unit_{std::make_shared<unit_t>()} {
+    fixed_resource_policy_impl(resource_container_t u, int offset) : backend_{std::make_shared<backend_t>()}, state_{std::make_shared<state_t>()} {
       backend_->initialize(u);
-      unit_->resources_ = get_resources();
+      state_->resources_ = get_resources();
+      state_->offset_ = offset;
     }
 
     template<typename ...Args>
-    static_policy_impl(Args&&... args) : backend_{std::make_shared<backend_t>(std::forward<Args>(args)...)}, unit_{std::make_shared<unit_t>()} {
-      unit_->resources_ = get_resources();
+    fixed_resource_policy_impl(Args&&... args) : backend_{std::make_shared<backend_t>(std::forward<Args>(args)...)}, state_{std::make_shared<state_t>()} {
+      state_->resources_ = get_resources();
     }
-
-    //
-    // Support for property queries
-    //
 
     auto get_resources()  const {
       return backend_->get_resources();
     }
 
-    template<typename ...Args>
-    auto initialize(Args&&... args) {
-        return backend_->initialize(std::forward<Args>(args)...);
+    void initialize(int offset=0) {
+      if (offset == deferred_initialization) return;
+      state_->offset_ = offset;
+      backend_->initialize();
+    }
+
+    void initialize(resource_container_t u, int offset=0) {
+      state_->offset_ = offset;
+      backend_->initialize(u);
     }
 
     template<typename ...Args>
     selection_type select(Args&&...) {
-      if(!unit_->resources_.empty()) {
-          return selection_type{*this, unit_->resources_[0]};
+      if(!state_->resources_.empty()) {
+          return selection_type{*this, state_->resources_[state_->offset_]};
       }
       return selection_type{*this};
     }
