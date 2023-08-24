38f27aa4e46c8ce970e359be79eb6bf8495e9bdf
egfefey <elvis.g.fefey@intel.com>
2023-08-24 15:25:21 -0500

    Add files via upload (#1100)

--- include/oneapi/dpl/internal/dynamic_selection_impl/round_robin_policy.h
+++ include/oneapi/dpl/internal/dynamic_selection_impl/round_robin_policy.h
@@ -11,6 +11,7 @@
 #define _ONEDPL_ROUND_ROBIN_POLICY_IMPL_H
 
 #include <atomic>
+#include <vector>
 #include "oneapi/dpl/internal/dynamic_selection_impl/scoring_policy_defs.h"
 #if _DS_BACKEND_SYCL != 0
     #include "oneapi/dpl/internal/dynamic_selection_impl/sycl_backend.h"
@@ -25,18 +26,21 @@ namespace experimental{
   template <typename Backend>
 #endif
   struct round_robin_policy {
+  private:
     using backend_t = Backend;
     using resource_container_t = typename backend_t::resource_container_t;
     using resource_container_size_t = typename resource_container_t::size_type;
-    using waiter_container_t = typename backend_t::waiter_container_t;
+    using wrapped_resource_t = typename std::decay<Backend>::type::execution_resource_t;
 
     using execution_resource_t = typename backend_t::execution_resource_t;
 
+  public:
     //Policy Traits
     using selection_type = oneapi::dpl::experimental::basic_selection_handle_t<round_robin_policy<Backend>, execution_resource_t>;
-    using resource_type = typename backend_t::resource_type;
+    using resource_type = decltype(unwrap(std::declval<wrapped_resource_t>()));
     using wait_type = typename backend_t::wait_type;
 
+  private:
     std::shared_ptr<backend_t> backend_;
 
     struct state_t{
@@ -48,6 +52,7 @@ namespace experimental{
 
     std::shared_ptr<state_t> state_;
 
+  public:
     round_robin_policy(int offset=0) : backend_{std::make_shared<backend_t>()}, state_{std::make_shared<state_t>()}  {
       state_->resources_ = get_resources();
       state_->num_contexts_ = state_->resources_.size();
@@ -55,19 +60,12 @@ namespace experimental{
       state_->next_context_ = state_->offset_;
     }
 
-    round_robin_policy(resource_container_t u, int offset=0) : backend_{std::make_shared<backend_t>()}, state_{std::make_shared<state_t>()}  {
+    round_robin_policy(const std::vector<resource_type>& u, int offset=0) : backend_{std::make_shared<backend_t>()}, state_{std::make_shared<state_t>()}  {
       backend_->initialize(u);
       state_->resources_ = get_resources();
       state_->num_contexts_ = state_->resources_.size();
       state_->offset_ = offset;
       state_->next_context_ = state_->offset_;
-    }
-
-    template<typename ...Args>
-    round_robin_policy(Args&&... args) : backend_{std::make_shared<backend_t>(std::forward<Args>(args)...)}, state_{std::make_shared<state_t>()} {
-      state_->resources_ = backend_->get_resources();
-      state_->num_contexts_ = state_->resources_.size();
-      state_->next_context_ = 0;
     }
 
     auto get_resources() const {
@@ -80,7 +78,7 @@ namespace experimental{
       backend_->initialize();
     }
 
-    void initialize(resource_container_t u, int offset=0) {
+    void initialize(const std::vector<resource_type>& u, int offset=0) {
       state_->offset_ = offset;
       backend_->initialize(u);
     }
@@ -119,6 +117,7 @@ namespace experimental{
     auto wait() {
       backend_->wait();
     }
+    
   };
 } // namespace experimental
 
