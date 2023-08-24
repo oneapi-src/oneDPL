f9e3e8b6e11a0febddaa503d4cdc291ee44ef5c2
Valentina Kats <valentina.kats@intel.com>
2023-08-24 16:33:46 +0200

    Update documentation version for oneDPL 2022.3.0 (#1027)
    * Update conf.py
    
    * Update conf.py

--- documentation/library_guide/conf.py
+++ documentation/library_guide/conf.py
@@ -37,7 +37,7 @@ copyright = 'Intel Corporation'
 author = 'Intel'
 
 # The full version, including alpha/beta/rc tags
-release = '2022.1.0'
+release = '2022.3.0'
 
 rst_epilog = """
 .. include:: /variables.txt
