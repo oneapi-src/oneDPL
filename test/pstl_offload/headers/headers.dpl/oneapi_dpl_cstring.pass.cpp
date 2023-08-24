d14a53d38aefc6e14d97eb2cbe843643586a0cb1
Boyarinov, Konstantin <konstantin.boyarinov@intel.com>
2023-08-24 08:24:02 -0700

    Add 2023 into copyrights in new files, remove LLVM project information from copyright

--- test/pstl_offload/headers/headers.dpl/oneapi_dpl_cstring.pass.cpp
+++ test/pstl_offload/headers/headers.dpl/oneapi_dpl_cstring.pass.cpp
@@ -1,15 +1,9 @@
 // -*- C++ -*-
-//===-- oneapi_dpl_cstring.pass.cpp ----------------------------------===//
+//===----------------------------------------------------------------------===//
 //
-// Copyright (C) Intel Corporation
+// Copyright (C) 2023 Intel Corporation
 //
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-// This file incorporates work covered by the following copyright and permission
-// notice:
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
 //
 //===----------------------------------------------------------------------===//
 
