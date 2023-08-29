d3150a87ed3104eb9d04367b9b4bee85a7ebbbf9
egfefey <elvis.g.fefey@intel.com>
2023-08-28 22:47:11 -0500

    Update round_robin_policy.h

--- include/oneapi/dpl/internal/dynamic_selection_impl/round_robin_policy.h
+++ include/oneapi/dpl/internal/dynamic_selection_impl/round_robin_policy.h
@@ -11,7 +11,8 @@
 #define _ONEDPL_ROUND_ROBIN_POLICY_IMPL_H
 
 #include <atomic>
-#include <vector>
+#include <type_traits>
+//#include <vector>
 #include "oneapi/dpl/internal/dynamic_selection_impl/scoring_policy_defs.h"
 #if _DS_BACKEND_SYCL != 0
     #include "oneapi/dpl/internal/dynamic_selection_impl/sycl_backend.h"
