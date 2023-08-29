2aeb7ff59d46d84d83dd9a8dd042e642b67ed0e7
egfefey <elvis.g.fefey@intel.com>
2023-08-29 00:08:17 -0500

    Update fixed_resource_policy.h

--- include/oneapi/dpl/internal/dynamic_selection_impl/fixed_resource_policy.h
+++ include/oneapi/dpl/internal/dynamic_selection_impl/fixed_resource_policy.h
@@ -11,6 +11,7 @@
 #define _ONEDPL_STATIC_POLICY_IMPL_H
 #include <vector>
 #include <type_traits>
+#include <memory>
 #include "oneapi/dpl/internal/dynamic_selection_impl/scoring_policy_defs.h"
 #if _DS_BACKEND_SYCL != 0
     #include "oneapi/dpl/internal/dynamic_selection_impl/sycl_backend.h"
