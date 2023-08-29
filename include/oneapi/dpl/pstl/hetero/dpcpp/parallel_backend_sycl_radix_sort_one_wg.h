636e59c7cbd0dc6c074079451fc4a9ca30ece77a
Matthew Michel <matthew.michel@intel.com>
2023-08-29 16:20:19 -0500

    Formatting
    Signed-off-by: Matthew Michel <matthew.michel@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_radix_sort_one_wg.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_radix_sort_one_wg.h
@@ -163,7 +163,7 @@ struct __subgroup_radix_sort
 #if !_ONEDPL_DETECT_NVPTX_COMPILATION
                         [[_ONEDPL_SYCL_REQD_SUB_GROUP_SIZE(__req_sub_group_size)]]
 #endif
-                        {
+                    {
                         union __storage { _ValT __v[__block_size]; __storage(){} } __values;
                         uint16_t __wi = __it.get_local_linear_id();
                         uint16_t __begin_bit = 0;
