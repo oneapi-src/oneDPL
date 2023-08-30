898dae114474798ff82291201d2d78837ef0430f
Anuya Welling <anuya.welling@intel.com>
2023-08-30 14:36:32 -0500

    Passing tests with offsets

--- test/parallel_api/dynamic_selection/sycl/test_fixed_resource_policy_sycl.pass.cpp
+++ test/parallel_api/dynamic_selection/sycl/test_fixed_resource_policy_sycl.pass.cpp
@@ -12,6 +12,20 @@
 #include "support/sycl_sanity.h"
 #include "support/test_dynamic_selection_utils.h"
 
+template<bool selection_option, typename Policy, typename UniverseContainer, typename ResourceFunction>
+int tests_with_offset(UniverseContainer u, ResourceFunction&& f, int universe_size) {
+    bool cond = false;
+    for(int offset=0;offset<universe_size;offset++){
+        std::cout<<"Condition : "<<selection_option<<" iteration : "<<offset<<"\n";
+        cond = cond
+               || test_submit_and_wait_on_event<selection_option, Policy>(u, f, offset)
+               || test_submit_and_wait<selection_option, Policy>(u, f, offset)
+               || test_submit_and_wait_on_group<selection_option, Policy>(u, f, offset);
+        if(cond==1) return 1;
+    }
+    return cond;
+}
+
 int main() {
   using policy_t = oneapi::dpl::experimental::fixed_resource_policy<oneapi::dpl::experimental::sycl_backend>;
   std::vector<sycl::queue> u;
@@ -21,24 +35,17 @@ int main() {
     std::cout << "PASS\n";
     return 0;
   }
+
   auto f = [u](int i, int offset=0) { return u[offset]; };
+
+  int n = u.size();
 
   constexpr bool just_call_submit = false;
   constexpr bool call_select_before_submit = true;
   if ( test_initialization<policy_t, sycl::queue>(u)
        || test_select<policy_t, decltype(u), decltype(f)&, false>(u, f)
-       || test_submit_and_wait_on_event<just_call_submit, policy_t>(u, f)
-       || test_submit_and_wait_on_event<just_call_submit, policy_t>(u, f, 1)
-       || test_submit_and_wait_on_event<call_select_before_submit, policy_t>(u, f)
-       || test_submit_and_wait_on_event<call_select_before_submit, policy_t>(u, f, 1)
-       || test_submit_and_wait<just_call_submit, policy_t>(u, f)
-       || test_submit_and_wait<just_call_submit, policy_t>(u, f, 1)
-       || test_submit_and_wait<call_select_before_submit, policy_t>(u, f)
-       || test_submit_and_wait<call_select_before_submit, policy_t>(u, f, 1)
-       || test_submit_and_wait_on_group<just_call_submit, policy_t>(u, f)
-       || test_submit_and_wait_on_group<just_call_submit, policy_t>(u, f, 1)
-       || test_submit_and_wait_on_group<call_select_before_submit, policy_t>(u, f)
-       || test_submit_and_wait_on_group<call_select_before_submit, policy_t>(u, f, 1)
+       || tests_with_offset<just_call_submit, policy_t>(u, f, n)
+       || tests_with_offset<call_select_before_submit, policy_t>(u, f, n)
      ) {
     std::cout << "FAIL\n";
     return 1;
--- test/parallel_api/dynamic_selection/sycl/test_round_robin_policy_sycl.pass.cpp
+++ test/parallel_api/dynamic_selection/sycl/test_round_robin_policy_sycl.pass.cpp
@@ -13,6 +13,20 @@
 #include "support/test_dynamic_selection_utils.h"
 #include "support/sycl_sanity.h"
 
+template<bool selection_option, typename Policy, typename UniverseContainer, typename ResourceFunction>
+int tests_with_offset(UniverseContainer u, ResourceFunction&& f, int universe_size) {
+    bool cond = false;
+    for(int offset=0;offset<universe_size;offset++){
+        std::cout<<"Condition : "<<selection_option<<" iteration : "<<offset<<"\n";
+        cond = cond
+               || test_submit_and_wait_on_event<selection_option, Policy>(u, f, offset)
+               || test_submit_and_wait<selection_option, Policy>(u, f, offset)
+               || test_submit_and_wait_on_group<selection_option, Policy>(u, f, offset);
+        if(cond==1) return 1;
+    }
+    return cond;
+}
+
 int main() {
   using policy_t = oneapi::dpl::experimental::round_robin_policy<oneapi::dpl::experimental::sycl_backend>;
   std::vector<sycl::queue> u;
@@ -24,25 +38,17 @@ int main() {
 
   auto n = u.size();
   std::cout << "UNIVERSE SIZE " << n << std::endl;
+  if(n==0) return 0;
 
   auto f = [u, n](int i, int offset=0) { return u[(i+offset-1)%n]; };
+
 
   constexpr bool just_call_submit = false;
   constexpr bool call_select_before_submit = true;
   if ( test_initialization<policy_t, sycl::queue>(u)
        || test_select<policy_t, decltype(u), decltype(f)&, false>(u, f)
-       || test_submit_and_wait_on_event<just_call_submit, policy_t>(u, f)
-       || test_submit_and_wait_on_event<just_call_submit, policy_t>(u, f, 1)
-       || test_submit_and_wait_on_event<call_select_before_submit, policy_t>(u, f)
-       || test_submit_and_wait_on_event<call_select_before_submit, policy_t>(u, f, 1)
-       || test_submit_and_wait<just_call_submit, policy_t>(u, f)
-       || test_submit_and_wait<just_call_submit, policy_t>(u, f, 1)
-       || test_submit_and_wait<call_select_before_submit, policy_t>(u, f)
-       || test_submit_and_wait<call_select_before_submit, policy_t>(u, f, 1)
-       || test_submit_and_wait_on_group<just_call_submit, policy_t>(u, f)
-       || test_submit_and_wait_on_group<just_call_submit, policy_t>(u, f, 1)
-       || test_submit_and_wait_on_group<call_select_before_submit, policy_t>(u, f)
-       || test_submit_and_wait_on_group<call_select_before_submit, policy_t>(u, f, 1)
+       || tests_with_offset<just_call_submit, policy_t>(u, f, n)
+       || tests_with_offset<call_select_before_submit, policy_t>(u, f, n)
      ) {
     std::cout << "FAIL\n";
     return 1;
