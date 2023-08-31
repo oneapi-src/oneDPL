6b2a6e174e2c2fc291a210f575f7f99009e70e72
Matthew Michel <106704043+mmichel11@users.noreply.github.com>
2023-08-31 07:57:00 -0500

    Check for device support of required subgroup size in single work group radix sort (#1117)
    In our one work group implementation of radix support, we specifically require a subgroup size of either 16 or 32. However, many non-Intel architectures do not support a subgroup size of 16 in particular. To resolve this, we add a preprocessor check to only enable the subgroup size requirement with Intel devices.
    
    ---------
    
    Signed-off-by: Matthew Michel <matthew.michel@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_radix_sort_one_wg.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_radix_sort_one_wg.h
@@ -158,7 +158,8 @@ struct __subgroup_radix_sort
                 auto __counter_lacc = __buf_count.get_acc(__cgh);
 
                 __cgh.parallel_for<_Name...>(
-                    __range, ([=](sycl::nd_item<1> __it)[[_ONEDPL_SYCL_REQD_SUB_GROUP_SIZE(__req_sub_group_size)]] {
+                    __range,
+                    ([=](sycl::nd_item<1> __it)[[_ONEDPL_SYCL_REQD_SUB_GROUP_SIZE_IF_SUPPORTED(__req_sub_group_size)]] {
                         union __storage { _ValT __v[__block_size]; __storage(){} } __values;
                         uint16_t __wi = __it.get_local_linear_id();
                         uint16_t __begin_bit = 0;
