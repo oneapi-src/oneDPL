1d3e3f39b95d89b39f71a7dfbad19162f4946c6b
egfefey <elvis.g.fefey@intel.com>
2023-08-31 21:53:00 -0500

    fixing includes (#1130)
    * Update dynamic_selection_traits.h
    
    * Update sycl_backend.h
    
    * Update round_robin_policy.h
    
    * Update fixed_resource_policy.h
    
    * Update dynamic_load_policy.h
    
    * Update dynamic_load_policy.h
    
    * Update auto_tune_policy.h
    
    * Update dynamic_selection
    
    * Update dynamic_selection
    
    * Update sycl_backend.h

--- include/oneapi/dpl/internal/dynamic_selection_impl/fixed_resource_policy.h
+++ include/oneapi/dpl/internal/dynamic_selection_impl/fixed_resource_policy.h
@@ -13,6 +13,7 @@
 #include <type_traits>
 #include <memory>
 #include <exception>
+#include <utility>
 #include "oneapi/dpl/internal/dynamic_selection_impl/scoring_policy_defs.h"
 #if _DS_BACKEND_SYCL != 0
     #include "oneapi/dpl/internal/dynamic_selection_impl/sycl_backend.h"
