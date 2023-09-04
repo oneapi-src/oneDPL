7f67b232ecb416a89db9a5c2fee101205902226e
Sergey Kopienko <sergey.kopienko@intel.com>
2023-09-04 10:11:28 +0200

    Introduce inline constexpr bool __is_hetero_execution_policy_v

--- include/oneapi/dpl/pstl/hetero/dpcpp/execution_sycl_defs.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/execution_sycl_defs.h
@@ -231,6 +231,9 @@ struct __is_hetero_execution_policy<execution::device_policy<PolicyParams...>> :
 {
 };
 
+template <typename... PolicyParams>
+inline constexpr bool __is_hetero_execution_policy_v = __is_hetero_execution_policy<PolicyParams...>::value;
+
 template <typename _T>
 struct __is_device_execution_policy : ::std::false_type
 {
@@ -286,7 +289,7 @@ using __enable_if_device_execution_policy =
 
 template <typename _ExecPolicy, typename _T = void>
 using __enable_if_hetero_execution_policy =
-    ::std::enable_if_t<oneapi::dpl::__internal::__is_hetero_execution_policy<::std::decay_t<_ExecPolicy>>::value, _T>;
+    ::std::enable_if_t<oneapi::dpl::__internal::__is_hetero_execution_policy_v<::std::decay_t<_ExecPolicy>>, _T>;
 
 template <typename _ExecPolicy, typename _T = void>
 using __enable_if_fpga_execution_policy =
