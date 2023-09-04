5d1a164fa60204417b9f0fa09cb0d0ee35091580
Sergey Kopienko <sergey.kopienko@intel.com>
2023-09-04 10:13:25 +0200

    Introduce inline constexpr bool can_use_default_less_operator_v

--- test/parallel_api/algorithm/alg.sorting/sort.pass.cpp
+++ test/parallel_api/algorithm/alg.sorting/sort.pass.cpp
@@ -314,7 +314,7 @@ struct test_sort_op
               typename... Compare>
     ::std::enable_if_t<
         TestUtils::is_base_of_iterator_category_v<::std::random_access_iterator_tag, InputIterator> &&
-            (TestUtils::can_use_default_less_operator<T>::value || sizeof...(Compare) > 0)>
+            (TestUtils::can_use_default_less_operator_v<T> || sizeof...(Compare) > 0)>
     operator()(Policy&& exec, OutputIterator tmp_first, OutputIterator tmp_last, OutputIterator2 expected_first,
                OutputIterator2 expected_last, InputIterator first, InputIterator last, Size n, Compare ...compare)
     {
@@ -326,7 +326,7 @@ struct test_sort_op
               typename... Compare>
     ::std::enable_if_t<
         !TestUtils::is_base_of_iterator_category_v<::std::random_access_iterator_tag, InputIterator> ||
-            !(TestUtils::can_use_default_less_operator<T>::value || sizeof...(Compare) > 0)>
+            !(TestUtils::can_use_default_less_operator_v<T> || sizeof...(Compare) > 0)>
     operator()(Policy&& /* exec */, OutputIterator /* tmp_first */, OutputIterator /* tmp_last */,
                OutputIterator2 /* expected_first */, OutputIterator2 /* expected_last */, InputIterator /* first */,
                InputIterator /* last */, Size /* n */, Compare .../*compare*/)
