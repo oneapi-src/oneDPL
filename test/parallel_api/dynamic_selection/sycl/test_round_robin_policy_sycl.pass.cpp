4cc4680f56e352f22c7bb6b9ee5ab1ec8e66f777
Sergey Kopienko <sergey.kopienko@intel.com>
2023-09-08 14:49:35 +0200

    Should #include "support/test_config.h" at first to avoid compile errors

--- test/parallel_api/dynamic_selection/sycl/test_round_robin_policy_sycl.pass.cpp
+++ test/parallel_api/dynamic_selection/sycl/test_round_robin_policy_sycl.pass.cpp
@@ -7,11 +7,12 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "support/test_config.h"
+#include "support/utils.h"
+
 #include <iostream>
 #include "oneapi/dpl/dynamic_selection"
 #include "support/test_dynamic_selection_utils.h"
-#include "support/test_config.h"
-#include "support/utils.h"
 #if TEST_DYNAMIC_SELECTION_AVAILABLE
 #    include "support/sycl_sanity.h"
 #endif // TEST_DYNAMIC_SELECTION_AVAILABLE
