a47e85fbfa3c334e9ff535e0b171ba16be45a425
Sergey Kopienko <sergey.kopienko@intel.com>
2023-09-08 09:45:00 +0200

    test/parallel_api/dynamic_selection/test_round_robin_policy_inline.pass.cpp - refactoring to looks like all other oneDPL tests

--- test/parallel_api/dynamic_selection/test_round_robin_policy_inline.pass.cpp
+++ test/parallel_api/dynamic_selection/test_round_robin_policy_inline.pass.cpp
@@ -11,6 +11,7 @@
 #include "oneapi/dpl/dynamic_selection"
 #include "support/test_dynamic_selection_utils.h"
 #include "support/inline_backend.h"
+#include "support/utils.h"
 
 int
 main()
@@ -21,20 +22,14 @@ main()
 
     constexpr bool just_call_submit = false;
     constexpr bool call_select_before_submit = true;
-    if (test_initialization<policy_t, int>(u) || test_select<policy_t, decltype(u), decltype(f)&, false>(u, f) ||
-        test_submit_and_wait_on_event<just_call_submit, policy_t>(u, f) ||
-        test_submit_and_wait_on_event<call_select_before_submit, policy_t>(u, f) ||
-        test_submit_and_wait<just_call_submit, policy_t>(u, f) ||
-        test_submit_and_wait<call_select_before_submit, policy_t>(u, f) ||
-        test_submit_and_wait_on_group<just_call_submit, policy_t>(u, f) ||
-        test_submit_and_wait_on_group<call_select_before_submit, policy_t>(u, f))
-    {
-        std::cout << "FAIL\n";
-        return 1;
-    }
-    else
-    {
-        std::cout << "PASS\n";
-        return 0;
-    }
+    EXPECT_EQ(0, test_initialization<policy_t, int>(u), "");
+    EXPECT_EQ(0, test_select<policy_t, decltype(u), decltype(f)&, false>(u, f), "");
+    EXPECT_EQ(0, test_submit_and_wait_on_event<just_call_submit, policy_t>(u, f), "");
+    EXPECT_EQ(0, test_submit_and_wait_on_event<call_select_before_submit, policy_t>(u, f), "");
+    EXPECT_EQ(0, test_submit_and_wait<just_call_submit, policy_t>(u, f), "");
+    EXPECT_EQ(0, test_submit_and_wait<call_select_before_submit, policy_t>(u, f), "");
+    EXPECT_EQ(0, test_submit_and_wait_on_group<just_call_submit, policy_t>(u, f), "");
+    EXPECT_EQ(0, test_submit_and_wait_on_group<call_select_before_submit, policy_t>(u, f))
+
+    return TestUtils::done();
 }
