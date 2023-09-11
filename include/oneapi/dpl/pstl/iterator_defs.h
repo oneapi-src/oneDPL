c4a0b7c7bd31e966edd6aa91d97fbdda447d8b27
Sergey Kopienko <sergey.kopienko@intel.com>
2023-09-11 14:19:09 +0200

    Use ::std::is_pointer_v<X> instead of ::std::is_pointer<X>::value

--- include/oneapi/dpl/pstl/iterator_defs.h
+++ include/oneapi/dpl/pstl/iterator_defs.h
@@ -100,7 +100,7 @@ struct is_passed_directly<Iter, ::std::enable_if_t<Iter::is_passed_directly::val
 };
 
 template <typename Iter> // for pointers to objects on device
-struct is_passed_directly<Iter, ::std::enable_if_t<::std::is_pointer<Iter>::value, void>>
+struct is_passed_directly<Iter, ::std::enable_if_t<::std::is_pointer_v<Iter>, void>>
     : ::std::true_type
 {
 };
