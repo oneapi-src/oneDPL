8726ac45c1616fdfb7922a4aca382d75424043f3
Dmitriy Sobolev <Dmitriy.Sobolev@intel.com>
2023-09-11 07:57:19 -0500

    [KT] Remove namespace injections (#1171)
    Signed-off-by: Dmitriy Sobolev <dmitriy.sobolev@intel.com>

--- include/oneapi/dpl/experimental/kt/esimd_radix_sort_one_wg.h
+++ include/oneapi/dpl/experimental/kt/esimd_radix_sort_one_wg.h
@@ -28,10 +28,6 @@ template <bool __is_ascending, ::std::uint8_t __radix_bits, ::std::uint16_t __da
 void
 __one_wg_kernel(sycl::nd_item<1> __idx, ::std::uint32_t __n, const _InputT& __input)
 {
-    using namespace sycl;
-    using namespace __dpl_esimd_ns;
-    using namespace __dpl_esimd_ens;
-
     using _BinT = ::std::uint16_t;
     using _HistT = ::std::uint16_t;
     using _DeviceAddrT = ::std::uint32_t;
@@ -48,18 +44,18 @@ __one_wg_kernel(sycl::nd_item<1> __idx, ::std::uint32_t __n, const _InputT& __in
 
     // max SLM is 256 * 4 * 64 + 256 * 2 * 64 + 257*2, 97KB, when  __data_per_work_item = 256, __bin_count = 256
     // to support 512 processing size, we can use all SLM as reorder buffer with cost of more barrier
-    slm_init(std::max(__reorder_slm_size, __bin_hist_slm_size + __incoming_offset_slm_size));
+    __dpl_esimd_ns::slm_init(::std::max(__reorder_slm_size, __bin_hist_slm_size + __incoming_offset_slm_size));
 
     const ::std::uint32_t __local_tid = __idx.get_local_linear_id();
 
     const ::std::uint32_t __slm_reorder_this_thread = __local_tid * __data_per_work_item * sizeof(_KeyT);
     const ::std::uint32_t __slm_bin_hist_this_thread = __local_tid * __hist_stride;
 
-    simd<_HistT, __bin_count> __bin_offset;
-    simd<_DeviceAddrT, __data_per_work_item> __write_addr;
-    simd<_KeyT, __data_per_work_item> __keys;
-    simd<_BinT, __data_per_work_item> __bins;
-    simd<_DeviceAddrT, __data_per_step> __lane_id(0, 1);
+    __dpl_esimd_ns::simd<_HistT, __bin_count> __bin_offset;
+    __dpl_esimd_ns::simd<_DeviceAddrT, __data_per_work_item> __write_addr;
+    __dpl_esimd_ns::simd<_KeyT, __data_per_work_item> __keys;
+    __dpl_esimd_ns::simd<_BinT, __data_per_work_item> __bins;
+    __dpl_esimd_ns::simd<_DeviceAddrT, __data_per_step> __lane_id(0, 1);
 
     const _DeviceAddrT __io_offset = __data_per_work_item * __local_tid;
 
@@ -69,10 +65,10 @@ __one_wg_kernel(sycl::nd_item<1> __idx, ::std::uint32_t __n, const _InputT& __in
 #pragma unroll
     for (::std::uint32_t __s = 0; __s < __data_per_work_item; __s += __data_per_step)
     {
-        simd_mask<__data_per_step> __m = (__io_offset + __lane_id + __s) < __n;
+        __dpl_esimd_ns::simd_mask<__data_per_step> __m = (__io_offset + __lane_id + __s) < __n;
         __keys.template select<__data_per_step, 1>(__s) =
-            merge(__utils::__gather<_KeyT, __data_per_step>(__input, __lane_id, __io_offset + __s, __m),
-                  simd<_KeyT, __data_per_step>(__utils::__sort_identity<_KeyT, __is_ascending>()), __m);
+            __dpl_esimd_ns::merge(__utils::__gather<_KeyT, __data_per_step>(__input, __lane_id, __io_offset + __s, __m),
+                  __dpl_esimd_ns::simd<_KeyT, __data_per_step>(__utils::__sort_identity<_KeyT, __is_ascending>()), __m);
     }
 
     for (::std::uint32_t __stage = 0; __stage < __stage_count; __stage++)
@@ -94,7 +90,7 @@ __one_wg_kernel(sycl::nd_item<1> __idx, ::std::uint32_t __n, const _InputT& __in
         then every thread add local sum with sum of previous group and incoming offset
         */
         {
-            barrier();
+            __dpl_esimd_ns::barrier();
 #pragma unroll
             for (::std::uint32_t __s = 0; __s < __bin_count; __s += 128)
             {
@@ -102,38 +98,38 @@ __one_wg_kernel(sycl::nd_item<1> __idx, ::std::uint32_t __n, const _InputT& __in
                     __slm_bin_hist_this_thread + __s * sizeof(_HistT),
                     __bin_offset.template select<128, 1>(__s).template bit_cast_view<::std::uint32_t>());
             }
-            barrier();
+            __dpl_esimd_ns::barrier();
             constexpr ::std::uint32_t __bin_summary_group_size = 8;
             if (__local_tid < __bin_summary_group_size)
             {
                 constexpr ::std::uint32_t __bin_width = __bin_count / __bin_summary_group_size;
-                constexpr ::std::uint32_t _BIN_WIDTH_UD = __bin_width * sizeof(_HistT) / sizeof(::std::uint32_t);
+                constexpr ::std::uint32_t __bin_width_ud = __bin_width * sizeof(_HistT) / sizeof(::std::uint32_t);
                 ::std::uint32_t __slm_bin_hist_summary_offset = __local_tid * __bin_width * sizeof(_HistT);
-                simd<_HistT, __bin_width> ___thread_grf_hist_summary;
-                simd<::std::uint32_t, _BIN_WIDTH_UD> __tmp;
+                __dpl_esimd_ns::simd<_HistT, __bin_width> ___thread_grf_hist_summary;
+                __dpl_esimd_ns::simd<::std::uint32_t, __bin_width_ud> __tmp;
 
                 ___thread_grf_hist_summary.template bit_cast_view<::std::uint32_t>() =
-                    __utils::__block_load_slm<::std::uint32_t, _BIN_WIDTH_UD>(__slm_bin_hist_summary_offset);
+                    __utils::__block_load_slm<::std::uint32_t, __bin_width_ud>(__slm_bin_hist_summary_offset);
                 __slm_bin_hist_summary_offset += __hist_stride;
                 for (::std::uint32_t __s = 1; __s < __work_group_size - 1; __s++)
                 {
-                    __tmp = __utils::__block_load_slm<::std::uint32_t, _BIN_WIDTH_UD>(__slm_bin_hist_summary_offset);
+                    __tmp = __utils::__block_load_slm<::std::uint32_t, __bin_width_ud>(__slm_bin_hist_summary_offset);
                     ___thread_grf_hist_summary += __tmp.template bit_cast_view<_HistT>();
-                    __utils::__block_store_slm<::std::uint32_t, _BIN_WIDTH_UD>(
+                    __utils::__block_store_slm<::std::uint32_t, __bin_width_ud>(
                         __slm_bin_hist_summary_offset, ___thread_grf_hist_summary.template bit_cast_view<::std::uint32_t>());
                     __slm_bin_hist_summary_offset += __hist_stride;
                 }
-                __tmp = __utils::__block_load_slm<::std::uint32_t, _BIN_WIDTH_UD>(__slm_bin_hist_summary_offset);
+                __tmp = __utils::__block_load_slm<::std::uint32_t, __bin_width_ud>(__slm_bin_hist_summary_offset);
                 ___thread_grf_hist_summary += __tmp.template bit_cast_view<_HistT>();
                 ___thread_grf_hist_summary = __utils::__scan<_HistT, _HistT>(___thread_grf_hist_summary);
-                __utils::__block_store_slm<::std::uint32_t, _BIN_WIDTH_UD>(
+                __utils::__block_store_slm<::std::uint32_t, __bin_width_ud>(
                     __slm_bin_hist_summary_offset, ___thread_grf_hist_summary.template bit_cast_view<::std::uint32_t>());
             }
-            barrier();
+            __dpl_esimd_ns::barrier();
             if (__local_tid == 0)
             {
-                simd<_HistT, __bin_count> __grf_hist_summary;
-                simd<_HistT, __bin_count + 1> __grf_hist_summary_scan;
+                __dpl_esimd_ns::simd<_HistT, __bin_count> __grf_hist_summary;
+                __dpl_esimd_ns::simd<_HistT, __bin_count + 1> __grf_hist_summary_scan;
 #pragma unroll
                 for (::std::uint32_t __s = 0; __s < __bin_count; __s += 128)
                 {
@@ -156,7 +152,7 @@ __one_wg_kernel(sycl::nd_item<1> __idx, ::std::uint32_t __n, const _InputT& __in
                         __grf_hist_summary_scan.template select<128, 1>(__s).template bit_cast_view<::std::uint32_t>());
                 }
             }
-            barrier();
+            __dpl_esimd_ns::barrier();
             {
 #pragma unroll
                 for (::std::uint32_t __s = 0; __s < __bin_count; __s += 128)
@@ -169,20 +165,20 @@ __one_wg_kernel(sycl::nd_item<1> __idx, ::std::uint32_t __n, const _InputT& __in
 #pragma unroll
                     for (::std::uint32_t __s = 0; __s < __bin_count; __s += 128)
                     {
-                        simd<_HistT, 128> __group_local_sum;
+                        __dpl_esimd_ns::simd<_HistT, 128> __group_local_sum;
                         __group_local_sum.template bit_cast_view<::std::uint32_t>() =
                             __utils::__block_load_slm<::std::uint32_t, 64>((__local_tid - 1) * __hist_stride + __s * sizeof(_HistT));
                         __bin_offset.template select<128, 1>(__s) += __group_local_sum;
                     }
                 }
             }
-            barrier();
+            __dpl_esimd_ns::barrier();
         }
 
 #pragma unroll
         for (::std::uint32_t __s = 0; __s < __data_per_work_item; __s += __data_per_step)
         {
-            simd<::std::uint16_t, __data_per_step> __bins_uw = __bins.template select<__data_per_step, 1>(__s);
+            __dpl_esimd_ns::simd<::std::uint16_t, __data_per_step> __bins_uw = __bins.template select<__data_per_step, 1>(__s);
             __write_addr.template select<__data_per_step, 1>(__s) += __bin_offset.template iselect(__bins_uw);
         }
 
@@ -195,7 +191,7 @@ __one_wg_kernel(sycl::nd_item<1> __idx, ::std::uint32_t __n, const _InputT& __in
                     __write_addr.template select<__data_per_step, 1>(__s) * sizeof(_KeyT),
                     __keys.template select<__data_per_step, 1>(__s));
             }
-            barrier();
+            __dpl_esimd_ns::barrier();
             __keys = __utils::__block_load_slm<_KeyT, __data_per_work_item>(__slm_reorder_this_thread);
         }
     }
