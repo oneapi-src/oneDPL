57d1ab7a8a42bce836a2a2b7b1559a9119a2d6b4
Boyarinov, Konstantin <konstantin.boyarinov@intel.com>
2023-09-18 07:58:13 -0700

    Fix numbers.pass

--- test/pstl_offload/headers/headers.standard/requires_header_presence/numbers.pass.cpp
+++ test/pstl_offload/headers/headers.standard/requires_header_presence/numbers.pass.cpp
@@ -12,7 +12,7 @@
 
 int main() {
 #if __cpp_lib_math_constants >= 201907L
-    [[maybe_unused]] auto phi = std::numbers::phi_v;
+    [[maybe_unused]] auto phi = std::numbers::phi;
 #endif
     return TestUtils::done();
 }
