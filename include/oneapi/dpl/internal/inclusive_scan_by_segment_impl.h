1e143fb47de99b739629dc26234ac3def05835e8
Sergey Kopienko <sergey.kopienko@intel.com>
2023-09-20 12:15:27 +0200

    include/oneapi/dpl/internal/inclusive_scan_by_segment_impl.h - ::std::decay_t<Policy> not required for internal::__buffer initialization, because all std::decay_t exist inside oneapi::dpl::__par_backend_hetero::__internal::__buffer implementation

--- include/oneapi/dpl/internal/inclusive_scan_by_segment_impl.h
+++ include/oneapi/dpl/internal/inclusive_scan_by_segment_impl.h
@@ -95,7 +95,6 @@ inclusive_scan_by_segment_impl(Policy&& policy, InputIterator1 first1, InputIter
 
     typedef unsigned int FlagType;
     typedef typename ::std::iterator_traits<InputIterator2>::value_type ValueType;
-    typedef ::std::decay_t<Policy> policy_type;
 
     const auto n = ::std::distance(first1, last1);
 
@@ -105,7 +104,7 @@ inclusive_scan_by_segment_impl(Policy&& policy, InputIterator1 first1, InputIter
 
     FlagType initial_mask = 1;
 
-    oneapi::dpl::__par_backend_hetero::__internal::__buffer<policy_type, FlagType> _mask(policy, n);
+    oneapi::dpl::__par_backend_hetero::__internal::__buffer<Policy, FlagType> _mask(policy, n);
     {
         auto mask_buf = _mask.get_buffer();
         auto mask = mask_buf.get_host_access(sycl::read_write);
