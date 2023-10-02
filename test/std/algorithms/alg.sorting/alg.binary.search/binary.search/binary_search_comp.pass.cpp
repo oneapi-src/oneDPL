3cf8be0e225a22c528492ca47c32d880e66fc251
Sergey Kopienko <sergey.kopienko@intel.com>
2023-10-02 15:06:42 +0200

    Fix review comment: add error messages

--- test/std/algorithms/alg.sorting/alg.binary.search/binary.search/binary_search_comp.pass.cpp
+++ test/std/algorithms/alg.sorting/alg.binary.search/binary.search/binary_search_comp.pass.cpp
@@ -81,7 +81,7 @@ kernel_test()
     }).wait();
 
     auto ret_access_host = buffer1.get_host_access(sycl::read_only);
-    EXPECT_TRUE(ret_access_host[0], "");
+    EXPECT_TRUE(ret_access_host[0], "Wrong result of binary_search");
 }
 #endif // TEST_DPCPP_BACKEND_PRESENT
 
