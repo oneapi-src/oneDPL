88980fa22db5c577041f1526d536bc901c927d58
Sergey Kopienko <sergey.kopienko@intel.com>
2023-10-13 11:29:25 +0200

    test/std/algorithms/alg.sorting/alg.binary.search/binary.search/binary_search1_g.pass.cpp - fix review comment

--- test/std/algorithms/alg.sorting/alg.binary.search/binary.search/binary_search1_g.pass.cpp
+++ test/std/algorithms/alg.sorting/alg.binary.search/binary.search/binary_search1_g.pass.cpp
@@ -121,7 +121,7 @@ main()
 #if TEST_DPCPP_BACKEND_PRESENT
     auto ret = kernel_test1();
     ret &= kernel_test2();
-    EXPECT_TRUE(ret, "Wrong result of binary_search");
+    EXPECT_TRUE(ret, "Wrong result of binary_search in kernel_test1 or kernel_test2");
 #endif // TEST_DPCPP_BACKEND_PRESENT
 
     return TestUtils::done(TEST_DPCPP_BACKEND_PRESENT);
