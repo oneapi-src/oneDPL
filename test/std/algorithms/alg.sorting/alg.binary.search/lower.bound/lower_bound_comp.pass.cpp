b494011cbcfd3bc78492643b752013b968694ef7
Sergey Kopienko <sergey.kopienko@intel.com>
2023-10-13 10:05:47 +0200

    test/std/algorithms/alg.sorting/alg.binary.search/lower.bound/lower_bound_comp.pass.cpp - fix review comment

--- test/std/algorithms/alg.sorting/alg.binary.search/lower.bound/lower_bound_comp.pass.cpp
+++ test/std/algorithms/alg.sorting/alg.binary.search/lower.bound/lower_bound_comp.pass.cpp
@@ -90,7 +90,7 @@ kernel_test()
     }).wait();
 
     auto ret_access_host = buffer1.get_host_access(sycl::read_only);
-    EXPECT_TRUE(ret_access_host[0], "Wrong result of lower_bound");
+    EXPECT_TRUE(ret_access_host[0], "Wrong result of lower_bound with comparator");
 }
 #endif // TEST_DPCPP_BACKEND_PRESENT
 
