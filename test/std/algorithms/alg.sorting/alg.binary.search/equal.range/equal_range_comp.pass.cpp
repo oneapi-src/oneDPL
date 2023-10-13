82484aca59ff7d488ffb6ccd6ab665bfcdc4322f
Sergey Kopienko <sergey.kopienko@intel.com>
2023-10-13 09:54:27 +0200

    test/std/algorithms/alg.sorting/alg.binary.search/equal.range/equal_range_comp.pass.cpp - fix review comment

--- test/std/algorithms/alg.sorting/alg.binary.search/equal.range/equal_range_comp.pass.cpp
+++ test/std/algorithms/alg.sorting/alg.binary.search/equal.range/equal_range_comp.pass.cpp
@@ -85,7 +85,7 @@ kernel_test()
     }).wait();
 
     auto ret_access_host = buffer1.get_host_access(sycl::read_only);
-    EXPECT_TRUE(ret_access_host[0], "Wrong result of equal_range");
+    EXPECT_TRUE(ret_access_host[0], "Wrong result of equal_range with comparator");
 }
 #endif // TEST_DPCPP_BACKEND_PRESENT
 
