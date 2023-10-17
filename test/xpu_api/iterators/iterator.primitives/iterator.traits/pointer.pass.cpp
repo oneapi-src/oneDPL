e8fa928d503c515c2cba16abc2fd1432e852f493
Sergey Kopienko <sergey.kopienko@intel.com>
2023-10-17 12:10:27 +0200

    Copy tests from source repository

---
+++ test/xpu_api/iterators/iterator.primitives/iterator.traits/pointer.pass.cpp
@@ -0,0 +1,66 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// <iterator>
+
+// template<class T>
+// struct iterator_traits<T*>
+// {
+//   typedef ptrdiff_t                  difference_type;
+//   typedef T                          value_type;
+//   typedef T*                         pointer;
+//   typedef T&                         reference;
+//   typedef random_access_iterator_tag iterator_category;
+//   typedef contiguous_iterator_tag iterator_category; // C++20
+// };
+
+#include "oneapi_std_test_config.h"
+#include <CL/sycl.hpp>
+#include "test_macros.h"
+
+#ifdef USE_ONEAPI_STD
+#    include _ONEAPI_STD_TEST_HEADER(iterator)
+#    include _ONEAPI_STD_TEST_HEADER(type_traits)
+#    include _ONEAPI_STD_TEST_HEADER(cstddef)
+namespace s = oneapi_cpp_ns;
+#else
+#    include <iterator>
+#    include <type_traits>
+namespace s = std;
+#endif
+
+struct A
+{
+};
+
+void
+kernelTest()
+{
+    cl::sycl::queue q;
+    q.submit([&](cl::sycl::handler& cgh) {
+        cgh.single_task<class IteratorTest>([=]() {
+            typedef s::iterator_traits<A*> It;
+            static_assert((s::is_same<It::difference_type, s::ptrdiff_t>::value), "");
+            static_assert((s::is_same<It::value_type, A>::value), "");
+            static_assert((s::is_same<It::pointer, A*>::value), "");
+            static_assert((s::is_same<It::reference, A&>::value), "");
+            static_assert((s::is_same<It::iterator_category, s::random_access_iterator_tag>::value), "");
+#if TEST_STD_VER > 17
+            ASSERT_SAME_TYPE(It::iterator_concept, s::contiguous_iterator_tag);
+#endif
+        });
+    });
+}
+
+int
+main(int, char**)
+{
+    kernelTest();
+    std::cout << "Pass" << std::endl;
+    return 0;
+}
---
+++ test/xpu_api/iterators/iterator.primitives/iterator.traits/volatile_pointer.pass.cpp
@@ -0,0 +1,55 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// <iterator>
+
+// template<class T>
+// struct iterator_traits<const T*>
+
+#include "oneapi_std_test_config.h"
+#include <CL/sycl.hpp>
+#include <iostream>
+#include "test_macros.h"
+
+#ifdef USE_ONEAPI_STD
+#    include _ONEAPI_STD_TEST_HEADER(iterator)
+#    include _ONEAPI_STD_TEST_HEADER(type_traits)
+#    include _ONEAPI_STD_TEST_HEADER(cstddef)
+namespace s = oneapi_cpp_ns;
+#else
+#    include <iterator>
+#    include <type_traits>
+namespace s = std;
+#endif
+
+struct A
+{
+};
+
+void
+kernelTest()
+{
+    cl::sycl::queue q;
+    q.submit([&](cl::sycl::handler& cgh) {
+        cgh.single_task<class IteratorTest>([=]() {
+            typedef s::iterator_traits<volatile A*> It;
+            static_assert((s::is_same<It::difference_type, s::ptrdiff_t>::value), "");
+            static_assert((s::is_same<It::pointer, volatile A*>::value), "");
+            static_assert((s::is_same<It::reference, volatile A&>::value), "");
+            static_assert((s::is_same<It::iterator_category, s::random_access_iterator_tag>::value), "");
+        });
+    });
+}
+
+int
+main(int, char**)
+{
+    kernelTest();
+    std::cout << "Pass" << std::endl;
+    return 0;
+}
