22e1f01796326bb9031a2ec40693c355f4fa0347
Joe Todd <joe.todd@codeplay.com>
2023-10-20 06:17:58 -0700

    Definition of `__impl::radix_sort`

--- include/oneapi/dpl/experimental/kt/internal/gpu_radix_sort_dispatchers.h
+++ include/oneapi/dpl/experimental/kt/internal/gpu_radix_sort_dispatchers.h
@@ -26,10 +26,50 @@
 
 namespace oneapi::dpl::experimental::kt::gpu::__impl
 {
+//TODO: Implement this using __subgroup_radix_sort from parallel-backend_sycl_radix_sort_one_wg.h
+template <typename _KernelName, bool __is_ascending, ::std::uint8_t __radix_bits, ::std::uint16_t __data_per_work_item,
+          ::std::uint16_t __work_group_size, typename _Range>
+sycl::event
+__one_wg(sycl::queue __q, _Range&& __rng, ::std::size_t __n);
 
+template <typename _KernelName, bool __is_ascending, ::std::uint8_t __radix_bits, ::std::uint16_t __data_per_work_item,
+          ::std::uint16_t __work_group_size, typename _KeysRng>
+sycl::event
+__onesweep(sycl::queue __q, _KeysRng&& __keys_rng, ::std::size_t __n);
+
+// TODO: allow calling it only for all_view (accessor) and guard_view (USM) ranges, views::subrange and sycl_iterator
 template <bool __is_ascending, ::std::uint8_t __radix_bits, typename _KernelParam, typename _Range>
 sycl::event
-__radix_sort(sycl::queue __q, _Range&& __rng, _KernelParam __param);
+__radix_sort(sycl::queue __q, _Range&& __rng, _KernelParam __param)
+{
+    // TODO: Redefine these static_asserts based on the requirements of the GPU onesweep algorithm
+    // static_assert(__radix_bits == 8);
+
+    // static_assert(32 <= __param.data_per_workitem && __param.data_per_workitem <= 512 &&
+    //               __param.data_per_workitem % 32 == 0);
+
+    const ::std::size_t __n = __rng.size();
+    assert(__n > 1);
+
+    // _PRINT_INFO_IN_DEBUG_MODE(__exec); TODO: extend the utility to work with queues
+    constexpr auto __data_per_workitem = _KernelParam::data_per_workitem;
+    constexpr auto __workgroup_size = _KernelParam::workgroup_size;
+    using _KernelName = typename _KernelParam::kernel_name;
+
+    constexpr ::std::uint32_t __one_wg_cap = __data_per_workitem * __workgroup_size;
+    if (__n <= __one_wg_cap)
+    {
+        return __one_wg<_KernelName, __is_ascending, __radix_bits, __data_per_workitem, __workgroup_size>(
+            __q, ::std::forward<_Range>(__rng), __n);
+    }
+    else
+    {
+        // TODO: avoid kernel duplication (generate the output storage with the same type as input storage and use swap)
+        // TODO: support different RadixBits, WorkGroupSize
+        return __onesweep<_KernelName, __is_ascending, __radix_bits, __data_per_workitem, __workgroup_size>(
+            __q, ::std::forward<_Range>(__rng), __n);
+    }
+}
 
 } // namespace oneapi::dpl::experimental::kt::gpu::__impl
 
