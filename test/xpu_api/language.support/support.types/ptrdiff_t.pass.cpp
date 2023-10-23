3a276ad65a4b49f1fe51d8f7b7a48c6145536f53
Sergey Kopienko <sergey.kopienko@intel.com>
2023-10-23 15:03:05 +0200

    Copy tests from source repository

---
+++ test/xpu_api/language.support/support.types/ptrdiff_t.pass.cpp
@@ -0,0 +1,53 @@
+//===----------------------------------------------------------------------===//
+//
+// ptrdiff_t should:
+//
+//  1. be in namespace std.
+//  2. be the same sizeof as void*.
+//  3. be a signed integral.
+//
+//===----------------------------------------------------------------------===//
+
+#include "oneapi_std_test_config.h"
+#include "test_macros.h"
+#include <CL/sycl.hpp>
+#include <iostream>
+
+#ifdef USE_ONEAPI_STD
+#    include _ONEAPI_STD_TEST_HEADER(type_traits)
+#    include _ONEAPI_STD_TEST_HEADER(cstddef)
+namespace s = oneapi_cpp_ns;
+#else
+#    include <cstddef>
+#    include <type_traits>
+namespace s = std;
+#endif
+
+int
+main(int, char**)
+{
+    const std::size_t N = 1;
+    bool ret = true;
+
+    {
+        cl::sycl::buffer<bool, 1> buf(&ret, cl::sycl::range<1>{N});
+        cl::sycl::queue q;
+        q.submit([&](cl::sycl::handler& cgh) {
+            auto acc = buf.get_access<cl::sycl::access::mode::write>(cgh);
+            cgh.single_task<class KernelTest1>([=]() {
+                static_assert(sizeof(s::ptrdiff_t) == sizeof(void*), "sizeof(s::ptrdiff_t) == sizeof(void*)");
+                static_assert(s::is_signed<s::ptrdiff_t>::value, "s::is_signed<s::ptrdiff_t>::value");
+                static_assert(s::is_integral<s::ptrdiff_t>::value, "s::is_integral<s::ptrdiff_t>::value");
+                acc[0] &= (sizeof(s::ptrdiff_t) == sizeof(void*));
+            });
+        });
+    }
+
+    if (ret)
+
+        std::cout << "Pass" << std::endl;
+    else
+        std::cout << "Fail" << std::endl;
+
+    return 0;
+}
