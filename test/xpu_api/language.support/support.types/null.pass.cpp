b6cd0afd3dba5dfa2859ca6faed77ab0226db745
Sergey Kopienko <sergey.kopienko@intel.com>
2023-10-25 15:36:58 +0200

    Remove #include <oneapi/dpl/cstddef> where it's really not required

--- test/xpu_api/language.support/support.types/null.pass.cpp
+++ test/xpu_api/language.support/support.types/null.pass.cpp
@@ -15,6 +15,10 @@
 
 #include "support/test_config.h"
 
+// TODO cstddef - really it's not required here but we should include it because
+// in support/utils.h we have
+//      #include _PSTL_TEST_HEADER(execution)
+// and should include something from standard library to avoid problems with tbb version
 #include <oneapi/dpl/cstddef>
 
 #include "support/test_macros.h"
