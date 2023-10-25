0f2a4ec2dc568c8ec7ca71c8e7eb58128994e3c5
Sergey Kopienko <sergey.kopienko@intel.com>
2023-10-25 17:13:21 +0200

    include/oneapi/dpl/pstl/parallel_impl.h - restore __parallel_find(_ExecutionPolicy&& __exec, _Index __first, _Index __last, _Brick __f, _IsFirst)

--- include/oneapi/dpl/pstl/parallel_impl.h
+++ include/oneapi/dpl/pstl/parallel_impl.h
@@ -34,6 +34,40 @@ namespace __internal
 Each f[i,j) must return a value in [i,j). */
 template <class _ExecutionPolicy, class _Index, class _Brick, class _IsFirst>
 _Index
+__parallel_find(_ExecutionPolicy&& __exec, _Index __first, _Index __last, _Brick __f, _IsFirst)
+{
+    typedef typename ::std::iterator_traits<_Index>::difference_type _DifferenceType;
+    const _DifferenceType __n = __last - __first;
+    _DifferenceType __initial_dist = _IsFirst::value ? __n : -1;
+
+    constexpr auto __comp = ::std::conditional_t<_IsFirst::value, __pstl_less, __pstl_greater>{};
+
+    ::std::atomic<_DifferenceType> __extremum(__initial_dist);
+    // TODO: find out what is better here: parallel_for or parallel_reduce
+    __par_backend::__parallel_for(::std::forward<_ExecutionPolicy>(__exec), __first, __last,
+                                  [__comp, __f, __first, &__extremum](_Index __i, _Index __j) {
+                                      // See "Reducing Contention Through Priority Updates", PPoPP '13, for discussion of
+                                      // why using a shared variable scales fairly well in this situation.
+                                      if (__comp(__i - __first, __extremum))
+                                      {
+                                          _Index __res = __f(__i, __j);
+                                          // If not '__last' returned then we found what we want so put this to extremum
+                                          if (__res != __j)
+                                          {
+                                              const _DifferenceType __k = __res - __first;
+                                              for (_DifferenceType __old = __extremum; __comp(__k, __old);
+                                                   __old = __extremum)
+                                              {
+                                                  __extremum.compare_exchange_weak(__old, __k);
+                                              }
+                                          }
+                                      }
+                                  });
+    return __extremum != __initial_dist ? __first + __extremum : __last;
+}
+
+template <class _ExecutionPolicy, class _Index, class _Brick, class _IsFirst>
+_Index
 __parallel_find(__tbb_backend_tag __tag, _ExecutionPolicy&& __exec, _Index __first, _Index __last, _Brick __f, _IsFirst)
 {
     typedef typename ::std::iterator_traits<_Index>::difference_type _DifferenceType;
