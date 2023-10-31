5f91d0c265b9d616afbf3a18a5a6866e06b00241
Sergey Kopienko <sergey.kopienko@intel.com>
2023-10-31 14:23:34 +0100

    Copy tests from source repository

---
+++ test/xpu_api/tuple/tuple.tuple/tuple.comparison/constexpr_g.pass.cpp
@@ -0,0 +1,33 @@
+#include "oneapi_std_test_config.h"
+#include "testsuite_common_types.h"
+#include <CL/sycl.hpp>
+#include <iostream>
+
+#ifdef USE_ONEAPI_STD
+#    include _ONEAPI_STD_TEST_HEADER(tuple)
+#    include _ONEAPI_STD_TEST_HEADER(utility)
+namespace s = oneapi_cpp_ns;
+#else
+#    include <tuple>
+#    include <utility>
+namespace s = std;
+#endif
+
+constexpr cl::sycl::access::mode sycl_read = cl::sycl::access::mode::read;
+constexpr cl::sycl::access::mode sycl_write = cl::sycl::access::mode::write;
+
+int
+main()
+{
+    cl::sycl::queue deviceQueue;
+    {
+        deviceQueue.submit([&](cl::sycl::handler& cgh) {
+            cgh.single_task<class KernelTest>([=]() {
+                constexpr_comparison_operators test;
+                test.operator()<s::tuple<int, int>>();
+            });
+        });
+    }
+    std::cout << "pass" << std::endl;
+    return 0;
+}
---
+++ test/xpu_api/tuple/tuple.tuple/tuple.cons/noexcept_move_construct_g.pass.cpp
@@ -0,0 +1,51 @@
+#include "oneapi_std_test_config.h"
+#include "testsuite_struct.h"
+#include <CL/sycl.hpp>
+#include <iostream>
+
+#ifdef USE_ONEAPI_STD
+#    include _ONEAPI_STD_TEST_HEADER(tuple)
+#    include _ONEAPI_STD_TEST_HEADER(utility)
+#    include _ONEAPI_STD_TEST_HEADER(type_traits)
+namespace s = oneapi_cpp_ns;
+#else
+#    include <tuple>
+#    include <utility>
+namespace s = std;
+#endif
+
+constexpr cl::sycl::access::mode sycl_read = cl::sycl::access::mode::read;
+constexpr cl::sycl::access::mode sycl_write = cl::sycl::access::mode::write;
+
+void
+kernel_test()
+{
+    cl::sycl::queue deviceQueue;
+    {
+        deviceQueue.submit([&](cl::sycl::handler& cgh) {
+            cgh.single_task<class KernelTest>([=]() {
+                typedef s::tuple<int> tt1;
+                typedef s::tuple<int, float> tt2;
+                typedef s::tuple<short, float, int> tt3;
+                typedef s::tuple<short, NoexceptMoveConsClass, float> tt4;
+                typedef s::tuple<NoexceptMoveConsClass, NoexceptMoveConsClass, float> tt5;
+                typedef s::tuple<NoexceptMoveConsClass, NoexceptMoveConsClass, NoexceptMoveConsClass> tt6;
+
+                static_assert(s::is_nothrow_move_constructible<tt1>::value, "Error");
+                static_assert(s::is_nothrow_move_constructible<tt2>::value, "Error");
+                static_assert(s::is_nothrow_move_constructible<tt3>::value, "Error");
+                static_assert(s::is_nothrow_move_constructible<tt4>::value, "Error");
+                static_assert(s::is_nothrow_move_constructible<tt5>::value, "Error");
+                static_assert(s::is_nothrow_move_constructible<tt6>::value, "Error");
+            });
+        });
+    }
+}
+
+int
+main()
+{
+    kernel_test();
+    std::cout << "pass" << std::endl;
+    return 0;
+}
