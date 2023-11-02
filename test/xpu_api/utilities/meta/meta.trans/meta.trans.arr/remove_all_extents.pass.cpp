de2dd0d0d8848d925e3cf82e6f357deadcb7997d
Sergey Kopienko <sergey.kopienko@intel.com>
2023-11-02 17:38:16 +0100

    Update test/xpu_api/utilities/meta/meta.trans/meta.trans.arr/remove_all_extents.pass.cpp
    Co-authored-by: Konstantin Boyarinov <konstantin.boyarinov@intel.com>

--- test/xpu_api/utilities/meta/meta.trans/meta.trans.arr/remove_all_extents.pass.cpp
+++ test/xpu_api/utilities/meta/meta.trans/meta.trans.arr/remove_all_extents.pass.cpp
@@ -13,10 +13,6 @@
 //
 //===----------------------------------------------------------------------===//
 
-// type_traits
-
-// remove_all_extents
-
 #include "support/test_config.h"
 
 #include <oneapi/dpl/type_traits>
