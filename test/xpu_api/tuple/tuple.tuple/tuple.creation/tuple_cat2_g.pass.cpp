d7667a8dee40bc4f25bb4dc6232b55b9981b0a3b
Sergey Kopienko <sergey.kopienko@intel.com>
2023-11-02 11:56:12 +0100

    Copy tests from source repository

---
+++ test/xpu_api/tuple/tuple.tuple/tuple.creation/tuple_cat2_g.pass.cpp
@@ -0,0 +1,102 @@
+#include "oneapi_std_test_config.h"
+#include "test_macros.h"
+#include <CL/sycl.hpp>
+#include <iostream>
+
+#ifdef USE_ONEAPI_STD
+#    include _ONEAPI_STD_TEST_HEADER(tuple)
+#    include _ONEAPI_STD_TEST_HEADER(type_traits)
+#    include _ONEAPI_STD_TEST_HEADER(utility)
+namespace s = oneapi_cpp_ns;
+#else
+#    include <tuple>
+#    include <type_traits>
+namespace s = std;
+#endif
+
+constexpr cl::sycl::access::mode sycl_read = cl::sycl::access::mode::read;
+constexpr cl::sycl::access::mode sycl_write = cl::sycl::access::mode::write;
+
+template <typename T>
+typename s::decay<T>::type
+copy(T&& x)
+{
+    return s::forward<T>(x);
+}
+
+template <typename... Args1, typename... Args2>
+bool
+check_tuple_cat(s::tuple<Args1...> t1, s::tuple<Args2...> t2)
+{
+    typedef s::tuple<Args1..., Args2...> concatenated;
+
+    auto cat1 = s::tuple_cat(t1, t2);
+    auto cat2 = s::tuple_cat(copy(t1), t2);
+    auto cat3 = s::tuple_cat(t1, copy(t2));
+    auto cat4 = s::tuple_cat(copy(t1), copy(t2));
+
+    static_assert(s::is_same<decltype(cat1), concatenated>::value, "");
+    static_assert(s::is_same<decltype(cat2), concatenated>::value, "");
+    static_assert(s::is_same<decltype(cat3), concatenated>::value, "");
+    static_assert(s::is_same<decltype(cat4), concatenated>::value, "");
+
+    auto ret = (cat1 == cat2);
+    ret &= (cat1 == cat3);
+    ret &= (cat1 == cat4);
+    return ret;
+}
+
+cl::sycl::cl_bool
+kernel_test()
+{
+    int i = 0;
+    s::tuple<> t0;
+    s::tuple<int&> t1(i);
+    s::tuple<int&, int> t2(i, 0);
+    s::tuple<int const&, int, float> t3(i, 0, 0.f);
+
+    auto ret = check_tuple_cat(t0, t0);
+    ret &= check_tuple_cat(t0, t1);
+    ret &= check_tuple_cat(t0, t2);
+    ret &= check_tuple_cat(t0, t3);
+
+    ret &= check_tuple_cat(t1, t0);
+    ret &= check_tuple_cat(t1, t1);
+    ret &= check_tuple_cat(t1, t2);
+    ret &= check_tuple_cat(t1, t3);
+
+    ret &= check_tuple_cat(t2, t0);
+    ret &= check_tuple_cat(t2, t1);
+    ret &= check_tuple_cat(t2, t2);
+    ret &= check_tuple_cat(t2, t3);
+
+    ret &= check_tuple_cat(t3, t0);
+    ret &= check_tuple_cat(t3, t1);
+    ret &= check_tuple_cat(t3, t2);
+    ret &= check_tuple_cat(t3, t3);
+    return ret;
+}
+
+int
+main()
+{
+    cl::sycl::queue deviceQueue;
+    cl::sycl::cl_bool ret = false;
+    cl::sycl::range<1> numOfItems{1};
+    {
+        cl::sycl::buffer<cl::sycl::cl_bool, 1> buffer1(&ret, numOfItems);
+        deviceQueue.submit([&](cl::sycl::handler& cgh) {
+            auto ret_access = buffer1.get_access<sycl_write>(cgh);
+            cgh.single_task<class KernelTest>([=]() { ret_access[0] = kernel_test(); });
+        });
+    }
+    if (ret)
+    {
+        std::cout << "pass" << std::endl;
+    }
+    else
+    {
+        std::cout << "fail" << std::endl;
+    }
+    return 0;
+}
---
+++ test/xpu_api/tuple/tuple.tuple/tuple.elem/get_const_rv.pass.cpp
@@ -0,0 +1,68 @@
+// <tuple>
+
+// template <class... Types> class tuple;
+
+// template <size_t I, class... Types>
+//   const typename tuple_element<I, tuple<Types...> >::type&&
+//   get(const tuple<Types...>&& t);
+
+#include "oneapi_std_test_config.h"
+#include "test_macros.h"
+#include <CL/sycl.hpp>
+#include <iostream>
+
+#ifdef USE_ONEAPI_STD
+#    include _ONEAPI_STD_TEST_HEADER(tuple)
+#    include _ONEAPI_STD_TEST_HEADER(utility)
+#    include _ONEAPI_STD_TEST_HEADER(type_traits)
+namespace s = oneapi_cpp_ns;
+#else
+#    include <tuple>
+#    include <utility>
+#    include <type_traits>
+namespace s = std;
+#endif
+
+constexpr cl::sycl::access::mode sycl_read = cl::sycl::access::mode::read;
+constexpr cl::sycl::access::mode sycl_write = cl::sycl::access::mode::write;
+
+class KernelGetConstRvTest;
+
+void
+kernel_test()
+{
+    cl::sycl::queue deviceQueue;
+    cl::sycl::cl_bool ret = false;
+    cl::sycl::range<1> numOfItems{1};
+    cl::sycl::buffer<cl::sycl::cl_bool, 1> buffer1(&ret, numOfItems);
+    deviceQueue.submit([&](cl::sycl::handler& cgh) {
+        auto ret_access = buffer1.get_access<sycl_write>(cgh);
+        cgh.single_task<class KernelGetConstRvTest>([=]() {
+            {
+                s::tuple<int> t(3);
+                static_assert(s::is_same<int&&, decltype(s::get<0>(s::move(t)))>::value, "");
+                static_assert(noexcept(s::get<0>(s::move(t))), "");
+                const int&& i = s::get<0>(s::move(t));
+                ret_access[0] = (i == 3);
+            }
+        });
+    });
+
+    auto ret_access_host = buffer1.get_access<sycl_read>();
+    if (ret_access_host[0])
+    {
+        std::cout << "Pass" << std::endl;
+    }
+    else
+    {
+        std::cout << "Fail" << std::endl;
+    }
+}
+
+int
+main()
+{
+
+    kernel_test();
+    return 0;
+}
