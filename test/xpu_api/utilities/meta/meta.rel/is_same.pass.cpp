2f2be136716bddb82e931766e5b7a30f353098e5
Sergey Kopienko <sergey.kopienko@intel.com>
2023-11-02 17:37:46 +0100

    Update test/xpu_api/utilities/meta/meta.rel/is_same.pass.cpp
    Co-authored-by: Konstantin Boyarinov <konstantin.boyarinov@intel.com>

--- test/xpu_api/utilities/meta/meta.rel/is_same.pass.cpp
+++ test/xpu_api/utilities/meta/meta.rel/is_same.pass.cpp
@@ -13,10 +13,6 @@
 //
 //===----------------------------------------------------------------------===//
 
-// type_traits
-
-// is_same
-
 #include "support/test_config.h"
 
 #include <oneapi/dpl/type_traits>
