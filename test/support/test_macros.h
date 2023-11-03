308411b9f52be62df75df63d61b8c47eb9ad74f6
Sergey Kopienko <sergey.kopienko@intel.com>
2023-11-03 14:38:05 +0100

    test/support/test_macros.h - remove #if TEST_STD_VER >= 11 check as not required

--- test/support/test_macros.h
+++ test/support/test_macros.h
@@ -117,25 +117,12 @@
 #endif
 #endif
 
-#if TEST_STD_VER >= 11
 # define TEST_ALIGNOF(...) alignof(__VA_ARGS__)
 # define TEST_ALIGNAS(...) alignas(__VA_ARGS__)
 # define TEST_CONSTEXPR constexpr
 # define TEST_NOEXCEPT noexcept
 # define TEST_NOEXCEPT_FALSE noexcept(false)
 # define TEST_NOEXCEPT_COND(...) noexcept(__VA_ARGS__)
-#else
-#   if defined(TEST_COMPILER_CLANG)
-#    define TEST_ALIGNOF(...) _Alignof(__VA_ARGS__)
-#   else
-#    define TEST_ALIGNOF(...) __alignof(__VA_ARGS__)
-#   endif
-# define TEST_ALIGNAS(...) __attribute__((__aligned__(__VA_ARGS__)))
-# define TEST_CONSTEXPR
-# define TEST_NOEXCEPT throw()
-# define TEST_NOEXCEPT_FALSE
-# define TEST_NOEXCEPT_COND(...)
-#endif
 
 #if TEST_STD_VER >= 11
 # define TEST_THROW_SPEC(...)
