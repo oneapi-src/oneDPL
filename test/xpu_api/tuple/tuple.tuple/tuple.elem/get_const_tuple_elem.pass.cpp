d81eb327bf3007d9303fa847252d4bcf11a7485f
Sergey Kopienko <sergey.kopienko@intel.com>
2023-11-06 11:25:49 +0100

    Rename test to avoid duplicated test target names

--- test/xpu_api/tuple/tuple.tuple/tuple.elem/get_const_tuple_elem.pass.cpp
+++ test/xpu_api/tuple/tuple.tuple/tuple.elem/get_const_tuple_elem.pass.cpp
@@ -46,7 +46,7 @@ kernel_test()
     });
 
     auto ret_access_host = buffer1.get_host_access(sycl::read_only);
-    EXPECT_TRUE(ret_access_host[0], "");
+    EXPECT_TRUE(ret_access_host[0], "Wrong result of dpl::get(const dpl::tuple) check");
 }
 #endif // TEST_DPCPP_BACKEND_PRESENT
 
