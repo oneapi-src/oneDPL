0f58c07c24ea397f1d63eabfe7ae7dac82cdf14f
Alberto Cabrera Pérez <alberto.cabrera@codeplay.com>
2023-11-07 10:51:21 +0000

    Scan_kt Flags and Values separated (#15)
    Atomic flags and the values used in Scan_kt separated to avoid truncating the range to 30bit values, and prepare for a more general scan implementation.

--- test/parallel_api/numeric/numeric.ops/scan_kt.pass.cpp
+++ test/parallel_api/numeric/numeric.ops/scan_kt.pass.cpp
@@ -22,23 +22,23 @@ int
 main()
 {
     bool all_passed = true;
+    sycl::queue q;
 
     for (int logn : {4, 8, 11, 16, 19, 21})
     {
-        std::cout << "Testing 2^" << logn << '\n';
+        std::cout << "Testing 2^" << logn << std::endl;
         int n = 1 << logn;
         std::vector<int> v(n, 1);
-        sycl::queue q;
         int* in_ptr = sycl::malloc_device<int>(n, q);
         int* out_ptr = sycl::malloc_device<int>(n, q);
 
-
-        q.copy(v.data(), in_ptr, n);
+        q.copy(v.data(), in_ptr, n).wait();
         using KernelParams = oneapi::dpl::experimental::kt::kernel_param<128, 2, class ScanKernel>;
         oneapi::dpl::experimental::kt::single_pass_inclusive_scan<KernelParams>(q, in_ptr, in_ptr+n, out_ptr, ::std::plus<int>());
 
         std::vector<int> tmp(n, 0);
         q.copy(out_ptr, tmp.data(), n);
+        q.wait();
 
         std::inclusive_scan(v.begin(), v.end(), v.begin());
 
@@ -53,11 +53,13 @@ main()
         }
 
         if (passed)
-            std::cout << "passed" << std::endl;
+            std::cout << " passed" << std::endl;
         else
-            std::cout << "failed" << std::endl;
+            std::cout << " failed" << std::endl;
 
         all_passed &= passed;
+        sycl::free(in_ptr, q);
+        sycl::free(out_ptr, q);
     }
 
     return !all_passed;
