4d00ceb0ce541a70718d4718b78183a27072142c
Dan Hoeflinger <dan.hoeflinger@intel.com>
2023-11-08 16:35:08 -0500

    simplifying serial implementation
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- test/support/histogram_serial_impl.h
+++ test/support/histogram_serial_impl.h
@@ -16,97 +16,48 @@
 #ifndef _HISTOGRAM_SERIAL_IMPL_H
 #define _HISTOGRAM_SERIAL_IMPL_H
 
-template <typename _T1, bool _IsFloatingPoint>
-struct evenly_divided_binhash_impl{};
-
-template <typename _T>
-struct evenly_divided_binhash_impl<_T, /* is_floating_point = */ true>
+template <typename T1, typename T2, typename Size>
+::std::enable_if_t<!std::is_floating_point_v<T1>, ::std::uint32_t>
+get_bin(const T1& value, const T2& min, const T2& max, const Size& num_bins)
 {
-    _T __minimum;
-    _T __maximum;
-    _T __scale;
+    return ::std::uint32_t(((::std::uint64_t(value) - min) * ::std::uint64_t(num_bins)) / (max - min));
+}
 
-    evenly_divided_binhash_impl(const _T& min, const _T& max, const ::std::uint32_t& num_bins)
-        : __minimum(min), __maximum(max), __scale(_T(num_bins) / (max - min))
-    {
-    }
-
-    template <typename _T2>
-    inline ::std::uint32_t
-    get_bin(_T2&& value) const
-    {
-        return ::std::uint32_t((::std::forward<_T2>(value) - __minimum) * __scale);
-    }
-
-    template <typename _T2>
-    inline bool
-    is_valid(const _T2& value) const
-    {
-        return (value >= __minimum) && (value < __maximum);
-    }
-};
-
-template <typename _T>
-struct evenly_divided_binhash_impl<_T, /* is_floating_point= */ false>
+template <typename T1, typename T2, typename Size>
+::std::enable_if_t<std::is_floating_point_v<T1>, ::std::uint32_t>
+get_bin(const T1& value, const T2& min, const T2& max, const Size& num_bins)
 {
-    _T __minimum;
-    _T __range_size;
-    ::std::uint32_t __num_bins;
-    evenly_divided_binhash_impl(const _T& min, const _T& max, const ::std::uint32_t& num_bins)
-        : __minimum(min), __num_bins(num_bins), __range_size(max - min)
-    {
-    }
-    template <typename _T2>
-    inline ::std::uint32_t
-    get_bin(_T2&& value) const
-    {
-        return ::std::uint32_t(((::std::uint64_t(::std::forward<_T2>(value)) - __minimum) * ::std::uint64_t(__num_bins)) / __range_size);
-    }
+    return ::std::uint32_t((value - min) * (T1(num_bins) / (max - min)));
+}
 
-    template <typename _T2>
-    inline bool
-    is_valid(const _T2& value) const
-    {
-        return (value >= __minimum) && (value < (__minimum + __range_size));
-    }
-
-};
-
-template <typename _T1>
-using evenly_divided_binhash = evenly_divided_binhash_impl<_T1, std::is_floating_point_v<_T1>>;
-
-template <typename _ForwardIterator>
-struct custom_range_binhash
+template <typename _InputIterator1, typename _Size, typename _T, typename _OutputIterator>
+_OutputIterator
+histogram_sequential(_InputIterator1 __first, _InputIterator1 __last, const _Size& __num_bins,
+                     const _T& __first_bin_min_val, const _T& __last_bin_max_val, _OutputIterator __histogram_first)
 {
-    _ForwardIterator __first;
-    _ForwardIterator __last;
-    custom_range_binhash(_ForwardIterator first, _ForwardIterator last)
-        : __first(first), __last(last)
+    for (int bin = 0; bin < __num_bins; bin++)
     {
+        __histogram_first[bin] = 0;
     }
 
-    template <typename _T>
-    ::std::uint32_t
-    get_bin(_T&& value) const
+    for (auto tmp = __first; tmp < __last; ++tmp)
     {
-        return (::std::upper_bound(__first, __last, ::std::forward<_T>(value)) - __first) - 1;
+        auto value = *tmp;
+        if (value >= __first_bin_min_val && value < __last_bin_max_val)
+        {
+            _Size bin = get_bin(value, __first_bin_min_val, __last_bin_max_val, __num_bins);
+            ++(__histogram_first[bin]);
+        }
     }
+    return __histogram_first + __num_bins;
+}
 
-    template <typename _T2>
-    inline bool
-    is_valid(const _T2& value) const
-    {
-        return (value >= (*__first)) && (value < (*(__last-1)));
-    }
-
-
-};
-
-template <typename _ForwardIterator, typename _RandomAccessIterator, typename _Size, typename _IdxHashFunc>
-_RandomAccessIterator
-histogram_general_sequential(_ForwardIterator __first, _ForwardIterator __last, _RandomAccessIterator __histogram_first,
-                  _Size num_bins, _IdxHashFunc __func)
+template <typename _InputIterator1, typename _InputIterator2, typename _OutputIterator>
+_OutputIterator
+histogram_sequential(_InputIterator1 __first, _InputIterator1 __last, _InputIterator2 __boundary_first,
+                     _InputIterator2 __boundary_last, _OutputIterator __histogram_first)
 {
+    int num_bins = (__boundary_last - __boundary_first) - 1;
     for (int bin = 0; bin < num_bins; bin++)
     {
         __histogram_first[bin] = 0;
@@ -114,35 +65,15 @@ histogram_general_sequential(_ForwardIterator __first, _ForwardIterator __last, 
 
     for (auto tmp = __first; tmp < __last; ++tmp)
     {
-        if (__func.is_valid(*tmp))
+        auto value = *tmp;
+        if ((value >= (*__boundary_first)) && (value < (*(__boundary_last - 1))))
         {
-            _Size bin = __func.get_bin(*tmp);
+            ::std::ptrdiff_t bin =
+                (::std::upper_bound(__boundary_first, __boundary_last, value) - __boundary_first) - 1;
             ++(__histogram_first[bin]);
         }
     }
     return __histogram_first + num_bins;
 }
 
-
-template <typename _InputIterator1, typename _Size, typename _T, typename _OutputIterator>
-_OutputIterator
-histogram_sequential(_InputIterator1 __first, _InputIterator1 __last, const _Size& __num_bins,
-                     const _T& __first_bin_min_val, const _T& __last_bin_max_val, _OutputIterator __histogram_first)
-{
-    return histogram_general_sequential(__first, __last, __histogram_first, __num_bins,
-                             evenly_divided_binhash<_T>(__first_bin_min_val, __last_bin_max_val, __num_bins));
-}
-
-
-template <typename _InputIterator1, typename _InputIterator2, typename _OutputIterator>
-_OutputIterator
-histogram_sequential(_InputIterator1 __first, _InputIterator1 __last, _InputIterator2 __boundary_first,
-                     _InputIterator2 __boundary_last, _OutputIterator __histogram_first)
-{
-    return histogram_general_sequential(__first, __last, __histogram_first, (__boundary_last - __boundary_first) - 1,
-                             custom_range_binhash{__boundary_first, __boundary_last});
-}
-
-
-
-#endif // _HISTOGRAM_SERIAL_IMPL_H 
+#endif // _HISTOGRAM_SERIAL_IMPL_H
