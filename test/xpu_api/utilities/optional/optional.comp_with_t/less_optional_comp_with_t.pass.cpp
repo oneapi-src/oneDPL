83370005e3b8d4f2667f015662be87873e2516f8
Sergey Kopienko <sergey.kopienko@intel.com>
2023-11-08 13:53:31 +0100

    Update test/xpu_api/utilities/optional/optional.comp_with_t/less_optional_comp_with_t.pass.cpp
    Co-authored-by: Konstantin Boyarinov <konstantin.boyarinov@intel.com>

--- test/xpu_api/utilities/optional/optional.comp_with_t/less_optional_comp_with_t.pass.cpp
+++ test/xpu_api/utilities/optional/optional.comp_with_t/less_optional_comp_with_t.pass.cpp
@@ -51,7 +51,7 @@ kernel_test()
         sycl::buffer<O, 1> buffer2(ia, numOfItems2);
         q.submit([&](sycl::handler& cgh) {
             auto ret_access = buffer1.get_access<sycl::access::mode::write>(cgh);
-            auto ia_acc = buffer2.get_access<sycl::access::mode::write>(cgh);
+            auto ia_acc = buffer2.get_access<sycl::access::mode::read>(cgh);
             cgh.single_task<class KernelTest>([=]() {
                 {
                     ret_access[0] &= ((ia_acc[0] < X(1)));
