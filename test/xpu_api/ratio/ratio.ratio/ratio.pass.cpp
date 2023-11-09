b67fb997b2139c730f18897210bae38f408256d9
Sergey Kopienko <sergey.kopienko@intel.com>
2023-11-09 18:36:44 +0100

    test/xpu_api/ratio/ratio.ratio/ratio.pass.cpp - fix review comment

--- test/xpu_api/ratio/ratio.ratio/ratio.pass.cpp
+++ test/xpu_api/ratio/ratio.ratio/ratio.pass.cpp
@@ -22,24 +22,19 @@
 
 #if TEST_DPCPP_BACKEND_PRESENT
 template <long long N, long long D, long long eN, long long eD, class KernelName>
-bool
+void
 test()
 {
     sycl::queue deviceQueue = TestUtils::get_test_queue();
-    bool ret = false;
     sycl::range<1> item1{1};
     {
-        sycl::buffer<bool, 1> buffer1(&ret, item1);
         deviceQueue.submit([&](sycl::handler& cgh) {
-            auto ret_acc = buffer1.get_access<sycl::access::mode::write>(cgh);
             cgh.single_task<KernelName>([=]() {
                 static_assert(dpl::ratio<N, D>::num == eN);
                 static_assert(dpl::ratio<N, D>::den == eD);
-                ret_acc[0] = true;
             });
         });
     }
-    return ret;
 }
 
 class T1;
@@ -64,31 +59,30 @@ class T19;
 class T20;
 #endif // TEST_DPCPP_BACKEND_PRESENT
 
-int
+void
 main()
 {
 #if TEST_DPCPP_BACKEND_PRESENT
-    auto ret = test<1, 1, 1, 1, T1>();
-    ret &= test<1, 10, 1, 10, T2>();
-    ret &= test<10, 10, 1, 1, T3>();
-    ret &= test<10, 1, 10, 1, T4>();
-    ret &= test<12, 4, 3, 1, T5>();
-    ret &= test<12, -4, -3, 1, T6>();
-    ret &= test<-12, 4, -3, 1, T7>();
-    ret &= test<-12, -4, 3, 1, T8>();
-    ret &= test<4, 12, 1, 3, T9>();
-    ret &= test<4, -12, -1, 3, T10>();
-    ret &= test<-4, 12, -1, 3, T11>();
-    ret &= test<-4, -12, 1, 3, T12>();
-    ret &= test<222, 333, 2, 3, T13>();
-    ret &= test<222, -333, -2, 3, T14>();
-    ret &= test<-222, 333, -2, 3, T15>();
-    ret &= test<-222, -333, 2, 3, T16>();
-    ret &= test<0x7FFFFFFFFFFFFFFFLL, 127, 72624976668147841LL, 1, T17>();
-    ret &= test<-0x7FFFFFFFFFFFFFFFLL, 127, -72624976668147841LL, 1, T18>();
-    ret &= test<0x7FFFFFFFFFFFFFFFLL, -127, -72624976668147841LL, 1, T19>();
-    ret &= test<-0x7FFFFFFFFFFFFFFFLL, -127, 72624976668147841LL, 1, T20>();
-    EXPECT_TRUE(ret, "Wrong result of dpl::ratio check");
+    test<1, 1, 1, 1, T1>();
+    test<1, 10, 1, 10, T2>();
+    test<10, 10, 1, 1, T3>();
+    test<10, 1, 10, 1, T4>();
+    test<12, 4, 3, 1, T5>();
+    test<12, -4, -3, 1, T6>();
+    test<-12, 4, -3, 1, T7>();
+    test<-12, -4, 3, 1, T8>();
+    test<4, 12, 1, 3, T9>();
+    test<4, -12, -1, 3, T10>();
+    test<-4, 12, -1, 3, T11>();
+    test<-4, -12, 1, 3, T12>();
+    test<222, 333, 2, 3, T13>();
+    test<222, -333, -2, 3, T14>();
+    test<-222, 333, -2, 3, T15>();
+    test<-222, -333, 2, 3, T16>();
+    test<0x7FFFFFFFFFFFFFFFLL, 127, 72624976668147841LL, 1, T17>();
+    test<-0x7FFFFFFFFFFFFFFFLL, 127, -72624976668147841LL, 1, T18>();
+    test<0x7FFFFFFFFFFFFFFFLL, -127, -72624976668147841LL, 1, T19>();
+    test<-0x7FFFFFFFFFFFFFFFLL, -127, 72624976668147841LL, 1, T20>();
 #endif // TEST_DPCPP_BACKEND_PRESENT
 
     return TestUtils::done(TEST_DPCPP_BACKEND_PRESENT);
