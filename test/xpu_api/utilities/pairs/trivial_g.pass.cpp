5aefd160ca2d4991ee229be7195968d602099865
Sergey Kopienko <sergey.kopienko@intel.com>
2023-11-10 13:05:13 +0100

    test/xpu_api/utilities/pairs/trivial_g.pass.cpp- fix review comments

--- test/xpu_api/utilities/pairs/trivial_g.pass.cpp
+++ test/xpu_api/utilities/pairs/trivial_g.pass.cpp
@@ -21,33 +21,21 @@
 #include "support/test_macros.h"
 #include "support/utils.h"
 
-#if TEST_DPCPP_BACKEND_PRESENT
-bool
-test_trivial()
-{
-    typedef dpl::pair<int, int> pair_type;
-    static_assert(dpl::is_trivially_copy_constructible<pair_type>::value, "! triv copy");
-    static_assert(dpl::is_trivially_destructible<pair_type>::value, "! triv destructor");
-    return true;
-}
-#endif // TEST_DPCPP_BACKEND_PRESENT
-
 int
 main()
 {
 #if TEST_DPCPP_BACKEND_PRESENT
     sycl::queue deviceQueue = TestUtils::get_test_queue();
-    bool ret = false;
     sycl::range<1> numOfItems{1};
     {
-        sycl::buffer<bool, 1> buffer1(&ret, numOfItems);
         deviceQueue.submit([&](sycl::handler& cgh) {
-            auto ret_access = buffer1.get_access<sycl::access::mode::write>(cgh);
-            cgh.single_task<class KernelTest>([=]() { ret_access[0] = test_trivial(); });
+            cgh.single_task<class KernelTest>([=]() {
+                typedef dpl::pair<int, int> pair_type;
+                static_assert(dpl::is_trivially_copy_constructible<pair_type>::value);
+                static_assert(dpl::is_trivially_destructible<pair_type>::value);
+            });
         });
     }
-
-    EXPECT_TRUE(ret, "Wrong result of dpl::pair check in test_trivial");
 #endif // TEST_DPCPP_BACKEND_PRESENT
 
     return TestUtils::done(TEST_DPCPP_BACKEND_PRESENT);
