45306f94ceca4d05a2ad7d3b494e8dcd54be750c
Sergey Kopienko <sergey.kopienko@intel.com>
2023-11-10 12:38:29 +0100

    test/xpu_api/utilities/pairs/pairs.pair/default_constructible_g.pass.cpp - fix review comments

--- test/xpu_api/utilities/pairs/pairs.pair/default_constructible_g.pass.cpp
+++ test/xpu_api/utilities/pairs/pairs.pair/default_constructible_g.pass.cpp
@@ -22,7 +22,7 @@
 #include "support/utils.h"
 
 #if TEST_DPCPP_BACKEND_PRESENT
-bool
+void
 test_non_default_constructible()
 {
     struct X
@@ -33,7 +33,6 @@ test_non_default_constructible()
     typedef dpl::pair<int, X> P;
     static_assert(!dpl::is_constructible<P>::value);
     static_assert(!dpl::is_default_constructible<P>::value);
-    return true;
 }
 #endif // TEST_DPCPP_BACKEND_PRESENT
 
@@ -42,17 +41,12 @@ main()
 {
 #if TEST_DPCPP_BACKEND_PRESENT
     sycl::queue deviceQueue = TestUtils::get_test_queue();
-    bool ret = false;
     sycl::range<1> numOfItems{1};
     {
-        sycl::buffer<bool, 1> buffer1(&ret, numOfItems);
         deviceQueue.submit([&](sycl::handler& cgh) {
-            auto ret_access = buffer1.get_access<sycl::access::mode::write>(cgh);
-            cgh.single_task<class KernelTest>([=]() { ret_access[0] = test_default_constructible(); });
+            cgh.single_task<class KernelTest>([=]() { test_non_default_constructible(); });
         });
     }
-
-    EXPECT_TRUE(ret, "Wrong result of dpl::pair default constructible check");
 #endif // TEST_DPCPP_BACKEND_PRESENT
 
     return TestUtils::done(TEST_DPCPP_BACKEND_PRESENT);
