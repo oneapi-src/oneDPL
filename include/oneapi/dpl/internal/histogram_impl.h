edc684e773eac0c17b53d2c3d48efd04972fa573
Dan Hoeflinger <dan.hoeflinger@intel.com>
2023-11-13 15:48:28 -0500

    formatting and template naming convention
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- include/oneapi/dpl/internal/histogram_impl.h
+++ include/oneapi/dpl/internal/histogram_impl.h
@@ -29,33 +29,37 @@ namespace dpl
 namespace __internal
 {
 
-template <typename _ExecutionPolicy, typename _Iter1, typename _Iter2, typename _Size, typename _IdxHashFunc, typename... _Range>
+template <typename _ExecutionPolicy, typename _RandomAccessIterator1, typename _RandomAccessIterator2, typename _Size,
+          typename _IdxHashFunc, typename... _Range>
 inline void
-__pattern_histogram(_ExecutionPolicy&& exec, _Iter1 __first, _Iter1 __last, _Iter2 __histogram_first, const _Size& __num_bins,
-                    _IdxHashFunc __func, _Range&&... __opt_range)
+__pattern_histogram(_ExecutionPolicy&& exec, _RandomAccessIterator1 __first, _RandomAccessIterator1 __last,
+                    _RandomAccessIterator2 __histogram_first, const _Size& __num_bins, _IdxHashFunc __func,
+                    _Range&&... __opt_range)
 {
     //If there are no histogram bins there is nothing to do
     if (__num_bins > 0)
     {
         oneapi::dpl::__par_backend_hetero::__parallel_histogram(::std::forward<_ExecutionPolicy>(exec), __first, __last,
-                                                            __histogram_first, __num_bins, __func, __opt_range...);
+                                                                __histogram_first, __num_bins, __func, __opt_range...);
     }
 }
 
-template <typename _ExecutionPolicy, typename Iter1, typename OutputIter, typename _Size, typename _T>
+template <typename _ExecutionPolicy, typename _RandomAccessIterator1, typename _RandomAccessIterator2, typename _Size,
+          typename _T>
 oneapi::dpl::__internal::__enable_if_hetero_execution_policy<typename ::std::decay<_ExecutionPolicy>::type>
-__histogram_impl(_ExecutionPolicy&& exec, Iter1 __first, Iter1 __last, OutputIter __histogram_first, const _Size& __num_bins,
-                 const _T& __first_bin_min_val, const _T& __last_bin_max_val)
+__histogram_impl(_ExecutionPolicy&& exec, _RandomAccessIterator1 __first, _RandomAccessIterator1 __last,
+                 _RandomAccessIterator2 __histogram_first, const _Size& __num_bins, const _T& __first_bin_min_val,
+                 const _T& __last_bin_max_val)
 {
     __internal::__pattern_histogram(
         ::std::forward<_ExecutionPolicy>(exec), __first, __last, __histogram_first, __num_bins,
         __internal::__evenly_divided_binhash<_T>(__first_bin_min_val, __last_bin_max_val, __num_bins));
 }
 
-template <typename _ExecutionPolicy, typename Iter1, typename OutputIter, typename Iter3>
+template <typename _ExecutionPolicy, typename _RandomAccessIterator1, typename _RandomAccessIterator2, typename Iter3>
 oneapi::dpl::__internal::__enable_if_hetero_execution_policy<typename ::std::decay<_ExecutionPolicy>::type>
-__histogram_impl(_ExecutionPolicy&& exec, Iter1 __first, Iter1 __last, OutputIter __histogram_first, Iter3 __boundary_first,
-                 Iter3 __boundary_last)
+__histogram_impl(_ExecutionPolicy&& exec, _RandomAccessIterator1 __first, _RandomAccessIterator1 __last,
+                 _RandomAccessIterator2 __histogram_first, Iter3 __boundary_first, Iter3 __boundary_last)
 {
     auto boundary_view = oneapi::dpl::__ranges::guard_view<Iter3>(__boundary_first, __boundary_last);
     __internal::__pattern_histogram(::std::forward<_ExecutionPolicy>(exec), __first, __last, __histogram_first,
@@ -64,24 +68,28 @@ __histogram_impl(_ExecutionPolicy&& exec, Iter1 __first, Iter1 __last, OutputIte
 
 } // namespace __internal
 
-template <typename _ExecutionPolicy, typename _RandomAccessIterator1, typename _Size, typename _T, typename _RandomAccessIterator2>
+template <typename _ExecutionPolicy, typename _RandomAccessIterator1, typename _Size, typename _T,
+          typename _RandomAccessIterator2>
 oneapi::dpl::__internal::__enable_if_execution_policy<_ExecutionPolicy, _RandomAccessIterator2>
-histogram(_ExecutionPolicy&& exec, _RandomAccessIterator1 __first, _RandomAccessIterator1 __last, const _Size& __num_bins,
-          const _T& __first_bin_min_val, const _T& __last_bin_max_val, _RandomAccessIterator2 __histogram_first)
+histogram(_ExecutionPolicy&& exec, _RandomAccessIterator1 __first, _RandomAccessIterator1 __last,
+          const _Size& __num_bins, const _T& __first_bin_min_val, const _T& __last_bin_max_val,
+          _RandomAccessIterator2 __histogram_first)
 {
-    __internal::__histogram_impl(::std::forward<_ExecutionPolicy>(exec), __first, __last, __histogram_first,
-                                    __num_bins, __first_bin_min_val, __last_bin_max_val);
+    __internal::__histogram_impl(::std::forward<_ExecutionPolicy>(exec), __first, __last, __histogram_first, __num_bins,
+                                 __first_bin_min_val, __last_bin_max_val);
     return __histogram_first + __num_bins;
 }
 
-template <typename _ExecutionPolicy, typename _RandomAccessIterator1, typename _RandomAccessIterator2, typename _RandomAccessIterator3>
+template <typename _ExecutionPolicy, typename _RandomAccessIterator1, typename _RandomAccessIterator2,
+          typename _RandomAccessIterator3>
 oneapi::dpl::__internal::__enable_if_execution_policy<_ExecutionPolicy, _RandomAccessIterator3>
-histogram(_ExecutionPolicy&& exec, _RandomAccessIterator1 __first, _RandomAccessIterator1 __last, _RandomAccessIterator2 __boundary_first,
-          _RandomAccessIterator2 __boundary_last, _RandomAccessIterator3 __histogram_first)
+histogram(_ExecutionPolicy&& exec, _RandomAccessIterator1 __first, _RandomAccessIterator1 __last,
+          _RandomAccessIterator2 __boundary_first, _RandomAccessIterator2 __boundary_last,
+          _RandomAccessIterator3 __histogram_first)
 {
     ::std::ptrdiff_t __num_bins = __boundary_last - __boundary_first - 1;
     __internal::__histogram_impl(::std::forward<_ExecutionPolicy>(exec), __first, __last, __histogram_first,
-                                    __boundary_first, __boundary_last);
+                                 __boundary_first, __boundary_last);
     return __histogram_first + __num_bins;
 }
 
