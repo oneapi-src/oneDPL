13da76c37de921be3905d1647d8ff711f836dba4
Sergey Kopienko <sergey.kopienko@intel.com>
2023-11-14 10:15:45 +0100

    test/xpu_api/tuple/tuple.tuple/tuple.helper/tuple_helper_size.pass.cpp - fix review comment: These checks can be static assertions

--- test/xpu_api/tuple/tuple.tuple/tuple.helper/tuple_helper_size.pass.cpp
+++ test/xpu_api/tuple/tuple.tuple/tuple.helper/tuple_helper_size.pass.cpp
@@ -30,22 +30,16 @@ void
 kernel_test()
 {
     sycl::queue deviceQueue = TestUtils::get_test_queue();
-    bool ret = false;
     sycl::range<1> numOfItems{1};
     sycl::buffer<bool, 1> buffer1(&ret, numOfItems);
     deviceQueue.submit([&](sycl::handler& cgh) {
-        auto ret_access = buffer1.get_access<sycl::access::mode::write>(cgh);
         cgh.single_task<class KernelTupleSizeTest>([=]() {
-            ret_access[0] = (dpl::is_base_of_v<dpl::integral_constant<dpl::size_t, N>, dpl::tuple_size<T>>);
-            ret_access[0] &= (dpl::is_base_of_v<dpl::integral_constant<dpl::size_t, N>, dpl::tuple_size<const T>>);
-            ret_access[0] &= (dpl::is_base_of_v<dpl::integral_constant<dpl::size_t, N>, dpl::tuple_size<volatile T>>);
-            ret_access[0] &=
-                (dpl::is_base_of_v<dpl::integral_constant<dpl::size_t, N>, dpl::tuple_size<const volatile T>>);
+            static_assert(dpl::is_base_of_v<dpl::integral_constant<dpl::size_t, N>, dpl::tuple_size<T>>);
+            static_assert(dpl::is_base_of_v<dpl::integral_constant<dpl::size_t, N>, dpl::tuple_size<const T>>);
+            static_assert(dpl::is_base_of_v<dpl::integral_constant<dpl::size_t, N>, dpl::tuple_size<volatile T>>);
+            static_assert(dpl::is_base_of_v<dpl::integral_constant<dpl::size_t, N>, dpl::tuple_size<const volatile T>>);
         });
     });
-
-    auto ret_access_host = buffer1.get_host_access(sycl::read_only);
-    EXPECT_TRUE(ret_access_host[0], "Wrong result of dpl::tuple_size check");
 }
 #endif // TEST_DPCPP_BACKEND_PRESENT
 
