3cf9db2a602718680151041df08a7cdb7d3e6a45
Dan Hoeflinger <dan.hoeflinger@intel.com>
2023-11-15 15:33:01 -0600

    rename binhash wrappe
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_histogram.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_histogram.h
@@ -37,10 +37,10 @@ namespace __par_backend_hetero
 
 //no boost baseline
 template <typename _BinHash>
-struct __SLM_boost
+struct __binhash_SLM_wrapper
 {
     _BinHash __bin_hash;
-    __SLM_boost(_BinHash __bin_hash) : __bin_hash(__bin_hash) {}
+    __binhash_SLM_wrapper(_BinHash __bin_hash) : __bin_hash(__bin_hash) {}
 
     template <typename _T2>
     inline ::std::uint32_t
@@ -83,14 +83,14 @@ struct __SLM_boost
 };
 
 template <typename _Range>
-struct __SLM_boost<oneapi::dpl::__internal::__custom_range_binhash<_Range>>
+struct __binhash_SLM_wrapper<oneapi::dpl::__internal::__custom_range_binhash<_Range>>
 {
     using _BinHashType = typename oneapi::dpl::__internal::__custom_range_binhash<_Range>;
     _BinHashType __bin_hash;
 
     using __boundary_type = typename _BinHashType::__boundary_type;
 
-    __SLM_boost(_BinHashType __bin_hash) : __bin_hash(__bin_hash) {}
+    __binhash_SLM_wrapper(_BinHashType __bin_hash) : __bin_hash(__bin_hash) {}
 
     template <typename _T2>
     inline ::std::uint32_t
@@ -504,8 +504,8 @@ __parallel_histogram_impl(_ExecutionPolicy&& __exec, _Iter1 __first, _Iter1 __la
 {
     //wrap binhash in a wrapper to allow shared memory boost where available
     return __parallel_histogram_sycl_impl<__iters_per_work_item>(
-        ::std::forward<_ExecutionPolicy>(__exec), __first, __last, __histogram_first, __num_bins, __SLM_boost(__func),
-        ::std::forward<_Range...>(__opt_range)...);
+        ::std::forward<_ExecutionPolicy>(__exec), __first, __last, __histogram_first, __num_bins,
+        __binhash_SLM_wrapper(__func), ::std::forward<_Range...>(__opt_range)...);
 }
 
 template <::std::uint16_t __iters_per_work_item, typename _ExecutionPolicy, typename _Iter1, typename _Iter2,
