fd8cd674c67a292174753082d6f9b14741be74c6
Sergey Kopienko <sergey.kopienko@intel.com>
2023-11-16 12:59:58 +0100

    test/parallel_api/algorithm/alg.modifying.operations/transform_binary_zip.pass.cpp - fix compile error with std::tuple usage

--- test/parallel_api/algorithm/alg.modifying.operations/transform_binary_zip.pass.cpp
+++ test/parallel_api/algorithm/alg.modifying.operations/transform_binary_zip.pass.cpp
@@ -22,24 +22,20 @@
 
 using namespace TestUtils;
 
-template <typename... Types>
-using typle_t = oneapi::dpl::__internal::tuple<Types...>;
-//using typle_t = ::std::tuple<Types...>;
-
-template <typename Out>
+template <typename TInput1, typename TInput2, typename TOutput>
 class TheOperation
 {
-    Out val;
+    TOutput val;
 
-  public:
+public:
 
-    TheOperation(Out v) : val(v) {}
+    TheOperation(TOutput v) : val(v) {}
 
-    template <typename T1A, typename T1B, typename T2A, typename T2B>
-    Out
-    operator()(const typle_t<T1A, T1B>& x, const typle_t<T2A, T2B>& y) const
+    TOutput
+    operator()(const ::std::tuple<const TInput1&, const TInput1&>& x,
+               const ::std::tuple<const TInput2&, const TInput2&>& y) const
     {
-        return Out(val + (std::get<0>(x) + std::get<1>(x)) - (std::get<0>(y) + std::get<1>(y)));
+        return TOutput(val + (::std::get<0>(x) + ::std::get<1>(x)) - (::std::get<0>(y) + ::std::get<1>(y)));
     }
 };
 
@@ -52,7 +48,7 @@ check_and_reset(InputIterator1 first1, InputIterator1 last1, InputIterator2 firs
     for (; first1 != last1; ++first1, ++first2, ++out_first, ++k)
     {
         // check
-        Out expected = Out(1.5) + (std::get<0>(*first1) + std::get<1>(*first1)) - (std::get<0>(*first2) + std::get<1>(*first2));
+        Out expected = Out(1.5) + (::std::get<0>(*first1) + ::std::get<1>(*first1)) - (::std::get<0>(*first2) + ::std::get<1>(*first2));
         Out actual = *out_first;
         if (::std::is_floating_point_v<Out>)
         {
@@ -82,25 +78,25 @@ struct test_one_policy
     }
 };
 
-template <typename In1, typename In2, typename Out, typename Predicate>
+template <typename TInput1, typename TInput2, typename TOutput, typename Predicate>
 void
 test(Predicate pred)
 {
     for (size_t n = 0; n <= 100000; n = n <= 16 ? n + 1 : size_t(3.1415 * n))
     {
-        Sequence<In1> in1_a(n, [](size_t k) { return k % 5 != 1 ? In1(3 * k + 7) : 0; });
-        Sequence<In1> in1_b(n, [](size_t k) { return 0; });
+        Sequence<TInput1> in1_a(n, [](size_t k) { return k % 5 != 1 ? TInput1(3 * k + 7) : 0; });
+        Sequence<TInput1> in1_b(n, [](size_t k) { return 0; });
         auto in1_zip_begin = dpl::make_zip_iterator(in1_a.begin(), in1_b.begin());
         auto in1_zip_end   = dpl::make_zip_iterator(in1_a.end(),   in1_b.end());
 
-        Sequence<In2> in2_a(n, [](size_t k) { return k % 7 != 2 ? In2(5 * k + 5) : 0; });
-        Sequence<In2> in2_b(n, [](size_t k) { return 0; });
+        Sequence<TInput2> in2_a(n, [](size_t k) { return k % 7 != 2 ? TInput2(5 * k + 5) : 0; });
+        Sequence<TInput2> in2_b(n, [](size_t k) { return 0; });
         auto in2_zip_begin = dpl::make_zip_iterator(in2_a.begin(), in1_b.begin());
         auto in2_zip_end   = dpl::make_zip_iterator(in2_a.end(),   in2_b.end());
 
-        Sequence<Out> out(n, [](size_t) { return -1; });
+        Sequence<TOutput> out(n, [](size_t) { return -1; });
 
-        invoke_on_all_policies<0>()(test_one_policy<In1, In2, Out>(),
+        invoke_on_all_policies<0>()(test_one_policy<TInput1, TInput2, TOutput>(),
                                     in1_zip_begin, in1_zip_end,
                                     in2_zip_begin, in2_zip_end,
                                     out.begin(), out.end(),
@@ -114,7 +110,7 @@ test(Predicate pred)
             auto in2_zip_cbegin = dpl::make_zip_iterator(in2_a.cbegin(), in1_b.cbegin());
             auto in2_zip_cend   = dpl::make_zip_iterator(in2_a.cend(),   in2_b.cend());
 
-            invoke_on_all_policies<1>()(test_one_policy<In1, In2, Out>(),
+            invoke_on_all_policies<1>()(test_one_policy<TInput1, TInput2, TOutput>(),
                                         in1_zip_cbegin, in1_zip_cend,
                                         in2_zip_cbegin, in2_zip_cend,
                                         out.begin(), out.end(),
@@ -124,29 +120,49 @@ test(Predicate pred)
     }
 }
 
+template <typename TInput1, typename TInput2, typename TOutput>
+void test_with_pred(TOutput initial_value)
+{
+    TheOperation<TInput1, TInput2, TOutput> pred(initial_value);
+
+    test<TInput1, TInput2, TOutput>(pred);
+}
+
+template <typename TInput1, typename TInput2, typename TOutput>
+void
+test_with_pred_non_const(TOutput initial_value)
+{
+    TheOperation<TInput1, TInput2, TOutput> pred(initial_value);
+
+    test<TInput1, TInput2, TOutput>(non_const(pred));
+}
+
+template <typename TInput1, typename TInput2, typename TOutput>
+void
+test_with_lambda(TOutput initial_value)
+{
+    test<TInput1, TInput2, TOutput>(
+        [initial_value](const ::std::tuple<const TInput1&, const TInput1&>& x,
+                        const ::std::tuple<const TInput2&, const TInput2&>& y)
+        {
+            return TOutput(initial_value + (::std::get<0>(x) + ::std::get<1>(x)) - (::std::get<0>(y) + ::std::get<1>(y)));
+        });
+}
+
 int
 main()
 {
     //const operator()
-    test<std::int32_t, std::int32_t, std::int32_t>(TheOperation<std::int32_t>(1));
-    test<float32_t, float32_t, float32_t>(TheOperation<float32_t>(1.5));
+    test_with_pred<std::int32_t, std::int32_t, std::int32_t>(1);
+    test_with_pred<float32_t, float32_t, float32_t>(1.5);
 
     //non-const operator()
-    test<std::int32_t, std::int32_t, std::int32_t>(non_const(TheOperation<std::int32_t>(1)));
-    test<float32_t, float32_t, float32_t>(non_const(TheOperation<float32_t>(1.5)));
+    test_with_pred_non_const<std::int32_t, std::int32_t, std::int32_t>(1);
+    test_with_pred_non_const<float32_t, float32_t, float32_t>(1.5);
 
     // lambda
-    test<std::int32_t, std::int32_t, std::int32_t>(
-        [](const typle_t<const std::int32_t&, const std::int32_t&>& x, const typle_t<const std::int32_t&, const std::int32_t&>& y)
-        {
-            return std::int32_t(1 + (std::get<0>(x) + std::get<1>(x)) - (std::get<0>(y) + std::get<1>(y)));
-        });
-
-    test<float32_t, float32_t, float32_t>(
-        [](const typle_t<const float32_t&, const float32_t&>& x, const typle_t<const float32_t&, const float32_t&>& y)
-        {
-            return float32_t(1 + (std::get<0>(x) + std::get<1>(x)) - (std::get<0>(y) + std::get<1>(y)));
-        });
+    test_with_lambda<std::int32_t, std::int32_t, std::int32_t>(1);
+    test_with_lambda<float32_t, float32_t, float32_t>(1.5);
 
     return TestUtils::done();
 }
