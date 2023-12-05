1b5048619292a3279d48d57317148ec6588b4d43
Joe Todd <joeatodd@users.noreply.github.com>
2023-12-05 09:20:00 +0000

    Enable pragma unroll for open-source DPC++ (#1299)
    Previously this was only defined for the Intel compiler

--- include/oneapi/dpl/pstl/onedpl_config.h
+++ include/oneapi/dpl/pstl/onedpl_config.h
@@ -117,7 +117,8 @@
 
 // Enable loop unrolling pragmas where supported
 #if (__INTEL_LLVM_COMPILER || __INTEL_COMPILER ||                                                                      \
-     (!defined(__INTEL_LLVM_COMPILER) && !defined(__INTEL_COMPILER) && _ONEDPL_GCC_VERSION >= 80000))
+     (!defined(__INTEL_LLVM_COMPILER) && !defined(__INTEL_COMPILER) &&                                                 \
+      ((_ONEDPL_GCC_VERSION >= 80000) || (_ONEDPL_CLANG_VERSION >= 30700))))
 #    define _ONEDPL_PRAGMA_UNROLL _ONEDPL_PRAGMA(unroll)
 #else //no pragma unroll
 #    define _ONEDPL_PRAGMA_UNROLL
