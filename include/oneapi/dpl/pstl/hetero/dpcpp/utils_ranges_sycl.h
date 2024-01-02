2df0e4191dbea60b8adda2495a622b7ae4a9b844
Dan Hoeflinger <dan.hoeflinger@intel.com>
2024-01-02 15:55:15 -0500

    adding legacy support for is_passed_directly trait
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/utils_ranges_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/utils_ranges_sycl.h
@@ -231,8 +231,19 @@ struct is_passed_directly<zip_iterator<Iters...>> : ::std::conjunction<is_passed
 {
 };
 
+template <typename Iter, typename Void = void>
+struct is_passed_directly_legacy_support : ::std::false_type
+{
+};
+
+template <typename Iter> // for permutation_iterators
+struct is_passed_directly_legacy_support<Iter, ::std::enable_if_t<Iter::is_passed_directly::value>> : ::std::true_type
+{
+};
+
 template <typename Iter>
-inline constexpr bool is_passed_directly_v = is_passed_directly<Iter>::value;
+inline constexpr bool is_passed_directly_v =
+    is_passed_directly<Iter>::value || is_passed_directly_legacy_support<Iter>::value;
 
 // A trait for checking if iterator is heterogeneous or not
 
