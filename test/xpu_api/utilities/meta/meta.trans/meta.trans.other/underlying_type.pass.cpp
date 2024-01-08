465f278d655be7c0fc23e746c7738341b0126da0
Sergey Kopienko <sergey.kopienko@intel.com>
2024-01-08 16:17:59 +0100

    [test] fix compile error: only enumeration types have underlying types (#1334)

--- test/xpu_api/utilities/meta/meta.trans/meta.trans.other/underlying_type.pass.cpp
+++ test/xpu_api/utilities/meta/meta.trans/meta.trans.other/underlying_type.pass.cpp
@@ -120,9 +120,12 @@ kernel_test1(sycl::queue& deviceQueue)
 void
 kernel_test2(sycl::queue& deviceQueue)
 {
+    // Until C++20 the behaviour of std::underlying_type<T> is undefined when T is not enum
+#if TEST_STD_VER >= 20
     deviceQueue.submit([&](sycl::handler& cgh) {
         cgh.single_task<class KernelTest2>([=]() { static_assert(!has_underlying_type_member<double>::value); });
     });
+#endif
 }
 
 int
