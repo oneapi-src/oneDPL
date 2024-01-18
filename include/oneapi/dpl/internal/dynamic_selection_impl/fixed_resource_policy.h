79045962ca365004339f59038775ed63cc36b66e
egfefey <elvis.g.fefey@intel.com>
2024-01-18 13:14:49 -0600

    Update fixed_resource_policy.h

--- include/oneapi/dpl/internal/dynamic_selection_impl/fixed_resource_policy.h
+++ include/oneapi/dpl/internal/dynamic_selection_impl/fixed_resource_policy.h
@@ -37,7 +37,6 @@ struct fixed_resource_policy
     using backend_t = Backend;
     using resource_container_t = typename backend_t::resource_container_t;
     using execution_resource_t = typename backend_t::execution_resource_t;
-    //using wrapped_resource_t = typename std::decay_t<Backend>::execution_resource_t;
     using wrapped_resource_t = execution_resource_t;
 
   public:
