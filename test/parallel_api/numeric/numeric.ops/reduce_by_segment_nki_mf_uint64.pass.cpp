c0ae25351af87d3013170e99d4effba8496ce2d7
Sergey Kopienko <>
2024-01-19 16:21:54 +0100

    Split test reduce_by_segment.pass.cpp into set of separate tests

---
+++ test/parallel_api/numeric/numeric.ops/reduce_by_segment_nki_mf_uint64.pass.cpp
@@ -0,0 +1,31 @@
+// -*- C++ -*-
+//===----------------------------------------------------------------------===//
+//
+// Copyright (C) Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// This file incorporates work covered by the following copyright and permission
+// notice:
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+//
+//===----------------------------------------------------------------------===//
+
+#define _ONEDPL_ICPX_USE_KNOWN_IDENTITY_FOR_ARITHMETIC_64BIT_DATA_TYPES 0
+
+#include "reduce_by_segment.h"
+
+int
+main()
+{
+    static_assert(_ONEDPL_ICPX_USE_KNOWN_IDENTITY_FOR_ARITHMETIC_64BIT_DATA_TYPES == 0);
+
+    using ValueType = ::std::uint64_t;
+    using BinaryPredicate = UserBinaryPredicate<ValueType>;
+    using BinaryOperation = MaxFunctor<ValueType>;
+    run_test<ValueType, BinaryPredicate, BinaryOperation>();
+
+    return TestUtils::done();
+}
