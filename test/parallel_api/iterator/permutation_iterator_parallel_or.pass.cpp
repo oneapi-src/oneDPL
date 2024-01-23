fe8dc373d5fe6557eccd063cc4dff35dca6f784f
Sergey Kopienko <>
2024-01-23 15:12:16 +0100

    test/parallel_api/iterator/permutation_iterator_parallel_or.pass.cpp - fix for ONEDPL_USE_UNNAMED_LAMBDA=OFF CMake option

--- test/parallel_api/iterator/permutation_iterator_parallel_or.pass.cpp
+++ test/parallel_api/iterator/permutation_iterator_parallel_or.pass.cpp
@@ -18,7 +18,7 @@
 #include "permutation_iterator_common.h"
 
 // dpl::is_heap, dpl::includes -> __parallel_or -> _parallel_find_or
-DEFINE_TEST_PERM_IT(test_is_heap, PermItIndexTag)
+DEFINE_TEST_PERM_IT(test_is_heap, PermItIndexTag, KernelName)
 {
     DEFINE_TEST_PERM_IT_CONSTRUCTOR(test_is_heap)
 
@@ -30,10 +30,12 @@ DEFINE_TEST_PERM_IT(test_is_heap, PermItIndexTag)
 
     template <typename Policy, typename Iterator1, typename Size>
     void
-    operator()(Policy&& exec, Iterator1 first1, Iterator1 last1, Size n)
+    operator()(Policy&& exec_src, Iterator1 first1, Iterator1 last1, Size n)
     {
         if constexpr (is_base_of_iterator_category_v<::std::random_access_iterator_tag, Iterator1>)
         {
+            auto exec = create_new_policy<KernelName>(::std::forward<Policy>(exec_src));
+
             for (bool bCallMakeHeap : {false, true})
             {
                 TestDataTransfer<UDTKind::eKeys, Size> host_keys(*this, n);     // source data for is_heap check
@@ -66,7 +68,7 @@ DEFINE_TEST_PERM_IT(test_is_heap, PermItIndexTag)
     }
 };
 
-template <typename ValueType, typename PermItIndexTag>
+template <typename ValueType, typename PermItIndexTag, typename KernelName>
 void
 run_algo_tests()
 {
@@ -75,13 +77,15 @@ run_algo_tests()
 #if TEST_DPCPP_BACKEND_PRESENT
     // Run tests on <USM::shared, USM::device, sycl::buffer> + <all_hetero_policies>
     // dpl::is_heap, dpl::includes -> __parallel_or -> _parallel_find_or
-    test1buffer<sycl::usm::alloc::shared, ValueType, test_is_heap<ValueType, PermItIndexTag>>();
-    test1buffer<sycl::usm::alloc::device, ValueType, test_is_heap<ValueType, PermItIndexTag>>();
+    test1buffer<sycl::usm::alloc::shared, ValueType, test_is_heap<ValueType, PermItIndexTag, new_kernel_name<KernelName, 10>>,
+                                                     test_is_heap<ValueType, PermItIndexTag, new_kernel_name<KernelName, 15>>>();
+    test1buffer<sycl::usm::alloc::device, ValueType, test_is_heap<ValueType, PermItIndexTag, new_kernel_name<KernelName, 20>>,
+                                                     test_is_heap<ValueType, PermItIndexTag, new_kernel_name<KernelName, 25>>>();
 #endif // TEST_DPCPP_BACKEND_PRESENT
 
     // Run tests on <std::vector::iterator> + <all_host_policies>
     // dpl::is_heap, dpl::includes -> __parallel_or -> _parallel_find_or
-    test_algo_one_sequence<ValueType, test_is_heap<ValueType, PermItIndexTag>>(kZeroOffset);
+    test_algo_one_sequence<ValueType, test_is_heap<ValueType, PermItIndexTag, KernelName>>(kZeroOffset);
 }
 
 int
@@ -90,13 +94,13 @@ main()
     using ValueType = ::std::uint32_t;
 
 #if TEST_DPCPP_BACKEND_PRESENT
-    run_algo_tests<ValueType, perm_it_index_tags::usm_shared>();
+    run_algo_tests<ValueType, perm_it_index_tags::usm_shared,         class KernelName1>();
 #endif // TEST_DPCPP_BACKEND_PRESENT
 
-    run_algo_tests<ValueType, perm_it_index_tags::counting>();
-    run_algo_tests<ValueType, perm_it_index_tags::host>();
-    run_algo_tests<ValueType, perm_it_index_tags::transform_iterator>();
-    run_algo_tests<ValueType, perm_it_index_tags::callable_object>();
+    run_algo_tests<ValueType, perm_it_index_tags::counting,           class KernelName2>();
+    run_algo_tests<ValueType, perm_it_index_tags::host,               class KernelName3>();
+    run_algo_tests<ValueType, perm_it_index_tags::transform_iterator, class KernelName4>();
+    run_algo_tests<ValueType, perm_it_index_tags::callable_object,    class KernelName5>();
 
     return TestUtils::done();
 }
