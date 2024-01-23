7d623e71708adf58474acffb779bd654582c9b8b
Alexandr Konovalov <alexandr.konovalov@intel.com>
2024-01-23 17:40:55 +0100

    Add test case for later realloc/free.

--- test/pstl_offload/general/free_after_unload.pass.cpp
+++ test/pstl_offload/general/free_after_unload.pass.cpp
@@ -26,9 +26,12 @@ int main()
 {
     sycl::context memory_context = TestUtils::get_pstl_offload_device().get_platform().ext_oneapi_get_default_context();
 
-    void* ptr = malloc(8);
-    EXPECT_TRUE(sycl::get_pointer_type(ptr, memory_context) == sycl::usm::alloc::shared, "Wrong pointer type while allocating with memalign");
-    register_mem_to_later_release(ptr);
+    pointers ptrs{malloc(8), new (std::align_val_t(8*1024)) int, std::aligned_alloc(8*1024, 10)};
+
+    EXPECT_TRUE(sycl::get_pointer_type(ptrs.p1, memory_context) == sycl::usm::alloc::shared, "Wrong pointer type while allocating with malloc");
+    EXPECT_TRUE(sycl::get_pointer_type(ptrs.p2, memory_context) == sycl::usm::alloc::shared, "Wrong pointer type while allocating with aligned new");
+    EXPECT_TRUE(sycl::get_pointer_type(ptrs.p3, memory_context) == sycl::usm::alloc::shared, "Wrong pointer type while allocating with aligned_alloc");
+    register_mem_to_later_release(&ptrs);
 
     return TestUtils::done();
 }
--- test/pstl_offload/general/free_after_unload_lib.h
+++ test/pstl_offload/general/free_after_unload_lib.h
@@ -10,7 +10,14 @@
 #ifndef _FREE_AFTER_UNLOAD_LIB_HEADERS_H
 #define _FREE_AFTER_UNLOAD_LIB_HEADERS_H
 
-void register_mem_to_later_release(void *);
+struct pointers
+{
+    void *p1;
+    int *p2;
+    void *p3;
+};
+
+void register_mem_to_later_release(pointers *);
 
 #endif // _FREE_AFTER_UNLOAD_LIB_HEADERS_H
 
