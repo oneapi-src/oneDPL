de2061bf659618792a5dcc2eba563dc822a2af4e
Adam Fidel <adam.fidel@intel.com>
2024-01-23 13:17:09 -0800

    Fix duplicate kernel name errors

--- include/oneapi/dpl/experimental/kt/single_pass_scan.h
+++ include/oneapi/dpl/experimental/kt/single_pass_scan.h
@@ -41,9 +41,9 @@ struct __scan_status_flag
 {
     using _FlagType = uint32_t;
     using _AtomicFlagT = sycl::atomic_ref<_FlagType, sycl::memory_order::acq_rel, sycl::memory_scope::device,
-                                         sycl::access::address_space::global_space>;
+                                          sycl::access::address_space::global_space>;
     using _AtomicValueT = sycl::atomic_ref<_T, sycl::memory_order::acq_rel, sycl::memory_scope::device,
-                                         sycl::access::address_space::global_space>;
+                                           sycl::access::address_space::global_space>;
 
     static constexpr _FlagType __initialized_status = 0;
     static constexpr _FlagType __partial_status = 1;
@@ -53,13 +53,10 @@ struct __scan_status_flag
     static constexpr int __padding = SUBGROUP_SIZE;
 
     __scan_status_flag(_FlagType* __flags, _T* __full_vals, _T* __partial_vals, const std::uint32_t __tile_id)
-        : __tile_id(__tile_id)
-        , __flags_begin(__flags)
-        , __full_vals_begin(__full_vals)
-        , __partial_vals_begin(__partial_vals)
-        , __atomic_flag(*(__flags + __tile_id + __padding))
-        , __atomic_partial_value(*(__partial_vals + __tile_id + __padding))
-        , __atomic_full_value(*(__full_vals + __tile_id + __padding))
+        : __tile_id(__tile_id), __flags_begin(__flags), __full_vals_begin(__full_vals),
+          __partial_vals_begin(__partial_vals), __atomic_flag(*(__flags + __tile_id + __padding)),
+          __atomic_partial_value(*(__partial_vals + __tile_id + __padding)),
+          __atomic_full_value(*(__full_vals + __tile_id + __padding))
     {
     }
 
@@ -96,17 +93,19 @@ struct __scan_status_flag
                 __tile_flag = __tile_flag_atomic.load(sycl::memory_order::acquire);
             } while (!sycl::all_of_group(__subgroup, __tile_flag != __initialized_status));
 
-
             bool __is_full = __tile_flag == __full_status;
             auto __is_full_ballot = sycl::ext::oneapi::group_ballot(__subgroup, __is_full);
             ::std::uint32_t __is_full_ballot_bits{};
             __is_full_ballot.extract_bits(__is_full_ballot_bits);
 
-            _AtomicValueT __tile_value_atomic(*((__is_full ? __full_vals_begin : __partial_vals_begin) + __tile + __padding - __local_id));
+            _AtomicValueT __tile_value_atomic(
+                *((__is_full ? __full_vals_begin : __partial_vals_begin) + __tile + __padding - __local_id));
             _T __tile_val = __tile_value_atomic.load(sycl::memory_order::acquire);
 
             auto __lowest_item_with_full = sycl::ctz(__is_full_ballot_bits);
-            _T __contribution = __local_id <= __lowest_item_with_full ? __tile_val : oneapi::dpl::unseq_backend::__known_identity<_BinaryOp, _T>;
+            _T __contribution = __local_id <= __lowest_item_with_full
+                                    ? __tile_val
+                                    : oneapi::dpl::unseq_backend::__known_identity<_BinaryOp, _T>;
 
             // Running reduction of all of the partial results from the tiles found, as well as the full contribution from the closest tile (if any)
             __running = __binary_op(__running, sycl::reduce_over_group(__subgroup, __contribution, __binary_op));
@@ -133,7 +132,8 @@ template <typename _FlagType, typename _KernelName>
 struct __lookback_init_submitter;
 
 template <typename _FlagType, typename... _Name>
-struct __lookback_init_submitter<_FlagType, oneapi::dpl::__par_backend_hetero::__internal::__optional_kernel_name<_Name...>>
+struct __lookback_init_submitter<_FlagType,
+                                 oneapi::dpl::__par_backend_hetero::__internal::__optional_kernel_name<_Name...>>
 {
     template <typename _StatusFlags>
     sycl::event
@@ -143,7 +143,8 @@ struct __lookback_init_submitter<_FlagType, oneapi::dpl::__par_backend_hetero::_
         return __q.submit([&](sycl::handler& __hdl) {
             __hdl.parallel_for<_Name...>(sycl::range<1>{__status_flags_size}, [=](const sycl::item<1>& __item) {
                 auto __id = __item.get_linear_id();
-                __status_flags[__id] = __id < __status_flag_padding ? _FlagType::__oob_status : _FlagType::__initialized_status;
+                __status_flags[__id] =
+                    __id < __status_flag_padding ? _FlagType::__oob_status : _FlagType::__initialized_status;
             });
         });
     }
@@ -154,100 +155,138 @@ template <::std::uint16_t __data_per_workitem, ::std::uint16_t __workgroup_size,
 struct __lookback_submitter;
 
 template <::std::uint16_t __data_per_workitem, ::std::uint16_t __workgroup_size, typename _Type, typename _FlagType,
-          typename... _Name>
-struct __lookback_submitter<__data_per_workitem, __workgroup_size, _Type, _FlagType,
-                            oneapi::dpl::__par_backend_hetero::__internal::__optional_kernel_name<_Name...>>
+          typename _InRng, typename _OutRng, typename _BinaryOp, typename _StatusFlags, typename _StatusValues,
+          typename _TileVals>
+struct __lookback_kernel_func
 {
     using _FlagStorageType = typename _FlagType::_FlagType;
     static constexpr std::uint32_t __elems_in_tile = __workgroup_size * __data_per_workitem;
 
+    _InRng __in_rng;
+    _OutRng __out_rng;
+    _BinaryOp __binary_op;
+    ::std::size_t __n;
+    _StatusFlags __status_flags;
+    ::std::size_t __status_flags_size;
+    _StatusValues __status_vals_full;
+    _StatusValues __status_vals_partial;
+    ::std::size_t __current_num_items;
+    _TileVals __tile_vals;
+
+    [[intel::reqd_sub_group_size(SUBGROUP_SIZE)]] void
+    operator()(const sycl::nd_item<1>& __item) const
+    {
+        auto __group = __item.get_group();
+        auto __subgroup = __item.get_sub_group();
+        auto __local_id = __item.get_local_id(0);
+
+        ::std::uint32_t __tile_id = 0;
+
+        // Obtain unique ID for this work-group that will be used in decoupled lookback
+        if (__group.leader())
+        {
+            sycl::atomic_ref<_FlagStorageType, sycl::memory_order::relaxed, sycl::memory_scope::device,
+                             sycl::access::address_space::global_space>
+                __idx_atomic(__status_flags[__status_flags_size - 1]);
+            __tile_id = __idx_atomic.fetch_add(1);
+        }
+
+        __tile_id = sycl::group_broadcast(__group, __tile_id, 0);
+
+        ::std::size_t __current_offset = static_cast<::std::size_t>(__tile_id) * __elems_in_tile;
+        auto __out_begin = __out_rng.begin() + __current_offset;
+
+        if (__current_offset >= __n)
+            return;
+
+        // Global load into local
+        auto __wg_current_offset = (__tile_id * __elems_in_tile);
+        auto __wg_next_offset = ((__tile_id + 1) * __elems_in_tile);
+        auto __wg_local_memory_size = __elems_in_tile;
+
+        if (__wg_next_offset > __n)
+            __wg_local_memory_size = __n - __wg_current_offset;
+
+        if (__wg_next_offset <= __n)
+        {
+#pragma unroll
+            for (std::uint32_t __i = 0; __i < __data_per_workitem; ++__i)
+            {
+                __tile_vals[__local_id + __workgroup_size * __i] =
+                    __in_rng[__wg_current_offset + __local_id + __workgroup_size * __i];
+            }
+        }
+        else
+        {
+#pragma unroll
+            for (std::uint32_t __i = 0; __i < __data_per_workitem; ++__i)
+            {
+                if (__wg_current_offset + __local_id + __workgroup_size * __i < __n)
+                {
+                    __tile_vals[__local_id + __workgroup_size * __i] =
+                        __in_rng[__wg_current_offset + __local_id + __workgroup_size * __i];
+                }
+            }
+        }
+
+        auto __tile_vals_ptr = __dpl_sycl::__get_accessor_ptr(__tile_vals);
+        _Type __local_reduction =
+            sycl::joint_reduce(__group, __tile_vals_ptr, __tile_vals_ptr + __wg_local_memory_size, __binary_op);
+        _Type __prev_tile_reduction{};
+
+        // The first sub-group will query the previous tiles to find a prefix
+        if (__subgroup.get_group_id() == 0)
+        {
+            _FlagType __flag(__status_flags, __status_vals_full, __status_vals_partial, __tile_id);
+
+            if (__group.leader())
+                __flag.set_partial(__local_reduction);
+
+            __prev_tile_reduction = __flag.cooperative_lookback(__subgroup, __binary_op);
+
+            if (__group.leader())
+                __flag.set_full(__binary_op(__prev_tile_reduction, __local_reduction));
+        }
+
+        __prev_tile_reduction = sycl::group_broadcast(__group, __prev_tile_reduction, 0);
+
+        sycl::joint_inclusive_scan(__group, __tile_vals_ptr, __tile_vals_ptr + __wg_local_memory_size, __out_begin,
+                                   __binary_op, __prev_tile_reduction);
+    }
+};
+
+template <::std::uint16_t __data_per_workitem, ::std::uint16_t __workgroup_size, typename _Type, typename _FlagType,
+          typename... _Name>
+struct __lookback_submitter<__data_per_workitem, __workgroup_size, _Type, _FlagType,
+                            oneapi::dpl::__par_backend_hetero::__internal::__optional_kernel_name<_Name...>>
+{
+
     template <typename _InRng, typename _OutRng, typename _BinaryOp, typename _StatusFlags, typename _StatusValues>
     sycl::event
     operator()(sycl::queue __q, sycl::event __prev_event, _InRng&& __in_rng, _OutRng&& __out_rng, _BinaryOp __binary_op,
-               ::std::size_t __n, _StatusFlags&& __status_flags, ::std::size_t __status_flags_size, _StatusValues&& __status_vals_full, _StatusValues&& __status_vals_partial,
+               ::std::size_t __n, _StatusFlags&& __status_flags, ::std::size_t __status_flags_size,
+               _StatusValues&& __status_vals_full, _StatusValues&& __status_vals_partial,
                ::std::size_t __current_num_items) const
     {
+        using _LocalAccessorType = sycl::local_accessor<_Type, 1>;
+        using _FlagStorageType = typename _FlagType::_FlagType;
+        using _KernelFunc =
+            __lookback_kernel_func<__data_per_workitem, __workgroup_size, _Type, _FlagType, ::std::decay_t<_InRng>,
+                                   ::std::decay_t<_OutRng>, ::std::decay_t<_BinaryOp>, ::std::decay_t<_StatusFlags>,
+                                   ::std::decay_t<_StatusValues>, ::std::decay_t<_LocalAccessorType>>;
+        using _KernelName = __lookback_kernel<_Name..., _KernelFunc>;
+
+        static constexpr std::uint32_t __elems_in_tile = __workgroup_size * __data_per_workitem;
+
         return __q.submit([&](sycl::handler& __hdl) {
-            auto __tile_vals = sycl::local_accessor<_Type, 1>(sycl::range<1>{__elems_in_tile}, __hdl);
+            auto __tile_vals = _LocalAccessorType(sycl::range<1>{__elems_in_tile}, __hdl);
             __hdl.depends_on(__prev_event);
 
             oneapi::dpl::__ranges::__require_access(__hdl, __in_rng, __out_rng);
-            __hdl.parallel_for<_Name...>(
-                sycl::nd_range<1>(__current_num_items, __workgroup_size),
-                [=](const sycl::nd_item<1>& __item) [[intel::reqd_sub_group_size(SUBGROUP_SIZE)]] {
-                    auto __group = __item.get_group();
-                    auto __subgroup = __item.get_sub_group();
-                    auto __local_id = __item.get_local_id(0);
-
-                    ::std::uint32_t __tile_id = 0;
-
-                    // Obtain unique ID for this work-group that will be used in decoupled lookback
-                    if (__group.leader())
-                    {
-                        sycl::atomic_ref<_FlagStorageType, sycl::memory_order::relaxed, sycl::memory_scope::device,
-                                         sycl::access::address_space::global_space>
-                            __idx_atomic(__status_flags[__status_flags_size - 1]);
-                        __tile_id = __idx_atomic.fetch_add(1);
-                    }
-
-                    __tile_id = sycl::group_broadcast(__group, __tile_id, 0);
-
-                    ::std::size_t __current_offset = static_cast<::std::size_t>(__tile_id) * __elems_in_tile;
-                    auto __out_begin = __out_rng.begin() + __current_offset;
-
-                    if (__current_offset >= __n)
-                        return;
-
-                    // Global load into local
-                    auto __wg_current_offset = (__tile_id * __elems_in_tile);
-                    auto __wg_next_offset = ((__tile_id + 1) * __elems_in_tile);
-                    auto __wg_local_memory_size = __elems_in_tile;
-
-                    if (__wg_next_offset > __n)
-                        __wg_local_memory_size = __n - __wg_current_offset;
-
-                    if (__wg_next_offset <= __n)
-                    {
-                        #pragma unroll
-                        for (std::uint32_t __i = 0; __i < __data_per_workitem; ++__i)
-                        {
-                            __tile_vals[__local_id + __workgroup_size * __i] = __in_rng[__wg_current_offset + __local_id + __workgroup_size * __i];
-                        }
-                    }
-                    else
-                    {
-                        #pragma unroll
-                        for (std::uint32_t __i = 0; __i < __data_per_workitem; ++__i)
-                        {
-                            if (__wg_current_offset + __local_id + __workgroup_size * __i < __n)
-                            {
-                                __tile_vals[__local_id + __workgroup_size * __i] = __in_rng[__wg_current_offset + __local_id + __workgroup_size * __i];
-                            }
-                        }
-                    }
-
-                    auto __tile_vals_ptr = __dpl_sycl::__get_accessor_ptr(__tile_vals);
-                    _Type __local_reduction = sycl::joint_reduce(__group, __tile_vals_ptr, __tile_vals_ptr+__wg_local_memory_size, __binary_op);
-                    _Type __prev_tile_reduction{};
-
-                    // The first sub-group will query the previous tiles to find a prefix
-                    if (__subgroup.get_group_id() == 0)
-                    {
-                        _FlagType __flag(__status_flags, __status_vals_full, __status_vals_partial, __tile_id);
-
-                        if (__group.leader())
-                            __flag.set_partial(__local_reduction);
-
-                        __prev_tile_reduction = __flag.cooperative_lookback(__subgroup, __binary_op);
-
-                        if (__group.leader())
-                            __flag.set_full(__binary_op(__prev_tile_reduction, __local_reduction));
-                    }
-
-                    __prev_tile_reduction = sycl::group_broadcast(__group, __prev_tile_reduction, 0);
-
-                    sycl::joint_inclusive_scan(__group, __tile_vals_ptr, __tile_vals_ptr+__wg_local_memory_size, __out_begin, __binary_op, __prev_tile_reduction);
-                });
+            __hdl.parallel_for<_KernelName>(sycl::nd_range<1>(__current_num_items, __workgroup_size),
+                                            _KernelFunc{__in_rng, __out_rng, __binary_op, __n, __status_flags,
+                                                        __status_flags_size, __status_vals_full, __status_vals_partial,
+                                                        __current_num_items, __tile_vals});
         });
     }
 };
@@ -272,7 +311,8 @@ __single_pass_scan(sycl::queue __queue, _InRange&& __in_rng, _OutRange&& __out_r
         return sycl::event{};
 
     static_assert(_Inclusive, "Single-pass scan only available for inclusive scan");
-    static_assert(oneapi::dpl::unseq_backend::__has_known_identity<_BinaryOp, _Type>::value, "Only binary operators with known identity values are supported");
+    static_assert(oneapi::dpl::unseq_backend::__has_known_identity<_BinaryOp, _Type>::value,
+                  "Only binary operators with known identity values are supported");
 
     assert("This device does not support 64-bit atomics" &&
            (sizeof(_Type) < 64 || __queue.get_device().has(sycl::aspect::atomic64)));
@@ -291,16 +331,16 @@ __single_pass_scan(sycl::queue __queue, _InRange&& __in_rng, _OutRange&& __out_r
     _Type* __status_vals_full = sycl::malloc_device<_Type>(__status_flags_size, __queue);
     _Type* __status_vals_partial = sycl::malloc_device<_Type>(__status_flags_size, __queue);
 
-    auto __fill_event = __lookback_init_submitter<_FlagType, _LookbackInitKernel>{}(__queue, __status_flags, __status_flags_size,
-                                                                         __status_flag_padding);
+    auto __fill_event = __lookback_init_submitter<_FlagType, _LookbackInitKernel>{}(
+        __queue, __status_flags, __status_flags_size, __status_flag_padding);
 
-    ::std::size_t __current_num_wgs =
-        oneapi::dpl::__internal::__dpl_ceiling_div(__n, __elems_in_tile);
+    ::std::size_t __current_num_wgs = oneapi::dpl::__internal::__dpl_ceiling_div(__n, __elems_in_tile);
     ::std::size_t __current_num_items = __current_num_wgs * __workgroup_size;
 
-    auto __prev_event = __lookback_submitter<__data_per_workitem, __workgroup_size, _Type, _FlagType, _LookbackKernel>{}(
-        __queue, __fill_event, __in_rng, __out_rng, __binary_op, __n, __status_flags, __status_flags_size, __status_vals_full, __status_vals_partial,
-        __current_num_items);
+    auto __prev_event =
+        __lookback_submitter<__data_per_workitem, __workgroup_size, _Type, _FlagType, _LookbackKernel>{}(
+            __queue, __fill_event, __in_rng, __out_rng, __binary_op, __n, __status_flags, __status_flags_size,
+            __status_vals_full, __status_vals_partial, __current_num_items);
 
     if (0)
     {
@@ -321,9 +361,9 @@ __single_pass_scan(sycl::queue __queue, _InRange&& __in_rng, _OutRange&& __out_r
 
 template <typename _InRng, typename _OutRng, typename _BinaryOp, typename _KernelParam>
 sycl::event
-inclusive_scan(sycl::queue __queue, _InRng __in_rng, _OutRng __out_rng, _BinaryOp __binary_op, _KernelParam __param = {})
+inclusive_scan(sycl::queue __queue, _InRng __in_rng, _OutRng __out_rng, _BinaryOp __binary_op,
+               _KernelParam __param = {})
 {
-
     return __impl::__single_pass_scan<true>(__queue, __in_rng, __out_rng, __binary_op, __param);
 }
 
--- test/kt/single_pass_scan.cpp
+++ test/kt/single_pass_scan.cpp
@@ -12,13 +12,12 @@
 // See https://llvm.org/LICENSE.txt for license information.
 //
 //===----------------------------------------------------------------------===//
-
 #include "../support/test_config.h"
 
 #include <oneapi/dpl/experimental/kernel_templates>
 
 #if LOG_TEST_INFO
-#include <iostream>
+#    include <iostream>
 #endif
 
 #if _ENABLE_RANGES_TESTING
@@ -36,25 +35,45 @@ inline const std::vector<std::size_t> scan_sizes = {
     100'000, 1 << 17,   179'581, 250'000,   1 << 18,       (1 << 18) + 1, 500'000,
     888'235, 1'000'000, 1 << 20, 10'000'000};
 
+template <typename T>
+typename ::std::enable_if_t<std::is_arithmetic_v<T>, void>
+generate_scan_data(T* input, std::size_t size, std::uint32_t seed)
+{
+    std::default_random_engine gen{seed};
+    if constexpr (std::is_integral_v<T>)
+    {
+        std::uniform_int_distribution<T> dist(-1000, 1000);
+        std::generate(input, input + size, [&] { return dist(gen); });
+    }
+    else
+    {
+        std::uniform_real_distribution<T> dist_real(0.0001, 1000.);
+        std::uniform_int_distribution<int> dist_binary(0, 1);
+        auto randomly_signed_real = [&dist_real, &dist_binary, &gen]() {
+            auto v = exp2(dist_real(gen));
+            return dist_binary(gen) == 0 ? v : -v;
+        };
+        std::generate(input, input + size, [&] { return randomly_signed_real(); });
+    }
+}
+
 #if _ENABLE_RANGES_TESTING
 template <typename T, typename BinOp, typename KernelParam>
 void
 test_all_view(sycl::queue q, std::size_t size, BinOp bin_op, KernelParam param)
 {
-#if LOG_TEST_INFO
+#    if LOG_TEST_INFO
     std::cout << "\ttest_all_view(" << size << ") : " << TypeInfo().name<T>() << std::endl;
-#endif
+#    endif
     std::vector<T> input(size);
-    generate_data(input.data(), size, 42);
+    generate_scan_data(input.data(), size, 42);
     std::vector<T> ref(input);
     std::inclusive_scan(std::begin(ref), std::end(ref), std::begin(ref), bin_op);
     {
         sycl::buffer<T> buf(input.data(), input.size());
         oneapi::dpl::experimental::ranges::all_view<T, sycl::access::mode::read_write> view(buf);
         sycl::buffer<T> buf_out(input.size());
-        oneapi::dpl::experimental::kt::inclusive_scan(q, view, view, bin_op,
-                                                                      param)
-            .wait();
+        oneapi::dpl::experimental::kt::inclusive_scan(q, view, view, bin_op, param).wait();
     }
 
     std::string msg = "wrong results with all_view, n: " + std::to_string(size);
@@ -67,18 +86,19 @@ void
 test_usm(sycl::queue q, std::size_t size, BinOp bin_op, KernelParam param)
 {
 #if LOG_TEST_INFO
-    std::cout << "\t\ttest_usm<" << TypeInfo().name<T>() << ", " << USMAllocPresentation().name<_alloc_type>()
-              << ">(" << size << ");" << std::endl;
+    std::cout << "\t\ttest_usm<" << TypeInfo().name<T>() << ", " << USMAllocPresentation().name<_alloc_type>() << ">("
+              << size << ");" << std::endl;
 #endif
     std::vector<T> expected(size);
-    generate_data(expected.data(), size, 42);
+    generate_scan_data(expected.data(), size, 42);
 
     TestUtils::usm_data_transfer<_alloc_type, T> dt_input(q, expected.begin(), expected.end());
     TestUtils::usm_data_transfer<_alloc_type, T> dt_output(q, size);
 
     std::inclusive_scan(expected.begin(), expected.end(), expected.begin(), bin_op);
 
-    oneapi::dpl::experimental::kt::inclusive_scan(q, dt_input.get_data(), dt_input.get_data() + size, dt_output.get_data(), bin_op, param)
+    oneapi::dpl::experimental::kt::inclusive_scan(q, dt_input.get_data(), dt_input.get_data() + size,
+                                                  dt_output.get_data(), bin_op, param)
         .wait();
 
     std::vector<T> actual(size);
@@ -90,20 +110,22 @@ test_usm(sycl::queue q, std::size_t size, BinOp bin_op, KernelParam param)
 
 template <typename T, typename BinOp, typename KernelParam>
 void
-test_sycl_iterators(sycl::queue q, std::size_t size, BinOp bin_op, KernelParam param)
+test_sycl_iterators(sycl::queue q, std::size_t size, BinOp bin_op, KernelParam)
 {
+    constexpr oneapi::dpl::experimental::kt::kernel_param<KernelParam::data_per_workitem, KernelParam::workgroup_size>
+        param;
 #if LOG_TEST_INFO
     std::cout << "\t\ttest_sycl_iterators<" << TypeInfo().name<T>() << ">(" << size << ");" << std::endl;
 #endif
     std::vector<T> input(size);
-    generate_data(input.data(), size, 42);
+    generate_scan_data(input.data(), size, 42);
     std::vector<T> ref(input);
     std::inclusive_scan(std::begin(ref), std::end(ref), std::begin(ref), bin_op);
     {
         sycl::buffer<T> buf(input.data(), input.size());
         sycl::buffer<T> buf_out(input.size());
-        oneapi::dpl::experimental::kt::inclusive_scan(q, oneapi::dpl::begin(buf), oneapi::dpl::end(buf), oneapi::dpl::begin(buf_out), bin_op,
-                                                                      param)
+        oneapi::dpl::experimental::kt::inclusive_scan(q, oneapi::dpl::begin(buf), oneapi::dpl::end(buf),
+                                                      oneapi::dpl::begin(buf_out), bin_op, param)
             .wait();
     }
 
@@ -117,9 +139,9 @@ test_general_cases(sycl::queue q, std::size_t size, BinOp bin_op, KernelParam pa
 {
     test_usm<T, sycl::usm::alloc::shared>(q, size, bin_op, param);
     test_usm<T, sycl::usm::alloc::device>(q, size, bin_op, param);
-    test_sycl_iterators<T>(q, size, param);
+    test_sycl_iterators<T>(q, size, bin_op, param);
 #if _ENABLE_RANGES_TESTING
-    test_all_view<T>(q, size, param);
+    test_all_view<T>(q, size, bin_op, param);
 #endif
 }
 
@@ -127,8 +149,8 @@ template <typename T, typename KernelParam>
 void
 test_all_cases(sycl::queue q, std::size_t size, KernelParam param)
 {
-  test_general_cases<T>(q, size, std::plus<T>{}, param);
-  test_general_cases<T>(q, size, std::multiplies<T>{}, param);
+    test_general_cases<T>(q, size, std::plus<T>{}, param);
+    test_general_cases<T>(q, size, std::multiplies<T>{}, param);
 }
 
 int
@@ -136,7 +158,7 @@ main()
 {
 #if LOG_TEST_INFO
     std::cout << "TEST_DATA_PER_WORK_ITEM : " << TEST_DATA_PER_WORK_ITEM << "\n"
-              << "TEST_WORK_GROUP_SIZE    : " << TEST_WORK_GROUP_SIZE    << "\n"
+              << "TEST_WORK_GROUP_SIZE    : " << TEST_WORK_GROUP_SIZE << "\n"
               << "TEST_TYPE               : " << TypeInfo().name<TEST_TYPE>() << std::endl;
 #endif
 
