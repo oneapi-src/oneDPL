c7bf2ef1a972ae7fde13b29ea809e7d919f95877
Sergey Kopienko <>
2024-01-23 14:44:21 +0100

    test/parallel_api/iterator/permutation_iterator_parallel_transform_scan.pass.cpp - fix for ONEDPL_USE_UNNAMED_LAMBDA=OFF CMake option

--- test/parallel_api/iterator/permutation_iterator_parallel_transform_scan.pass.cpp
+++ test/parallel_api/iterator/permutation_iterator_parallel_transform_scan.pass.cpp
@@ -19,7 +19,7 @@
 
 // dpl::remove_if -> __parallel_transform_scan
 // Requirements: only for random_access_iterator
-DEFINE_TEST_PERM_IT(test_remove_if, PermItIndexTag)
+DEFINE_TEST_PERM_IT(test_remove_if, PermItIndexTag, KernelName)
 {
     DEFINE_TEST_PERM_IT_CONSTRUCTOR(test_remove_if)
 
@@ -33,10 +33,12 @@ DEFINE_TEST_PERM_IT(test_remove_if, PermItIndexTag)
 
     template <typename Policy, typename Iterator1, typename Size>
     void
-    operator()(Policy&& exec, Iterator1 first1, Iterator1 last1, Size n)
+    operator()(Policy&& exec_src, Iterator1 first1, Iterator1 last1, Size n)
     {
         if constexpr (is_base_of_iterator_category_v<::std::random_access_iterator_tag, Iterator1>)
         {
+            auto exec = create_new_policy<KernelName>(::std::forward<Policy>(exec_src));
+
             TestDataTransfer<UDTKind::eKeys, Size> host_keys(*this, n);     // source data for remove_if
             const auto host_keys_ptr = host_keys.get();
 
@@ -79,7 +81,7 @@ DEFINE_TEST_PERM_IT(test_remove_if, PermItIndexTag)
     }
 };
 
-template <typename ValueType, typename PermItIndexTag>
+template <typename ValueType, typename PermItIndexTag, typename KernelName>
 void
 run_algo_tests()
 {
@@ -88,13 +90,15 @@ run_algo_tests()
 #if TEST_DPCPP_BACKEND_PRESENT
     // Run tests on <USM::shared, USM::device, sycl::buffer> + <all_hetero_policies>
     // dpl::remove_if -> __parallel_transform_scan (only for random_access_iterator)
-    test1buffer<sycl::usm::alloc::shared, ValueType, test_remove_if<ValueType, PermItIndexTag>>();
-    test1buffer<sycl::usm::alloc::device, ValueType, test_remove_if<ValueType, PermItIndexTag>>();
+    test1buffer<sycl::usm::alloc::shared, ValueType, test_remove_if<ValueType, PermItIndexTag, new_kernel_name<KernelName, 10>>,
+                                                     test_remove_if<ValueType, PermItIndexTag, new_kernel_name<KernelName, 15>>>();
+    test1buffer<sycl::usm::alloc::device, ValueType, test_remove_if<ValueType, PermItIndexTag, new_kernel_name<KernelName, 20>>,
+                                                     test_remove_if<ValueType, PermItIndexTag, new_kernel_name<KernelName, 25>>>();
 #endif // TEST_DPCPP_BACKEND_PRESENT
 
     // Run tests on <std::vector::iterator> + <all_host_policies>
     // dpl::remove_if -> __parallel_transform_scan (only for random_access_iterator)
-    test_algo_one_sequence<ValueType, test_remove_if<ValueType, PermItIndexTag>>(kZeroOffset);
+    test_algo_one_sequence<ValueType, test_remove_if<ValueType, PermItIndexTag, KernelName>>(kZeroOffset);
 }
 
 int
@@ -103,13 +107,13 @@ main()
     using ValueType = ::std::uint32_t;
 
 #if TEST_DPCPP_BACKEND_PRESENT
-    run_algo_tests<ValueType, perm_it_index_tags::usm_shared>();
+    run_algo_tests<ValueType, perm_it_index_tags::usm_shared,         class KernelName1>();
 #endif // TEST_DPCPP_BACKEND_PRESENT
 
-    run_algo_tests<ValueType, perm_it_index_tags::counting>();
-    run_algo_tests<ValueType, perm_it_index_tags::host>();
-    run_algo_tests<ValueType, perm_it_index_tags::transform_iterator>();
-    run_algo_tests<ValueType, perm_it_index_tags::callable_object>();
+    run_algo_tests<ValueType, perm_it_index_tags::counting,           class KernelName2>();
+    run_algo_tests<ValueType, perm_it_index_tags::host,               class KernelName3>();
+    run_algo_tests<ValueType, perm_it_index_tags::transform_iterator, class KernelName4>();
+    run_algo_tests<ValueType, perm_it_index_tags::callable_object,    class KernelName5>();
 
     return TestUtils::done();
 }
