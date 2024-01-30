1af189b6716ab9fdbfcaa630239dcb3b52dc9c68
Sergey Kopienko <>
2024-01-30 16:32:27 +0100

    include/oneapi/dpl/pstl/execution_impl.h - remove struct __prefer_unsequenced_tag as unused

--- include/oneapi/dpl/pstl/execution_impl.h
+++ include/oneapi/dpl/pstl/execution_impl.h
@@ -125,14 +125,6 @@ __is_parallelization_preferred(_ExecutionPolicy& __exec)
 }
 
 template <typename policy, typename... _IteratorTypes>
-struct __prefer_unsequenced_tag
-{
-    static constexpr bool value =
-        __internal::__allow_unsequenced<policy>::value && __internal::__is_random_access_iterator_v<_IteratorTypes...>;
-    typedef ::std::integral_constant<bool, value> type;
-};
-
-template <typename policy, typename... _IteratorTypes>
 struct __prefer_parallel_tag
 {
     static constexpr bool value =
