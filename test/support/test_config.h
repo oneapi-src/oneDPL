bed59ccf9f037fd42efd9f7ed09cd8e2e98f80b2
Matthew Michel <matthew.michel@intel.com>
2024-01-31 13:35:37 -0600

    Update broken macro to only apply if workaround macro is undefined
    Signed-off-by: Matthew Michel <matthew.michel@intel.com>

--- test/support/test_config.h
+++ test/support/test_config.h
@@ -155,7 +155,7 @@
 // in reduce_by_segment.pass.cpp.
 // TODO: When a driver fix is provided to resolve this issue, consider altering this macro or checking the driver version at runtime
 // of the underlying sycl::device to determine whether to include or exclude 64-bit type tests.
-#if !ONEDPL_WORKAROUND_FOR_IGPU_64BIT_REDUCTION && defined(__INTEL_LLVM_COMPILER)
+#if !defined(ONEDPL_WORKAROUND_FOR_IGPU_64BIT_REDUCTION) && defined(__INTEL_LLVM_COMPILER)
 #    define _PSTL_ICPX_TEST_RED_BY_SEG_BROKEN_64BIT_TYPES
 #endif
 
