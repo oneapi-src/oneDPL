442a9b91479cf2441c06a77c746dc47d86f89c3e
Sergey Kopienko <>
2024-02-06 12:20:52 +0100

    __pattern_nth_element + tag calls

--- include/oneapi/dpl/pstl/glue_algorithm_impl.h
+++ include/oneapi/dpl/pstl/glue_algorithm_impl.h
@@ -1177,10 +1177,11 @@ oneapi::dpl::__internal::__enable_if_execution_policy<_ExecutionPolicy>
 nth_element(_ExecutionPolicy&& __exec, _RandomAccessIterator __first, _RandomAccessIterator __nth,
             _RandomAccessIterator __last, _Compare __comp)
 {
-    oneapi::dpl::__internal::__pattern_nth_element(
-        ::std::forward<_ExecutionPolicy>(__exec), __first, __nth, __last, __comp,
-        oneapi::dpl::__internal::__is_vectorization_preferred<_ExecutionPolicy, _RandomAccessIterator>(),
-        oneapi::dpl::__internal::__is_parallelization_preferred<_ExecutionPolicy, _RandomAccessIterator>());
+    constexpr auto __dispatch_tag =
+        oneapi::dpl::__internal::__select_backend<_ExecutionPolicy, _RandomAccessIterator>();
+
+    oneapi::dpl::__internal::__pattern_nth_element(__dispatch_tag, ::std::forward<_ExecutionPolicy>(__exec), __first,
+                                                   __nth, __last, __comp);
 }
 
 template <class _ExecutionPolicy, class _RandomAccessIterator>
