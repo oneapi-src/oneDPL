668271be4aa6ff24bb1bd34688b76ed5af886cf7
Sergey Kopienko <>
2024-02-06 17:06:19 +0100

    __pattern_rotate_copy + tag calls

--- include/oneapi/dpl/pstl/glue_algorithm_impl.h
+++ include/oneapi/dpl/pstl/glue_algorithm_impl.h
@@ -611,10 +611,11 @@ oneapi::dpl::__internal::__enable_if_execution_policy<_ExecutionPolicy, _Forward
 rotate_copy(_ExecutionPolicy&& __exec, _ForwardIterator1 __first, _ForwardIterator1 __middle, _ForwardIterator1 __last,
             _ForwardIterator2 __result)
 {
-    return oneapi::dpl::__internal::__pattern_rotate_copy(
-        ::std::forward<_ExecutionPolicy>(__exec), __first, __middle, __last, __result,
-        oneapi::dpl::__internal::__is_vectorization_preferred<_ExecutionPolicy, _ForwardIterator1, _ForwardIterator2>(),
-        oneapi::dpl::__internal::__is_parallelization_preferred<_ExecutionPolicy, _ForwardIterator1, _ForwardIterator2>());
+    constexpr auto __dispatch_tag =
+        oneapi::dpl::__internal::__select_backend<_ExecutionPolicy, _ForwardIterator1, _ForwardIterator2>();
+
+    return oneapi::dpl::__internal::__pattern_rotate_copy(__dispatch_tag, ::std::forward<_ExecutionPolicy>(__exec),
+                                                          __first, __middle, __last, __result);
 }
 
 // [alg.partitions]
