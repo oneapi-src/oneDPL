3c493ea4a56e6a35470277dfd63a26e240e25658
Sergey Kopienko <>
2024-02-16 09:55:26 +0100

    Remove __parallel_transform_scan implementations without tags

--- include/oneapi/dpl/pstl/parallel_backend_serial.h
+++ include/oneapi/dpl/pstl/parallel_backend_serial.h
@@ -146,13 +146,6 @@ __parallel_strict_scan(oneapi::dpl::__internal::__serial_backend_tag, _Execution
 
 template <class _ExecutionPolicy, class _Index, class _UnaryOp, class _Tp, class _BinaryOp, class _Reduce, class _Scan>
 _Tp
-__parallel_transform_scan(_ExecutionPolicy&&, _Index __n, _UnaryOp, _Tp __init, _BinaryOp, _Reduce, _Scan __scan)
-{
-    return __scan(_Index(0), __n, __init);
-}
-
-template <class _ExecutionPolicy, class _Index, class _UnaryOp, class _Tp, class _BinaryOp, class _Reduce, class _Scan>
-_Tp
 __parallel_transform_scan(oneapi::dpl::__internal::__serial_backend_tag, _ExecutionPolicy&&, _Index __n, _UnaryOp,
                           _Tp __init, _BinaryOp, _Reduce, _Scan __scan)
 {
