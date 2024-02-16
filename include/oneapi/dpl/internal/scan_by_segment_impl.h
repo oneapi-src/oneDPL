c195783439bb99b94088548f49483173b382b95e
Sergey Kopienko <>
2024-02-16 14:10:00 +0100

    internal::__scan_by_segment_impl_common + tag impls

--- include/oneapi/dpl/internal/scan_by_segment_impl.h
+++ include/oneapi/dpl/internal/scan_by_segment_impl.h
@@ -364,11 +364,12 @@ struct __sycl_scan_by_segment_impl
     }
 };
 
-template <typename Policy, typename InputIterator1, typename InputIterator2, typename OutputIterator, typename T,
-          typename BinaryPredicate, typename Operator, typename Inclusive>
-oneapi::dpl::__internal::__enable_if_hetero_execution_policy<Policy, OutputIterator>
-__scan_by_segment_impl_common(Policy&& policy, InputIterator1 first1, InputIterator1 last1, InputIterator2 first2,
-                              OutputIterator result, T init, BinaryPredicate binary_pred, Operator binary_op, Inclusive)
+template <typename _BackendTag, typename Policy, typename InputIterator1, typename InputIterator2,
+          typename OutputIterator, typename T, typename BinaryPredicate, typename Operator, typename Inclusive>
+OutputIterator
+__scan_by_segment_impl_common(__hetero_tag<_BackendTag>, Policy&& policy, InputIterator1 first1, InputIterator1 last1,
+                              InputIterator2 first2, OutputIterator result, T init, BinaryPredicate binary_pred,
+                              Operator binary_op, Inclusive)
 {
     const auto n = ::std::distance(first1, last1);
 
