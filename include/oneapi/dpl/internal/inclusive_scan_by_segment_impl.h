41980d370a24c7423e94d6a3754e05a159811c83
Sergey Kopienko <>
2024-02-16 14:10:07 +0100

    internal::__scan_by_segment_impl_common + tag calls

--- include/oneapi/dpl/internal/inclusive_scan_by_segment_impl.h
+++ include/oneapi/dpl/internal/inclusive_scan_by_segment_impl.h
@@ -78,13 +78,13 @@ pattern_inclusive_scan_by_segment(_Tag, Policy&& policy, InputIterator1 first1, 
 template <typename _BackendTag, typename Policy, typename InputIterator1, typename InputIterator2,
           typename OutputIterator, typename BinaryPredicate, typename BinaryOperator>
 OutputIterator
-inclusive_scan_by_segment_impl(__hetero_tag<_BackendTag>, Policy&& policy, InputIterator1 first1, InputIterator1 last1,
+inclusive_scan_by_segment_impl(__hetero_tag<_BackendTag> __tag, Policy&& policy, InputIterator1 first1, InputIterator1 last1,
                                InputIterator2 first2, OutputIterator result, BinaryPredicate binary_pred,
                                BinaryOperator binary_op, ::std::true_type /* has_known_identity */)
 {
     using iter_value_t = typename ::std::iterator_traits<InputIterator2>::value_type;
     iter_value_t identity = unseq_backend::__known_identity<BinaryOperator, iter_value_t>;
-    return internal::__scan_by_segment_impl_common(::std::forward<Policy>(policy), first1, last1, first2, result,
+    return internal::__scan_by_segment_impl_common(__tag, ::std::forward<Policy>(policy), first1, last1, first2, result,
                                                    identity, binary_pred, binary_op, ::std::true_type{});
 }
 
