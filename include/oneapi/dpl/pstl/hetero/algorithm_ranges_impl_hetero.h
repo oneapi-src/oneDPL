88a3e14636aa87561fac77731735ae641172070e
Sergey Kopienko <>
2024-02-19 10:03:57 +0100

    include/oneapi/dpl/pstl/algorithm_impl.h - fix compile error: no matching function for call to '__parallel_merge'

--- include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h
+++ include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h
@@ -517,9 +517,10 @@ __pattern_merge(_ExecutionPolicy&& __exec, _Range1&& __rng1, _Range2&& __rng2, _
     }
     else
     {
-        __par_backend_hetero::__parallel_merge(::std::forward<_ExecutionPolicy>(__exec),
-                                               ::std::forward<_Range1>(__rng1), ::std::forward<_Range2>(__rng2),
-                                               ::std::forward<_Range3>(__rng3), __comp)
+        __par_backend_hetero::__parallel_merge(
+            oneapi::dpl::__internal::__device_backend_tag{}, // TODO required to fix backend tag eval
+            ::std::forward<_ExecutionPolicy>(__exec), ::std::forward<_Range1>(__rng1), ::std::forward<_Range2>(__rng2),
+            ::std::forward<_Range3>(__rng3), __comp)
             .wait();
     }
 
