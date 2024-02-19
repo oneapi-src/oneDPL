a8c0a10e2895698761227329ccc3005434c9c87f
Sergey Kopienko <>
2024-02-19 10:22:02 +0100

    include/oneapi/dpl/pstl/hetero/numeric_ranges_impl_hetero.h - fix compile error: no matching function for call to '__parallel_transform_reduce'

--- include/oneapi/dpl/pstl/hetero/numeric_ranges_impl_hetero.h
+++ include/oneapi/dpl/pstl/hetero/numeric_ranges_impl_hetero.h
@@ -51,6 +51,7 @@ __pattern_transform_reduce(_ExecutionPolicy&& __exec, _Range1&& __rng1, _Range2&
 
     return oneapi::dpl::__par_backend_hetero::__parallel_transform_reduce<_RepackedTp,
                                                                           ::std::true_type /*is_commutative*/>(
+               oneapi::dpl::__internal::__device_backend_tag{}, // TODO required to fix backend tag eval
                ::std::forward<_ExecutionPolicy>(__exec), __binary_op1, _Functor{__binary_op2},
                unseq_backend::__init_value<_RepackedTp>{__init}, // initial value
                ::std::forward<_Range1>(__rng1), ::std::forward<_Range2>(__rng2))
