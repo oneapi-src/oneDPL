b7a51ff8424c952a515b38604e2e2eacbaa21023
Sergey Kopienko <>
2024-02-20 09:43:58 +0100

    __ranges::__pattern_minmax_element + tag impls

--- include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h
+++ include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h
@@ -585,11 +585,9 @@ __pattern_min_element(_ExecutionPolicy&& __exec, _Range&& __rng, _Compare __comp
 // minmax_element
 //------------------------------------------------------------------------
 
-template <typename _ExecutionPolicy, typename _Range, typename _Compare>
-oneapi::dpl::__internal::__enable_if_hetero_execution_policy<
-    _ExecutionPolicy,
-    ::std::pair<oneapi::dpl::__internal::__difference_t<_Range>, oneapi::dpl::__internal::__difference_t<_Range>>>
-__pattern_minmax_element(_ExecutionPolicy&& __exec, _Range&& __rng, _Compare __comp)
+template <typename _BackendTag, typename _ExecutionPolicy, typename _Range, typename _Compare>
+::std::pair<oneapi::dpl::__internal::__difference_t<_Range>, oneapi::dpl::__internal::__difference_t<_Range>>
+__pattern_minmax_element(__hetero_tag<_BackendTag>, _ExecutionPolicy&& __exec, _Range&& __rng, _Compare __comp)
 {
     //If size == 1, result is the zero-indexed element. If size == 0, result is 0.
     if (__rng.size() < 2)
