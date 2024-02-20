0e3738ec17f5438db6b778b184cfc5986cebebca
Sergey Kopienko <>
2024-02-20 09:46:00 +0100

    __ranges::__pattern_find_if + tag calls

--- include/oneapi/dpl/pstl/glue_algorithm_ranges_impl.h
+++ include/oneapi/dpl/pstl/glue_algorithm_ranges_impl.h
@@ -84,7 +84,9 @@ template <typename _ExecutionPolicy, typename _Range, typename _Predicate>
 oneapi::dpl::__internal::__enable_if_execution_policy<_ExecutionPolicy, oneapi::dpl::__internal::__difference_t<_Range>>
 find_if(_ExecutionPolicy&& __exec, _Range&& __rng, _Predicate __pred)
 {
-    return oneapi::dpl::__internal::__ranges::__pattern_find_if(::std::forward<_ExecutionPolicy>(__exec),
+    constexpr auto __dispatch_tag = oneapi::dpl::__ranges::__select_backend<_ExecutionPolicy, _Range>();
+
+    return oneapi::dpl::__internal::__ranges::__pattern_find_if(__dispatch_tag, ::std::forward<_ExecutionPolicy>(__exec),
                                                                 views::all_read(::std::forward<_Range>(__rng)), __pred);
 }
 
