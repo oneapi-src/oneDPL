dd108dd15982beeb95827eb534ce9283bdaae325
Mikhail Dvorskiy <mikhail.dvorskiy@intel.com>
2024-02-26 18:33:03 +0100

    [oneDPL][ranges] + tests for oneapi::dpl::ranges::all_of, any_of, none_of

--- test/parallel_api/ranges/std_ranges.pass.cpp
+++ test/parallel_api/ranges/std_ranges.pass.cpp
@@ -34,6 +34,17 @@ main()
     test_range_algo<1>{}(oneapi::dpl::ranges::find_if_not, std::ranges::find_if_not, pred, proj);
     test_range_algo<1>{}(oneapi::dpl::ranges::find, std::ranges::find, 4, proj);
 
+    auto pred1 = [](auto&& val) -> decltype(auto) { return val > 0; };
+    auto pred2 = [](auto&& val) -> decltype(auto) { return val == 4; };
+    auto pred3 = [](auto&& val) -> decltype(auto) { return val < 0; };
+
+    test_range_algo<1>{}(oneapi::dpl::ranges::for_each,  std::ranges::for_each, f_mutuable, proj_mutuable);
+    test_range_algo<2>{}(oneapi::dpl::ranges::transform, std::ranges::transform, f, proj);
+
+    test_range_algo<1>{}(oneapi::dpl::ranges::all_of,  std::ranges::all_of, pred1, proj);
+    test_range_algo<1>{}(oneapi::dpl::ranges::any_of,  std::ranges::any_of, pred2, std::identity{});
+    test_range_algo<1>{}(oneapi::dpl::ranges::none_of,  std::ranges::none_of, pred3, std::identity{});
+
 #endif //_ENABLE_STD_RANGES_TESTING
 
     return TestUtils::done(_ENABLE_STD_RANGES_TESTING);
