dd8b74ec4d7d16e005411ccd332a5c9bba01aaa6
Dan Hoeflinger <dan.hoeflinger@intel.com>
2024-03-07 14:35:30 -0500

    adding bool type for host_iterator
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- test/parallel_api/iterator/input_data_sweep_host_iter.pass.cpp
+++ test/parallel_api/iterator/input_data_sweep_host_iter.pass.cpp
@@ -35,13 +35,23 @@ test(Policy&& policy, T trash, size_t n, const std::string& type_text)
     {
 
         TestUtils::usm_data_transfer<sycl::usm::alloc::shared, T> copy_out(policy.queue(), n);
-        oneapi::dpl::counting_iterator<int> counting(0);
+        auto copy_from = []() {
+            if constexpr (std::is_same_v<T, bool>)
+            {
+                return oneapi::dpl::transform_iterator(oneapi::dpl::counting_iterator<int>(0),
+                                                       [](int i) { return i % 2 == 0; });
+            }
+            else
+            {
+                return oneapi::dpl::counting_iterator<int>(0);
+            }
+        }();
         // host iterator
         std::vector<T> host_iter(n);
         wrap_recurse<__recurse, 0, /*__read =*/true, /*__reset_read=*/true, /*__write=*/true,
                      /*__check_write=*/true, /*__usable_as_perm_map=*/true, /*__usable_as_perm_src=*/false,
-                     /*__is_reversible=*/true>(policy, host_iter.begin(), host_iter.end(), counting,
-                                               copy_out.get_data(), host_iter.begin(), copy_out.get_data(), counting,
+                     /*__is_reversible=*/true>(policy, host_iter.begin(), host_iter.end(), copy_from,
+                                               copy_out.get_data(), host_iter.begin(), copy_out.get_data(), copy_from,
                                                trash, std::string("host_iterator<") + type_text + std::string(">"));
     }
     else
@@ -69,6 +79,7 @@ main()
     auto policy4 = TestUtils::create_new_policy_idx<decltype(policy), 3>(policy);
 
     // baseline with no wrapping
+    test<bool, 0>(policy1, false, n, "bool");
     test<float, 0>(policy1, -666.0f, n, "float");
     test<double, 0>(policy2, -666.0, n, "double");
     test<std::uint64_t, 0>(policy3, 999, n, "uint64_t");
