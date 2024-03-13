3b3a4d227a69c326a62d77bdf26d5f4bf98a1b38
Sergey Kopienko <>
2024-03-13 12:57:49 +0100

    include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_fpga.h - fix review comment: What's the purpose of those changes?

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_fpga.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_fpga.h
@@ -82,8 +82,8 @@ auto
 __parallel_for(oneapi::dpl::__internal::__fpga_backend_tag, _ExecutionPolicy&& __exec, _Fp __brick, _Index __count,
                _Ranges&&... __rngs)
 {
-    using _Policy = ::std::decay_t<_ExecutionPolicy>;
-    using __parallel_for_name = __internal::__kernel_name_provider<typename _Policy::kernel_name>;
+    using _CustomName = oneapi::dpl::__internal::__policy_kernel_name<_ExecutionPolicy>;
+    using __parallel_for_name = __internal::__kernel_name_provider<_CustomName>;
 
     return __parallel_for_fpga_submitter<__parallel_for_name>()(std::forward<_ExecutionPolicy>(__exec), __brick,
                                                                 __count, std::forward<_Ranges>(__rngs)...);
