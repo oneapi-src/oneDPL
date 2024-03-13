690591bb8fba45773ff97e9618282107e708763c
Sergey Kopienko <>
2024-03-13 15:15:21 +0100

    Pack __pattern_walk1_async calls into __except_handler

--- include/oneapi/dpl/internal/async_impl/glue_async_impl.h
+++ include/oneapi/dpl/internal/async_impl/glue_async_impl.h
@@ -129,9 +129,12 @@ for_each_async(_ExecutionPolicy&& __exec, _ForwardIterator __first, _ForwardIter
     const auto __dispatch_tag = oneapi::dpl::__internal::__select_backend(__exec, __first);
 
     wait_for_all(::std::forward<_Events>(__dependencies)...);
-    auto ret_val = oneapi::dpl::__internal::__pattern_walk1_async(
-        __dispatch_tag, ::std::forward<_ExecutionPolicy>(__exec), __first, __last, __f);
-    return ret_val;
+
+    return __internal::__except_handler([&]() {
+        auto ret_val = oneapi::dpl::__internal::__pattern_walk1_async(
+            __dispatch_tag, ::std::forward<_ExecutionPolicy>(__exec), __first, __last, __f);
+        return ret_val;
+    });
 }
 
 // [async.reduce]
