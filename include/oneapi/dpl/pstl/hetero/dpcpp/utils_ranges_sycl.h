ba00bdcf858b3881b55c84c09be1483091dd502a
Dan Hoeflinger <dan.hoeflinger@intel.com>
2024-03-15 08:17:45 -0400

    name change for helper struct
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/utils_ranges_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/utils_ranges_sycl.h
@@ -49,12 +49,12 @@ __create_accessor(_BufferType& __buf, _DiffType __offset, _DiffType __n)
 // Evaluates to true if the provided type is an iterator with a value_type and if the implementation of a
 //  std::vector<value_type>::iterator can be distinguished from std::vector<value_type, usm_allocator>::iterator
 template <typename Iter, typename Void = void>
-struct __vector_impl_distinguishes_usm_allocator_from_default : ::std::false_type
+struct __vector_iter_distinguishes_usm_allocator_from_default : ::std::false_type
 {
 };
 
 template <typename Iter>
-struct __vector_impl_distinguishes_usm_allocator_from_default<
+struct __vector_iter_distinguishes_usm_allocator_from_default<
     Iter, ::std::enable_if_t<!::std::is_same_v<
               typename ::std::vector<typename ::std::iterator_traits<Iter>::value_type>::iterator,
               typename ::std::vector<typename ::std::iterator_traits<Iter>::value_type,
@@ -230,7 +230,7 @@ template <class Iter>
 struct is_passed_directly<
     Iter,
     ::std::enable_if_t<
-        oneapi::dpl::__ranges::__internal::__vector_impl_distinguishes_usm_allocator_from_default<Iter>::value &&
+        oneapi::dpl::__ranges::__internal::__vector_iter_distinguishes_usm_allocator_from_default<Iter>::value &&
         (::std::is_same_v<Iter, typename ::std::vector<
                                     typename ::std::iterator_traits<Iter>::value_type,
                                     typename sycl::usm_allocator<typename ::std::iterator_traits<Iter>::value_type,
