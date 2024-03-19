f5185e20bfc04ed4a02ca971f0e2b399f4224c31
Mikhail Dvorskiy <mikhail.dvorskiy@intel.com>
2024-03-19 22:26:33 +0100

    [oneDPL][ranges] + a test for ranges::adjacent_find; + range tests improvements

--- test/parallel_api/ranges/std_ranges.pass.cpp
+++ test/parallel_api/ranges/std_ranges.pass.cpp
@@ -27,20 +27,27 @@ main()
     auto proj = [](auto&& val) -> decltype(auto) { return val * 2; };
     auto pred = [](auto&& val) -> decltype(auto) { return val == 5; };
 
-    test_range_algo<1>{}(oneapi::dpl::ranges::for_each, std::ranges::for_each, f_mutuable, proj_mutuable);
-    test_range_algo<2>{}(oneapi::dpl::ranges::transform, std::ranges::transform, f, proj);
+    using namespace test_std_ranges;
 
-    test_range_algo<1>{}(oneapi::dpl::ranges::find_if, std::ranges::find_if, pred, proj);
-    test_range_algo<1>{}(oneapi::dpl::ranges::find_if_not, std::ranges::find_if_not, pred, proj);
-    test_range_algo<1>{}(oneapi::dpl::ranges::find, std::ranges::find, 4, proj);
+    test_range_algo{}(oneapi::dpl::ranges::for_each, std::ranges::for_each, f_mutuable, proj_mutuable);
+
+    //TODO: oneapi::dpl::ranges::transform has output range as return type, std::ranges::trasnform - output iterator.
+    test_range_algo<data_in_out, false/*return type check*/>{}(oneapi::dpl::ranges::transform, std::ranges::transform, f, proj);
+
+    test_range_algo{}(oneapi::dpl::ranges::find_if, std::ranges::find_if, pred, proj);
+    test_range_algo{}(oneapi::dpl::ranges::find_if_not, std::ranges::find_if_not, pred, proj);
+    test_range_algo{}(oneapi::dpl::ranges::find, std::ranges::find, 4, proj);
 
     auto pred1 = [](auto&& val) -> decltype(auto) { return val > 0; };
     auto pred2 = [](auto&& val) -> decltype(auto) { return val == 4; };
     auto pred3 = [](auto&& val) -> decltype(auto) { return val < 0; };
 
-    test_range_algo<1>{}(oneapi::dpl::ranges::all_of,  std::ranges::all_of, pred1, proj);
-    test_range_algo<1>{}(oneapi::dpl::ranges::any_of,  std::ranges::any_of, pred2, std::identity{});
-    test_range_algo<1>{}(oneapi::dpl::ranges::none_of,  std::ranges::none_of, pred3, std::identity{});
+    test_range_algo{}(oneapi::dpl::ranges::all_of,  std::ranges::all_of, pred1, proj);
+    test_range_algo{}(oneapi::dpl::ranges::any_of,  std::ranges::any_of, pred2, std::identity{});
+    test_range_algo{}(oneapi::dpl::ranges::none_of,  std::ranges::none_of, pred3, std::identity{});
+
+    auto pred_2 = [](auto&& val1, auto&& val2) -> decltype(auto) { return val1 == val2; };
+    test_range_algo{}(oneapi::dpl::ranges::adjacent_find,  std::ranges::adjacent_find, pred_2, proj);
 
 #endif //_ENABLE_STD_RANGES_TESTING
 
