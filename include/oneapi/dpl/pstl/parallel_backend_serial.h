fe9eedfb7b5e898fa6352be49489315d9dc7df5d
Sergey Kopienko <>
2024-03-22 13:11:35 +0100

    Apply GitHUB clang format

--- include/oneapi/dpl/pstl/parallel_backend_serial.h
+++ include/oneapi/dpl/pstl/parallel_backend_serial.h
@@ -34,11 +34,12 @@ namespace dpl
 namespace __internal
 {
 template <typename _T>
-constexpr decltype(auto) __get_buffer_allocator(oneapi::dpl::__internal::__serial_backend_tag)
+constexpr decltype(auto)
+__get_buffer_allocator(oneapi::dpl::__internal::__serial_backend_tag)
 {
     return ::std::allocator<_T>{};
 }
-};
+}; // namespace __internal
 
 namespace __serial_backend
 {
