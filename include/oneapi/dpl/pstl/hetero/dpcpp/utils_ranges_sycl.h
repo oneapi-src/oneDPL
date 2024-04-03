23ed3125ab08904d2ee53b78c82342cdb73571db
Dan Hoeflinger <dan.hoeflinger@intel.com>
2024-04-03 12:51:34 -0400

    fixes for public utility
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/utils_ranges_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/utils_ranges_sycl.h
@@ -209,8 +209,10 @@ struct is_passed_directly<Iter, ::std::enable_if_t<Iter::is_passed_directly::val
 };
 
 //support std::vector::iterator with usm host / shared allocator as passed directly
-template <class Iter>
-struct is_passed_directly<Iter, std::enable_if_t<oneapi::dpl::is_usm_allocator_vector_iterator<Iter>>> : std::true_type
+template <typename Iter>
+struct is_passed_directly<Iter, std::enable_if_t<(std::is_same_v<Iter, oneapi::dpl::__internal::__usm_shared_alloc_vec_iter<Iter>> ||
+                        std::is_same_v<Iter, oneapi::dpl::__internal::__usm_host_alloc_vec_iter<Iter>>) &&
+                        oneapi::dpl::__internal::__vector_iter_distinguishes_by_allocator<Iter>::value>> : std::true_type
 {
 };
 
