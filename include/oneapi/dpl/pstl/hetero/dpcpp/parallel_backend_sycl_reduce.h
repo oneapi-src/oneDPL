a8bf26f0a13fb0809a5cc939e6bfb2bbdeb9b53f
Aidan <aidan.belton@codeplay.com>
2024-04-09 14:51:05 +0100

    Fix erorr

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_reduce.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_reduce.h
@@ -223,11 +223,11 @@ struct __parallel_transform_reduce_work_group_kernel_submitter<
     _Tp, __work_group_size, __iters_per_work_item, _Commutative, __internal::__optional_kernel_name<_KernelName...>>
 {
     template <typename _ExecutionPolicy, typename _Size, typename _ReduceOp, typename _TransformOp, typename _InitType,
-              typename _ExecutionPolicy2,
-              auto operator()(oneapi::dpl::__internal::__device_backend_tag, _ExecutionPolicy&& __exec,
-                              sycl::event& __reduce_event, _Size __n, _ReduceOp __reduce_op,
-                              _TransformOp __transform_op, _InitType __init,
-                              __result_and_scratch_storage<_ExecutionPolicy2, _Tp> __scratch_container) const
+              typename _ExecutionPolicy2>
+    auto
+    operator()(oneapi::dpl::__internal::__device_backend_tag, _ExecutionPolicy&& __exec, sycl::event& __reduce_event,
+               _Size __n, _ReduceOp __reduce_op, _TransformOp __transform_op, _InitType __init,
+               __result_and_scratch_storage<_ExecutionPolicy2, _Tp> __scratch_container) const
     {
         using _NoOpFunctor = unseq_backend::walk_n<_ExecutionPolicy, oneapi::dpl::__internal::__no_op>;
         auto __transform_pattern =
