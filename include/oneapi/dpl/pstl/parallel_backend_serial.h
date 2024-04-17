775eff653ad8235df31bf93895fad084384e8e5e
Sergey Kopienko <sergey.kopienko@intel.com>
2024-04-17 10:45:44 +0200

    struct __backend_impl<::oneapi::dpl::__internal::__serial_backend_tag> - implementation of SERIAL backend
    Signed-off-by: Sergey Kopienko <sergey.kopienko@intel.com>

--- include/oneapi/dpl/pstl/parallel_backend_serial.h
+++ include/oneapi/dpl/pstl/parallel_backend_serial.h
@@ -25,35 +25,92 @@
 #include <utility>
 #include <type_traits>
 
+#include "execution_defs.h"
+
 #include "parallel_backend_utils.h"
 
 namespace oneapi
 {
 namespace dpl
 {
-namespace __serial_backend
+namespace __backend
 {
 
-template <typename _ExecutionPolicy, typename _Tp>
-using __buffer = oneapi::dpl::__utils::__buffer_impl<std::decay_t<_ExecutionPolicy>, _Tp, std::allocator>;
+template <>
+struct __backend_impl<::oneapi::dpl::__internal::__serial_backend_tag>
+{
+    template <typename _ExecutionPolicy, typename _Tp>
+    using __buffer = oneapi::dpl::__utils::__buffer_impl<std::decay_t<_ExecutionPolicy>, _Tp, std::allocator>;
+
+    static void
+    __cancel_execution();
+
+    template <class _ExecutionPolicy, class _Index, class _Fp>
+    static void
+    __parallel_for(_ExecutionPolicy&&, _Index __first, _Index __last, _Fp __f);
+
+    template <class _ExecutionPolicy, class _ForwardIterator, class _Fp>
+    static void
+    __parallel_for_each(_ExecutionPolicy&&, _ForwardIterator __begin, _ForwardIterator __end, _Fp __f);
+
+    template <class _ExecutionPolicy, typename _F1, typename _F2>
+    static void
+    __parallel_invoke(_ExecutionPolicy&&, _F1&& __f1, _F2&& __f2);
+
+    template <class _ExecutionPolicy, typename _RandomAccessIterator1, typename _RandomAccessIterator2,
+              typename _RandomAccessIterator3, typename _Compare, typename _LeafMerge>
+    static void
+    __parallel_merge(_ExecutionPolicy&&, _RandomAccessIterator1 __first1, _RandomAccessIterator1 __last1,
+                     _RandomAccessIterator2 __first2, _RandomAccessIterator2 __last2, _RandomAccessIterator3 __outit,
+                     _Compare __comp, _LeafMerge __leaf_merge);
+
+    template <class _ExecutionPolicy, class _Value, class _Index, typename _RealBody, typename _Reduction>
+    static _Value
+    __parallel_reduce(_ExecutionPolicy&&, _Index __first, _Index __last, const _Value& __identity,
+                      const _RealBody& __real_body, const _Reduction&);
+
+    template <class _ExecutionPolicy, typename _RandomAccessIterator, typename _Compare, typename _LeafSort>
+    static void
+    __parallel_stable_sort(_ExecutionPolicy&&, _RandomAccessIterator __first, _RandomAccessIterator __last,
+                           _Compare __comp, _LeafSort __leaf_sort, ::std::size_t = 0);
+
+    template <class _ExecutionPolicy, typename _Index, typename _Tp, typename _Rp, typename _Cp, typename _Sp,
+              typename _Ap>
+    static void
+    __parallel_strict_scan(_ExecutionPolicy&&, _Index __n, _Tp __initial, _Rp __reduce, _Cp __combine, _Sp __scan,
+                           _Ap __apex);
+
+    template <class _ExecutionPolicy, class _Index, class _UnaryOp, class _Tp, class _BinaryOp, class _Reduce>
+    static _Tp
+    __parallel_transform_reduce(_ExecutionPolicy&&, _Index __first, _Index __last, _UnaryOp, _Tp __init, _BinaryOp,
+                                _Reduce __reduce);
+
+    template <class _ExecutionPolicy, class _Index, class _UnaryOp, class _Tp, class _BinaryOp, class _Reduce,
+              class _Scan>
+    static _Tp
+    __parallel_transform_scan(_ExecutionPolicy&&, _Index __n, _UnaryOp, _Tp __init, _BinaryOp, _Reduce, _Scan __scan);
+};
 
 inline void
-__cancel_execution(oneapi::dpl::__internal::__serial_backend_tag)
+__backend_impl<::oneapi::dpl::__internal::__serial_backend_tag>::__cancel_execution()
 {
 }
 
 template <class _ExecutionPolicy, class _Index, class _Fp>
 void
-__parallel_for(oneapi::dpl::__internal::__serial_backend_tag, _ExecutionPolicy&&, _Index __first, _Index __last,
-               _Fp __f)
+__backend_impl<::oneapi::dpl::__internal::__serial_backend_tag>::__parallel_for(_ExecutionPolicy&&, _Index __first,
+                                                                                _Index __last, _Fp __f)
 {
     __f(__first, __last);
 }
 
 template <class _ExecutionPolicy, class _Value, class _Index, typename _RealBody, typename _Reduction>
 _Value
-__parallel_reduce(oneapi::dpl::__internal::__serial_backend_tag, _ExecutionPolicy&&, _Index __first, _Index __last,
-                  const _Value& __identity, const _RealBody& __real_body, const _Reduction&)
+__backend_impl<::oneapi::dpl::__internal::__serial_backend_tag>::__parallel_reduce(_ExecutionPolicy&&, _Index __first,
+                                                                                   _Index __last,
+                                                                                   const _Value& __identity,
+                                                                                   const _RealBody& __real_body,
+                                                                                   const _Reduction&)
 {
     if (__first == __last)
     {
@@ -67,16 +124,18 @@ __parallel_reduce(oneapi::dpl::__internal::__serial_backend_tag, _ExecutionPolic
 
 template <class _ExecutionPolicy, class _Index, class _UnaryOp, class _Tp, class _BinaryOp, class _Reduce>
 _Tp
-__parallel_transform_reduce(oneapi::dpl::__internal::__serial_backend_tag, _ExecutionPolicy&&, _Index __first,
-                            _Index __last, _UnaryOp, _Tp __init, _BinaryOp, _Reduce __reduce)
+__backend_impl<::oneapi::dpl::__internal::__serial_backend_tag>::__parallel_transform_reduce(
+    _ExecutionPolicy&&, _Index __first, _Index __last, _UnaryOp, _Tp __init, _BinaryOp, _Reduce __reduce)
 {
     return __reduce(__first, __last, __init);
 }
 
 template <class _ExecutionPolicy, typename _Index, typename _Tp, typename _Rp, typename _Cp, typename _Sp, typename _Ap>
 void
-__parallel_strict_scan(oneapi::dpl::__internal::__serial_backend_tag, _ExecutionPolicy&&, _Index __n, _Tp __initial,
-                       _Rp __reduce, _Cp __combine, _Sp __scan, _Ap __apex)
+__backend_impl<::oneapi::dpl::__internal::__serial_backend_tag>::__parallel_strict_scan(_ExecutionPolicy&&, _Index __n,
+                                                                                        _Tp __initial, _Rp __reduce,
+                                                                                        _Cp __combine, _Sp __scan,
+                                                                                        _Ap __apex)
 {
     _Tp __sum = __initial;
     if (__n)
@@ -88,16 +147,19 @@ __parallel_strict_scan(oneapi::dpl::__internal::__serial_backend_tag, _Execution
 
 template <class _ExecutionPolicy, class _Index, class _UnaryOp, class _Tp, class _BinaryOp, class _Reduce, class _Scan>
 _Tp
-__parallel_transform_scan(oneapi::dpl::__internal::__serial_backend_tag, _ExecutionPolicy&&, _Index __n, _UnaryOp,
-                          _Tp __init, _BinaryOp, _Reduce, _Scan __scan)
+__backend_impl<::oneapi::dpl::__internal::__serial_backend_tag>::__parallel_transform_scan(_ExecutionPolicy&&,
+                                                                                           _Index __n, _UnaryOp,
+                                                                                           _Tp __init, _BinaryOp,
+                                                                                           _Reduce, _Scan __scan)
 {
     return __scan(_Index(0), __n, __init);
 }
 
 template <class _ExecutionPolicy, typename _RandomAccessIterator, typename _Compare, typename _LeafSort>
 void
-__parallel_stable_sort(oneapi::dpl::__internal::__serial_backend_tag, _ExecutionPolicy&&, _RandomAccessIterator __first,
-                       _RandomAccessIterator __last, _Compare __comp, _LeafSort __leaf_sort, ::std::size_t = 0)
+__backend_impl<::oneapi::dpl::__internal::__serial_backend_tag>::__parallel_stable_sort(
+    _ExecutionPolicy&&, _RandomAccessIterator __first, _RandomAccessIterator __last, _Compare __comp,
+    _LeafSort __leaf_sort, ::std::size_t /*= 0*/)
 {
     __leaf_sort(__first, __last, __comp);
 }
@@ -105,16 +167,18 @@ __parallel_stable_sort(oneapi::dpl::__internal::__serial_backend_tag, _Execution
 template <class _ExecutionPolicy, typename _RandomAccessIterator1, typename _RandomAccessIterator2,
           typename _RandomAccessIterator3, typename _Compare, typename _LeafMerge>
 void
-__parallel_merge(oneapi::dpl::__internal::__serial_backend_tag, _ExecutionPolicy&&, _RandomAccessIterator1 __first1,
-                 _RandomAccessIterator1 __last1, _RandomAccessIterator2 __first2, _RandomAccessIterator2 __last2,
-                 _RandomAccessIterator3 __outit, _Compare __comp, _LeafMerge __leaf_merge)
+__backend_impl<::oneapi::dpl::__internal::__serial_backend_tag>::__parallel_merge(
+    _ExecutionPolicy&&, _RandomAccessIterator1 __first1, _RandomAccessIterator1 __last1,
+    _RandomAccessIterator2 __first2, _RandomAccessIterator2 __last2, _RandomAccessIterator3 __outit, _Compare __comp,
+    _LeafMerge __leaf_merge)
 {
     __leaf_merge(__first1, __last1, __first2, __last2, __outit, __comp);
 }
 
 template <class _ExecutionPolicy, typename _F1, typename _F2>
 void
-__parallel_invoke(oneapi::dpl::__internal::__serial_backend_tag, _ExecutionPolicy&&, _F1&& __f1, _F2&& __f2)
+__backend_impl<::oneapi::dpl::__internal::__serial_backend_tag>::__parallel_invoke(_ExecutionPolicy&&, _F1&& __f1,
+                                                                                   _F2&& __f2)
 {
     ::std::forward<_F1>(__f1)();
     ::std::forward<_F2>(__f2)();
@@ -122,14 +186,15 @@ __parallel_invoke(oneapi::dpl::__internal::__serial_backend_tag, _ExecutionPolic
 
 template <class _ExecutionPolicy, class _ForwardIterator, class _Fp>
 void
-__parallel_for_each(oneapi::dpl::__internal::__serial_backend_tag, _ExecutionPolicy&&, _ForwardIterator __begin,
-                    _ForwardIterator __end, _Fp __f)
+__backend_impl<::oneapi::dpl::__internal::__serial_backend_tag>::__parallel_for_each(_ExecutionPolicy&&,
+                                                                                     _ForwardIterator __begin,
+                                                                                     _ForwardIterator __end, _Fp __f)
 {
     for (auto __iter = __begin; __iter != __end; ++__iter)
         __f(*__iter);
 }
 
-} // namespace __serial_backend
+} // namespace __backend
 } // namespace dpl
 } // namespace oneapi
 
