a53aa090b8d18d5db70b320c1a428353d5751fc9
Łukasz Ślusarczyk <lukasz.slusarczyk@intel.com>
2024-04-30 10:57:21 +0200

    not using fmt library, only format from std

--- test/distributed-ranges/shp/xhp-tests.hpp
+++ test/distributed-ranges/shp/xhp-tests.hpp
@@ -8,19 +8,10 @@
 #include <gtest/gtest.h>
 #include <oneapi/dpl/distributed-ranges>
 
-// #ifdef __cpp_lib_format
-// #include <format>
-// namespace drfmt {
-//   using std::format;
-// }
-// #else
-#include <fmt/format.h>
-#include <fmt/core.h>
-#include <fmt/ranges.h>
+#include <format>
 namespace drfmt {
-  using fmt::format;
+   using std::format;
 }
-// #endif
 
 #define TEST_SHP
 // To share tests with MHP
