c31b957b346826c1a5327909151a171b182bac15
Dan Hoeflinger <dan.hoeflinger@intel.com>
2024-05-10 11:37:17 -0400

    Avoid usm allocated vector iterators wrapped in custom iterators when building for FPGA
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- test/support/test_config.h
+++ test/support/test_config.h
@@ -195,4 +195,14 @@
 #define _PSTL_ICPX_TEST_MINMAX_ELEMENT_BROKEN                                                                         \
     (TEST_DPCPP_BACKEND_PRESENT && defined(__INTEL_LLVM_COMPILER) && __INTEL_LLVM_COMPILER < 20220300)
 
+// oneAPI DPC++ compiler fails to compile the sum of an integer and an iterator to a usm-allocated std vector when
+// building for an FPGA device.  This prevents fpga compilation of usm-allocated std vector wrapped in zip transform and
+// permutation iterators (as a map).
+#if (TEST_DPCPP_BACKEND_PRESENT && defined(ONEDPL_FPGA_DEVICE) && defined(__INTEL_LLVM_COMPILER) &&                   \
+        __INTEL_LLVM_COMPILER <= 20240200)
+#    define _PSTL_ICPX_FPGA_TEST_USM_VECTOR_ITERATOR_BROKEN 1
+#else
+#    define _PSTL_ICPX_FPGA_TEST_USM_VECTOR_ITERATOR_BROKEN 0
+#endif
+
 #endif // _TEST_CONFIG_H
