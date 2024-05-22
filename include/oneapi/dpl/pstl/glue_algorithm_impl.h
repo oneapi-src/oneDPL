3f9005f9dfdf8b91859bd38f9f1b1dbb6731ac5e
Sergey Kopienko <sergey.kopienko@intel.com>
2024-05-22 14:43:16 +0200

    include/oneapi/dpl/pstl/glue_algorithm_impl.h - restrictions impl for sort(_ExecutionPolicy&&, _RandomAccessIterator, _RandomAccessIterator, _Compare)
    Signed-off-by: Sergey Kopienko <sergey.kopienko@intel.com>

--- include/oneapi/dpl/pstl/glue_algorithm_impl.h
+++ include/oneapi/dpl/pstl/glue_algorithm_impl.h
@@ -664,14 +664,16 @@ sort(_ExecutionPolicy&& __exec, _RandomAccessIterator __first, _RandomAccessIter
 {
     const auto __dispatch_tag = oneapi::dpl::__internal::__select_backend(__exec, __first);
 
-    typedef typename ::std::iterator_traits<_RandomAccessIterator>::value_type _InputType;
-
-    // According to https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2023/n4928.pdf,
-    // 27.8.2.1 sort
+    // According to https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2023/n4928.pdf, 27.8.2.1 sort
+    //              https://en.cppreference.com/w/cpp/algorithm/sort
     // Preconditions:
     //      For the overloads in namespace std, RandomAccessIterator meets the Cpp17ValueSwappable requirements (16.4.4.3)
     //      and the type of *first meets the Cpp17MoveConstructible (Table 32)
     //      and Cpp17MoveAssignable(Table 34) requirements.
+    static_assert(std::is_swappable_v<_RandomAccessIterator>);
+    typedef typename ::std::iterator_traits<_RandomAccessIterator>::value_type _ValueType;
+    static_assert(std::is_move_constructible_v<_ValueType>);
+    static_assert(std::is_move_assignable_v<_ValueType>);
 
     oneapi::dpl::__internal::__pattern_sort(__dispatch_tag, ::std::forward<_ExecutionPolicy>(__exec), __first, __last,
                                             __comp);
