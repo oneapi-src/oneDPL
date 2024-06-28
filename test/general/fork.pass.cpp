978d27ac8262456cc7cb92cb5c008c7dd4788bd0
Alexey Kukanov <alexey.kukanov@intel.com>
2024-06-28 19:24:52 +0200

    Check for POSIX instead of !_WIN32

--- test/general/fork.pass.cpp
+++ test/general/fork.pass.cpp
@@ -1,12 +1,12 @@
 #include <oneapi/dpl/execution>
 #include "support/utils.h"
 
-#ifndef _WIN32
+#if __has_include(<unistd.h>)
 #include <unistd.h>
 #endif
 
 int main(){
-#ifndef _WIN32
+#ifdef _POSIX_VERSION // defined in <unistd.h>
     [[maybe_unused]] pid_t pid = fork();
 #endif
     return TestUtils::done();
