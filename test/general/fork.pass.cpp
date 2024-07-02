8e5d5dd5ea6eea243e63df814798955ba7fa9774
Alexey Kukanov <alexey.kukanov@intel.com>
2024-07-02 12:28:04 +0200

    Add waiting for the child process created by fork

--- test/general/fork.pass.cpp
+++ test/general/fork.pass.cpp
@@ -1,13 +1,27 @@
 #include <oneapi/dpl/execution>
 #include "support/utils.h"
 
-#if __has_include(<unistd.h>)
+#if __has_include(<unistd.h>) && __has_include(<sys/wait.h>)
 #include <unistd.h>
+#include <sys/wait.h>
+#include <cerrno>
 #endif
 
 int main(){
 #ifdef _POSIX_VERSION // defined in <unistd.h>
-    [[maybe_unused]] pid_t pid = fork();
+    pid_t pid = fork();
+    if (pid < 0) // error
+        return errno;
+    if (pid == 0) // child
+        return 0;
+    int status;
+    pid_t pid2 = waitpid(pid, &status, 0);
+    if (pid2 != pid) // error
+        return errno;
+    if (WIFEXITED(status))
+        return WEXITSTATUS(status);
+    return -1;
+#else
+    return TestUtils::done(0); // skipped
 #endif
-    return TestUtils::done();
 }
