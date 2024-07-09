896677ed310dfbed905486c146f16fa6da2bdf82
Mikolaj Komar <mikolaj.komar@intel.com>
2024-07-09 17:03:18 +0200

    Add for_each_n as in distributed-ranges

--- test/distributed-ranges/common/for_each.cpp
+++ test/distributed-ranges/common/for_each.cpp
@@ -9,6 +9,22 @@ template <typename T> class ForEach : public testing::Test {
 public:
 };
 
+template <typename T>
+void test_foreach_n(std::vector<T> v, int n, int initial_skip, auto func) {
+  auto size = v.size();
+  xhp::distributed_vector<T> d_v(size);
+
+  for (std::size_t idx = 0; idx < size; idx++) {
+    d_v[idx] = v[idx];
+  }
+  barrier();
+
+  xhp::for_each_n(d_v.begin() + initial_skip, n, func);
+  rng::for_each_n(v.begin() + initial_skip, n, func);
+
+  EXPECT_TRUE(equal(v, d_v));
+}
+
 TYPED_TEST_SUITE(ForEach, AllTypes);
 
 TYPED_TEST(ForEach, Range) {
@@ -43,6 +59,33 @@ TYPED_TEST(ForEach, RangeAlignedZip) {
   xhp::for_each(dist, copy);
   rng::for_each(local, copy);
   EXPECT_EQ(local, dist);
+}
+
+TYPED_TEST(ForEach, ForEachN) {
+  auto negate = [](auto &&v) { v = -v; };
+  test_foreach_n<int>({1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5, 0, negate);
+}
+
+TYPED_TEST(ForEach, ForEachNLongerThanSize) {
+  auto negate = [](auto &&v) { v = -v; };
+  test_foreach_n<int>({1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 10, 0, negate);
+}
+
+TYPED_TEST(ForEach, ForEachNPartial) {
+  auto negate = [](auto &&v) { v = -v; };
+  test_foreach_n<int>({1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5, 2, negate);
+}
+
+TYPED_TEST(ForEach, ForEachNWholeLength) {
+  Ops1<TypeParam> ops(10);
+
+  auto negate = [](auto &&v) { v = -v; };
+  auto input = ops.vec;
+
+  xhp::for_each_n(ops.dist_vec.begin(), 10, negate);
+  rng::for_each(ops.vec.begin(), ops.vec.end(), negate);
+
+  EXPECT_TRUE(check_unary_op(input, ops.vec, ops.dist_vec));
 }
 
 // Disabled. Not sure how to support this properly for MHP. We need to
