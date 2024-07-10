9944c46bd31e7344a591467c9821d1a2fdaa7f2b
Łukasz Ślusarczyk <112692748+lslusarczyk@users.noreply.github.com>
2024-07-10 12:10:40 +0200

    changed copyright comments in DR to be oneDPL compliant (#1687)

--- test/distributed-ranges/shp/containers.cpp
+++ test/distributed-ranges/shp/containers.cpp
@@ -1,6 +1,17 @@
-// SPDX-FileCopyrightText: Intel Corporation
+// -*- C++ -*-
+//===----------------------------------------------------------------------===//
 //
-// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (C) Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// This file incorporates work covered by the following copyright and permission
+// notice:
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+//
+//===----------------------------------------------------------------------===//
 
 #include "containers.hpp"
 
