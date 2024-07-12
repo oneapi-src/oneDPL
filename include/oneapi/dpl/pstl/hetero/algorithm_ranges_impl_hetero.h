eeb062fdd7e12ccbf4265381a1418c352c05bbdc
Dan Hoeflinger <dan.hoeflinger@intel.com>
2024-07-12 08:38:04 -0400

    templated size_type
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h
+++ include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h
@@ -380,26 +380,26 @@ __pattern_scan_copy(__hetero_tag<_BackendTag>, _ExecutionPolicy&& __exec, _Range
     return __res;
 }
 
-template <typename _Predicate>
+template <typename _SizeType, typename _Predicate>
 struct __gen_count_pred
 {
     template <typename _InRng>
-    std::size_t operator()(_InRng&& __in_rng, std::size_t __idx)
+    _SizeType operator()(_InRng&& __in_rng, _SizeType __idx)
     {
-        return __pred(__in_rng[__idx]) ? std::size_t{1} : std::size_t{0};
+        return __pred(__in_rng[__idx]) ? _SizeType{1} : _SizeType{0};
     }
     _Predicate __pred;
 };
 
-template <typename _Predicate>
+template <typename _SizeType, typename _Predicate>
 struct __gen_expand_count_pred
 {
     template <typename _InRng>
-    auto operator()(_InRng&& __in_rng, std::size_t __idx)
+    auto operator()(_InRng&& __in_rng, _SizeType __idx)
     {
         auto ele = __in_rng[__idx];
         bool mask = __pred(ele);
-        return std::tuple( mask ? std::size_t{1} : std::size_t{0}, mask, ele);
+        return std::tuple( mask ? _SizeType{1} : _SizeType{0}, mask, ele);
     }
     _Predicate __pred;
 };
@@ -408,14 +408,14 @@ struct __gen_expand_count_pred
 template <typename _BinaryOp>
 struct __scan_expanded_count
 {
-    template <typename _ValueType>
-    auto operator()(std::size_t __carry_in, const std::tuple<std::size_t, bool, _ValueType>& b)
+    template <typename _SizeType, typename _ValueType>
+    auto operator()(_SizeType __carry_in, const std::tuple<_SizeType, bool, _ValueType>& b)
     {
         return std::tuple(__binary_op(__carry_in, std::get<0>(b)), std::get<1>(b), std::get<2>(b));
     }
 
     template <typename _ValueType>
-    auto operator()(const std::tuple<std::size_t, bool, _ValueType>& a, const std::tuple<std::size_t, bool, _ValueType>& b)
+    auto operator()(const std::tuple<_SizeType, bool, _ValueType>& a, const std::tuple<_SizeType, bool, _ValueType>& b)
     {
         return this->operator()(std::get<0>(a), b);
     }
@@ -425,8 +425,8 @@ struct __scan_expanded_count
 
 struct __write_to_idx_if
 {
-    template<typename _OutRng, typename ValueType>
-    void operator()(_OutRng&& __out, std::size_t __idx, const ValueType& __v) const
+    template<typename _OutRng, typename _SizeType, typename ValueType>
+    void operator()(_OutRng&& __out, _SizeType __idx, const ValueType& __v) const
     {
         if (std::get<1>(__v))
             __out[std::get<0>(__v)] = std::get<2>(__v);
@@ -442,15 +442,11 @@ __pattern_copy_if(__hetero_tag<_BackendTag> __tag, _ExecutionPolicy&& __exec, _R
     using _SizeType = decltype(__rng1.size());
     using _ReduceOp = ::std::plus<_SizeType>;
 
-    unseq_backend::__create_mask<_Predicate, _SizeType> __create_mask_op{__pred};
-    unseq_backend::__copy_by_mask<_ReduceOp, _Assign, /*inclusive*/ ::std::true_type, 1> __copy_by_mask_op;
-
-
     return __future(__parallel_transform_reduce_then_scan(__tag, ::std::forward<_ExecutionPolicy>(__exec),
                                                           ::std::forward<_Range1>(__rng1),
                                                           ::std::forward<_Range2>(__rng2),
-                                                          __gen_count_pred{__pred}, std::plus<std::size_t>{},
-                                                          __gen_expand_count_pred{__pred},
+                                                          __gen_count_pred<_SizeType, _ReduceOp>{__pred}, std::plus<std::size_t>{},
+                                                          __gen_expand_count_pred<_SizeType, _ReduceOp>{__pred},
                                                           __scan_expanded_count<std::plus<std::size_t>>{},
                                                           __write_to_idx_if{},
                                                           oneapi::dpl::unseq_backend::__no_init_value{},
