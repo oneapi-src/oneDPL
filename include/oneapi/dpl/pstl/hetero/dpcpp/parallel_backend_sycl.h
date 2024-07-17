6e71ad8b289099bfb1c3e6f172bf39ae0462cf9c
Dan Hoeflinger <dan.hoeflinger@intel.com>
2024-07-17 11:07:39 -0400

    bugfix for zip iterator inputs
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
@@ -819,7 +819,12 @@ struct __gen_expand_count_pred
     auto
     operator()(_InRng&& __in_rng, _SizeType __idx) const
     {
-        auto ele = __in_rng[__idx];
+        // Explicitly creating this element type is necessary to avoid modifying the input data when _InRng is a
+        //  zip_iterator which will return a tuple of references when dereferenced. With this explicit type, we copy
+        //  the values of zipped the input types rather than their references.
+        using _ElementType =
+            oneapi::dpl::__internal::__decay_with_tuple_specialization_t<oneapi::dpl::__internal::__value_t<_InRng>>;
+        _ElementType ele = __in_rng[__idx];
         bool mask = __pred(ele);
         return std::tuple(mask ? _SizeType{1} : _SizeType{0}, mask, ele);
     }
