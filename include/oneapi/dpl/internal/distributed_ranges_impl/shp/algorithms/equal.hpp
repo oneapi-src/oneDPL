4b9e59cf5a3943484d29f88883723e0f6acea1d3
Mikolaj Komar <mikolaj.komar@intel.com>
2024-07-17 16:07:15 +0200

    Fixed imports in shp::equal

--- include/oneapi/dpl/internal/distributed_ranges_impl/shp/algorithms/equal.hpp
+++ include/oneapi/dpl/internal/distributed_ranges_impl/shp/algorithms/equal.hpp
@@ -3,17 +3,21 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 #pragma once
-#include <concepts>
-#include <dr/concepts/concepts.hpp>
-#include <dr/mhp/algorithms/reduce.hpp>
-#include <dr/mhp/algorithms/transform.hpp>
-#include <dr/mhp/views/zip.hpp>
 
-namespace dr::mhp::_detail {
-template <dr::distributed_range R1, dr::distributed_range R2>
+#include <oneapi/dpl/internal/distributed_ranges_impl/shp/algorithms/reduce.hpp>
+#include <oneapi/dpl/internal/distributed_ranges_impl/shp/detail.hpp>
+#include <oneapi/dpl/internal/distributed_ranges_impl/shp/init.hpp>
+#include <oneapi/dpl/internal/distributed_ranges_impl/shp/util.hpp>
+#include <oneapi/dpl/internal/distributed_ranges_impl/shp/views/views.hpp>
+#include <oneapi/dpl/internal/distributed_ranges_impl/shp/zip_view.hpp>
+
+namespace dr::shp {
+
+template <typename ExecutionPolicy, dr::distributed_range R1,
+          dr::distributed_range R2>
   requires std::equality_comparable_with<rng::range_value_t<R1>,
                                          rng::range_value_t<R2>>
-bool equal(std::size_t root, bool root_provided, R1 &&r1, R2 &&r2) {
+bool equal(ExecutionPolicy &&policy, R1 &&r1, R2 &&r2) {
 
   if (rng::distance(r1) != rng::distance(r2)) {
     return false;
@@ -26,36 +30,14 @@ bool equal(std::size_t root, bool root_provided, R1 &&r1, R2 &&r2) {
   };
 
   auto zipped_views = views::zip(r1, r2);
-
-  // we are using mhp::transform instead of mhp::views::transform due to
-  // compilation error refer to DRA-192 and test/gtest/mhp/reduce.cpp
-  mhp::distributed_vector<int> compared(rng::distance(r1));
-  mhp::transform(zipped_views, compared.begin(), compare);
-
+  auto compared = shp::views::transform(zipped_views, compare);
   auto min = [](double x, double y) { return std::min(x, y); };
-  if (root_provided) {
-    auto result = mhp::reduce(root, compared, 1, min);
-    return result == 1;
-  }
-  auto result = mhp::reduce(compared, 1, min);
+  auto result = shp::reduce(policy, compared, 1, min);
   return result == 1;
 }
 
-} // namespace dr::mhp::_detail
-
-namespace dr::mhp {
 template <dr::distributed_range R1, dr::distributed_range R2>
-  requires std::equality_comparable_with<rng::range_value_t<R1>,
-                                         rng::range_value_t<R2>>
-bool equal(std::size_t root, R1 &&r1, R2 &&r2) {
-  return _detail::equal(root, true, r1, r2);
-}
-
-template <dr::distributed_range R1, dr::distributed_range R2>
-  requires std::equality_comparable_with<rng::range_value_t<R1>,
-                                         rng::range_value_t<R2>>
 bool equal(R1 &&r1, R2 &&r2) {
-  return _detail::equal(0, false, r1, r2);
+  return equal(dr::shp::par_unseq, r1, r2);
 }
-
-} // namespace dr::mhp
+} // namespace dr::shp
