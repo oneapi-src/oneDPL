67f2dcd41212e96a8ffa4051a0b0249969cfda67
Matthew Michel <106704043+mmichel11@users.noreply.github.com>
2024-07-19 16:44:22 -0500

    [PROTOTYPE] Resolve conversion issues between internal tuple and std::tuple in zip_iterator.pass (#1714)
    * Fix for zip_iterator.pass in copy_if assignment
    
    Signed-off-by: Matthew Michel <matthew.michel@intel.com>
    
    * Add similar fix to __simple_write_to_idx
    
    Signed-off-by: Matthew Michel <matthew.michel@intel.com>
    
    ---------
    
    Signed-off-by: Matthew Michel <matthew.michel@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
@@ -801,7 +801,12 @@ struct __simple_write_to_idx
     void
     operator()(_OutRng&& __out, std::size_t __idx, const ValueType& __v) const
     {
-        __out[__idx] = __v;
+        // Use of an explicit cast to our internal tuple type is required to resolve conversion issues between our
+        // internal tuple and std::tuple. If the underlying type is not a tuple, then the type will just be passed through.
+        using _ConvertedTupleType =
+            typename oneapi::dpl::__internal::__get_tuple_type<std::decay_t<decltype(__v)>,
+                                                               std::decay_t<decltype(__out[__idx])>>::__type;
+        __out[__idx] = static_cast<_ConvertedTupleType>(__v);
     }
 };
 
@@ -852,8 +857,13 @@ struct __write_to_idx_if
     void
     operator()(_OutRng&& __out, _SizeType __idx, const ValueType& __v) const
     {
+        // Use of an explicit cast to our internal tuple type is required to resolve conversion issues between our
+        // internal tuple and std::tuple. If the underlying type is not a tuple, then the type will just be passed through.
+        using _ConvertedTupleType =
+            typename oneapi::dpl::__internal::__get_tuple_type<std::decay_t<decltype(std::get<2>(__v))>,
+                                                               std::decay_t<decltype(__out[__idx])>>::__type;
         if (std::get<1>(__v))
-            __out[std::get<0>(__v) - 1] = std::get<2>(__v);
+            __out[std::get<0>(__v) - 1] = static_cast<_ConvertedTupleType>(std::get<2>(__v));
     }
 };
 
