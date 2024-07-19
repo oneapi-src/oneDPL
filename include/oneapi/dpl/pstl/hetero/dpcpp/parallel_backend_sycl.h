33b399dca684527b14074f3e3b3d6caa7dd438f9
Sergey Kopienko <sergey.kopienko@intel.com>
2024-07-19 10:03:41 +0200

    include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h - require sub-group size 32
    Signed-off-by: Sergey Kopienko <sergey.kopienko@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
@@ -1188,6 +1188,10 @@ __parallel_find_or(oneapi::dpl::__internal::__device_backend_tag, _ExecutionPoli
 
     auto __pred = oneapi::dpl::__par_backend_hetero::__early_exit_find_or<_ExecutionPolicy, _Brick>{__f};
 
+    const auto __max_sg_size = oneapi::dpl::__internal::__max_sub_group_size(__exec);
+    if (32 != __max_sg_size)
+        assert(false);
+
     // scope is to copy data back to __result after destruction of temporary sycl:buffer
     {
         sycl::buffer<_AtomicType, 1> __result_sycl_buf(&__result, 1); // temporary storage for global atomic
@@ -1206,7 +1210,7 @@ __parallel_find_or(oneapi::dpl::__internal::__device_backend_tag, _ExecutionPoli
 #endif
                 sycl::nd_range</*dim=*/1>(sycl::range</*dim=*/1>(__n_groups * __wgroup_size),
                                           sycl::range</*dim=*/1>(__wgroup_size)),
-                [=](sycl::nd_item</*dim=*/1> __item_id) {
+                [=](sycl::nd_item</*dim=*/1> __item_id) [[_ONEDPL_SYCL_REQD_SUB_GROUP_SIZE_IF_SUPPORTED(32)]] {
                     auto __local_idx = __item_id.get_local_id(0);
 
                     // 1. Set initial value to local found state
