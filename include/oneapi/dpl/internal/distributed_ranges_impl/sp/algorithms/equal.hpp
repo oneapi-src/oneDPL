b8aa3b06a139c201a2a67e85b1ac7d7cce03793f
Mikolaj Komar <mikolaj.komar@intel.com>
2024-07-22 12:32:19 +0200

    Fixed equal to use sp instead of shp

--- include/oneapi/dpl/internal/distributed_ranges_impl/sp/algorithms/equal.hpp
+++ include/oneapi/dpl/internal/distributed_ranges_impl/sp/algorithms/equal.hpp
@@ -4,15 +4,15 @@
 
 #pragma once
 
-#include <oneapi/dpl/internal/distributed_ranges_impl/shp/algorithms/fill.hpp>
-#include <oneapi/dpl/internal/distributed_ranges_impl/shp/algorithms/reduce.hpp>
-#include <oneapi/dpl/internal/distributed_ranges_impl/shp/detail.hpp>
-#include <oneapi/dpl/internal/distributed_ranges_impl/shp/init.hpp>
-#include <oneapi/dpl/internal/distributed_ranges_impl/shp/util.hpp>
-#include <oneapi/dpl/internal/distributed_ranges_impl/shp/views/views.hpp>
-#include <oneapi/dpl/internal/distributed_ranges_impl/shp/zip_view.hpp>
+#include <oneapi/dpl/internal/distributed_ranges_impl/sp/algorithms/fill.hpp>
+#include <oneapi/dpl/internal/distributed_ranges_impl/sp/algorithms/reduce.hpp>
+#include <oneapi/dpl/internal/distributed_ranges_impl/sp/detail.hpp>
+#include <oneapi/dpl/internal/distributed_ranges_impl/sp/init.hpp>
+#include <oneapi/dpl/internal/distributed_ranges_impl/sp/util.hpp>
+#include <oneapi/dpl/internal/distributed_ranges_impl/sp/views/views.hpp>
+#include <oneapi/dpl/internal/distributed_ranges_impl/sp/zip_view.hpp>
 
-namespace oneapi::dpl::experimental::dr::shp
+namespace oneapi::dpl::experimental::dr::sp
 {
 
 template <typename ExecutionPolicy, dr::distributed_range R1, dr::distributed_range R2>
@@ -31,9 +31,9 @@ equal(ExecutionPolicy&& policy, R1&& r1, R2&& r2)
     auto compare = [](auto&& elems) { return elems.first == elems.second ? 1 : 0; };
 
     auto zipped_views = views::zip(r1, r2);
-    auto compared = shp::views::transform(zipped_views, compare);
+    auto compared = sp::views::transform(zipped_views, compare);
     auto min = [](double x, double y) { return std::min(x, y); };
-    auto result = shp::reduce(policy, compared, 1, min);
+    auto result = sp::reduce(policy, compared, 1, min);
     return result == 1;
 }
 
@@ -41,6 +41,6 @@ template <dr::distributed_range R1, dr::distributed_range R2>
 bool
 equal(R1&& r1, R2&& r2)
 {
-    return equal(dr::shp::par_unseq, r1, r2);
+    return equal(dr::sp::par_unseq, r1, r2);
 }
-} // namespace oneapi::dpl::experimental::dr::shp
+} // namespace oneapi::dpl::experimental::dr::sp
