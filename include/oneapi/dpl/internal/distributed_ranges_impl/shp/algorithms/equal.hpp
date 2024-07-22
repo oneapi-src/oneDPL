04c1669c879553464e8a3c12a94c525281ae39af
Mikolaj Komar <mikolaj.komar@intel.com>
2024-07-22 11:46:50 +0200

    Formatted some files form equal PR

--- include/oneapi/dpl/internal/distributed_ranges_impl/shp/algorithms/equal.hpp
+++ include/oneapi/dpl/internal/distributed_ranges_impl/shp/algorithms/equal.hpp
@@ -12,33 +12,35 @@
 #include <oneapi/dpl/internal/distributed_ranges_impl/shp/views/views.hpp>
 #include <oneapi/dpl/internal/distributed_ranges_impl/shp/zip_view.hpp>
 
-namespace oneapi::dpl::experimental::dr::shp {
+namespace oneapi::dpl::experimental::dr::shp
+{
 
-template <typename ExecutionPolicy, dr::distributed_range R1,
-          dr::distributed_range R2>
-  requires std::equality_comparable_with<stdrng::range_value_t<R1>,
-                                         stdrng::range_value_t<R2>>
-bool equal(ExecutionPolicy &&policy, R1 &&r1, R2 &&r2) {
+template <typename ExecutionPolicy, dr::distributed_range R1, dr::distributed_range R2>
+requires std::equality_comparable_with<stdrng::range_value_t<R1>, stdrng::range_value_t<R2>>
+bool
+equal(ExecutionPolicy&& policy, R1&& r1, R2&& r2)
+{
 
-  if (stdrng::distance(r1) != stdrng::distance(r2)) {
-    return false;
-  }
+    if (stdrng::distance(r1) != stdrng::distance(r2))
+    {
+        return false;
+    }
 
-  // we must use ints instead of bools, because distributed ranges do not
-  // support bools
-  auto compare = [](auto &&elems) {
-    return elems.first == elems.second ? 1 : 0;
-  };
+    // we must use ints instead of bools, because distributed ranges do not
+    // support bools
+    auto compare = [](auto&& elems) { return elems.first == elems.second ? 1 : 0; };
 
-  auto zipped_views = views::zip(r1, r2);
-  auto compared = shp::views::transform(zipped_views, compare);
-  auto min = [](double x, double y) { return std::min(x, y); };
-  auto result = shp::reduce(policy, compared, 1, min);
-  return result == 1;
+    auto zipped_views = views::zip(r1, r2);
+    auto compared = shp::views::transform(zipped_views, compare);
+    auto min = [](double x, double y) { return std::min(x, y); };
+    auto result = shp::reduce(policy, compared, 1, min);
+    return result == 1;
 }
 
 template <dr::distributed_range R1, dr::distributed_range R2>
-bool equal(R1 &&r1, R2 &&r2) {
-  return equal(dr::shp::par_unseq, r1, r2);
+bool
+equal(R1&& r1, R2&& r2)
+{
+    return equal(dr::shp::par_unseq, r1, r2);
 }
-} // namespace dr::shp
+} // namespace oneapi::dpl::experimental::dr::shp
