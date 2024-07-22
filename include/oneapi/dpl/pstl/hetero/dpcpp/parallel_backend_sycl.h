1c63be692a63e44565c84ba45386611a5921cca8
Sergey Kopienko <sergey.kopienko@intel.com>
2024-07-22 18:07:38 +0200

    @@@ #1.9 + additional fix: 16 - 32 - 64 - 128 - 256
    Signed-off-by: Sergey Kopienko <sergey.kopienko@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl.h
@@ -1167,7 +1167,7 @@ struct __parallel_find_or_tuner
         // Size: [     65'536, ...,    262'144 ) -> minimum number of iterations per work-item is   8
         // Size: [     16'384, ...,     65'536 ) -> minimum number of iterations per work-item is   4
         constexpr std::array<std::size_t, 5> __lower_bounds_of_sizes         = { 1'048'576, 4'194'304, 16'777'216, 67'108'864, 268'435'456 };
-        constexpr std::array<std::size_t, 5> __required_iters_per_work_items = {        16,        32,         32,        32,         32 };
+        constexpr std::array<std::size_t, 5> __required_iters_per_work_items = {        16,        32,         64,        128,         256 };
         
         const auto __it_bound = std::find_if(__lower_bounds_of_sizes.cbegin(), __lower_bounds_of_sizes.cend(),
                                              [__rng_n](std::size_t __i) { return __i <= __rng_n; });
