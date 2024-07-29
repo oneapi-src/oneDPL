598f56997cbfc7cb1117f29891aa42655f6028ad
Dan Hoeflinger <dan.hoeflinger@intel.com>
2024-07-29 12:36:54 -0400

    fixing device copyable for helpers
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/sycl_traits.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/sycl_traits.h
@@ -233,10 +233,39 @@ struct sycl::is_device_copyable<_ONEDPL_SPECIALIZE_FOR(oneapi::dpl::__internal::
 namespace oneapi::dpl::__par_backend_hetero
 {
 
+template <typename _UnaryOp>
+struct __gen_transform_input;
+
+template <typename _Predicate>
+struct __gen_count_pred;
+
+template <typename _Predicate>
+struct __gen_expand_count_pred;
+
 template <typename _ExecutionPolicy, typename _Pred>
 struct __early_exit_find_or;
 
 } // namespace oneapi::dpl::__par_backend_hetero
+
+template <typename _UnaryOp>
+struct sycl::is_device_copyable<_ONEDPL_SPECIALIZE_FOR(oneapi::dpl::__par_backend_hetero::__gen_transform_input,
+                                                       _UnaryOp)>
+    : oneapi::dpl::__internal::__are_all_device_copyable<_UnaryOp>
+{
+};
+
+template <typename _Predicate>
+struct sycl::is_device_copyable<_ONEDPL_SPECIALIZE_FOR(oneapi::dpl::__par_backend_hetero::__gen_count_pred, _Predicate)>
+    : oneapi::dpl::__internal::__are_all_device_copyable<_Predicate>
+{
+};
+
+template <typename _Predicate>
+struct sycl::is_device_copyable<_ONEDPL_SPECIALIZE_FOR(oneapi::dpl::__par_backend_hetero::__gen_expand_count_pred,
+                                                       _Predicate)>
+    : oneapi::dpl::__internal::__are_all_device_copyable<_Predicate>
+{
+};
 
 template <typename _ExecutionPolicy, typename _Pred>
 struct sycl::is_device_copyable<_ONEDPL_SPECIALIZE_FOR(oneapi::dpl::__par_backend_hetero::__early_exit_find_or,
