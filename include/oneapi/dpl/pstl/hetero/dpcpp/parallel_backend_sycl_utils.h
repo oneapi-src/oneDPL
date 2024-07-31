5942d7338d43e75d2e87c67e1968fd8d6590062e
Matthew Michel <matthew.michel@intel.com>
2024-07-31 15:13:26 -0500

    Remove unnecessary templated _SizeType
    Signed-off-by: Matthew Michel <matthew.michel@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_utils.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_utils.h
@@ -770,12 +770,12 @@ class __static_monotonic_dispatcher<::std::integer_sequence<::std::uint16_t, _X,
     }
 };
 
-template <typename _ExecutionPolicy, typename _SizeType>
+template <typename _ExecutionPolicy>
 bool
-__supports_sub_group_size(const _ExecutionPolicy& __exec, _SizeType __target_size)
+__supports_sub_group_size(const _ExecutionPolicy& __exec, std::size_t __target_size)
 {
     const auto __subgroup_sizes = __exec.queue().get_device().template get_info<sycl::info::device::sub_group_sizes>();
-    return std::find(__subgroup_sizes.begin(), __subgroup_sizes.end(), static_cast<std::size_t>(__target_size)) !=
+    return std::find(__subgroup_sizes.begin(), __subgroup_sizes.end(), __target_size) !=
            __subgroup_sizes.end();
 }
 
