57360f684cb5867f6ad5b53742b0bc25ee7792e5
Sergey Kopienko <sergey.kopienko@intel.com>
2024-08-02 14:33:21 +0200

    include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_merge_sort.h - rename __d to __device and change the type
    Signed-off-by: Sergey Kopienko <sergey.kopienko@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_merge_sort.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_merge_sort.h
@@ -327,12 +327,12 @@ struct __leaf_sorter_selector
     select(const sycl::queue& __q, _Range& __rng, _Compare __comp) const
     {
         const std::size_t __n = __rng.size();
-        auto&& __d = __q.get_device();
+        auto __device = __q.get_device();
 
-        std::size_t __max_wg_size = __d.template get_info<sycl::info::device::max_work_group_size>();
+        std::size_t __max_wg_size = __device.template get_info<sycl::info::device::max_work_group_size>();
         __max_wg_size = oneapi::dpl::__internal::__dpl_bit_floor(__max_wg_size);
 
-        const auto __sg_sizes = __d.template get_info<sycl::info::device::sub_group_sizes>();
+        const auto __sg_sizes = __device.template get_info<sycl::info::device::sub_group_sizes>();
         const auto __max_sg_size = __sg_sizes.empty() ? 1 : *std::max_element(__sg_sizes.begin(), __sg_sizes.end());
         // __oversubscription is similar to "theoretical occupancy" in GPU, or "multithreading" in CPU
         // TODO: reconsider the constant if the corresponding query appears in the SYCL specification
@@ -343,36 +343,30 @@ struct __leaf_sorter_selector
         //         see: "Active Thread Blocks per Multiprocessor" (6)
         // AMD: https://rocm.docs.amd.com/en/latest/how-to/tuning-guides/mi300x/workload.html#mi300x-occupancy-vgpr-table
         //         see: "Occupancy per EU" (8)
-        const std::uint8_t __oversubscription = __d.is_gpu() ? 8 : 1;
-        const auto __max_cu = __d.template get_info<sycl::info::device::max_compute_units>();
+        const std::uint8_t __oversubscription = __device.is_gpu() ? 8 : 1;
+        const auto __max_cu = __device.template get_info<sycl::info::device::max_compute_units>();
         const auto __saturation_point = __max_cu * __max_sg_size * __oversubscription;
         const auto __desired_data_per_workitem = __n / __saturation_point;
 
         // Pessimistically double the memory requirement to take into account memory used by compiled kernel.
         // TODO: investigate if the adjustment can be less conservative
         const std::size_t __max_slm_items =
-            __d.template get_info<sycl::info::device::local_mem_size>() / (sizeof(_Tp) * 2);
+            __device.template get_info<sycl::info::device::local_mem_size>() / (sizeof(_Tp) * 2);
         if (__max_slm_items >= _Leaf8::storage_size(__max_wg_size) && __desired_data_per_workitem >= 8)
-        {
             return _Leaf8(__rng, __comp, __max_wg_size);
-        }
-        else if (__max_slm_items >= _Leaf4::storage_size(__max_wg_size) && __desired_data_per_workitem >= 4)
-        {
+
+        if (__max_slm_items >= _Leaf4::storage_size(__max_wg_size) && __desired_data_per_workitem >= 4)
             return _Leaf4(__rng, __comp, __max_wg_size);
-        }
-        else if (__max_slm_items >= _Leaf2::storage_size(__max_wg_size) && __desired_data_per_workitem >= 2)
-        {
+
+        if (__max_slm_items >= _Leaf2::storage_size(__max_wg_size) && __desired_data_per_workitem >= 2)
             return _Leaf2(__rng, __comp, __max_wg_size);
-        }
-        else
-        {
-            std::size_t __slm_max_wg_size = __max_slm_items / _Leaf2::storage_size(1);
-            // __n is taken as is because of the bit floor and processing 2 items per work-item
-            // hence the processed size always fits a single work-group if __n is chosen
-            __max_wg_size = std::min<std::size_t>({__max_wg_size, __slm_max_wg_size, __n});
-            __max_wg_size = oneapi::dpl::__internal::__dpl_bit_floor(__max_wg_size);
-            return _Leaf2(__rng, __comp, __max_wg_size);
-        }
+
+        std::size_t __slm_max_wg_size = __max_slm_items / _Leaf2::storage_size(1);
+        // __n is taken as is because of the bit floor and processing 2 items per work-item
+        // hence the processed size always fits a single work-group if __n is chosen
+        __max_wg_size = std::min<std::size_t>({__max_wg_size, __slm_max_wg_size, __n});
+        __max_wg_size = oneapi::dpl::__internal::__dpl_bit_floor(__max_wg_size);
+        return _Leaf2(__rng, __comp, __max_wg_size);
     }
 };
 
