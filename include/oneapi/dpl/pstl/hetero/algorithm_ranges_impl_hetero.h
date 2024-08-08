3b99ea99401abeaa8e0ba8e72254d82f931c0666
Dan Hoeflinger <dan.hoeflinger@intel.com>
2024-08-08 12:19:18 -0400

    removing assign argument for ranges impl
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h
+++ include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h
@@ -435,8 +435,7 @@ __pattern_unique(__hetero_tag<_BackendTag> __tag, _ExecutionPolicy&& __exec, _Ra
     oneapi::dpl::__par_backend_hetero::__buffer<_ExecutionPolicy, _ValueType> __buf(__exec, __rng.size());
     auto res_rng = oneapi::dpl::__ranges::views::all(__buf.get_buffer());
     auto res = __ranges::__pattern_unique_copy(
-        __tag, oneapi::dpl::__par_backend_hetero::make_wrapped_policy<__unique_wrapper>(__exec), __rng, res_rng, __pred,
-        oneapi::dpl::__internal::__pstl_assign());
+        __tag, oneapi::dpl::__par_backend_hetero::make_wrapped_policy<__unique_wrapper>(__exec), __rng, res_rng, __pred);
 
     __ranges::__pattern_walk_n(__tag,
                                oneapi::dpl::__par_backend_hetero::make_wrapped_policy<__copy_wrapper>(
