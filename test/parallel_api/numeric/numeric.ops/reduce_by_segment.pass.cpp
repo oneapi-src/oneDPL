c9a1f03e0ef44f2bd961db912cafcae9fc51d4c4
Matthew Michel <106704043+mmichel11@users.noreply.github.com>
2024-08-08 08:13:18 -0500

    Workaround Windows correctness issue in reduce_by_segment.pass (#1771)
    * Workaround Windows compilation order bug in reduce_by_segment.pass
    
    Signed-off-by: Matthew Michel <matthew.michel@intel.com>
    
    * Provide upper-bound of compiler version to enable the workaround
    
    Signed-off-by: Matthew Michel <matthew.michel@intel.com>
    
    ---------
    
    Signed-off-by: Matthew Michel <matthew.michel@intel.com>

--- test/parallel_api/numeric/numeric.ops/reduce_by_segment.pass.cpp
+++ test/parallel_api/numeric/numeric.ops/reduce_by_segment.pass.cpp
@@ -346,13 +346,22 @@ run_test()
 int
 main()
 {
+    // On Windows, we observe incorrect results with this test with a specific compilation order of the
+    // kernels. This is being filed to the compiler team. In the meantime, we can rearrange this test
+    // to resolve the issue on our side.
+#if _PSTL_RED_BY_SEG_WINDOWS_COMPILE_ORDER_BROKEN
+    run_test<MatrixPoint<float>, UserBinaryPredicate<MatrixPoint<float>>, MaxFunctor<MatrixPoint<float>>>();
+#endif
+
 #if TEST_DPCPP_BACKEND_PRESENT
     // test with flag pred
     test_flag_pred<sycl::usm::alloc::device, class KernelName1, std::uint64_t>();
     test_flag_pred<sycl::usm::alloc::device, class KernelName2, MatrixPoint<float>>();
 #endif // TEST_DPCPP_BACKEND_PRESENT
 
+#if !_PSTL_RED_BY_SEG_WINDOWS_COMPILE_ORDER_BROKEN
     run_test<MatrixPoint<float>, UserBinaryPredicate<MatrixPoint<float>>, MaxFunctor<MatrixPoint<float>>>();
+#endif
 
     run_test<int, ::std::equal_to<int>, ::std::plus<int>>();
     run_test<float, ::std::equal_to<float>, ::std::plus<float>>();
--- test/support/test_config.h
+++ test/support/test_config.h
@@ -221,4 +221,10 @@
 #    define _PSTL_ICPX_FPGA_TEST_USM_VECTOR_ITERATOR_BROKEN 0
 #endif
 
+// A specific kernel compilation order causes incorrect results on Windows with the DPCPP backend. For now, we reorder
+// the test while the issue is being reported to the compiler team. Once it is resolved, this macro can be removed
+// or limited to older compiler versions.
+#define _PSTL_RED_BY_SEG_WINDOWS_COMPILE_ORDER_BROKEN                                                                  \
+    (_MSC_VER && TEST_DPCPP_BACKEND_PRESENT && __INTEL_LLVM_COMPILER <= 20250000)
+
 #endif // _TEST_CONFIG_H
