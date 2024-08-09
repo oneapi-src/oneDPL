1d80158eb26713e918756f16e6040db446bcaaa4
MikeDvorskiy <mikhail.dvorskiy@intel.com>
2024-08-09 15:43:08 +0200

    [oneDPL][ranges][test] + check return type: copy, copy_if

--- test/parallel_api/ranges/std_ranges_copy_if.pass.cpp
+++ test/parallel_api/ranges/std_ranges_copy_if.pass.cpp
@@ -22,10 +22,21 @@ main()
     using namespace test_std_ranges;
     namespace dpl_ranges = oneapi::dpl::ranges;
 
-    test_range_algo<0, int, data_in_out, /*RetTypeCheck*/false>{}(dpl_ranges::copy_if,  std::ranges::copy_if, pred);
-    test_range_algo<1, int, data_in_out, /*RetTypeCheck*/false>{}(dpl_ranges::copy_if,  std::ranges::copy_if, pred, proj);
-    test_range_algo<2, P2, data_in_out, /*RetTypeCheck*/false>{}(dpl_ranges::copy_if,  std::ranges::copy_if, pred, &P2::x);
-    test_range_algo<3, P2, data_in_out, /*RetTypeCheck*/false>{}(dpl_ranges::copy_if,  std::ranges::copy_if, pred, &P2::proj);
+    auto copy_if_checker = [](std::ranges::random_access_range auto&& __r_in,
+                           std::ranges::random_access_range auto&& __r_out, auto&&... args)
+    {
+        auto res = std::ranges::copy_if(std::forward<decltype(__r_in)>(__r_in), std::ranges::begin(__r_out),
+            std::forward<decltype(args)>(args)...);
+
+        using ret_type = std::ranges::copy_if_result<std::ranges::borrowed_iterator_t<decltype(__r_in)>,
+            std::ranges::borrowed_iterator_t<decltype(__r_out)>>;
+        return ret_type{res.in, res.out};
+    };
+
+    test_range_algo<0, int, data_in_out>{}(dpl_ranges::copy_if,  copy_if_checker, pred);
+    test_range_algo<1, int, data_in_out>{}(dpl_ranges::copy_if,  copy_if_checker, pred, proj);
+    test_range_algo<2, P2, data_in_out>{}(dpl_ranges::copy_if,  copy_if_checker, pred, &P2::x);
+    test_range_algo<3, P2, data_in_out>{}(dpl_ranges::copy_if,  copy_if_checker, pred, &P2::proj);
 #endif //_ENABLE_STD_RANGES_TESTING
 
     return TestUtils::done(_ENABLE_STD_RANGES_TESTING);
