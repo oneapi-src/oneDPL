8aac8502510379bdcea8432fc704f81897a3be2b
MikeDvorskiy <mikhail.dvorskiy@intel.com>
2024-08-13 18:19:21 +0200

    [oneDPL][ranges][test] + sort / stable_sort test cases w/o comparator

--- test/parallel_api/ranges/std_ranges_stable_sort.pass.cpp
+++ test/parallel_api/ranges/std_ranges_stable_sort.pass.cpp
@@ -22,16 +22,17 @@ main()
     using namespace test_std_ranges;
     namespace dpl_ranges = oneapi::dpl::ranges;
 
-    test_range_algo<0>{}(dpl_ranges::stable_sort, std::ranges::stable_sort, std::ranges::less{});
+    test_range_algo<0>{}(dpl_ranges::stable_sort, std::ranges::stable_sort);
+    test_range_algo<1>{}(dpl_ranges::stable_sort, std::ranges::stable_sort, std::ranges::less{});
 
-    test_range_algo<1>{}(dpl_ranges::stable_sort, std::ranges::stable_sort, std::ranges::less{}, proj);
-    test_range_algo<2>{}(dpl_ranges::stable_sort, std::ranges::stable_sort, std::ranges::greater{}, proj);
+    test_range_algo<2>{}(dpl_ranges::stable_sort, std::ranges::stable_sort, std::ranges::less{}, proj);
+    test_range_algo<3>{}(dpl_ranges::stable_sort, std::ranges::stable_sort, std::ranges::greater{}, proj);
 
-    test_range_algo<3, P2>{}(dpl_ranges::stable_sort, std::ranges::stable_sort, std::ranges::less{}, &P2::x);
-    test_range_algo<4, P2>{}(dpl_ranges::stable_sort, std::ranges::stable_sort, std::ranges::greater{}, &P2::x);
+    test_range_algo<4, P2>{}(dpl_ranges::stable_sort, std::ranges::stable_sort, std::ranges::less{}, &P2::x);
+    test_range_algo<5, P2>{}(dpl_ranges::stable_sort, std::ranges::stable_sort, std::ranges::greater{}, &P2::x);
 
-    test_range_algo<5, P2>{}(dpl_ranges::stable_sort, std::ranges::stable_sort, std::ranges::less{}, &P2::proj);
-    test_range_algo<6, P2>{}(dpl_ranges::stable_sort, std::ranges::stable_sort, std::ranges::greater{}, &P2::proj);
+    test_range_algo<6, P2>{}(dpl_ranges::stable_sort, std::ranges::stable_sort, std::ranges::less{}, &P2::proj);
+    test_range_algo<7, P2>{}(dpl_ranges::stable_sort, std::ranges::stable_sort, std::ranges::greater{}, &P2::proj);
 #endif //_ENABLE_STD_RANGES_TESTING
 
     return TestUtils::done(_ENABLE_STD_RANGES_TESTING);
