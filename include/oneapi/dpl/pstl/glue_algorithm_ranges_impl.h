c76785b6cb1225051ad2ba4c228c0fd0b8538a13
MikeDvorskiy <mikhail.dvorskiy@intel.com>
2024-08-23 10:38:03 +0200

    [oneDPL][ranges] + unsized range support for ranges::transform

--- include/oneapi/dpl/pstl/glue_algorithm_ranges_impl.h
+++ include/oneapi/dpl/pstl/glue_algorithm_ranges_impl.h
@@ -83,7 +83,8 @@ struct __transform_fn
     {
         const auto __dispatch_tag = oneapi::dpl::__ranges::__select_backend(__exec);
 
-        const auto _size = std::ranges::min(std::ranges::size(__r), std::ranges::size(__out_r));
+        using Size = std::common_type_t<std::ranges::range_size_t<_R>, std::ranges::range_size_t<_OutRange>>;
+        const Size _size = std::ranges::min((Size)std::ranges::size(__r), (Size)std::ranges::size(__out_r));
 
         oneapi::dpl::__internal::__ranges::__pattern_transform(__dispatch_tag, std::forward<_ExecutionPolicy>(__exec),
             std::ranges::take_view(__r, _size), std::ranges::take_view(__out_r, _size), __op, __proj);
@@ -94,8 +95,8 @@ struct __transform_fn
     template<typename _ExecutionPolicy, std::ranges::random_access_range _R1, std::ranges::random_access_range _R2,
              std::ranges::random_access_range _OutRange, std::copy_constructible _F, typename _Proj1 = std::identity,
              typename _Proj2 = std::identity>
-    requires oneapi::dpl::is_execution_policy_v<std::remove_cvref_t<_ExecutionPolicy>> && std::ranges::sized_range<_R1>
-        && std::ranges::sized_range<_R2> && std::ranges::sized_range<_OutRange>
+    requires oneapi::dpl::is_execution_policy_v<std::remove_cvref_t<_ExecutionPolicy>> && (std::ranges::sized_range<_R1>
+        || std::ranges::sized_range<_R2>) && std::ranges::sized_range<_OutRange>
         && std::indirectly_writable<std::ranges::iterator_t<_OutRange>,
             std::indirect_result_t<_F&, std::projected<std::ranges::iterator_t<_R1>, _Proj1>,
             std::projected<std::ranges::iterator_t<_R2>, _Proj2>>>
@@ -107,9 +108,18 @@ struct __transform_fn
     {
         const auto __dispatch_tag = oneapi::dpl::__ranges::__select_backend(__exec);
 
-        const auto _size = std::ranges::min({std::ranges::size(__r1), std::ranges::size(__r2), std::ranges::size(__out_r)});
+        using Size = std::common_type_t<std::ranges::range_size_t<_R1>, std::ranges::range_size_t<_R2>,
+            std::ranges::range_size_t<_OutRange>>;
+        Size _size = std::ranges::size(__out_r);
+        if constexpr(std::ranges::sized_range<_R1>)
+            _size = std::ranges::min(_size, (Size)std::ranges::size(__r1));
+        if constexpr(std::ranges::sized_range<_R2>)
+            _size = std::ranges::min(_size, (Size)std::ranges::size(__r2));
+
+        //take_view doesn't make unsized range sized, so subrange is used below
         oneapi::dpl::__internal::__ranges::__pattern_transform(__dispatch_tag, std::forward<_ExecutionPolicy>(__exec),
-            std::ranges::take_view(__r1, _size), std::ranges::take_view(__r2, _size),
+            std::ranges::subrange(std::ranges::begin(__r1), std::ranges::begin(__r1) + _size),
+            std::ranges::subrange(std::ranges::begin(__r2), std::ranges::begin(__r2) + _size),
             std::ranges::take_view(__out_r, _size), __binary_op, __proj1, __proj2);
 
         return {std::ranges::begin(__r1) + _size, std::ranges::begin(__r2) + _size, std::ranges::begin(__out_r) + _size};
