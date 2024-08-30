d396a9bb25f613f98e2909199f950f138ef0a57d
MikeDvorskiy <mikhail.dvorskiy@intel.com>
2024-08-30 19:41:39 +0200

    [oneDPL][ranges][test] + #include <oneapi/dpl/algorithm>

--- test/parallel_api/ranges/std_ranges_test.h
+++ test/parallel_api/ranges/std_ranges_test.h
@@ -14,6 +14,7 @@
 //===----------------------------------------------------------------------===//
 
 #include <oneapi/dpl/execution>
+#include <oneapi/dpl/algorithm>
 
 #include "support/test_config.h"
 
