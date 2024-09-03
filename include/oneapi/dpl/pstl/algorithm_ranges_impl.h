6c70d82633a4f1af7ce35fc743382d084f58b48b
Dmitriy Sobolev <dmitriy.sobolev@intel.com>
2024-09-03 13:56:07 -0500

    Add a comment for using unstable sort for ranges::sort
    Signed-off-by: Dmitriy Sobolev <dmitriy.sobolev@intel.com>

--- include/oneapi/dpl/pstl/algorithm_ranges_impl.h
+++ include/oneapi/dpl/pstl/algorithm_ranges_impl.h
@@ -342,7 +342,8 @@ __pattern_sort_ranges(_Tag __tag, _ExecutionPolicy&& __exec, _R&& __r, _Comp __c
 
     auto __comp_2 = [__comp, __proj](auto&& __val1, auto&& __val2) { return std::invoke(__comp, std::invoke(__proj,
         std::forward<decltype(__val1)>(__val1)), std::invoke(__proj, std::forward<decltype(__val2)>(__val2)));};
-
+    // Call stable_sort pattern since __pattern_sort_ranges is shared between sort and stable_sort
+    // TODO: add a separate pattern for ranges::sort for better performance
     oneapi::dpl::__internal::__pattern_stable_sort(__tag, std::forward<_ExecutionPolicy>(__exec),
         std::ranges::begin(__r), std::ranges::begin(__r) + std::ranges::size(__r), __comp_2);
 
