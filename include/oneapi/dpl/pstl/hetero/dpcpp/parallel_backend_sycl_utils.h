35dd1fbb6fda65b84c0d10d650898e7e1b9903c1
Sergey Kopienko <sergey.kopienko@intel.com>
2024-09-06 17:48:42 +0200

    include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_utils.h - remove _USE_KERNEL_DEVICE_SPECIFIC_API due _ONEDPL_LIBSYCL_VERSION >= 50700 is always true
    Signed-off-by: Sergey Kopienko <sergey.kopienko@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_utils.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_utils.h
@@ -108,19 +108,12 @@ __supports_sub_group_size(const _ExecutionPolicy& __exec, std::size_t __target_s
 // Kernel run-time information helpers
 //-----------------------------------------------------------------------------
 
-// 20201214 value corresponds to Intel(R) oneAPI C++ Compiler Classic 2021.1.2 Patch release
-#define _USE_KERNEL_DEVICE_SPECIFIC_API (__SYCL_COMPILER_VERSION > 20201214) || (_ONEDPL_LIBSYCL_VERSION >= 50700)
-
 template <typename _ExecutionPolicy>
 ::std::size_t
 __kernel_work_group_size(const _ExecutionPolicy& __policy, const sycl::kernel& __kernel)
 {
     const sycl::device& __device = __policy.queue().get_device();
-#if _USE_KERNEL_DEVICE_SPECIFIC_API
     return __kernel.template get_info<sycl::info::kernel_device_specific::work_group_size>(__device);
-#else
-    return __kernel.template get_work_group_info<sycl::info::kernel_work_group::work_group_size>(__device);
-#endif
 }
 
 template <typename _ExecutionPolicy>
@@ -130,7 +123,6 @@ __kernel_sub_group_size(const _ExecutionPolicy& __policy, const sycl::kernel& __
     const sycl::device& __device = __policy.queue().get_device();
     [[maybe_unused]] const ::std::size_t __wg_size = __kernel_work_group_size(__policy, __kernel);
     const ::std::uint32_t __sg_size =
-#if _USE_KERNEL_DEVICE_SPECIFIC_API
         __kernel.template get_info<sycl::info::kernel_device_specific::max_sub_group_size>(
             __device
 #    if _ONEDPL_LIBSYCL_VERSION < 60000
@@ -138,9 +130,6 @@ __kernel_sub_group_size(const _ExecutionPolicy& __policy, const sycl::kernel& __
             sycl::range<3> { __wg_size, 1, 1 }
 #    endif
         );
-#else
-        __kernel.template get_sub_group_info<sycl::info::kernel_sub_group::max_sub_group_size>(
-            __device, sycl::range<3>{__wg_size, 1, 1});
 #endif
     return __sg_size;
 }
