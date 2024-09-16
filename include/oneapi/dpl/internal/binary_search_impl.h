44e5deccd05d9833d759fdcacfaac7596feeca55
Sergey Kopienko <sergey.kopienko@intel.com>
2024-09-16 14:44:59 +0200

    include/oneapi/dpl/internal/binary_search_impl.h - fix binary_search_impl + _Tag impl
    Signed-off-by: Sergey Kopienko <sergey.kopienko@intel.com>

--- include/oneapi/dpl/internal/binary_search_impl.h
+++ include/oneapi/dpl/internal/binary_search_impl.h
@@ -115,15 +115,16 @@ upper_bound_impl(_Tag tag, Policy&& policy, InputIterator1 start, InputIterator1
 template <class _Tag, typename Policy, typename InputIterator1, typename InputIterator2, typename OutputIterator,
           typename StrictWeakOrdering>
 OutputIterator
-binary_search_impl(_Tag, Policy&& policy, InputIterator1 start, InputIterator1 end, InputIterator2 value_start,
+binary_search_impl(_Tag tag, Policy&& policy, InputIterator1 start, InputIterator1 end, InputIterator2 value_start,
                    InputIterator2 value_end, OutputIterator result, StrictWeakOrdering comp)
 {
     static_assert(__internal::__is_host_dispatch_tag_v<_Tag>);
 
-    return oneapi::dpl::transform(policy, value_start, value_end, result,
-                                  [=](typename ::std::iterator_traits<InputIterator2>::reference val) {
-                                      return ::std::binary_search(start, end, val, comp);
-                                  });
+    return oneapi::dpl::__internal::__pattern_walk2(
+        tag, std::forward<Policy>(policy), value_start, value_end, result,
+        oneapi::dpl::__internal::__transform_functor{[=](typename std::iterator_traits<InputIterator2>::reference val) {
+            return std::binary_search(start, end, val, comp);
+        }});
 }
 
 #if _ONEDPL_BACKEND_SYCL
