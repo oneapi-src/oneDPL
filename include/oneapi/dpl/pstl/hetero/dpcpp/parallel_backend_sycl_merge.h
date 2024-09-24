5fc2026170ca95356a7179c3af734ce704b85e2a
Sergey Kopienko <sergey.kopienko@intel.com>
2024-09-24 10:03:40 +0200

    include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_merge.h - parse result value of std::pair<_Index, _Index> type to two values after __find_start_point calls
    Signed-off-by: Sergey Kopienko <sergey.kopienko@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_merge.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_merge.h
@@ -169,9 +169,10 @@ struct __parallel_merge_submitter<_IdType, __internal::__optional_kernel_name<_N
             oneapi::dpl::__ranges::__require_access(__cgh, __rng1, __rng2, __rng3);
             __cgh.parallel_for<_Name...>(sycl::range</*dim=*/1>(__steps), [=](sycl::item</*dim=*/1> __item_id) {
                 const _IdType __i_elem = __item_id.get_linear_id() * __chunk;
-                const auto __start = __find_start_point(__rng1, __rng2, __i_elem, __n1, __n2, __comp);
-                __serial_merge(__rng1, __rng2, __rng3, __start.first, __start.second, __i_elem, __chunk, __n1, __n2,
-                               __comp);
+                const auto[__rng1_start_idx, __rng2_start_idx] = __find_start_point(__rng1, __rng2, __i_elem, __n1, __n2, __comp);
+                __serial_merge(__rng1, __rng2, __rng3,
+                               __rng1_start_idx, __rng2_start_idx,
+                               __i_elem, __chunk, __n1, __n2, __comp);
             });
         });
         return __future(__event);
