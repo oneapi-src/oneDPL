0bb1e9fc7557e59f8117b8f0c42848d2934c7240
MikeDvorskiy <mikhail.dvorskiy@intel.com>
2024-09-25 16:09:27 +0200

    [oneDPL][ranges][zip_view][test] -> oneapi::dpl::ranges

--- test/parallel_api/ranges/std_ranges_zip_view.pass.cpp
+++ test/parallel_api/ranges/std_ranges_zip_view.pass.cpp
@@ -28,14 +28,14 @@ main()
     constexpr int max_n = 10;
     int data[max_n] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    auto zip_view = my::zip(data, std::views::iota(0, max_n)) | std::views::take(5);
+    auto zip_view = dpl_ranges::zip(data, std::views::iota(0, max_n)) | std::views::take(5);
     std::ranges::for_each(zip_view, test_std_ranges::f_mutuable, [](const auto& val) { return std::get<1>(val); });
 
     test_std_ranges::call_with_host_policies(dpl_ranges::for_each, zip_view, test_std_ranges::f_mutuable, [](const auto& val) { return std::get<1>(val); });
 
     dpl_ranges::for_each(test_std_ranges::dpcpp_policy(), zip_view, test_std_ranges::f_mutuable, [](const auto& val) { return std::get<1>(val); });
 
-    auto zip_view_sort = my::zip(data, data);
+    auto zip_view_sort = dpl_ranges::zip(data, data);
 
     std::sort(zip_view_sort.begin(), zip_view_sort.begin() + max_n, [](const auto& val1, const auto& val2) { return std::get<0>(val1) > std::get<0>(val2); });
     for(int i = 0; i < max_n; ++i)
