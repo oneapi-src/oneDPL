b99e73522bdafa43fcc135a79de1505b733e41e5
MikeDvorskiy <mikhail.dvorskiy@intel.com>
2024-09-26 14:18:20 +0200

    Revert "[oneDPL][ranges][zip_view] + begin_imp, end_impl to reduce code duplication"
    This reverts commit fc04cafc39bc694473a2b8a34ea578dc6fcf4562.

--- include/oneapi/dpl/pstl/zip_view_impl.h
+++ include/oneapi/dpl/pstl/zip_view_impl.h
@@ -293,22 +293,25 @@ public:
                                                     tuple_type<std::ranges::sentinel_t<const Views>...>>;
 
         end_type end_;
-    }; // class sentinel
+    }; // class sentinel    
 
-private:
-    template<bool Const>
-    constexpr auto begin_impl()
+    constexpr auto begin() requires (std::ranges::range<Views> && ...) // !simple_view?
     {
-        auto __tr = [](auto... __args) { return iterator<Const>(__args...);};
+        auto __tr = [](auto... __args) { return iterator<false>(__args...);};
         return apply_to_tuple(__tr, std::ranges::begin, views_);
     }
 
-    template<bool Const>
-    constexpr auto end_impl()
+    constexpr auto begin() const requires ( std::ranges::range<const Views> && ... )
+    {
+        auto __tr = [](auto... __args) { return iterator<true>(__args...);};
+        return apply_to_tuple(__tr, std::ranges::begin, views_);
+    }
+
+    constexpr auto end() requires (std::ranges::range<Views> && ...) // requires !simple_view {
     {
         if constexpr (!zip_is_common<Views...>)
         {
-            auto __tr = [](auto... __args) { return sentinel<Const>(__args...);};
+            auto __tr = [](auto... __args) { return sentinel<false>(__args...);};
             return apply_to_tuple(__tr, std::ranges::end, views_);
         }
         else if constexpr ((std::ranges::random_access_range<Views> && ...))
@@ -319,30 +322,29 @@ private:
         }
         else
         {
-            auto __tr = [](auto... __args) { return iterator<Const>(__args...);};
+            auto __tr = [](auto... __args) { return iterator<false>(__args...);};
             return apply_to_tuple(__tr, std::ranges::end, views_);
         }
     }
 
-public:
-    constexpr auto begin() requires (std::ranges::range<Views> && ...) // !simple_view?
-    {
-        return begin_impl<false>();
-    }
-
-    constexpr auto begin() const requires ( std::ranges::range<const Views> && ... )
-    {
-        return const_cast<zip_view*>(this)->begin_impl<true>();
-    }
-
-    constexpr auto end() requires (std::ranges::range<Views> && ...) // requires !simple_view {
-    {
-        return end_impl<false>();
-    }
-
     constexpr auto end() const requires (std::ranges::range<const Views> && ...)
     {
-        return const_cast<zip_view*>(this)->end_impl<true>();
+        if constexpr (!zip_is_common<Views...>)
+        {
+            auto __tr = [](auto... __args) { return sentinel<true>(__args...);};
+            return apply_to_tuple(__tr, std::ranges::end, views_);
+        } 
+        else if constexpr ((std::ranges::random_access_range<Views> && ...))
+        {
+            auto it = begin();
+            it += size();
+            return it;
+        }
+        else
+        {
+            auto __tr = [](auto... __args) { return iterator<true>(__args...);};
+            return apply_to_tuple(__tr, std::ranges::end, views_);
+        }
     }
 
     constexpr auto size() requires (std::ranges::sized_range<Views> && ...)
