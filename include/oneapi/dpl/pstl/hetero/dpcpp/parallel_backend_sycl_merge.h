0d20e5f8f29ad6bd34f09b3074e851ffa9c11fb6
Sergey Kopienko <sergey.kopienko@intel.com>
2024-10-14 17:56:21 +0200

    @@@ calculate params on GPU
    Signed-off-by: Sergey Kopienko <sergey.kopienko@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_merge.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_merge.h
@@ -173,6 +173,8 @@ struct __parallel_merge_submitter<_IdType, __internal::__optional_kernel_name<_N
 template <typename _IdType, typename _Name>
 struct __parallel_merge_submitter_large;
 
+#define USING_PARALLEL_MERGE_SUBMITTER_LARGE_ALWAYS 1
+
 template <typename _IdType, typename... _Name>
 struct __parallel_merge_submitter_large<_IdType, __internal::__optional_kernel_name<_Name...>>
 {
@@ -180,56 +182,105 @@ struct __parallel_merge_submitter_large<_IdType, __internal::__optional_kernel_n
     auto
     operator()(_ExecutionPolicy&& __exec, _Range1&& __rng1, _Range2&& __rng2, _Range3&& __rng3, _Compare __comp) const
     {
-        const _IdType __n1 = __rng1.size();
-        const _IdType __n2 = __rng2.size();
-        const _IdType __n = __n1 + __n2;
+        const std::size_t __n1 = __rng1.size();         // Horizontal data
+        const std::size_t __n2 = __rng2.size();         // Vertical data
+        const std::size_t __n = __n1 + __n2;
+
+        using _ValueType1 = typename std::iterator_traits<decltype(__rng1.begin())>::value_type;
+        using _ValueType2 = typename std::iterator_traits<decltype(__rng2.begin())>::value_type;
 
         assert(__n > 0);
 
         _PRINT_INFO_IN_DEBUG_MODE(__exec);
 
+        ////////////////////////////////
+        // Detect SLM size
+
+        // Pessimistically only use half of the memory to take into account memory used by compiled kernel                                          // 10'000 + 5'000;  100'000 + 50'000; 16'000'000 + 8'000'000
+        const std::size_t __max_slm_size = __exec.queue().get_device().template get_info<sycl::info::device::local_mem_size>() / 2;                 // ==65535          ==65'535          ==65'535
+
+        // The size of the data type in bytes
+        const std::size_t __wg_src_pairs = oneapi::dpl::__internal::__dpl_ceiling_div(__max_slm_size, sizeof(_ValueType1) + sizeof(_ValueType2));   // ==8'192          == 8'192          == 8'192
+
+        ////////////////////////////////
+        // Eval sub-window size processing by work-group
+        const std::size_t __window_items_count_h = std::min(__wg_src_pairs, (std::size_t)__n1); // Horizontal size                                  //                  == 8'192          == 8'192
+        const std::size_t __window_items_count_v = std::min(__wg_src_pairs, (std::size_t)__n2); // Vertical size                                    //                  == 8'192          == 8'192
+
+        ////////////////////////////////
+        // Eval diagonal's distance: each work-group processing one sub-window
+
+        // Returns the maximum number of work-items that this device is capable of executing in a work-group.
+        // The minimum value specified in the __wg_size_limit
+        const std::size_t __wgroup_size = oneapi::dpl::__internal::__max_work_group_size(__exec, (std::size_t)4096);                                // ==1'024          ==1'024           == 1'024
+                   
         // Empirical number of values to process per work-item
-        const std::uint8_t __diagonals_interval = __exec.queue().get_device().is_cpu() ? 128 : 4;
+        const std::size_t __diagonals_interval_min = __exec.queue().get_device().is_cpu() ? 128 : 4;
+        const std::size_t __diagonals_interval = std::max(__diagonals_interval_min,
+                                                  oneapi::dpl::__internal::__dpl_ceiling_div(__window_items_count_h, __wgroup_size));               // ==8              ==8
 
-        // Returns the number of parallel compute units available to the device. The minimum value is 1.
-        const std::uint32_t __max_cu = oneapi::dpl::__internal::__max_compute_units(__exec);
+        ////////////////////////////////
+        // Calculate the sub-window amount
 
-        // Eval the amount of diagonals in all data to process
-        const _IdType __diagonals_count_per_all_data = oneapi::dpl::__internal::__dpl_ceiling_div(__n, __diagonals_interval);
+        const std::size_t __sub_window_count_h = oneapi::dpl::__internal::__dpl_ceiling_div(__n1, __window_items_count_h);                          // ==2              ==13              ==26 ?
+        const std::size_t __sub_window_count_v = oneapi::dpl::__internal::__dpl_ceiling_div(__n2, __window_items_count_v);                          // ==1              ==7               ==9  ?
 
-        // Eval the amount of diagonals in each work item
-        const _IdType __diagonals_count_per_work_item = oneapi::dpl::__internal::__dpl_ceiling_div(__diagonals_count_per_all_data, __max_cu);
+        ////////////////////////////////
+        // Calculate full diagonal count for all data size
+        const std::size_t __diagonals_count = oneapi::dpl::__internal::__dpl_ceiling_div(__n, __diagonals_interval);
 
-        // Eval the required amount of work items
-        const auto __work_items_count = oneapi::dpl::__internal::__dpl_ceiling_div(__diagonals_count_per_all_data, __diagonals_count_per_work_item);
+        // Create storage for split points (in the best case - in device memory to avoid copy data to host)
+        using __split_points_scratch_storage_t = __result_and_scratch_storage<_ExecutionPolicy, _IdType>;
+        __split_points_scratch_storage_t __split_points_scratch_storage(__exec, /* 1 item - result, but it's doesn-t used*/ 0, /* split points in diagonals*/ __diagonals_count);
 
-        auto __event = __exec.queue().submit([&](sycl::handler& __cgh) {
-            oneapi::dpl::__ranges::__require_access(__cgh, __rng1, __rng2, __rng3);
-            __cgh.parallel_for<_Name...>(
-                sycl::range</*dim=*/1>(__work_items_count), [=](sycl::item</*dim=*/1> __item_id) {
+        ////////////////////////////////
+        // Two kernels:
+        // 1. Find split points on the diagonals
+        // 2. Merge by split points
 
-                    const auto __item_idx = __item_id.get_linear_id();
+        // Run Kernel 1: find split points at the diagonals
+        auto __event_find_split_points = __exec.queue().submit([&](sycl::handler& __cgh) {
 
-                    // Eval the global diagonal index for the first diagonal in the current work item
-                    const auto __global_diagonal_idx = __item_idx * __diagonals_count_per_work_item;
 
-                    // Iterate all diagonals in the current work item
-                    for (_IdType __diagonal_idx = 0; __diagonal_idx < __diagonals_count_per_work_item; ++__diagonal_idx)
-                    {
-                        // Eval the offset for the current processing diagonal
-                        const _IdType __diagonal_offset = (__global_diagonal_idx + __diagonal_idx) * __diagonals_interval;
-                        if (__diagonal_offset < __n)
-                        {
-                            const auto __start = __find_start_point(__rng1, __rng2, __diagonal_offset, __n1, __n2, __comp);
+            });
 
-                            __serial_merge(__rng1, __rng2, __rng3, __start.first, __start.second, __diagonal_offset,
-                                           __diagonals_interval, __n1, __n2, __comp);
-                        }
-                    }
-                });
-        });
+        // Run Kernel 2: merge by split points
+        auto __event_merge = __exec.queue().submit([&](sycl::handler& __cgh) {
 
-        return __future(__event);
+                // We should wait for the first kernel to finish
+                __cgh.depends_on(__event_find_split_points);
+
+                [[maybe_unused]] auto __split_points_acc = __split_points_scratch_storage.__get_scratch_acc(__cgh);
+
+            });
+
+        //auto __event = __exec.queue().submit([&](sycl::handler& __cgh) {
+        //    oneapi::dpl::__ranges::__require_access(__cgh, __rng1, __rng2, __rng3);
+        //    __cgh.parallel_for<_Name...>(
+        //        sycl::range</*dim=*/1>(__work_items_count), [=](sycl::item</*dim=*/1> __item_id) {
+
+        //            const auto __item_idx = __item_id.get_linear_id();
+
+        //            // Eval the global diagonal index for the first diagonal in the current work item
+        //            const auto __global_diagonal_idx = __item_idx * __diagonals_count_per_work_item;
+
+        //            // Iterate all diagonals in the current work item
+        //            for (_IdType __diagonal_idx = 0; __diagonal_idx < __diagonals_count_per_work_item; ++__diagonal_idx)
+        //            {
+        //                // Eval the offset for the current processing diagonal
+        //                const _IdType __diagonal_offset = (__global_diagonal_idx + __diagonal_idx) * __diagonals_interval;
+        //                if (__diagonal_offset < __n)
+        //                {
+        //                    const auto __start = __find_start_point(__rng1, __rng2, __diagonal_offset, __n1, __n2, __comp);
+
+        //                    __serial_merge(__rng1, __rng2, __rng3, __start.first, __start.second, __diagonal_offset,
+        //                                   __diagonals_interval, __n1, __n2, __comp);
+        //                }
+        //            }
+        //        });
+        //});
+
+        return __future(__event_merge);
     }
 };
 
@@ -249,6 +300,7 @@ __parallel_merge(oneapi::dpl::__internal::__device_backend_tag, _ExecutionPolicy
     const auto __n = __rng1.size() + __rng2.size();
 
     // TODO required to find the better value instead of 16 * 1'048'576 on GPU
+#if !USING_PARALLEL_MERGE_SUBMITTER_LARGE_ALWAYS
     if (__n < 16 * 1'048'576)
     {
         if (__n <= std::numeric_limits<std::uint32_t>::max())
@@ -271,6 +323,7 @@ __parallel_merge(oneapi::dpl::__internal::__device_backend_tag, _ExecutionPolicy
         }
     }
     else
+#endif // USING_PARALLEL_MERGE_SUBMITTER_LARGE_ALWAYS
     {
         if (__n <= std::numeric_limits<std::uint32_t>::max())
         {
