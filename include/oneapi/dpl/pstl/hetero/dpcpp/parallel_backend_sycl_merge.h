6147cbb326dc3111720b938ae78adcdcc63d3333
Sergey Kopienko <sergey.kopienko@intel.com>
2024-10-15 16:02:38 +0200

    @@@ impl

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_merge.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_merge.h
@@ -175,6 +175,12 @@ struct __parallel_merge_submitter_large;
 
 #define USING_PARALLEL_MERGE_SUBMITTER_LARGE_ALWAYS 1
 
+template <typename... _Name>
+class __merge_kernel_name_large;
+
+template <typename... _Name>
+class _find_split_points_kernel;
+
 template <typename _IdType, typename... _Name>
 struct __parallel_merge_submitter_large<_IdType, __internal::__optional_kernel_name<_Name...>>
 {
@@ -189,9 +195,19 @@ struct __parallel_merge_submitter_large<_IdType, __internal::__optional_kernel_n
         using _ValueType1 = typename std::iterator_traits<decltype(__rng1.begin())>::value_type;
         using _ValueType2 = typename std::iterator_traits<decltype(__rng2.begin())>::value_type;
 
-        assert(__n > 0);
+        assert(__n1 > 0);
+        assert(__n2 > 0);
+        assert(__n > 0);        // TODO should we remove this assert?
+
+        // Build Kernel name for split points Kernel
+        using _CustomName = oneapi::dpl::__internal::__policy_kernel_name<_ExecutionPolicy>;
+        using _FindSplitPointsKernel = oneapi::dpl::__par_backend_hetero::__internal::__kernel_name_generator<
+            _find_split_points_kernel, _CustomName, _Range1, _Range2, _Range3, _IdType, _Compare>;
 
         _PRINT_INFO_IN_DEBUG_MODE(__exec);
+
+        // TODO required special implementation for two corner cases when *__rng1.rbegin() <= *__rng2.begin()
+        // or *__rng2.rbegin() <= *__rng1.begin() : in these cases we should simple copy source data into __rng3
 
         ////////////////////////////////
         // Detect SLM size
@@ -204,8 +220,8 @@ struct __parallel_merge_submitter_large<_IdType, __internal::__optional_kernel_n
 
         ////////////////////////////////
         // Eval sub-window size processing by work-group
-        const std::size_t __window_items_count_h = std::min(__wg_src_pairs, (std::size_t)__n1); // Horizontal size                                  //                  == 8'192          == 8'192
-        const std::size_t __window_items_count_v = std::min(__wg_src_pairs, (std::size_t)__n2); // Vertical size                                    //                  == 8'192          == 8'192
+        const std::size_t __window_items_count_h = std::min(__wg_src_pairs, __n1); // Horizontal size                                               //                  == 8'192          == 8'192
+        const std::size_t __window_items_count_v = std::min(__wg_src_pairs, __n2); // Vertical size                                                 //                  == 8'192          == 8'192
 
         ////////////////////////////////
         // Eval diagonal's distance: each work-group processing one sub-window
@@ -230,8 +246,11 @@ struct __parallel_merge_submitter_large<_IdType, __internal::__optional_kernel_n
         const std::size_t __diagonals_count = oneapi::dpl::__internal::__dpl_ceiling_div(__n, __diagonals_interval);
 
         // Create storage for split points (in the best case - in device memory to avoid copy data to host)
-        using __split_points_scratch_storage_t = __result_and_scratch_storage<_ExecutionPolicy, _IdType>;
-        __split_points_scratch_storage_t __split_points_scratch_storage(__exec, /* 1 item - result, but it's doesn-t used*/ 0, /* split points in diagonals*/ __diagonals_count);
+        using __split_point_t = std::pair<_IdType, _IdType>;
+        using __split_points_scratch_storage_t = __result_and_scratch_storage<_ExecutionPolicy, __split_point_t>;
+        __split_points_scratch_storage_t __split_points(__exec,
+                                                        0,                      // The amount of result items
+                                                        __diagonals_count);     // The amount of split points
 
         ////////////////////////////////
         // Two kernels:
@@ -241,44 +260,130 @@ struct __parallel_merge_submitter_large<_IdType, __internal::__optional_kernel_n
         // Run Kernel 1: find split points at the diagonals
         auto __event_find_split_points = __exec.queue().submit([&](sycl::handler& __cgh) {
 
+            // Cash the portion of source processing data for the current work-group in SLM
+            __dpl_sycl::__local_accessor<_ValueType1> __loc_acc_rng1_h(__window_items_count_h + 1, __cgh);      // Cashed data from __rng1
+            __dpl_sycl::__local_accessor<_ValueType2> __loc_acc_rng2_v(__window_items_count_v + 1, __cgh);      // Cashed data from __rng2
 
-            });
+            // Get access to the split points
+            auto __split_points_acc = __split_points.__get_scratch_acc(__cgh);
+            auto __split_points_ptr = __split_points_scratch_storage_t::__get_usm_or_buffer_accessor_ptr(__split_points_acc);
+
+            constexpr int _Dims2 = 2;           // Number of dimensions in nd-range: 2
+            constexpr int _Dim_H = 0;           // Horizontal dimension index in nd-range
+            constexpr int _Dim_V = 1;           // Vertical dimension index in nd-range
+            const sycl::nd_range<_Dims2> __merge_matrix_nd_range{
+                sycl::range<_Dims2>{__n1, __n2},                                    // Global size: full size of source data __rng1 by horizontal and __rng2 by vertical
+                sycl::range<_Dims2>{__sub_window_count_h, __sub_window_count_v}};   // Local (group) size: sub-window size by horizontal and vertical
+
+            __cgh.parallel_for<_FindSplitPointsKernel>(
+                __merge_matrix_nd_range,
+                [=](sycl::nd_item<_Dims2> __nd_item) {
+
+                    // Return the constituent element of the global id representing the work-item’s position in the nd-range in the given Dimension.
+                    const auto __global_id_h = __nd_item.get_global_id(_Dim_H);
+                    const auto __global_id_v = __nd_item.get_global_id(_Dim_V);
+                    // Return the constituent element of the local id representing the work-item’s position within the current work-group in the given Dimension.
+                    const auto __local_id_h = __nd_item.get_local_id(_Dim_H);
+                    const auto __local_id_v = __nd_item.get_local_id(_Dim_V);
+
+                    const auto __current_diagonal_global_offset = __global_id_h + __global_id_v;
+
+                    // 1. Load data into SLM, if we are on first row or first column
+                    const bool __is_first_row = __local_id_v == 0;
+                    const bool __is_first_col = __local_id_h == 0;
+                    if (__is_first_row || __is_first_col)
+                    {
+                        //          Work-items in the current work-group: 'L' - load data; '.' - doing nothing.
+                        //          0 1 2 3 4 5 6 7 8 9 .....  <-- __local_id_h
+                        //        0 L L L L L L L L L L 
+                        //        1 L . . . . . . . . .
+                        //        2 L . . . . . . . . .
+                        //        3 L . . . . . . . . .
+                        //        4 L . . . . . . . . .
+                        //      ...
+                        //        ^
+                        // __local_id_v
+                        if (__is_first_row)
+                            if (__local_id_h < __window_items_count_h && __global_id_h < __n1)
+                                __loc_acc_rng1_h[__local_id_h] = __rng1[__global_id_h];
+
+                        if (__is_first_col)
+                            if (__local_id_v < __window_items_count_v && __global_id_v < __n2)
+                                __loc_acc_rng2_v[__local_id_v] = __rng2[__global_id_v];                             
+
+                        // Load the extra elements on the right and on the bottom side of the current sub-window
+                        if (__is_first_row && __is_first_col)
+                        {
+                            if (__global_id_h + __sub_window_count_h < __n1)
+                                __loc_acc_rng1_h[__sub_window_count_h] = __rng1[__global_id_h + __sub_window_count_h];
+                            if (__global_id_v + __sub_window_count_v < __n2)
+                                __loc_acc_rng2_v[__sub_window_count_v] = __rng2[__global_id_v + __sub_window_count_v];
+                        }
+                    }
+
+                    // 2. Barrier to wait for all work-items in the current work-group to load data into SLM
+                    __dpl_sycl::__group_barrier(__nd_item);
+
+                    // 3. Analyze data placed on diagonal: find split points and save results into the scratch storage
+                    //          Work-items in the current work-group: L - load data; X - doing nothing.
+                    //          0 1 2 3 4 5 6 7 8 9 .....  <-- __local_id_h
+                    //        0        /
+                    //        1       <upper-right point> (3, 1)
+                    //        2      <current point> (2, 2)    -> check comparison for current and upper-right points: __comp(__rng1[2], __rng2[2]) != __comp(__rng1[3], __rng2[1])
+                    //        3     /
+                    //        4    /
+                    //      ...
+                    //        ^
+                    // __local_id_v
+                    if (__current_diagonal_global_offset % __diagonals_interval == 0)
+                    {
+                        // 3.1 Add init point
+                        if (__global_id_h == 0 && __global_id_v == 0)
+                        {
+                            __split_points_ptr[0] = __split_point_t{0, 0};
+                        }
+
+                        // TODO required analyze the case when __n1 == 1 and / or __n2 == 1
+
+                        // 3.2 Analyze current point and upper-right point
+                        else if (__local_id_h + 1 < __window_items_count_v + 1 && __global_id_h + 1 < __n1 && __local_id_v > 0)
+                        {
+                            if (__comp(__loc_acc_rng1_h[__local_id_h], __loc_acc_rng2_v[__local_id_v]) !=
+                                __comp(__loc_acc_rng1_h[__local_id_h + 1], __loc_acc_rng2_v[__local_id_v - 1]))
+                            {
+                                // Add the global coordinates of upper-right point of current as a found split point
+                                __split_points_ptr[__current_diagonal_global_offset] = __split_point_t{__global_id_h + 1, __global_id_v - 1};
+                            }
+                        }
+                    }
+                });
+        });
 
         // Run Kernel 2: merge by split points
         auto __event_merge = __exec.queue().submit([&](sycl::handler& __cgh) {
 
-                // We should wait for the first kernel to finish
-                __cgh.depends_on(__event_find_split_points);
+            // We should wait for the first kernel to finish
+            __cgh.depends_on(__event_find_split_points);
 
-                [[maybe_unused]] auto __split_points_acc = __split_points_scratch_storage.__get_scratch_acc(__cgh);
+            // Get access to the split points
+            auto __split_points_acc = __split_points.__get_scratch_acc(__cgh);
+            auto __split_points_ptr = __split_points_scratch_storage_t::__get_usm_or_buffer_accessor_ptr(__split_points_acc);
 
-            });
+            __cgh.parallel_for<_Name...>(
+                sycl::range</*dim=*/1>(__diagonals_count), [=](sycl::item</*dim=*/1> __item_id) {
+                    const auto __idx = __item_id.get_linear_id();
 
-        //auto __event = __exec.queue().submit([&](sycl::handler& __cgh) {
-        //    oneapi::dpl::__ranges::__require_access(__cgh, __rng1, __rng2, __rng3);
-        //    __cgh.parallel_for<_Name...>(
-        //        sycl::range</*dim=*/1>(__work_items_count), [=](sycl::item</*dim=*/1> __item_id) {
+                    // Get current splitting point
+                    const auto& __start = __split_points_ptr[__idx];
+                    assert(__start.first > 0 || __start.second > 0 || __idx == 0);
 
-        //            const auto __item_idx = __item_id.get_linear_id();
+                    const _IdType __diagonal_offset = __idx * __diagonals_interval;
 
-        //            // Eval the global diagonal index for the first diagonal in the current work item
-        //            const auto __global_diagonal_idx = __item_idx * __diagonals_count_per_work_item;
-
-        //            // Iterate all diagonals in the current work item
-        //            for (_IdType __diagonal_idx = 0; __diagonal_idx < __diagonals_count_per_work_item; ++__diagonal_idx)
-        //            {
-        //                // Eval the offset for the current processing diagonal
-        //                const _IdType __diagonal_offset = (__global_diagonal_idx + __diagonal_idx) * __diagonals_interval;
-        //                if (__diagonal_offset < __n)
-        //                {
-        //                    const auto __start = __find_start_point(__rng1, __rng2, __diagonal_offset, __n1, __n2, __comp);
-
-        //                    __serial_merge(__rng1, __rng2, __rng3, __start.first, __start.second, __diagonal_offset,
-        //                                   __diagonals_interval, __n1, __n2, __comp);
-        //                }
-        //            }
-        //        });
-        //});
+                    // Run merge from current splitting point
+                    __serial_merge(__rng1, __rng2, __rng3, (std::size_t)__start.first, (std::size_t)__start.second,
+                                   (std::size_t)__diagonal_offset, __diagonals_interval, __n1, __n2, __comp);
+                });
+        });
 
         return __future(__event_merge);
     }
@@ -286,9 +391,6 @@ struct __parallel_merge_submitter_large<_IdType, __internal::__optional_kernel_n
 
 template <typename... _Name>
 class __merge_kernel_name;
-
-template <typename... _Name>
-class __merge_kernel_name_large;
 
 template <typename _ExecutionPolicy, typename _Range1, typename _Range2, typename _Range3, typename _Compare>
 auto
