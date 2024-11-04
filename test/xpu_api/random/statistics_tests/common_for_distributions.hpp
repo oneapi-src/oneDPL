b75db3c17ef691926abbb611dc3b9c1272281b25
Grabovskaya, Ekaterina <ekaterina.grabovskaya@intel.com>
2024-11-04 07:04:53 -0800

    license was updated for all test in statistics_tests and interface_tests folders

--- test/xpu_api/random/statistics_tests/common_for_distributions.hpp
+++ test/xpu_api/random/statistics_tests/common_for_distributions.hpp
@@ -5,12 +5,6 @@
 //
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
-// This file incorporates work covered by the following copyright and permission
-// notice:
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-//
 //===----------------------------------------------------------===//
 
 #ifndef _ONEDPL_RANDOM_STATISTICS_TESTS_COMMON_FOR_DISTRS_H
