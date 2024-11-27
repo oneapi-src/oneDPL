eb6110d3e87f25d816c07e6ed8ab3edf09a6f27b
egfefey <elvis.g.fefey@intel.com>
2024-11-27 01:07:16 -0500

    Update test_dynamic_load_policy_sycl.pass.cpp

--- test/parallel_api/dynamic_selection/sycl/test_dynamic_load_policy_sycl.pass.cpp
+++ test/parallel_api/dynamic_selection/sycl/test_dynamic_load_policy_sycl.pass.cpp
@@ -71,13 +71,13 @@ main()
 //        actual = test_select<policy_t, decltype(u), decltype(f2)&, false>(u, f2);
 
         actual = test_submit_and_wait_on_event<just_call_submit, policy_t>(u, f2);
-/*
+
         actual = test_submit_and_wait_on_event<call_select_before_submit, policy_t>(u, f2);
         actual = test_submit_and_wait<just_call_submit, policy_t>(u, f2);
         actual = test_submit_and_wait<call_select_before_submit, policy_t>(u, f2);
         actual = test_submit_and_wait_on_group<just_call_submit, policy_t>(u, f);
         actual = test_submit_and_wait_on_group<call_select_before_submit, policy_t>(u, f);
-*/
+
         bProcessed = true;
     }
 #endif // Devices available are CPU and GPU
