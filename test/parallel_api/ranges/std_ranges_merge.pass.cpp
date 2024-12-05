94800f8cb8be20372887f0814231cb959fb78a3e
MikeDvorskiy <mikhail.dvorskiy@intel.com>
2024-12-05 18:32:05 +0100

    [oneDPL][ranges][merge][test]  support size limit for output; + fixes

--- test/parallel_api/ranges/std_ranges_merge.pass.cpp
+++ test/parallel_api/ranges/std_ranges_merge.pass.cpp
@@ -34,48 +34,50 @@ main()
         auto it_out = std::ranges::begin(r_out);
         auto it_1 = std::ranges::begin(r_1);
         auto it_2 = std::ranges::begin(r_2);
-        for(;;)
-        {
-            if(it_out == std::ranges::end(r_out))
-                return ret_type{it_1, it_2, it_out};
-            
-            if(it_1 == std::ranges::end(r_1))
-            {
-                for(; it_out != std::ranges::end(r_out); ++it_2, ++it_out)
-                     *it_out = *it_2;
-                return ret_type{it_1, it_2, it_out};
-            }
-            else if(it_2 == std::ranges::end(r_2))
-            {
-                for(; it_out != std::ranges::end(r_out); ++it_1, ++it_out)
-                     *it_out = *it_1;
-                return ret_type{it_1, it_2, it_out};
-            }
+        auto it_1_e = std::ranges::end(r_1);
+        auto it_2_e = std::ranges::end(r_2);
+        auto it_out_e = std::ranges::end(r_out);
 
+
+        while(it_1 != it_1_e && it_2 != it_2_e)
+        {
             if (std::invoke(comp, std::invoke(proj2, *it_2), std::invoke(proj1, *it_1)))
             {
                 *it_out = *it_2;
-                ++it_2, ++it_out;
+                ++it_out, ++it_2;
             }
             else
             {
                 *it_out = *it_1;
-                ++it_1, ++it_out;
+                ++it_out, ++it_1;
             }
+            if(it_out == it_out_e)
+                return ret_type{it_1, it_2, it_out};
+        }
+    
+        if(it_1 == it_1_e)
+        {
+            for(; it_2 != it_2_e && it_out != it_out_e; ++it_2, ++it_out)
+                *it_out = *it_2;
+        }
+        else
+        {
+            for(; it_1 != it_1_e && it_out != it_out_e; ++it_1, ++it_out)
+                *it_out = *it_1;
         }
 
         return ret_type{it_1, it_2, it_out};
     };
 
-    test_range_algo<0, int, data_in_in_out>{big_sz}(dpl_ranges::merge, merge_checker, std::ranges::less{}, std::identity{}, std::identity{});
+    test_range_algo<0, int, data_in_in_out_lim>{big_sz}(dpl_ranges::merge, merge_checker, std::ranges::less{}, std::identity{}, std::identity{});
 
-    test_range_algo<1, int, data_in_in_out>{}(dpl_ranges::merge, merge_checker, std::ranges::less{}, proj, proj);
-    test_range_algo<2, P2, data_in_in_out>{}(dpl_ranges::merge, merge_checker, std::ranges::less{}, &P2::x, &P2::x);
-    test_range_algo<3, P2, data_in_in_out>{}(dpl_ranges::merge, merge_checker, std::ranges::less{}, &P2::proj, &P2::proj);
+    test_range_algo<1, int, data_in_in_out_lim>{}(dpl_ranges::merge, merge_checker, std::ranges::less{}, proj, proj);
+    test_range_algo<2, P2, data_in_in_out_lim>{}(dpl_ranges::merge, merge_checker, std::ranges::less{}, &P2::x, &P2::x);
+    test_range_algo<3, P2, data_in_in_out_lim>{}(dpl_ranges::merge, merge_checker, std::ranges::less{}, &P2::proj, &P2::proj);
 
-    test_range_algo<4, int, data_in_in_out>{}(dpl_ranges::merge, merge_checker, std::ranges::greater{}, proj, proj);
-    test_range_algo<5, P2, data_in_in_out>{}(dpl_ranges::merge, merge_checker, std::ranges::greater{}, &P2::x, &P2::x);
-    test_range_algo<6, P2, data_in_in_out>{}(dpl_ranges::merge, merge_checker, std::ranges::greater{}, &P2::proj, &P2::proj);
+    test_range_algo<4, int, data_in_in_out_lim>{}(dpl_ranges::merge, merge_checker, std::ranges::greater{}, proj, proj);
+    test_range_algo<5, P2, data_in_in_out_lim>{}(dpl_ranges::merge, merge_checker, std::ranges::greater{}, &P2::x, &P2::x);
+    test_range_algo<6, P2, data_in_in_out_lim>{}(dpl_ranges::merge, merge_checker, std::ranges::greater{}, &P2::proj, &P2::proj);
 #endif //_ENABLE_STD_RANGES_TESTING
 
     return TestUtils::done(_ENABLE_STD_RANGES_TESTING);
