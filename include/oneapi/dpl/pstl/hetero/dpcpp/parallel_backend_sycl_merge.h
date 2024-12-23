9d792099df1a7a7bef20c71a3a39ece2af6c22d5
Sergey Kopienko <sergey.kopienko@intel.com>
2024-12-23 14:19:27 +0100

    include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_merge.h - fix compile error
    Signed-off-by: Sergey Kopienko <sergey.kopienko@intel.com>

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_merge.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_merge.h
@@ -283,10 +283,10 @@ struct __parallel_merge_submitter_large<_IdType, _CustomName,
         });
     }
 
-    template <typename _Rng1, typename _Rng2, typename _Index, typename _Compare>
+    template <typename _Rng1, typename _Rng2, typename _Compare>
     inline static _split_point_t<_Index>
     __find_start_point_w(const _Rng1& __rng1, const _Rng2& __rng2, const _split_point_t<_IdType>& __sp_left,
-                         const _split_point_t<_IdType>& __sp_right, const _Index __i_elem, _Compare __comp)
+                         const _split_point_t<_IdType>& __sp_right, const _IdType __i_elem, _Compare __comp)
     {
         return _find_start_point(__rng1, __sp_left.first, __sp_right.first, __rng2, __sp_left.second, __sp_right.second,
                                  __i_elem, __comp);
