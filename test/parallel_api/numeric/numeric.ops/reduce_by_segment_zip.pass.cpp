dbdfe64a96cb646000aa4e91e257777b9fce345b
Matthew Michel <matthew.michel@intel.com>
2025-01-03 13:10:54 -0600

    Add a discard iterator for one of the zipped output values
    Signed-off-by: Matthew Michel <matthew.michel@intel.com>

--- test/parallel_api/numeric/numeric.ops/reduce_by_segment_zip.pass.cpp
+++ test/parallel_api/numeric/numeric.ops/reduce_by_segment_zip.pass.cpp
@@ -124,8 +124,7 @@ test_zip_with_discard()
     int values1[n] = {1, 1, 1, 1, 1};
     int values2[n] = {2, 2, 2, 2, 2};
     int output_keys[n] = {};
-    int output_values1[n] = {};
-    int output_values2[n] = {};
+    int output_values[n] = {};
 
     // allocate USM memory and copying data to USM shared/device memory
     TestUtils::usm_data_transfer<alloc_type, int> dt_helper1(q, keys1, n);
@@ -133,22 +132,20 @@ test_zip_with_discard()
     TestUtils::usm_data_transfer<alloc_type, int> dt_helper3(q, values1, n);
     TestUtils::usm_data_transfer<alloc_type, int> dt_helper4(q, values2, n);
     TestUtils::usm_data_transfer<alloc_type, int> dt_helper5(q, output_keys, n);
-    TestUtils::usm_data_transfer<alloc_type, int> dt_helper6(q, output_values1, n);
-    TestUtils::usm_data_transfer<alloc_type, int> dt_helper7(q, output_values2, n);
+    TestUtils::usm_data_transfer<alloc_type, int> dt_helper6(q, output_values, n);
     auto d_keys1 = dt_helper1.get_data();
     auto d_keys2 = dt_helper2.get_data();
     auto d_values1 = dt_helper3.get_data();
     auto d_values2 = dt_helper4.get_data();
     auto d_output_keys = dt_helper5.get_data();
-    auto d_output_values1 = dt_helper6.get_data();
-    auto d_output_values2 = dt_helper7.get_data();
+    auto d_output_values = dt_helper6.get_data();
 
     //make zip iterators
     auto begin_keys_in = oneapi::dpl::make_zip_iterator(d_keys1, d_keys2);
     auto end_keys_in = oneapi::dpl::make_zip_iterator(d_keys1 + n, d_keys2 + n);
     auto begin_vals_in = oneapi::dpl::make_zip_iterator(d_values1, d_values2);
     auto begin_keys_out = oneapi::dpl::make_zip_iterator(d_output_keys, oneapi::dpl::discard_iterator());
-    auto begin_vals_out = oneapi::dpl::make_zip_iterator(d_output_values1, d_output_values2);
+    auto begin_vals_out = oneapi::dpl::make_zip_iterator(oneapi::dpl::discard_iterator(), d_output_values);
 
     //run reduce_by_segment algorithm
     auto new_last = oneapi::dpl::reduce_by_segment(TestUtils::make_device_policy<KernelName>(q), begin_keys_in,
@@ -157,15 +154,12 @@ test_zip_with_discard()
 
     //retrieve result on the host and check the result
     dt_helper5.retrieve_data(output_keys);
-    dt_helper6.retrieve_data(output_values1);
-    dt_helper7.retrieve_data(output_values2);
+    dt_helper6.retrieve_data(output_values);
 
     const int exp_keys[n] = {1, 2, 3};
-    const int exp_values1[n] = {2, 2, 1};
-    const int exp_values2[n] = {4, 4, 2};
+    const int exp_values[n] = {4, 4, 2};
     EXPECT_EQ_N(exp_keys, output_keys, n, "wrong keys from reduce_by_segment");
-    EXPECT_EQ_N(exp_values1, output_values1, n, "wrong values1 from reduce_by_segment");
-    EXPECT_EQ_N(exp_values2, output_values2, n, "wrong values2 from reduce_by_segment");
+    EXPECT_EQ_N(exp_values, output_values, n, "wrong values from reduce_by_segment");
 }
 #endif
 
