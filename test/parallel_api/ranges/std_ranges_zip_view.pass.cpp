5ef3c27d476caf88db3bf9ac2d00e8ebb34f1428
MikeDvorskiy <mikhail.dvorskiy@intel.com>
2025-01-16 15:43:39 +0100

    [oneDPL][ranges][zip_view][test] + result checks

--- test/parallel_api/ranges/std_ranges_zip_view.pass.cpp
+++ test/parallel_api/ranges/std_ranges_zip_view.pass.cpp
@@ -64,32 +64,46 @@ main()
     int data[max_n] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
     auto zip_view = dpl_ranges::zip(data, std::views::iota(0, max_n)) | std::views::take(5);
-    std::ranges::for_each(zip_view, test_std_ranges::f_mutuable, [](const auto& val) { return std::get<1>(val); });
+    std::ranges::for_each(zip_view, test_std_ranges::f_mutuable, [](auto&& val) ->decltype(auto) { return std::get<0>(val); });
+    for(int i = 0; i < zip_view.size(); ++i)
+        EXPECT_TRUE(std::get<0>(zip_view[i]) == i*i && std::get<1>(zip_view[i]) == i, "Wrong effect for std::ranges::for_each with zip_view.");
 
-    test_std_ranges::call_with_host_policies(dpl_ranges::for_each, zip_view, test_std_ranges::f_mutuable, [](const auto& val) { return std::get<1>(val); });
+    test_std_ranges::call_with_host_policies(dpl_ranges::for_each, zip_view, test_std_ranges::f_mutuable,
+        [](const auto& val) { return std::get<1>(val); });
 
 #if TEST_DPCPP_BACKEND_PRESENT
-    dpl_ranges::for_each(test_std_ranges::dpcpp_policy(), zip_view, test_std_ranges::f_mutuable, [](const auto& val) { return std::get<1>(val); });
+    dpl_ranges::for_each(test_std_ranges::dpcpp_policy(), zip_view, test_std_ranges::f_mutuable,
+        [](const auto& val) { return std::get<1>(val); });
 #endif
 
+    {
+    int data[max_n] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     auto zip_view_sort = dpl_ranges::zip(data, data);
 
     oneapi::dpl::zip_iterator<int*, int*> zip_it = zip_view_sort.begin(); //check conversion to oneapi::dpl::zip_iterator
 
-    std::sort(zip_view_sort.begin(), zip_view_sort.begin() + max_n, [](const auto& val1, const auto& val2) { return std::get<0>(val1) > std::get<0>(val2); });
+    std::sort(zip_view_sort.begin(), zip_view_sort.begin() + max_n,
+        [](const auto& val1, const auto& val2) { return std::get<0>(val1) > std::get<0>(val2); });
+
     for(int i = 0; i < max_n; ++i)
-        EXPECT_TRUE(std::get<0>(zip_view_sort[i]) == max_n - 1 - i, "Wrong effect for std::sort with zip_view.");
+        EXPECT_TRUE(std::get<0>(zip_view_sort[i]) == max_n - 1 - i && std::get<1>(zip_view_sort[i]) == max_n - 1 - i,
+            "Wrong effect for std::sort with zip_view.");
 
     std::ranges::sort(zip_view_sort, std::less{}, [](auto&& val) { return std::get<0>(val); });
     for(int i = 0; i < max_n; ++i)
-        EXPECT_TRUE(std::get<0>(zip_view_sort[i]) == i, "Wrong effect for std::ranges::sort with zip_view.");
+        EXPECT_TRUE(std::get<0>(zip_view_sort[i]) == i && std::get<1>(zip_view_sort[i]) == i,
+            "Wrong effect for std::ranges::sort with zip_view.");
 
     static_assert(std::ranges::random_access_range<decltype(zip_view_sort)>);
     static_assert(std::random_access_iterator<decltype(zip_view_sort.begin())>);
 
-    test_std_ranges::call_with_host_policies(dpl_ranges::sort, zip_view_sort, std::greater{}, [](const auto& val) { return std::get<0>(val); });
+    test_std_ranges::call_with_host_policies(dpl_ranges::sort, zip_view_sort, std::greater{},
+        [](const auto& val) { return std::get<0>(val); });
+
     for(int i = 0; i < max_n; ++i)
-        EXPECT_TRUE(std::get<0>(zip_view_sort[i]) == max_n - 1 - i, "Wrong effect for oneapi::dpl::ranges::sort with zip_view.");
+        EXPECT_TRUE(std::get<0>(zip_view_sort[i]) == max_n - 1 - i && std::get<1>(zip_view_sort[i]) == max_n - 1 - i,
+            "Wrong effect for oneapi::dpl::ranges::sort with zip_view.");
+    }
 
 #endif //_ENABLE_STD_RANGES_TESTING
 
