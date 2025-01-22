5cc4c4c318cf601c70d176553b0ec4cf256ae66b
Sergey Kopienko <sergey.kopienko@intel.com>
2025-01-22 12:15:24 +0100

    include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_merge.h - fix compile error in __serial_merge for case when the types of __rng2 and __rng1 aren't the same

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_merge.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_merge.h
@@ -148,6 +148,8 @@ __serial_merge(const _Rng1& __rng1, const _Rng2& __rng2, _Rng3& __rng3, const _I
     _Index __rng1_idx = __start1;
     _Index __rng2_idx = __start2;
 
+    using _Rng3DataType = decltype(__rng3[__start3]);
+
     for (_Index __rng3_idx = __start3; __rng3_idx < __rng3_idx_end; ++__rng3_idx)
     {
         const bool __rng1_idx_less_n1 = __rng1_idx < __rng1_idx_end;
@@ -159,8 +161,8 @@ __serial_merge(const _Rng1& __rng1, const _Rng2& __rng2, _Rng3& __rng3, const _I
         __rng3[__rng3_idx] =
             ((__rng1_idx_less_n1 && __rng2_idx_less_n2 && __comp(__rng2[__rng2_idx], __rng1[__rng1_idx])) ||
              !__rng1_idx_less_n1)
-                ? __rng2[__rng2_idx++]
-                : __rng1[__rng1_idx++];
+                ? static_cast<_Rng3DataType>(__rng2[__rng2_idx++])
+                : static_cast<_Rng3DataType>(__rng1[__rng1_idx++]);
     }
 }
 
