b619105f2ea39b99488e18f6d6ef3c4f7bc8a2a2
Sergey Kopienko <sergey.kopienko@intel.com>
2025-01-22 21:19:28 +0100

    Fix compile error in `__serial_merge` (#2013)

--- include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_merge.h
+++ include/oneapi/dpl/pstl/hetero/dpcpp/parallel_backend_sycl_merge.h
@@ -156,11 +156,11 @@ __serial_merge(const _Rng1& __rng1, const _Rng2& __rng2, _Rng3& __rng3, const _I
         // One of __rng1_idx_less_n1 and __rng2_idx_less_n2 should be true here
         // because 1) we should fill output data with elements from one of the input ranges
         // 2) we calculate __rng3_idx_end as std::min<_Index>(__rng1_size + __rng2_size, __chunk).
-        __rng3[__rng3_idx] =
-            ((__rng1_idx_less_n1 && __rng2_idx_less_n2 && __comp(__rng2[__rng2_idx], __rng1[__rng1_idx])) ||
-             !__rng1_idx_less_n1)
-                ? __rng2[__rng2_idx++]
-                : __rng1[__rng1_idx++];
+        if (__rng1_idx_less_n1 && __rng2_idx_less_n2 && __comp(__rng2[__rng2_idx], __rng1[__rng1_idx]) ||
+            !__rng1_idx_less_n1)
+            __rng3[__rng3_idx] = __rng2[__rng2_idx++];
+        else
+            __rng3[__rng3_idx] = __rng1[__rng1_idx++];
     }
 }
 
