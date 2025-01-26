6f4d7011eacabd9eb97c7a567da100773b28a21f
Matthew Michel <matthew.michel@intel.com>
2025-01-26 11:49:10 -0600

    Limit maximum work-group size in pattern_for_max_n utility
    CPU devices have large max work group size (e.g. 8192) which has led to excessive test
    times in some Windows + CPU + Debug CI tests. The SYCL parallel for
    implementation only uses a work-group size of 512, so we limit it to
    this.
    
    Signed-off-by: Matthew Michel <matthew.michel@intel.com>
    
    Signed-off-by: Matthew Michel <matthew.michel@intel.com>

--- test/support/utils.h
+++ test/support/utils.h
@@ -1026,7 +1026,8 @@ get_pattern_for_max_n()
     sycl::device d = q.get_device();
     constexpr std::size_t max_iters_per_item = 16;
     constexpr std::size_t multiplier = 4;
-    std::size_t __max_n = multiplier * max_iters_per_item * d.get_info<sycl::info::device::max_work_group_size>() *
+    constexpr std::size_t max_work_group_size = 512;
+    std::size_t __max_n = multiplier * max_iters_per_item * max_work_group_size *
                           d.get_info<sycl::info::device::max_compute_units>();
     __max_n = std::min(std::size_t{10000000}, __max_n);
     return __max_n;
