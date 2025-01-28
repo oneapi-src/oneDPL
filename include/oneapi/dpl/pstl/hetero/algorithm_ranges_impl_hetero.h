13b8b329bb95529b99ae292f230d832996887930
MikeDvorskiy <mikhail.dvorskiy@intel.com>
2025-01-28 15:42:29 +0100

    Update include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h
    Co-authored-by: Alexey Kukanov <alexey.kukanov@intel.com>

--- include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h
+++ include/oneapi/dpl/pstl/hetero/algorithm_ranges_impl_hetero.h
@@ -51,7 +51,7 @@ namespace __ranges
 //------------------------------------------------------------------------
 
 template <typename _BackendTag, typename _ExecutionPolicy, typename _Function, typename... _Ranges>
-auto
+auto /* see _Size inside the function */
 __pattern_walk_n(__hetero_tag<_BackendTag>, _ExecutionPolicy&& __exec, _Function __f, _Ranges&&... __rngs)
 {
     using _Size = std::make_unsigned_t<std::common_type_t<oneapi::dpl::__internal::__difference_t<_Ranges>...>>;
