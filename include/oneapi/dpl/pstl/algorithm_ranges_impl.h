b3a6db049f5b4bf14b68bd3e263b4f92f9624259
MikeDvorskiy <mikhail.dvorskiy@intel.com>
2025-01-31 11:16:52 +0100

    Update include/oneapi/dpl/pstl/algorithm_ranges_impl.h
    Co-authored-by: Alexey Kukanov <alexey.kukanov@intel.com>

--- include/oneapi/dpl/pstl/algorithm_ranges_impl.h
+++ include/oneapi/dpl/pstl/algorithm_ranges_impl.h
@@ -471,9 +471,9 @@ __pattern_merge(_Tag __tag, _ExecutionPolicy&& __exec, _R1&& __r1, _R2&& __r2, _
     if (__n_out == 0)
         return __return_type{__it_1, __it_2, __it_out};
 
-    //Parallel and serial versions of ___merge_path_out_lim merges 1st sequience and 2nd sequience in "revert maner":
-    //the identical elements from 2nd sequience are being merged first.
-    //So, the following call passes 1st sequience 2nd sequience in "a revert maner".
+    // Parallel and serial versions of ___merge_path_out_lim merge the 1st sequence and the 2nd sequence in "reverse order":
+    // the identical elements from the 2nd sequence are merged first.
+    // So, the call to ___merge_path_out_lim swaps the order of sequences.
     std::pair __res = ___merge_path_out_lim(__tag, std::forward<_ExecutionPolicy>(__exec), __it_2, __n_2, __it_1, __n_1,
                                             __it_out, __n_out, __comp_2);
 
