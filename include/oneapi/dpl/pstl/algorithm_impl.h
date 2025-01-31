a5ac2346f1cbf794d5cd37061c28989256e5e081
MikeDvorskiy <mikhail.dvorskiy@intel.com>
2025-01-31 11:32:26 +0100

    [oneDPL][ranges][merge] + renamed  a couple of arguments in __serial_merge_out_lim

--- include/oneapi/dpl/pstl/algorithm_impl.h
+++ include/oneapi/dpl/pstl/algorithm_impl.h
@@ -2954,10 +2954,10 @@ __pattern_remove_if(__parallel_tag<_IsVector> __tag, _ExecutionPolicy&& __exec, 
 // the identical elements from the 2nd sequence are merged first.
 template <typename _Iterator1, typename _Iterator2, typename _Iterator3, typename _Comp>
 std::pair<_Iterator1, _Iterator2>
-__serial_merge_out_lim(_Iterator1 __x, _Iterator1 __x_e, _Iterator2 __y, _Iterator2 __y_e, _Iterator3 __i,
-                       _Iterator3 __j, _Comp __comp)
+__serial_merge_out_lim(_Iterator1 __x, _Iterator1 __x_e, _Iterator2 __y, _Iterator2 __y_e, _Iterator3 __out,
+                       _Iterator3 __out_e, _Comp __comp)
 {
-    for (_Iterator3 __k = __i; __k != __j; ++__k)
+    for (_Iterator3 __k = __out; __k != __out_e; ++__k)
     {
         if (__x == __x_e)
         {
