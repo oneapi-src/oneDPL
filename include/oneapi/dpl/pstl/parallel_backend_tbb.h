2604ef45076c43d61a4639bec252585a140cc170
Dan Hoeflinger <dan.hoeflinger@intel.com>
2025-02-03 11:26:40 -0500

    formatting
    Signed-off-by: Dan Hoeflinger <dan.hoeflinger@intel.com>

--- include/oneapi/dpl/pstl/parallel_backend_tbb.h
+++ include/oneapi/dpl/pstl/parallel_backend_tbb.h
@@ -36,23 +36,23 @@
 #include <tbb/tbb_allocator.h>
 
 #if _WIN32 || _WIN64
-    // For windows, TBB prior to version 2021.12 requires NOMINMAX to be defined before including
-    // tbb/enumerable_thread_specific.h, which includes windows.h. This is not required for TBB 2021.12 and later due
-    // to an TBB fix.
-    #if TBB_VERSION_MAJOR < 2021 || (TBB_VERSION_MAJOR == 2021 && TBB_VERSION_MINOR < 12)
-        #ifndef NOMINMAX
-            #define NOMINMAX
-            #define __ONEDPL_DEFINED_NOMINMAX 1
-        #endif
-    #endif
+// For windows, TBB prior to version 2021.12 requires NOMINMAX to be defined before including
+// tbb/enumerable_thread_specific.h, which includes windows.h. This is not required for TBB 2021.12 and later due
+// to an TBB fix.
+#    if TBB_VERSION_MAJOR < 2021 || (TBB_VERSION_MAJOR == 2021 && TBB_VERSION_MINOR < 12)
+#        ifndef NOMINMAX
+#            define NOMINMAX
+#            define __ONEDPL_DEFINED_NOMINMAX 1
+#        endif
+#    endif
 #endif
 
 #include <tbb/enumerable_thread_specific.h>
 
 #if __ONEDPL_DEFINED_NOMINMAX
-    // Avoid affecting outside code with NOMINMAX
-    #undef NOMINMAX
-    #undef __ONEDPL_DEFINED_NOMINMAX
+// Avoid affecting outside code with NOMINMAX
+#    undef NOMINMAX
+#    undef __ONEDPL_DEFINED_NOMINMAX
 #endif
 
 #if TBB_INTERFACE_VERSION > 12000
