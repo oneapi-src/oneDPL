40b33a99965954adc398d0d47ec6a42989c01ec4
egfefey <elvis.g.fefey@intel.com>
2025-02-09 16:34:15 -0500

    Update backend_traits.h

--- include/oneapi/dpl/internal/dynamic_selection_impl/backend_traits.h
+++ include/oneapi/dpl/internal/dynamic_selection_impl/backend_traits.h
@@ -21,6 +21,7 @@ namespace experimental
 {
 namespace internal
 {
+//lazy_report
 template <typename Backend>
 auto
 has_lazy_report_impl(...) -> std::false_type;
@@ -34,10 +35,25 @@ struct has_lazy_report : decltype(has_lazy_report_impl<Backend>(0))
 {
 };
 
+//get_resources
+template <typename Backend>
+auto
+has_get_resources_impl(...) -> std::false_type;
+
+template <typename Backend>
+auto
+has_get_resources_impl(int) -> decltype(std::declval<Backend>().get_resources(), std::true_type{});
+
+template <typename Backend>
+struct has_get_resources : decltype(has_get_resources_impl<Backend>(0))
+{
+};
+
 } //namespace internal
 
 namespace backend_traits
 {
+//lazy_report
 template <typename S>
 struct lazy_report_value
 {
@@ -46,6 +62,14 @@ struct lazy_report_value
 template <typename S>
 inline constexpr bool lazy_report_v = lazy_report_value<S>::value;
 
+//get_resources
+template <typename S>
+struct get_resources_value
+{
+    static constexpr bool value = ::oneapi::dpl::experimental::internal::get_resources_report<S>::value;
+};
+template <typename S>
+inline constexpr get_resources_report_v = get_resources_value<S>::value;
 } //namespace backend_traits
 
 } // namespace experimental
