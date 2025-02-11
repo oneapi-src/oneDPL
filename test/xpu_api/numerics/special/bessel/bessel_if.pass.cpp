dc68dfca647fada6da923007e6a7dcaa455214ab
Sergey Kopienko <sergey.kopienko@intel.com>
2025-02-11 10:55:32 +0100

    test/xpu_api/numerics/special/bessel/bessel_if.pass.cpp

---
+++ test/xpu_api/numerics/special/bessel/bessel_if.pass.cpp
@@ -0,0 +1,36 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "support/test_complex.h"
+
+//  cyl_bessel_if
+#include <cmath>
+
+void
+test(float)
+{
+    float nuf = 1.0F / 3.0F, xf = 0.5F;
+
+    std::cyl_bessel_if(nuf, xf);
+}
+
+void
+test(double)
+{
+    double nud = 1.0 / 3.0, xd = 0.5;
+
+    std::cyl_bessel_if(nud, xd);
+}
+
+ONEDPL_TEST_NUM_MAIN
+{
+    test(float{});
+    IF_DOUBLE_SUPPORT(test(double{}))
+
+    return 0;
+}
