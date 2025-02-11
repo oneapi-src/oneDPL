5863a0a48bd02ca9bc8a1521b9d0666d1908b8f9
Sergey Kopienko <sergey.kopienko@intel.com>
2025-02-11 12:35:46 +0100

    test/xpu_api/numerics/special/bessel/bessel_k.pass.cpp

---
+++ test/xpu_api/numerics/special/bessel/bessel_k.pass.cpp
@@ -0,0 +1,27 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "support/test_complex.h"
+
+//  cyl_bessel_jl
+#include <cmath>
+
+void
+test()
+{
+    double nud = 1.0 / 3.0, xd = 0.5;
+
+    std::cyl_bessel_k(nud, xd);
+}
+
+ONEDPL_TEST_NUM_MAIN
+{
+    IF_DOUBLE_SUPPORT(test())
+
+    return 0;
+}
