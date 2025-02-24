e87bcb2204684dfb3f7308241af5aedcb030b14c
Sergey Kopienko <sergey.kopienko@intel.com>
2025-02-24 14:35:23 +0100

    Add check nan tests

---
+++ test/xpu_api/numerics/special/bessel/bessel_k_check_nan.pass.cpp
@@ -0,0 +1,53 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "support/test_bessel.h"
+
+void
+test01()
+{
+    float xf = std::numeric_limits<float>::quiet_NaN();
+    double xd = std::numeric_limits<double>::quiet_NaN();
+
+    float nuf = 0.0F;
+    double nud = 0.0;
+
+    float a = std::cyl_bessel_k(nuf, xf);
+    float b = std::cyl_bessel_kf(nuf, xf);
+    double c = std::cyl_bessel_k(nud, xd);
+
+    assert(std::isnan(a));
+    assert(std::isnan(b));
+    assert(std::isnan(c));
+}
+
+void
+test02()
+{
+    float xf = 1.0F;
+    double xd = 1.0;
+
+    float nuf = std::numeric_limits<float>::quiet_NaN();
+    double nud = std::numeric_limits<double>::quiet_NaN();
+
+    float a = std::cyl_bessel_k(nuf, xf);
+    float b = std::cyl_bessel_kf(nuf, xf);
+    double c = std::cyl_bessel_k(nud, xd);
+
+    assert(std::isnan(a));
+    assert(std::isnan(b));
+    assert(std::isnan(c));
+}
+
+ONEDPL_TEST_NUM_MAIN
+{
+    IF_DOUBLE_SUPPORT(test01())
+    IF_DOUBLE_SUPPORT(test02())
+
+    return 0;
+}
